856113aa4495dd7c47e9bf99e82b3f5429182512
Marcelo dos Santos <a@a.com>
2015-11-17 19:17:43 -0200

    Exercício de Servlets concluído.

---
+++ caelum/java-web/fj21-agenda/src/AdicionaContato.java
@@ -0,0 +1,64 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import entity.Contato;
+import jdbc.ContatoDAO;
+import jdbc.DAO;
+
+@WebServlet("/add")
+public class AdicionaContato extends HttpServlet {
+    private static final long serialVersionUID = 5594330465046634984L;
+
+    @Override
+    protected void service(HttpServletRequest request,
+                           HttpServletResponse response)
+                                   throws ServletException, IOException {
+        // busca o writer
+        PrintWriter out = response.getWriter();
+
+        // buscando os parâmetros no request
+        String nome = request.getParameter("nome");
+        String endereco = request.getParameter("endereco");
+        String email = request.getParameter("email");
+        String dataEmTexto = request.getParameter("dataNascimento");
+        Calendar dataNascimento = null;
+
+        // fazendo a conversão da data
+        try {
+            Date date = new SimpleDateFormat("dd/MM/yyyy").parse(dataEmTexto);
+            dataNascimento = Calendar.getInstance();
+            dataNascimento.setTime(date);
+        } catch (ParseException e) {
+            out.println("Erro de conversão da data");
+            return;
+        }
+
+        // monta um objeto contato
+        Contato contato = new Contato();
+        contato.setNome(nome);
+        contato.setEndereco(endereco);
+        contato.setEmail(email);
+        contato.setDataNascimento(dataNascimento);
+
+        // salva o contato
+        DAO dao = new ContatoDAO();
+        dao.add(contato);
+
+        // imprime o nome do contato que foi adicionado
+        out.println("<html>");
+        out.println("<body>");
+        out.println("Contato " + contato.getNome() + " adicionado com sucesso");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
---
+++ caelum/java-web/fj21-agenda/src/jdbc/connection/ConnectionFactory.java
@@ -0,0 +1,8 @@
+package jdbc.connection;
+
+import java.sql.Connection;
+
+public interface ConnectionFactory {
+
+    Connection getConnection();
+}
