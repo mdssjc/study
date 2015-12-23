e0bd9b6aa96d238c94118e49a1fe6d6d1da9e20a
Marcelo dos Santos <mdssjc@gmail.com>
2015-12-23 12:16:55 -0200

    Implementação do Indicador Fechamento.

---
+++ caelum/java-lab/fj22-argentum/src/main/java/argentum/indicadores/IndicadorFechamento.java
@@ -0,0 +1,12 @@
+package argentum.indicadores;
+
+import argentum.modelo.SerieTemporal;
+
+public class IndicadorFechamento implements Indicador {
+
+    @Override
+    public double calcula(int posicao, SerieTemporal serie) {
+        return serie.getCandle(posicao)
+                    .getFechamento();
+    }
+}
