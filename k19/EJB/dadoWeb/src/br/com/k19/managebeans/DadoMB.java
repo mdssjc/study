970e307b615d9402061d3cff5f8d81f41dd39b4f
Marcelo dos Santos <mdssjc@gmail.com>
2015-12-31 13:36:51 -0200

    Atualização para acesso remoto.

--- k19/EJB/dadoWeb/src/br/com/k19/managebeans/DadoMB.java
+++ k19/EJB/dadoWeb/src/br/com/k19/managebeans/DadoMB.java
@@ -3,13 +3,13 @@ package br.com.k19.managebeans;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 
-import br.com.k19.sessionbeans.LancadorDeDadoBean;
+import br.com.k19.sessionbeans.LancadorDeDado;
 
 @ManagedBean
 public class DadoMB {
 
     @EJB
-    private LancadorDeDadoBean lancadorDeDadoBean;
+    private LancadorDeDado lancadorDeDadoBean;
 
     private int                resultado;
 
--- k19/EJB/dadoWeb/src/br/com/k19/sessionbeans/LancadorDeDadoBean.java
+++ k19/EJB/dadoWeb/src/br/com/k19/sessionbeans/LancadorDeDadoBean.java
@@ -2,13 +2,16 @@ package br.com.k19.sessionbeans;
 
 import java.util.Random;
 
+import javax.ejb.Remote;
 import javax.ejb.Stateless;
 
 @Stateless
-public class LancadorDeDadoBean {
+@Remote(LancadorDeDado.class)
+public class LancadorDeDadoBean implements LancadorDeDado {
 
     private Random gerador = new Random();
 
+    @Override
     public int lanca() {
         return gerador.nextInt(6) + 1;
     }
