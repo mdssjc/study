7b1f35a57f2cbefe7918e28c042454c656170fea
Marcelo dos Santos <mdssjc@gmail.com>
2016-01-01 11:30:36 -0200

    Método assincrono para calculo de frequencia.

--- k19/EJB/dadoWeb/src/br/com/k19/sessionbeans/LancadorDeDadoBean.java
+++ k19/EJB/dadoWeb/src/br/com/k19/sessionbeans/LancadorDeDadoBean.java
@@ -1,9 +1,14 @@
 package br.com.k19.sessionbeans;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
+import java.util.concurrent.Future;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.ejb.AsyncResult;
+import javax.ejb.Asynchronous;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
 
@@ -11,8 +16,9 @@ import javax.ejb.Stateless;
 @Remote(LancadorDeDado.class)
 public class LancadorDeDadoBean implements LancadorDeDado {
 
-    private Random     gerador  = new Random();
-    private static int contador = 0;
+    private static final int WAIT     = 100;
+    private Random           gerador  = new Random();
+    private static int       contador = 0;
 
     @PostConstruct
     public void inicializando() {
@@ -35,5 +41,30 @@ public class LancadorDeDadoBean implements LancadorDeDado {
     @Override
     public int lanca() {
         return gerador.nextInt(6) + 1;
+    }
+
+    @Override
+    @Asynchronous
+    public Future<Map<Integer, Integer>> calculaFrequencia() {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(1, 0);
+        map.put(2, 0);
+        map.put(3, 0);
+        map.put(4, 0);
+        map.put(5, 0);
+        map.put(6, 0);
+
+        for (int i = 0; i < 500; i++) {
+            int v = lanca();
+            map.put(v, map.get(v) + 1);
+            try {
+                Thread.sleep(WAIT);
+            } catch (InterruptedException e) {
+                System.err.println(e.getMessage());
+            }
+            System.out.println(i);
+        }
+
+        return new AsyncResult<Map<Integer, Integer>>(map);
     }
 }
