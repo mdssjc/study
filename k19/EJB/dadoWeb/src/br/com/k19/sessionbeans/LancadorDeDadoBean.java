59bc818c3be761faaad2f998b10bd8cf6311078f
Marcelo dos Santos <mdssjc@gmail.com>
2015-12-31 22:15:17 -0200

    Inclusão de callbacks.

--- k19/EJB/dadoWeb/src/br/com/k19/sessionbeans/LancadorDeDadoBean.java
+++ k19/EJB/dadoWeb/src/br/com/k19/sessionbeans/LancadorDeDadoBean.java
@@ -2,6 +2,8 @@ package br.com.k19.sessionbeans;
 
 import java.util.Random;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
 
@@ -9,7 +11,26 @@ import javax.ejb.Stateless;
 @Remote(LancadorDeDado.class)
 public class LancadorDeDadoBean implements LancadorDeDado {
 
-    private Random gerador = new Random();
+    private Random     gerador  = new Random();
+    private static int contador = 0;
+
+    @PostConstruct
+    public void inicializando() {
+        synchronized (LancadorDeDadoBean.class) {
+            LancadorDeDadoBean.contador++;
+            System.out.println("Criando um lançador de dado...");
+            System.out.println("Total: " + LancadorDeDadoBean.contador);
+        }
+    }
+
+    @PreDestroy
+    public void destruindo() {
+        synchronized (LancadorDeDadoBean.class) {
+            LancadorDeDadoBean.contador--;
+            System.out.println("Destruindo um lançador de dado...");
+            System.out.println("Total: " + LancadorDeDadoBean.contador);
+        }
+    }
 
     @Override
     public int lanca() {
