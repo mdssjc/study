65aa5d5c8683bf975ad310ad90e022c8c8fa1f57
Marcelo dos Santos <mdssjc@gmail.com>
2016-01-01 20:25:17 -0200

    Ajuste do timeout pela EJB em 1 minuto.

--- k19/EJB/carrinhoWeb/src/br/com/k19/sessionbeans/CarrinhoBean.java
+++ k19/EJB/carrinhoWeb/src/br/com/k19/sessionbeans/CarrinhoBean.java
@@ -2,6 +2,7 @@ package br.com.k19.sessionbeans;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -10,9 +11,11 @@ import javax.ejb.PrePassivate;
 import javax.ejb.Remote;
 import javax.ejb.Remove;
 import javax.ejb.Stateful;
+import javax.ejb.StatefulTimeout;
 
 @Stateful
 @Remote(Carrinho.class)
+@StatefulTimeout(value = 1, unit = TimeUnit.MINUTES)
 public class CarrinhoBean implements Carrinho {
 
     private Set<String> produtos = new HashSet<>();
