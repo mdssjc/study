630653122b4715d837b889be1752aa721fe1752a
Marcelo dos Santos <mdssjc@gmail.com>
2016-01-04 14:38:55 -0200

    Implementação de concorrência do EJB.

---
+++ k19/EJB/chatWeb/src/br/com/k19/sessionbeans/ContadorBean.java
@@ -0,0 +1,24 @@
+package br.com.k19.sessionbeans;
+
+import javax.ejb.Lock;
+import javax.ejb.LockType;
+import javax.ejb.Remote;
+import javax.ejb.Singleton;
+
+@Singleton
+@Lock(LockType.READ)
+@Remote(Contador.class)
+public class ContadorBean implements Contador {
+
+    private int valor;
+
+    @Override
+    public void incrementa() {
+        valor++;
+    }
+
+    @Override
+    public int getValor() {
+        return valor;
+    }
+}
