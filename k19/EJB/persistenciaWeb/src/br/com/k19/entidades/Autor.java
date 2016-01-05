063858c947e91a1bdc10c5a041111ef19b2f010f
Marcelo dos Santos <mdssjc@gmail.com>
2016-01-05 13:40:10 -0200

    Implementação dos entity beans e configuração de persistência.

---
+++ k19/EJB/persistenciaWeb/src/br/com/k19/entidades/Autor.java
@@ -0,0 +1,45 @@
+package br.com.k19.entidades;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Autor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long        id;
+
+    private String      nome;
+
+    private List<Livro> livros = new ArrayList<>();
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Livro> getLivros() {
+        return this.livros;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setLivros(List<Livro> livros) {
+        this.livros = livros;
+    }
+}
---
+++ k19/EJB/persistenciaWeb/src/br/com/k19/entidades/Livro.java
@@ -0,0 +1,42 @@
+package br.com.k19.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long   id;
+
+    private String nome;
+
+    private Double preco;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return this.preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+}
