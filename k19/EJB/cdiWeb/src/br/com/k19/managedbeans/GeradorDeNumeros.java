2efbdc49fa76791a14c48779225696ebf51f15ee
Marcelo dos Santos <mdssjc@gmail.com>
2016-01-12 15:19:24 -0200

    Alteração de escopo do gerador de números.

--- k19/EJB/cdiWeb/src/br/com/k19/managedbeans/GeradorDeNumeros.java
+++ k19/EJB/cdiWeb/src/br/com/k19/managedbeans/GeradorDeNumeros.java
@@ -3,6 +3,7 @@ package br.com.k19.managedbeans;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Produces;
 import javax.inject.Named;
 
@@ -10,8 +11,11 @@ public class GeradorDeNumeros {
 
     @Named
     @Produces
+    @RequestScoped
     public List<Double> getNumeros() {
-        ArrayList<Double> numeros = new ArrayList<>();
+        System.out.println("GERANDO NÚMEROS");
+
+        List<Double> numeros = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
             numeros.add(Math.random());
         }
