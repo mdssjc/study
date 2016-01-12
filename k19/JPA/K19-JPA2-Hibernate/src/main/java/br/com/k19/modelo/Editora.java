ab18b6ebed8176caeb48e9edfaac2c3133248ab7
Marcelo dos Santos <mdssjc@gmail.com>
2016-01-12 20:40:42 -0200

    Exercício de JPA - recurso local e uso do lombok.

---
+++ k19/JPA/K19-JPA2-Hibernate/src/main/java/br/com/k19/modelo/Editora.java
@@ -0,0 +1,20 @@
+package br.com.k19.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Editora {
+
+    @Id
+    @GeneratedValue
+    private Long   id;
+
+    private String nome;
+
+    private String email;
+}
