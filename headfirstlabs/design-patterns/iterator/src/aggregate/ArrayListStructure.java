cbb5588656ffb748d14a3c8ccc2d0eeba7a6a9de
Marcelo dos Santos <mdssjc@gmail.com>
2016-01-14 20:41:18 -0200

    Cleanup de imports.

--- headfirstlabs/design-patterns/iterator/src/aggregate/ArrayListStructure.java
+++ headfirstlabs/design-patterns/iterator/src/aggregate/ArrayListStructure.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 import iterator.ArrayListIterator;
 
