b0f9ca0d8dcf3d930cf77186964a8cd64057d16a
Marcelo dos Santos <mdssjc@gmail.com>
2016-01-14 17:23:22 -0200

    Implementação de converter para LocalDate e Date com JPA.

---
+++ k19/JPA/K19-Mapeamento/src/main/java/br/com/k19/conversores/LocalDateConverter.java
@@ -0,0 +1,27 @@
+package br.com.k19.conversores;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter(autoApply = true)
+public class LocalDateConverter implements AttributeConverter<LocalDate, Date> {
+
+    @Override
+    public Date convertToDatabaseColumn(LocalDate attribute) {
+        return Date.from(attribute.atStartOfDay()
+                                  .atZone(ZoneId.systemDefault())
+                                  .toInstant());
+    }
+
+    @Override
+    public LocalDate convertToEntityAttribute(Date dbData) {
+        return LocalDateTime.ofInstant(dbData.toInstant(),
+                ZoneId.systemDefault())
+                            .toLocalDate();
+    }
+}
---
+++ k19/JPA/K19-Mapeamento/src/main/java/br/com/k19/modelo/Reserva.java
@@ -0,0 +1,22 @@
+package br.com.k19.modelo;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Reserva {
+
+    @Id
+    @GeneratedValue
+    private Long      id;
+
+    private LocalDate inicio;
+
+    private LocalDate termino;
+}
