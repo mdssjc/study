c9b30ef6a7b5bc51d9b38148c50a8204dd311f9e
Marcelo dos Santos <mdssjc@gmail.com>
2016-01-15 11:57:16 -0200

    Estados Managed e Detached.

---
+++ k19/JPA/K19-EntityManager/src/test/java/br/com/k19/testes/TesteDetached.java
@@ -0,0 +1,31 @@
+package br.com.k19.testes;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.k19.modelo.Pessoa;
+
+public class TesteDetached {
+
+    public static void main(String[] args) {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory(
+                "K21_entity_manager_pu");
+        EntityManager manager = factory.createEntityManager();
+
+        manager.getTransaction()
+               .begin();
+
+        Pessoa p = manager.find(Pessoa.class, 1L);
+
+        manager.detach(p);
+
+        p.setNome("Jonas Hirata");
+
+        manager.getTransaction()
+               .commit();
+
+        manager.close();
+        factory.close();
+    }
+}
