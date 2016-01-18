7a2e309ef0344d5abaf6acbdb65a7ea43ac87a59
Marcelo dos Santos <mdssjc@gmail.com>
2016-01-18 20:22:40 -0200

    Local Date API

---
+++ oracle/1z0-808/local-date-api/src/base/Main.java
@@ -0,0 +1,48 @@
+package base;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Local Date API (1Z0-808)
+ * Revisão para a certificação.
+ *
+ * @author mdssjc
+ *
+ */
+public class Main {
+
+  public static void main(final String[] args) {
+    new Date();
+    Calendar.getInstance();
+
+    // Datas para Computadores
+    final Instant instantA = Instant.now();
+    for (int i = 0; i < 100_000_000; i++) {
+    }
+    final Instant instantB = Instant.now();
+
+    final Duration duration = Duration.between(instantA, instantB);
+    System.out.println(duration);
+
+    // Datas para Humanos
+    // Date
+    final LocalDate now = LocalDate.now(); // Immutable
+    final LocalDate birthday = LocalDate.of(2016, Month.JULY, 10);
+    LocalDate.of(2016, 1, 1); // Month started in 1
+
+    System.out.println(now); // YYYY-MM-dd (ISO-8601)
+    System.out.println(birthday);
+
+    final Period period = Period.between(now, birthday); // Immutable
+
+    System.out.printf("%s months and %s days.\n", period.getMonths(),
+        period.getDays());
+    // Time
+  }
+}
