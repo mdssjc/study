b5936527cc1d6c411b1fe22028c8f320ae7fea89
Marcelo dos Santos <mdssjc@gmail.com>
2016-01-20 17:00:47 -0200

    Padrão non GoF: Multiton.

---
+++ k19/DP/Multiton/src/main/java/br/com/k19/multiton/Tema.java
@@ -0,0 +1,48 @@
+package br.com.k19.multiton;
+
+import java.awt.Color;
+import java.util.HashMap;
+import java.util.Map;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Tema {
+
+  @Getter
+  @Setter
+  private String                   nome;
+  @Getter
+  @Setter
+  private Color                    corDoFundo;
+  @Getter
+  @Setter
+  private Color                    corDaFonte;
+
+  private static Map<String, Tema> temas = new HashMap<>();
+
+  public static final String       SKY   = "Sky";
+  public static final String       FIRE  = "Fire";
+
+  static {
+    final Tema tema1 = new Tema();
+    tema1.setNome(Tema.SKY);
+    tema1.setCorDoFundo(Color.BLUE);
+    tema1.setCorDaFonte(Color.BLACK);
+
+    final Tema tema2 = new Tema();
+    tema2.setNome(Tema.FIRE);
+    tema2.setCorDoFundo(Color.RED);
+    tema2.setCorDaFonte(Color.WHITE);
+
+    Tema.temas.put(tema1.getNome(), tema1);
+    Tema.temas.put(tema2.getNome(), tema2);
+  }
+
+  private Tema() {
+  }
+
+  public static Tema getInstance(final String nomeDoTema) {
+    return Tema.temas.get(nomeDoTema);
+  }
+}
---
+++ k19/DP/Multiton/src/test/java/br/com/k19/tests/Main.java
@@ -0,0 +1,31 @@
+package br.com.k19.tests;
+
+import br.com.k19.multiton.Tema;
+
+/**
+ * Design Pattern
+ * Multiton (non GoF)
+ *
+ * @author mdssjc
+ *
+ */
+public class Main {
+
+  public static void main(final String[] args) {
+    final Tema temaFire = Tema.getInstance(Tema.FIRE);
+    Main.print(temaFire);
+
+    final Tema temaFire2 = Tema.getInstance(Tema.FIRE);
+    Main.print(temaFire2);
+
+    System.out.println("--------");
+    System.out.println("Comparando as referências...");
+    System.out.println(temaFire == temaFire2);
+  }
+
+  public static void print(final Tema temaFire) {
+    System.out.println("Tema " + temaFire.getNome());
+    System.out.println("Cor da Fonte : " + temaFire.getCorDaFonte());
+    System.out.println("Cor do Fundo : " + temaFire.getCorDoFundo());
+  }
+}
