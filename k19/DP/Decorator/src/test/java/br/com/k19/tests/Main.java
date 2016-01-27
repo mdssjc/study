a29fe037c59676afefe299c6c88d9d10c09cf450
Marcelo dos Santos <mdssjc@gmail.com>
2016-01-26 22:46:01 -0200

    Início do padrão decorator.

---
+++ k19/DP/Decorator/src/test/java/br/com/k19/tests/Main.java
@@ -0,0 +1,15 @@
+package br.com.k19.tests;
+
+/**
+ * Design Pattern
+ * Structural - Decorator (Wrapper)
+ *
+ * @author mdssjc <Marcelo dos Santos>
+ *
+ */
+public class Main {
+
+  public static void main(final String[] args) {
+
+  }
+}
