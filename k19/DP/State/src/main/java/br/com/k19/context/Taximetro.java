99f3bcb881862d5cc0453475553583543d26dbbe
Marcelo dos Santos <mdssjc@gmail.com>
2016-01-30 16:53:54 -0200

    Atualização para o cliente comunicar apenas com o 'context'.

--- k19/DP/State/src/main/java/br/com/k19/context/Taximetro.java
+++ k19/DP/State/src/main/java/br/com/k19/context/Taximetro.java
@@ -1,9 +1,8 @@
 package br.com.k19.context;
 
 import br.com.k19.state.Bandeira;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import br.com.k19.state.concrete.Bandeira1;
+import br.com.k19.state.concrete.Bandeira2;
 
 /**
  * Context Class
@@ -11,12 +10,23 @@ import lombok.Setter;
  * @author mdssjc <Marcelo dos Santos>
  *
  */
-@AllArgsConstructor
 public class Taximetro {
 
-  @Getter
-  @Setter
   private Bandeira bandeira;
+  final Bandeira   bandeira1 = new Bandeira1();
+  final Bandeira   bandeira2 = new Bandeira2();
+
+  public Taximetro() {
+    this.bandeira = this.bandeira1;
+  }
+
+  public void bandeira1() {
+    this.bandeira = this.bandeira1;
+  }
+
+  public void bandeira2() {
+    this.bandeira = this.bandeira2;
+  }
 
   public double calculaValorDaCorrida(final double tempo, final double distancia) {
     return this.bandeira.calculaValorDaCorrida(tempo, distancia);
--- k19/DP/State/src/test/java/br/com/k19/tests/Main.java
+++ k19/DP/State/src/test/java/br/com/k19/tests/Main.java
@@ -1,9 +1,6 @@
 package br.com.k19.tests;
 
 import br.com.k19.context.Taximetro;
-import br.com.k19.state.Bandeira;
-import br.com.k19.state.concrete.Bandeira1;
-import br.com.k19.state.concrete.Bandeira2;
 
 /**
  * Design Pattern
@@ -15,17 +12,16 @@ import br.com.k19.state.concrete.Bandeira2;
 public class Main {
 
   public static void main(final String[] args) {
-    final Bandeira b1 = new Bandeira1();
-    final Bandeira b2 = new Bandeira2();
+    final Taximetro taximetro = new Taximetro();
 
-    final Taximetro taximetro = new Taximetro(b1);
+    taximetro.bandeira1();
 
     final double valor1 = taximetro.calculaValorDaCorrida(10, 20);
     System.out.println("Valor da corrida em bandeira 1: " + valor1);
 
-    taximetro.setBandeira(b2);
+    taximetro.bandeira2();
 
     final double valor2 = taximetro.calculaValorDaCorrida(10, 20);
-    System.out.println("Valor da corrida em bandeira 1: " + valor2);
+    System.out.println("Valor da corrida em bandeira 2: " + valor2);
   }
 }
