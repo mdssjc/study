13f890ec13543283778299159b59098ae824624b
Marcelo dos Santos <mdssjc@gmail.com>
2016-02-12 15:57:23 -0200

    Adicionado a estrutura de composição da lista linkada.

---
+++ mds/algorithms/src/main/java/com/github/mdssjc/algorithms/linkedlist/Node.java
@@ -0,0 +1,18 @@
+package com.github.mdssjc.algorithms.linkedlist;
+
+/**
+ * Classe de composição da Linked List - Lista Linkada.
+ * </p>
+ * Item: valor armazenado
+ * Node: próximo nó
+ *
+ * @author Marcelo dos Santos
+ *
+ * @param <T>
+ *          Tipo de Dado
+ */
+public class Node<T> {
+
+  public T       item;
+  public Node<T> next;
+}
