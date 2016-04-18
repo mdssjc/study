e13bcdb73baafb4709992ea2b61d356035e09e72
Marcelo dos Santos <mdssjc@gmail.com>
2016-04-18 11:56:43 -0300

    Código após refatoração da Classe.

--- diversos/pragmatic-junit/iloveyouboss/test/iloveyouboss/ProfileTest.java
+++ diversos/pragmatic-junit/iloveyouboss/test/iloveyouboss/ProfileTest.java
@@ -1,132 +1,41 @@
 package iloveyouboss;
 
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
+
+import java.util.Collection;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class ProfileTest {
 
-  private Profile         profile;
-  private BooleanQuestion questionIsThereRelocation;
-  private Answer          answerThereIsRelocation;
-  private Answer          answerThereIsNotRelocation;
-  private BooleanQuestion questionReimbursesTuition;
-  private Answer          answerReimbursesTuition;
-  private Answer          answerDoesNotReimbursesTuition;
-  private Criteria        criteria;
+  private Profile profile;
 
   @Before
   public void createProfile() {
-    this.profile = new Profile();
+    this.profile = new Profile("");
   }
 
-  @Before
-  public void createQuestionAndAnswer() {
-    this.questionIsThereRelocation = new BooleanQuestion(1,
-        "Relocation package?");
-    this.answerThereIsRelocation = new Answer(this.questionIsThereRelocation,
-        Bool.TRUE);
-    this.answerThereIsNotRelocation = new Answer(this.questionIsThereRelocation,
-        Bool.FALSE);
-
-    this.questionReimbursesTuition = new BooleanQuestion(1,
-        "Reimburses tuition?");
-    this.answerReimbursesTuition = new Answer(this.questionReimbursesTuition,
-        Bool.TRUE);
-    this.answerDoesNotReimbursesTuition = new Answer(
-        this.questionReimbursesTuition,
-        Bool.FALSE);
-  }
-
-  @Before
-  public void createCriteria() {
-    this.criteria = new Criteria();
+  int[] ids(final Collection<Answer> answers) {
+    return answers.stream()
+                  .mapToInt(a -> a.getQuestion()
+                                  .getId())
+                  .toArray();
   }
 
   @Test
-  public void matchesCriterionWhenMatchesSoleAnswer() {
-    this.profile.add(this.answerThereIsRelocation);
-    final Criterion criterion = new Criterion(this.answerThereIsRelocation,
-        Weight.Important);
+  public void findsAnswersBasedOnPredicate() {
+    this.profile.add(new Answer(new BooleanQuestion(1, "1"), Bool.FALSE));
+    this.profile.add(
+        new Answer(new PercentileQuestion(2, "2", new String[] {}), 0));
+    this.profile.add(
+        new Answer(new PercentileQuestion(3, "3", new String[] {}), 0));
 
-    assertTrue(this.profile.matches(criterion));
-  }
+    final List<Answer> answers = this.profile.find(a -> a.getQuestion()
+                                                         .getClass() == PercentileQuestion.class);
 
-  @Test
-  public void doesNotMatchCriterionWhenNoMatchingAnswerContained() {
-    this.profile.add(this.answerThereIsNotRelocation);
-    final Criterion criterion = new Criterion(this.answerThereIsRelocation,
-        Weight.Important);
-
-    assertFalse(this.profile.matches(criterion));
-  }
-
-  @Test
-  public void matchesCriterionWhenOneOfMultipleAnswerMatches() {
-    this.profile.add(this.answerThereIsRelocation);
-    this.profile.add(this.answerDoesNotReimbursesTuition);
-    final Criterion criterion = new Criterion(this.answerThereIsRelocation,
-        Weight.Important);
-
-    assertTrue(this.profile.matches(criterion));
-  }
-
-  @Test
-  public void doesNotMatchCriteriaWhenNoneOfMultipleCriteriaMatch() {
-    this.profile.add(this.answerDoesNotReimbursesTuition);
-    this.criteria = new Criteria();
-    this.criteria.add(
-        new Criterion(this.answerThereIsRelocation, Weight.Important));
-    this.criteria.add(
-        new Criterion(this.answerReimbursesTuition, Weight.Important));
-
-    assertFalse(this.profile.matches(this.criteria));
-  }
-
-  @Test
-  public void matchesCriteriaWhenAnyOfMultipleCriteriaMatch() {
-    this.profile.add(this.answerThereIsRelocation);
-    this.criteria = new Criteria();
-    this.criteria.add(
-        new Criterion(this.answerThereIsRelocation, Weight.Important));
-    this.criteria.add(
-        new Criterion(this.answerReimbursesTuition, Weight.Important));
-
-    assertTrue(this.profile.matches(this.criteria));
-  }
-
-  @Test
-  public void doesNotMatchWhenAnyMustMeetCriteriaNotMet() {
-    this.profile.add(this.answerThereIsRelocation);
-    this.profile.add(this.answerDoesNotReimbursesTuition);
-    this.criteria.add(
-        new Criterion(this.answerThereIsRelocation, Weight.Important));
-    this.criteria.add(
-        new Criterion(this.answerReimbursesTuition, Weight.MustMatch));
-
-    assertFalse(this.profile.matches(this.criteria));
-  }
-
-  @Test
-  public void alwaysMatchesDontCareCriterion() {
-    this.profile.add(this.answerDoesNotReimbursesTuition);
-    final Criterion criterion = new Criterion(this.answerReimbursesTuition,
-        Weight.DontCare);
-
-    assertTrue(this.profile.matches(criterion));
-  }
-
-  @Test
-  public void scoreIsZeroWhenThereAreNoMatches() {
-    this.criteria.add(
-        new Criterion(this.answerThereIsRelocation, Weight.Important));
-
-    final ProfileMatch match = this.profile.match(this.criteria);
-
-    assertThat(match.getScore(), equalTo(0));
+    assertThat(ids(answers), equalTo(new int[] { 2, 3 }));
   }
 }
