38933c3a117b3d99bb183d1d60a8dfd7e57e9c86
Marcelo dos Santos <mdssjc@gmail.com>
2016-04-18 20:15:11 -0300

    Automatização de testes sem framework.

--- diversos/cdc-testes_automatizados_software/src/main/java/com/mysql/mdssjc/cdc_testes_automatizados_software/capitulo1/TesteDoAvaliador.java
+++ diversos/cdc-testes_automatizados_software/src/main/java/com/mysql/mdssjc/cdc_testes_automatizados_software/capitulo1/TesteDoAvaliador.java
@@ -3,22 +3,26 @@ package com.mysql.mdssjc.cdc_testes_automatizados_software.capitulo1;
 public class TesteDoAvaliador {
 
   public static void main(String[] args) {
+    // cenário: 3 lances em ordem crescente
     Usuario joao = new Usuario("Joao");
     Usuario jose = new Usuario("José");
     Usuario maria = new Usuario("Maria");
 
     Leilao leilao = new Leilao("Playstation 3 Novo");
 
+    leilao.propoe(new Lance(maria, 250.0));
     leilao.propoe(new Lance(joao, 300.0));
     leilao.propoe(new Lance(jose, 400.0));
-    leilao.propoe(new Lance(maria, 250.0));
 
+    // executando a ação
     Avaliador leiloeiro = new Avaliador();
     leiloeiro.avalia(leilao);
 
-    // imprime 400.0
-    System.out.println(leiloeiro.getMaiorLance());
-    // imprime 250.0
-    System.out.println(leiloeiro.getMenorDeTodos());
+    // comparando a saída com o esperado
+    double maiorEsperado = 400;
+    double menorEsperado = 250;
+
+    System.out.println(maiorEsperado == leiloeiro.getMaiorLance());
+    System.out.println(menorEsperado == leiloeiro.getMenorDeTodos());
   }
 }
