014a5f0774ba9209bad0c9f84e83a82f665c4ec1
Marcelo dos Santos <mdssjc@gmail.com>
2016-04-21 11:35:12 -0300

    Implementação dos testes em LeilaoTest.java.

---
+++ diversos/cdc-testes_automatizados_software/src/test/java/com/github/mdssjc/cdc/tas/capitulo1/LeilaoTest.java
@@ -0,0 +1,43 @@
+package com.github.mdssjc.cdc.tas.capitulo1;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class LeilaoTest {
+
+  @Test
+  public void deveReceberUmLance() {
+    final Leilao leilao = new Leilao("MacBook Pro 15");
+
+    assertEquals(0, leilao.getLances()
+                          .size());
+
+    leilao.propoe(new Lance(new Usuario("Steve Jobs"), 2000));
+
+    assertEquals(1, leilao.getLances()
+                          .size());
+    assertEquals(2000, leilao.getLances()
+                             .get(0)
+                             .getValor(),
+        0.00001);
+  }
+
+  @Test
+  public void deveReceberVariosLances() {
+    final Leilao leilao = new Leilao("MacBook Pro 15");
+    leilao.propoe(new Lance(new Usuario("Steve Jobs"), 2000));
+    leilao.propoe(new Lance(new Usuario("Steve Wozniak"), 3000));
+
+    assertEquals(2, leilao.getLances()
+                          .size());
+    assertEquals(2000, leilao.getLances()
+                             .get(0)
+                             .getValor(),
+        0.00001);
+    assertEquals(3000, leilao.getLances()
+                             .get(1)
+                             .getValor(),
+        0.00001);
+  }
+}
