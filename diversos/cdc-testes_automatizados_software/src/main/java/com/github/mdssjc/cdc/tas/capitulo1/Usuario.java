cf6d672474d1f37624f6e931ca3fcd6234fed546
Marcelo dos Santos <mdssjc@gmail.com>
2016-04-21 11:42:10 -0300

    Implementação do equals e hashcode.

--- diversos/cdc-testes_automatizados_software/src/main/java/com/github/mdssjc/cdc/tas/capitulo1/Usuario.java
+++ diversos/cdc-testes_automatizados_software/src/main/java/com/github/mdssjc/cdc/tas/capitulo1/Usuario.java
@@ -7,4 +7,40 @@ public class Usuario {
   public Usuario(final String nome) {
     this.nome = nome;
   }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((this.getNome() == null) ? 0 : this.getNome()
+                                                                  .hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(final Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    final Usuario other = (Usuario) obj;
+    if (this.getNome() == null) {
+      if (other.getNome() != null) {
+        return false;
+      }
+    } else if (!this.getNome()
+                    .equals(other.getNome())) {
+      return false;
+    }
+    return true;
+  }
+
+  public String getNome() {
+    return this.nome;
+  }
 }
