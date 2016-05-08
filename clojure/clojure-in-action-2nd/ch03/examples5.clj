3a7c3d5cde4e6f6bb7b065da0b990f8d2696af7d
Marcelo dos Santos <mdssjc@gmail.com>
2016-05-07 22:18:29 -0300

    Uso do require para aplicar alias.

--- clojure/clojure-in-action-2nd/ch03/examples5.clj
+++ clojure/clojure-in-action-2nd/ch03/examples5.clj
@@ -1,12 +1,14 @@
 (ns org.currylogic.damages.http.expenses)
-(use 'clojure.data.json)
-(use 'clojure.xml)
+(require '(clojure.data [json :as json-lib]))
+(require '(clojure [xml :as xml-core]))
+;;(use 'clojure.data.json)
+;;(use 'clojure.xml)
 
 (declare load-totals)
 
 (defn import-transactions-xml-from-bank [url]
-  (let [xml-document (parse url)]))
+  (let [xml-document (xml-core/parse url)]))
 
 (defn totals-by-day [start-date end-date]
   (let [expenses-by-day (load-totals start-date end-date)]
-  (json-str expenses-by-day)))
+  (json-lib/json-str expenses-by-day)))
