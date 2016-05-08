327b8e3af0149bec162b090cef8b4eba708fbbf7
Marcelo dos Santos <mdssjc@gmail.com>
2016-05-08 20:47:13 -0300

    Exemplo com destrutor.

--- clojure/clojure-in-action-2nd/ch03/examples5.clj
+++ clojure/clojure-in-action-2nd/ch03/examples5.clj
@@ -14,3 +14,22 @@
 (defn totals-by-day [start-date end-date]
   (let [expenses-by-day (load-totals start-date end-date)]
   (json-lib/json-str expenses-by-day)))
+
+;; ---
+(def mds {:first-name "Marcelo"
+          :last-name "dos Santos"
+          :salary 100})
+
+(defn describe-salary [person]
+  (let [first (:first-name person)
+        last (:last-name person)
+        annual (:salary person)]
+    (println first last "earns" annual)))
+
+(defn describe-salary2 [{first :first-name
+                         last :last-name
+                         annual :salary}]
+  (println first last "earns" annual))
+
+(describe-salary mds)
+(describe-salary2 mds)
