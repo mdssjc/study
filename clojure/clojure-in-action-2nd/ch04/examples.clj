a5b73f2649cbc1571e397cb3534d8828af6f998d
Marcelo dos Santos <mdssjc@gmail.com>
2016-05-28 21:21:13 -0300

    Início com multimethods.

--- clojure/clojure-in-action-2nd/ch04/examples.clj
+++ clojure/clojure-in-action-2nd/ch04/examples.clj
@@ -43,3 +43,16 @@
 (subtyping-map-type-namer (hash-map))
 (subtyping-map-type-namer (array-map))
 (subtyping-map-type-namer (sorted-map))
+
+;; Polymorphism using multimethods
+(def example-user {:login "rob" :referrer "mint.com" :salary 100000})
+
+(defn fee-amount [percentage user]
+  (with-precision 16 :rounding HALF_EVEN
+    (* 0.01M percentage (:salary user))))
+(defn affiliate-fee [user]
+  (case (:referrer user)
+    "google.com" (fee-amount 0.01M user)
+    "mint.com" (fee-amount 0.03M user)
+    (fee-amount 0.02M user)))
+(affiliate-fee example-user)
