86853f259127373562d7ea3c43f4f2cc5bf21d35
Marcelo dos Santos <mdssjc@gmail.com>
2016-05-28 22:13:52 -0300

    Uso do defmulti.

--- clojure/clojure-in-action-2nd/ch04/examples.clj
+++ clojure/clojure-in-action-2nd/ch04/examples.clj
@@ -50,9 +50,25 @@
 (defn fee-amount [percentage user]
   (with-precision 16 :rounding HALF_EVEN
     (* 0.01M percentage (:salary user))))
+
 (defn affiliate-fee [user]
   (case (:referrer user)
     "google.com" (fee-amount 0.01M user)
     "mint.com" (fee-amount 0.03M user)
     (fee-amount 0.02M user)))
+(affiliate-fee example-user)
+
+(defmulti affiliate-fee (fn [user] (:referrer user)))
+(defmethod affiliate-fee "mint.com" [user]
+  (fee-amount 0.03M user))
+(defmethod affiliate-fee "google.com" [user]
+  (fee-amount 0.01M user))
+(defmethod affiliate-fee :default [user]
+  (fee-amount 0.02M user))
+(affiliate-fee example-user)
+
+(ns-unmap 'user 'affiliate-fee)
+(defmulti affiliate-fee :referrer :default "*")
+(defmethod affiliate-fee "*" [user]
+  (fee-amount 0.02M user))
 (affiliate-fee example-user)
