c3b0ccfc3ff94f9d3042d875efb39df46ec04f11
Marcelo dos Santos <mdssjc@gmail.com>
2016-05-30 19:55:10 -0300

    Inclusão do teste validate01.

--- automatizacao-teste-software/topico-1/identifier/src/test/java/com/github/mdssjc/ats/identifier/IdentifierTestCase.java
+++ automatizacao-teste-software/topico-1/identifier/src/test/java/com/github/mdssjc/ats/identifier/IdentifierTestCase.java
@@ -1,13 +1,17 @@
 package com.github.mdssjc.ats.identifier;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class IdentifierTestCase {
 
   @Test
-  public void test() {
-    fail("Not yet implemented");
+  public void validate01() {
+    Identifier identifier = new Identifier();
+
+    boolean obtido = identifier.validateIdentifier("a1");
+
+    assertTrue(obtido);
   }
 }
