2c61a4a803e43b5933acf64911f1777389d7d8e9
Marcelo dos Santos <mdssjc@gmail.com>
2016-05-30 19:50:20 -0300

    Inclusão do template do JUnit.

---
+++ automatizacao-teste-software/topico-1/identifier/src/test/java/com/github/mdssjc/ats/identifier/IdentifierTestCase.java
@@ -0,0 +1,13 @@
+package com.github.mdssjc.ats.identifier;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class IdentifierTestCase {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+}
