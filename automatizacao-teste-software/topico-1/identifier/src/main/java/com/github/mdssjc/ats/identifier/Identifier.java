1386fd83bbfe8901e9b5657801bb4bda5b718347
Marcelo dos Santos <mdssjc@gmail.com>
2016-05-30 20:31:16 -0300

    Correção do bug apontado pelo teste 'validate02'.

--- automatizacao-teste-software/topico-1/identifier/src/main/java/com/github/mdssjc/ats/identifier/Identifier.java
+++ automatizacao-teste-software/topico-1/identifier/src/main/java/com/github/mdssjc/ats/identifier/Identifier.java
@@ -5,20 +5,21 @@ public class Identifier {
   public boolean validateIdentifier(final String s) {
     char achar;
     boolean valid_id = false;
-    achar = s.charAt(0);
-    valid_id = valid_s(achar);
-    if (s.length() > 1) {
-      achar = s.charAt(1);
-      int i = 1;
-      while (i < s.length() - 1) {
-        achar = s.charAt(i);
-        if (!valid_f(achar)) {
-          valid_id = false;
+    if (s.length() > 0) {
+      achar = s.charAt(0);
+      valid_id = valid_s(achar);
+      if (s.length() > 1) {
+        achar = s.charAt(1);
+        int i = 1;
+        while (i < s.length() - 1) {
+          achar = s.charAt(i);
+          if (!valid_f(achar)) {
+            valid_id = false;
+          }
+          i++;
         }
-        i++;
       }
     }
-
     if (valid_id && (s.length() >= 1) && (s.length() < 6)) {
       return true;
     } else {
