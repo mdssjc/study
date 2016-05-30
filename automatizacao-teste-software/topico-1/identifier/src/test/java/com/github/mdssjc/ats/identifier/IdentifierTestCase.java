7e4825998bf4652b12bd39f77889cc5afe1add11
Marcelo dos Santos <mdssjc@gmail.com>
2016-05-30 20:24:09 -0300

    Mais quatro testes adicionados.

--- automatizacao-teste-software/topico-1/identifier/src/test/java/com/github/mdssjc/ats/identifier/IdentifierTestCase.java
+++ automatizacao-teste-software/topico-1/identifier/src/test/java/com/github/mdssjc/ats/identifier/IdentifierTestCase.java
@@ -1,5 +1,6 @@
 package com.github.mdssjc.ats.identifier;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -13,5 +14,41 @@ public class IdentifierTestCase {
     boolean obtido = identifier.validateIdentifier("a1");
 
     assertTrue(obtido);
+  }
+
+  @Test
+  public void validate02() {
+    Identifier identifier = new Identifier();
+
+    boolean obtido = identifier.validateIdentifier("");
+
+    assertFalse(obtido);
+  }
+
+  @Test
+  public void validate03() {
+    Identifier identifier = new Identifier();
+
+    boolean obtido = identifier.validateIdentifier("A1b2C3d");
+
+    assertFalse(obtido);
+  }
+
+  @Test
+  public void validate04() {
+    Identifier identifier = new Identifier();
+
+    boolean obtido = identifier.validateIdentifier("2B3");
+
+    assertFalse(obtido);
+  }
+
+  @Test
+  public void validate05() {
+    Identifier identifier = new Identifier();
+
+    boolean obtido = identifier.validateIdentifier("Z#12");
+
+    assertFalse(obtido);
   }
 }
