5197b4c67ed625e303ad9c58d3b1a0f3cffe5814
Marcelo dos Santos <mdssjc@gmail.com>
2016-05-31 20:12:31 -0300

    Teste com timeout de 200 ms.

--- automatizacao-teste-software/topico-1/identifier/src/test/java/com/github/mdssjc/ats/identifier/IdentifierTestCase.java
+++ automatizacao-teste-software/topico-1/identifier/src/test/java/com/github/mdssjc/ats/identifier/IdentifierTestCase.java
@@ -8,42 +8,43 @@ import org.junit.Test;
 
 public class IdentifierTestCase {
 
-  private Identifier identifier;
+  private Identifier       identifier;
+  private static final int LIMIT = 200;
 
   @Before
   public void init() {
     this.identifier = new Identifier();
   }
 
-  @Test
+  @Test(timeout = LIMIT)
   public void validate01() {
     final boolean obtido = this.identifier.validateIdentifier("a1");
 
     assertTrue(obtido);
   }
 
-  @Test
+  @Test(timeout = 200)
   public void validate02() {
     final boolean obtido = this.identifier.validateIdentifier("");
 
     assertFalse(obtido);
   }
 
-  @Test
+  @Test(timeout = 200)
   public void validate03() {
     final boolean obtido = this.identifier.validateIdentifier("A1b2C3d");
 
     assertFalse(obtido);
   }
 
-  @Test
+  @Test(timeout = 200)
   public void validate04() {
     final boolean obtido = this.identifier.validateIdentifier("2B3");
 
     assertFalse(obtido);
   }
 
-  @Test
+  @Test(timeout = 200)
   public void validate05() {
     final boolean obtido = this.identifier.validateIdentifier("Z#12");
 
