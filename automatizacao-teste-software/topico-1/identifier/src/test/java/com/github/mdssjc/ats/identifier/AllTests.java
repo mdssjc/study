c34480fdd65830ecf07380b103e412429b39e288
Marcelo dos Santos <mdssjc@gmail.com>
2016-05-31 20:23:25 -0300

    Inclusão da Suite de Testes.

---
+++ automatizacao-teste-software/topico-1/identifier/src/test/java/com/github/mdssjc/ats/identifier/AllTests.java
@@ -0,0 +1,11 @@
+package com.github.mdssjc.ats.identifier;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ IdentifierTestCase.class })
+public class AllTests {
+
+}
