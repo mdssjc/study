f3f0f4dc4796734dfe02d3bdcf621a9221e04a92
Marcelo dos Santos <mdssjc@gmail.com>
2016-06-05 22:51:47 -0300

    Uso do 'memfn'.

---
+++ clojure/clojure-in-action-2nd/ch05/examples2.clj
@@ -0,0 +1,8 @@
+(map (fn [x] (.getBytes x)) ["amit" "rob" "kyle"])
+(map #(.getBytes %) ["amit" "rob" "kyle"])
+
+(memfn ^String getBytes)
+(map (memfn getBytes) ["amit" "rob" "kyle"])
+
+(.subSequence "Clojure" 2 5)
+((memfn ^String subSequence ^Long start ^Long end) "Clojure" 2 5)
