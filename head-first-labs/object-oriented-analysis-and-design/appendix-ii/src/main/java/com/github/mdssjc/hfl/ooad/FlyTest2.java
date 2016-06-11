396d13aa2282fd0557ac2ccda22e7f15c643f22a
Marcelo dos Santos <mdssjc@gmail.com>
2016-06-11 12:16:28 -0300

    Exemplo de encapsulamento.

---
+++ head-first-labs/object-oriented-analysis-and-design/appendix-ii/src/main/java/com/github/mdssjc/hfl/ooad/FlyTest2.java
@@ -0,0 +1,10 @@
+package com.github.mdssjc.hfl.ooad;
+
+public class FlyTest2 {
+
+  public static void main(final String[] args) {
+    final Airplane2 biplane = new Airplane2();
+    biplane.speed = 212;
+    System.out.println(biplane.speed);
+  }
+}
