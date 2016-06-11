8aeda247e7bd4571620e9b60123ebcaeb7360243
Marcelo dos Santos <mdssjc@gmail.com>
2016-06-11 12:19:40 -0300

    Exemplo com encapsulamento.

---
+++ head-first-labs/object-oriented-analysis-and-design/appendix-ii/src/main/java/com/github/mdssjc/hfl/ooad/FlyTest3.java
@@ -0,0 +1,14 @@
+package com.github.mdssjc.hfl.ooad;
+
+public class FlyTest3 {
+
+  public static void main(final String[] args) {
+    final Jet2 jet1 = new Jet2();
+    jet1.speed = 212;
+    System.out.println(jet1.speed);
+
+    final Jet2 jet2 = new Jet2();
+    jet2.setSpeed(212);
+    System.out.println(jet2.getSpeed());
+  }
+}
