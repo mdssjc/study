438ba1e6fbb97135d924960672d3f61076866ec3
Marcelo dos Santos <mdssjc@gmail.com>
2016-06-11 11:04:50 -0300

    Inclusão do pacote groupid.

---
+++ head-first-labs/object-oriented-analysis-and-design/Appendix-II/src/main/java/com/github/mdssjc/hfl/ooad/Airplane.java
@@ -0,0 +1,18 @@
+package com.github.mdssjc.hfl.ooad;
+
+// Airplane Class
+public class Airplane {
+
+  private int speed;
+
+  public Airplane() {
+  }
+
+  public void setSpeed(int speed) {
+    this.speed = speed;
+  }
+
+  public int getSpeed() {
+    return speed;
+  }
+}
