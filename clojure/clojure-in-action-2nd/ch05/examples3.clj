ae7ff5e34f7720e0641110f7956f7b4039a768b5
Marcelo dos Santos <mdssjc@gmail.com>
2016-06-11 21:28:34 -0300

    Exemplos com proxy e reify.

---
+++ clojure/clojure-in-action-2nd/ch05/examples3.clj
@@ -0,0 +1,9 @@
+(import 'java.awt.event.MouseAdapter)
+
+(proxy [MouseAdapter] []
+  (mousePressed [event]
+    (println "Hey!")))
+
+(reify java.io.FileFilter
+  (accept [this f]
+    (.isDirectory f)))
