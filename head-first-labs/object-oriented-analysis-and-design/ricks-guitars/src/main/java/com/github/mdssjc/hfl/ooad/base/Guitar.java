d4340b71dc584ba643a31547257ccd6fd11ff4c2
Marcelo dos Santos <mdssjc@gmail.com>
2016-06-12 15:09:39 -0300

    Melhorias no equals e hashcode.

--- head-first-labs/object-oriented-analysis-and-design/ricks-guitars/src/main/java/com/github/mdssjc/hfl/ooad/base/Guitar.java
+++ head-first-labs/object-oriented-analysis-and-design/ricks-guitars/src/main/java/com/github/mdssjc/hfl/ooad/base/Guitar.java
@@ -29,8 +29,7 @@ public class Guitar {
     return this.spec;
   }
 
-  @Override
-  public boolean equals(final Object other) {
+  public boolean matches(final Object other) {
     if (other instanceof Guitar) {
       return this.serialNumber.equals(((Guitar) other).getSerialNumber());
     } else if (other instanceof GuitarSpec) {
@@ -39,5 +38,44 @@ public class Guitar {
       return this.serialNumber.equals(other);
     }
     return false;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result
+        + ((this.serialNumber == null) ? 0 : this.serialNumber.hashCode());
+    result = prime * result + ((this.spec == null) ? 0 : this.spec.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(final Object object) {
+    if (this == object) {
+      return true;
+    }
+    if (object == null) {
+      return false;
+    }
+    if (getClass() != object.getClass()) {
+      return matches(object);
+    }
+    final Guitar other = (Guitar) object;
+    if (this.serialNumber == null) {
+      if (other.serialNumber != null) {
+        return false;
+      }
+    } else if (!this.serialNumber.equals(other.serialNumber)) {
+      return false;
+    }
+    if (this.spec == null) {
+      if (other.spec != null) {
+        return false;
+      }
+    } else if (!this.spec.equals(other.spec)) {
+      return false;
+    }
+    return true;
   }
 }
