3cd711f7314ddf606a110611aabd15b3bca5a0fa
Marcelo dos Santos <mdssjc@gmail.com>
2016-06-19 13:50:50 -0300

    Unit Test da Classe Inventory.

---
+++ head-first-labs/object-oriented-analysis-and-design/ricks-guitars/src/test/java/com/github/mdssjc/hfooad/InventoryTest.java
@@ -0,0 +1,74 @@
+package com.github.mdssjc.hfooad;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import java.util.Iterator;
+
+import org.junit.Test;
+
+import com.github.mdssjc.hfooad.base.Guitar;
+import com.github.mdssjc.hfooad.base.GuitarSpec;
+import com.github.mdssjc.hfooad.base.Inventory;
+import com.github.mdssjc.hfooad.base.builder.GuitarBuilder;
+import com.github.mdssjc.hfooad.types.Builder;
+import com.github.mdssjc.hfooad.types.Type;
+import com.github.mdssjc.hfooad.types.Wood;
+
+public class InventoryTest {
+
+  @Test
+  public void addAndGetGuitar() {
+    final String serialNumber = "12345";
+    final Guitar guitar = new Guitar(serialNumber, 1250.00,
+        new GuitarSpec(Builder.GIBSON, "Test", Type.ELECTRIC, 6,
+            Wood.BRAZILIAN_ROSEWOOD, Wood.BRAZILIAN_ROSEWOOD));
+
+    final Inventory<Guitar, GuitarSpec> inventory = new Inventory<>();
+    inventory.add(guitar);
+
+    final Guitar guitarResult = inventory.get(serialNumber);
+
+    assertEquals(guitar, guitarResult);
+  }
+
+  @Test
+  public void searchGuitar() {
+    final Inventory<Guitar, GuitarSpec> inventory = new Inventory<>();
+    GuitarsTestDataBuilder.initializeInventory(inventory);
+
+    final GuitarSpec spec = new GuitarSpec(Builder.FENDER,
+        "Stratocastor", Type.ELECTRIC, 6, Wood.ALDER, Wood.ALDER);
+
+    final Iterator<Guitar> guitars = inventory.search(spec);
+    final Guitar guitar1 = guitars.next();
+    final Guitar guitar2 = guitars.next();
+    final boolean hasNext = guitars.hasNext();
+
+    final Guitar guitarRef1 = new GuitarBuilder()
+                                                 .setSerialNumber("V95693")
+                                                 .setPrice(1499.95)
+                                                 .setBuilder(Builder.FENDER)
+                                                 .setModel("Stratocastor")
+                                                 .setType(Type.ELECTRIC)
+                                                 .setNumStrings(6)
+                                                 .setBackWood(Wood.ALDER)
+                                                 .setTopWood(Wood.ALDER)
+                                                 .makeGuitar();
+
+    final Guitar guitarRef2 = new GuitarBuilder()
+                                                 .setSerialNumber("V9512")
+                                                 .setPrice(1549.95)
+                                                 .setBuilder(Builder.FENDER)
+                                                 .setModel("Stratocastor")
+                                                 .setType(Type.ELECTRIC)
+                                                 .setNumStrings(6)
+                                                 .setBackWood(Wood.ALDER)
+                                                 .setTopWood(Wood.ALDER)
+                                                 .makeGuitar();
+
+    assertEquals(guitarRef1, guitar1);
+    assertEquals(guitarRef2, guitar2);
+    assertFalse(hasNext);
+  }
+}
