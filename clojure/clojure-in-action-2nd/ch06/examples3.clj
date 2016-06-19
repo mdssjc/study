f7f02aed0827cea363ac69bb29b8902641fc3691
Marcelo dos Santos <mdssjc@gmail.com>
2016-06-18 23:08:07 -0300

    Uso de Futures.

---
+++ clojure/clojure-in-action-2nd/ch06/examples3.clj
@@ -0,0 +1,18 @@
+;; FUTURES
+(defn long-calculation [num1 num2]
+  (Thread/sleep 5000)
+  (* num1 num2))
+
+(defn long-run []
+  (let [x (long-calculation 11 13)
+        y (long-calculation 13 17)
+        z (long-calculation 17 19)]
+    (* x y z)))
+(time (long-run))
+
+(defn fast-run []
+  (let [x (future (long-calculation 11 13))
+        y (future (long-calculation 13 17))
+        z (future (long-calculation 17 19))]
+    (* @x @y @z)))
+(time (fast-run))
