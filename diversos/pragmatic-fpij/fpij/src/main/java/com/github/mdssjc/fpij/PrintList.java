68dfa0ca75d837153d19bbfee661391d3356097b
Marcelo dos Santos <mdssjc@gmail.com>
2016-06-23 20:35:57 -0300

    Joining no collect.

---
+++ diversos/pragmatic-fpij/fpij/src/main/java/com/github/mdssjc/fpij/PrintList.java
@@ -0,0 +1,36 @@
+package com.github.mdssjc.fpij;
+
+import static com.github.mdssjc.fpij.Folks.friends;
+import static java.util.stream.Collectors.joining;
+
+public class PrintList {
+
+  public static void main(final String[] args) {
+    System.out.println("//" + "START:FOREACH_OUTPUT");
+    for (String name : friends) {
+      System.out.print(name + ", ");
+    }
+    System.out.println();
+    System.out.println("//" + "END:FOREACH_OUTPUT");
+
+    System.out.println("//" + "START:FOR_OUTPUT");
+    for (int i = 0; i < friends.size() - 1; i++) {
+      System.out.print(friends.get(i) + ", ");
+    }
+
+    if (friends.size() > 0)
+      System.out.println(friends.get(friends.size() - 1));
+    System.out.println("//" + "END:FOR_OUTPUT");
+
+    System.out.println("//" + "START:JOIN_OUTPUT");
+    System.out.println(String.join(", ", friends));
+    System.out.println("//" + "END:JOIN_OUTPUT");
+
+    System.out.println("//" + "START:MAP_JOIN_OUTPUT");
+    System.out.println(
+        friends.stream()
+               .map(String::toUpperCase)
+               .collect(joining(", ")));
+    System.out.println("//" + "END:MAP_JOIN_OUTPUT");
+  }
+}
