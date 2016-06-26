49bee37ababb7d827971b68fc59e79eeecfc10fe
Marcelo dos Santos <mdssjc@gmail.com>
2016-06-25 22:31:10 -0300

    Função Filter.

--- clojure/clojure-in-action-2nd/ch08/examples.clj
+++ clojure/clojure-in-action-2nd/ch08/examples.clj
@@ -63,3 +63,17 @@
 (defn all-greater-than [threshold numbers]
   (reduce #(if (> %2 threshold) (conj %1 %2) %1) [] numbers))
 (all-greater-than 5 [5 7 9 3 4 1 2 8])
+
+;;;
+(defn all-lesser-than [threshold numbers]
+  (compute-across #(if (< %2 threshold) (conj %1 %2) %1) numbers []))
+(all-lesser-than 5 [5 7 9 3 4 1 2 8])
+
+(defn select-if [pred elements]
+  (compute-across #(if (pred %2) (conj %1 %2) %1) elements []))
+(select-if odd? [5 7 9 3 4 1 2 8])
+
+(defn all-lesser-than [threshold numbers]
+  (select-if #(< % threshold) numbers))
+
+(filter odd? [5 7 9 3 4 1 2 8])
