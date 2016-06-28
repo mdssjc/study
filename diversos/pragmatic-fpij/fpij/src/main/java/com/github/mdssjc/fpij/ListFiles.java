1f980e6e249d195e0dd2f081f23d4f29c31fd228
Marcelo dos Santos <mdssjc@gmail.com>
2016-06-27 21:15:59 -0300

    Stream com arquivos.

---
+++ diversos/pragmatic-fpij/fpij/src/main/java/com/github/mdssjc/fpij/ListFiles.java
@@ -0,0 +1,13 @@
+package com.github.mdssjc.fpij;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ListFiles {
+
+  public static void main(String[] args) throws IOException {
+    Files.list(Paths.get("."))
+         .forEach(System.out::println);
+  }
+}
