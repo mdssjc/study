c5368848e85951fad275147cf0cb7f0794d5cbcb
Marcelo dos Santos <mdssjc@gmail.com>
2016-06-29 15:20:32 -0300

    Padrão Utility/Helper.

---
+++ java-code-geeks/advanced/src/main/java/com/javacodegeeks/advanced/construction/patterns/HelperClass.java
@@ -0,0 +1,13 @@
+package com.javacodegeeks.advanced.construction.patterns;
+
+public final class HelperClass {
+
+  private HelperClass() {
+  }
+
+  public static void helperMethod1() {
+  }
+
+  public static void helperMethod2() {
+  }
+}
