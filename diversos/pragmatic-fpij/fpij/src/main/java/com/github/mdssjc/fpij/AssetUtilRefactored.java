53d0368ef6bcac3aa02bfe55a749f41deca2e260
Marcelo dos Santos <mdssjc@gmail.com>
2016-06-28 22:03:57 -0300

    Strategy pattern no nível de método com lambda.

---
+++ diversos/pragmatic-fpij/fpij/src/main/java/com/github/mdssjc/fpij/AssetUtilRefactored.java
@@ -0,0 +1,35 @@
+package com.github.mdssjc.fpij;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+import com.github.mdssjc.fpij.Asset.AssetType;
+
+public class AssetUtilRefactored {
+
+  public static int totalAssetValues(final List<Asset> assets,
+      final Predicate<Asset> assetSelector) {
+    return assets.stream()
+                 .filter(assetSelector)
+                 .mapToInt(Asset::getValue)
+                 .sum();
+  }
+
+  public static void main(final String[] args) {
+    List<Asset> assets = Arrays.asList(
+        new Asset(Asset.AssetType.BOND, 1000),
+        new Asset(Asset.AssetType.BOND, 2000),
+        new Asset(Asset.AssetType.STOCK, 3000),
+        new Asset(Asset.AssetType.STOCK, 4000));
+
+    System.out.println("Total of all assets: " +
+        totalAssetValues(assets, asset -> true));
+
+    System.out.println("Total of bonds: " +
+        totalAssetValues(assets, asset -> asset.getType() == AssetType.BOND));
+
+    System.out.println("Total of stocks: " +
+        totalAssetValues(assets, asset -> asset.getType() == AssetType.STOCK));
+  }
+}
