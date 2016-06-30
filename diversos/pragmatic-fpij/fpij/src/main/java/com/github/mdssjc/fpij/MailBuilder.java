ae38ce0ebb3ac2b59b557160e02ed44a60965371
Marcelo dos Santos <mdssjc@gmail.com>
2016-06-29 23:29:22 -0300

    Métodos aninhados e padrão de projeto Builder.

---
+++ diversos/pragmatic-fpij/fpij/src/main/java/com/github/mdssjc/fpij/MailBuilder.java
@@ -0,0 +1,38 @@
+package com.github.mdssjc.fpij;
+
+public class MailBuilder {
+
+  public MailBuilder from(final String address) {
+    /* ... */
+    return this;
+  }
+
+  public MailBuilder to(final String address) {
+    /* ... */
+    return this;
+  }
+
+  public MailBuilder subject(final String line) {
+    /* ... */
+    return this;
+  }
+
+  public MailBuilder body(final String message) {
+    /* ... */
+    return this;
+  }
+
+  public void send() {
+    System.out.println("sending...");
+  }
+
+  // ...
+  public static void main(final String[] args) {
+    new MailBuilder()
+                     .from("build@agiledeveloper.com")
+                     .to("venkats@agiledeveloper.com")
+                     .subject("build notification")
+                     .body("...it sucks less...")
+                     .send();
+  }
+}
---
+++ diversos/pragmatic-fpij/fpij/src/main/java/com/github/mdssjc/fpij/Mailer.java
@@ -0,0 +1,34 @@
+package com.github.mdssjc.fpij;
+
+public class Mailer {
+
+  public void from(final String address) {
+    /* ... */
+  }
+
+  public void to(final String address) {
+    /* ... */
+  }
+
+  public void subject(final String line) {
+    /* ... */
+  }
+
+  public void body(final String message) {
+    /* ... */
+  }
+
+  public void send() {
+    System.out.println("sending...");
+  }
+
+  // ...
+  public static void main(final String[] args) {
+    Mailer mailer = new Mailer();
+    mailer.from("build@agiledeveloper.com");
+    mailer.to("venkats@agiledeveloper.com");
+    mailer.subject("build notification");
+    mailer.body("...your code sucks...");
+    mailer.send();
+  }
+}
