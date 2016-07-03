fc7533394ff4652c8d0177a2e2bf547e560b2639
Marcelo dos Santos <mdssjc@gmail.com>
2016-07-02 23:05:38 -0300

    Simulator: Creative Programming Assignments.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/assignments/loops/Simulator.java
@@ -0,0 +1,28 @@
+package com.github.mdssjc.algorithms.assignments.loops;
+
+/**
+ * Simulator: Creative Programming Assignments
+ * </p>
+ * Assignment: Loops and Conditionals
+ * Description: Binary logarithm, checkerboard pattern, random walk,
+ * Gaussian distribution.
+ * Concepts: loops and conditionals
+ * Difficulty: 1
+ * 
+ * @author mdssjc (Marcelo dos Santos)
+ *
+ */
+public class Simulator {
+
+  public static void main(String[] args) {
+    // Ordered
+    String[][] inputs = {
+        { "10", "17", "49" },
+        { "49", "17", "10" },
+        { "10", "49", "17" }
+    };
+    for (String[] input : inputs) {
+      Ordered.main(input);
+    }
+  }
+}
