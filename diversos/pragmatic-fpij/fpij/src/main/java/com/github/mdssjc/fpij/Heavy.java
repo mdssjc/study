78a6e6f978d881d38b10d9ccb4623f47b9bdacc1
Marcelo dos Santos <mdssjc@gmail.com>
2016-07-05 19:41:24 -0300

    Criação de objetos caros com o método tradicional.

---
+++ diversos/pragmatic-fpij/fpij/src/main/java/com/github/mdssjc/fpij/Heavy.java
@@ -0,0 +1,13 @@
+package com.github.mdssjc.fpij;
+
+public class Heavy {
+
+  public Heavy() {
+    System.out.println("Heavy created");
+  }
+
+  @Override
+  public String toString() {
+    return "quite heavy";
+  }
+}
---
+++ diversos/pragmatic-fpij/fpij/src/main/java/com/github/mdssjc/fpij/HolderNaive.java
@@ -0,0 +1,27 @@
+package com.github.mdssjc.fpij;
+
+public class HolderNaive {
+
+  private Heavy heavy;
+
+  public HolderNaive() {
+    System.out.println("Holder created");
+  }
+
+  public synchronized Heavy getHeavy() {
+    if (heavy == null) {
+      heavy = new Heavy();
+    }
+
+    return heavy;
+  }
+
+  // ...
+
+  public static void main(final String[] args) {
+    final HolderNaive holder = new HolderNaive();
+    System.out.println("deferring heavy creation...");
+    System.out.println(holder.getHeavy());
+    System.out.println(holder.getHeavy());
+  }
+}
