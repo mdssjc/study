95d423ea384566ee61f4a68a1eff4ece549d8980
Marcelo dos Santos <mdssjc@gmail.com>
2016-07-04 21:31:01 -0300

    Padrão Execute Around Method para lock.

---
+++ diversos/pragmatic-fpij/fpij/src/main/java/com/github/mdssjc/fpij/Locker.java
@@ -0,0 +1,16 @@
+package com.github.mdssjc.fpij;
+
+import java.util.concurrent.locks.Lock;
+
+public class Locker {
+
+  public static void runLocked(Lock lock, Runnable block) {
+    lock.lock();
+
+    try {
+      block.run();
+    } finally {
+      lock.unlock();
+    }
+  }
+}
---
+++ diversos/pragmatic-fpij/fpij/src/main/java/com/github/mdssjc/fpij/Locking.java
@@ -0,0 +1,43 @@
+package com.github.mdssjc.fpij;
+
+import static com.github.mdssjc.fpij.Locker.runLocked;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Locking {
+
+  Lock lock = new ReentrantLock(); // or mock
+
+  protected void setLock(final Lock mock) {
+    lock = mock;
+  }
+
+  public void doOp1() {
+    lock.lock();
+    try {
+      // ...critical code...
+    } finally {
+      lock.unlock();
+    }
+  }
+  // ...
+
+  public void doOp2() {
+    runLocked(lock, () -> {
+      /* ...critical code ... */
+    });
+  }
+
+  public void doOp3() {
+    runLocked(lock, () -> {
+      /* ...critical code ... */
+    });
+  }
+
+  public void doOp4() {
+    runLocked(lock, () -> {
+      /* ...critical code ... */
+    });
+  }
+}
