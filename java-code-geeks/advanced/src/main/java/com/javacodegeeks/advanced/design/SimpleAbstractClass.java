781c9dffa9530474211917099993142c8b95cf7c
Marcelo dos Santos <mdssjc@gmail.com>
2016-07-05 23:59:02 -0300

    Abstract classes.

---
+++ java-code-geeks/advanced/src/main/java/com/javacodegeeks/advanced/design/SimpleAbstractClass.java
@@ -0,0 +1,10 @@
+package com.javacodegeeks.advanced.design;
+
+public abstract class SimpleAbstractClass {
+
+  public void performAction() {
+    // Implementation here
+  }
+
+  public abstract void performAnotherAction();
+}
