f624c1fb5520a9fb86521a0606f2b22f4e752086
Marcelo dos Santos <mdssjc@gmail.com>
2016-07-05 23:50:23 -0300

    Functional interfaces, default and static methods.

---
+++ java-code-geeks/advanced/src/main/java/com/javacodegeeks/advanced/design/InterfaceWithDefaultMethods.java
@@ -0,0 +1,14 @@
+package com.javacodegeeks.advanced.design;
+
+public interface InterfaceWithDefaultMethods {
+
+  void performAction();
+
+  default void performDefaulAction() {
+    // Implementation here
+  }
+
+  static void createAction() {
+    // Implementation here
+  }
+}
