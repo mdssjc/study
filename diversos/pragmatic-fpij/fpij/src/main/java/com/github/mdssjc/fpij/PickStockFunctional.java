447e9813b3f2931d4c2f934e5d5156bf1257011f
Marcelo dos Santos <mdssjc@gmail.com>
2016-07-08 13:47:07 -0300

    Exemplo com MapReduce no modo funcional.

---
+++ diversos/pragmatic-fpij/fpij/src/main/java/com/github/mdssjc/fpij/PickStockFunctional.java
@@ -0,0 +1,19 @@
+package com.github.mdssjc.fpij;
+
+import java.util.stream.Stream;
+
+public class PickStockFunctional {
+
+  public static void findHighPriced(final Stream<String> symbols) {
+    final StockInfo highPriced = symbols.map(StockUtil::getPrice)
+                                        .filter(StockUtil.isPriceLessThan(500))
+                                        .reduce(StockUtil::pickHigh)
+                                        .get();
+
+    System.out.println("High priced under $500 is " + highPriced);
+  }
+
+  public static void main(final String[] args) {
+    findHighPriced(Tickers.symbols.stream());
+  }
+}
