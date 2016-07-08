0d141b47ebed6249aee6032521c4822145a4376d
Marcelo dos Santos <mdssjc@gmail.com>
2016-07-08 12:04:58 -0300

    Inheritance.

---
+++ java-code-geeks/advanced/src/main/java/com/javacodegeeks/advanced/design/Child.java
@@ -0,0 +1,20 @@
+package com.javacodegeeks.advanced.design;
+
+// Resides in the same package as parent class
+public class Child extends Parent implements Parent.ProtectedInterface {
+
+  @Override
+  protected void protectedAction() {
+    // Calls parent's method implementation
+    super.protectedAction();
+  }
+
+  @Override
+  void packageAction() {
+    // Do nothing, no call to parent's method implementation
+  }
+
+  public void childAction() {
+    this.protectedField = "value";
+  }
+}
