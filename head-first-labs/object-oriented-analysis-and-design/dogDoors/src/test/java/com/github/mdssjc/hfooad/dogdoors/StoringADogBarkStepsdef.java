95562164a1e7452318b8fafbed64f9ef3a35f0a0
Marcelo dos Santos <mdssjc@gmail.com>
2016-07-09 15:09:26 -0300

    Novo caso de uso.

---
+++ head-first-labs/object-oriented-analysis-and-design/dogDoors/src/test/java/com/github/mdssjc/hfooad/dogdoors/StoringADogBarkStepsdef.java
@@ -0,0 +1,28 @@
+package com.github.mdssjc.hfooad.dogdoors;
+
+import static org.junit.Assert.assertTrue;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class StoringADogBarkStepsdef {
+
+  private DogDoor door;
+
+  @Given("^Need for storing the bark\\.$")
+  public void need_for_storing_the_bark() {
+    door = new DogDoor();
+  }
+
+  @When("^The owner’s dog barks into the dog door \\(\"([^\"]*)\"\\)\\.$")
+  public void the_owner_s_dog_barks_into_the_dog_door(String bark) {
+    door.setAllowedBark(new Bark(bark));
+  }
+
+  @Then("^The dog door stores the owner’s dog’s bark \\(\"([^\"]*)\"\\)\\.$")
+  public void the_dog_door_stores_the_owner_s_dog_s_bark(String bark) {
+    assertTrue(door.getAllowedBark()
+                   .equals(new Bark(bark)));
+  }
+}
---
+++ head-first-labs/object-oriented-analysis-and-design/dogDoors/src/test/resources/com/github/mdssjc/hfooad/dogdoors/storing-a-dog-bark.feature
@@ -0,0 +1,7 @@
+# The Ultimate Dog Door, version 3.0
+Feature: Storing a Dog Bark 
+
+Scenario: Remote Path 
+  Given Need for storing the bark.
+  When The owner’s dog barks into the dog door ("Woof").
+  Then The dog door stores the owner’s dog’s bark ("Woof").
