0d8ef757d87234037dd266114c9746c67e6fcf98
Marcelo dos Santos <mdssjc@gmail.com>
2016-07-11 00:04:45 -0300

    Simulador dos exercícios do loops.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/assignments/loops/Simulator.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/assignments/loops/Simulator.java
@@ -1,14 +1,15 @@
 package com.github.mdssjc.algorithms.assignments.loops;
 
-import com.github.mdssjc.algorithms.assignments.hello.Ordered;
+import static com.github.mdssjc.algorithms.utils.Executor.convert;
+import static com.github.mdssjc.algorithms.utils.Executor.execute;
 
 /**
- * Simulator: Creative Programming Assignments
+ * Simulator: Creative Programming Assignments*.
  * </p>
  * Assignment: Conditionals & Loops
- * Description: Binary logarithm, checkerboard pattern, random walk,
- * Gaussian distribution
- * Concepts: loops and conditionals
+ * Description: Write five short Java programs to gain practice with loops,
+ * conditionals, and arrays
+ * Concepts: conditionals, loops and arrays
  * Difficulty: 1
  * 
  * @author mdssjc (Marcelo dos Santos)
@@ -16,14 +17,13 @@ import com.github.mdssjc.algorithms.assignments.hello.Ordered;
 public class Simulator {
 
   public static void main(String[] args) {
-    // Ordered
-    String[][] inputs = {
-        { "10", "17", "49" },
-        { "49", "17", "10" },
-        { "10", "49", "17" }
-    };
-    for (String[] input : inputs) {
-      Ordered.main(input);
-    }
+    // Bits
+    System.out.println("Bits");
+    execute(Bits::main,
+        convert(0), convert(1), convert(2), convert(4),
+        convert(8), convert(16), convert(1000), convert(-23));
+
+    // Noon snooze
+    System.out.println("Noon snooze");
   }
 }
