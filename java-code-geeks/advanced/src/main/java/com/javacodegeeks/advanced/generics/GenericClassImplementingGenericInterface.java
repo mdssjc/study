27dcdc3c9dd2f5916085e1837d8eadc34ba44533
Marcelo dos Santos <mdssjc@gmail.com>
2016-07-11 13:17:22 -0300

    Generics and classes.

---
+++ java-code-geeks/advanced/src/main/java/com/javacodegeeks/advanced/generics/GenericClassImplementingGenericInterface.java
@@ -0,0 +1,10 @@
+package com.javacodegeeks.advanced.generics;
+
+public class GenericClassImplementingGenericInterface<T>
+    implements GenericInterfaceOneType<T> {
+
+  @Override
+  public void performAction(final T action) {
+    // Implementation here
+  }
+}
---
+++ java-code-geeks/advanced/src/main/java/com/javacodegeeks/advanced/generics/GenericClassOneType.java
@@ -0,0 +1,8 @@
+package com.javacodegeeks.advanced.generics;
+
+public class GenericClassOneType<T> {
+
+  public void performAction(final T action) {
+    // Implementation here
+  }
+}
