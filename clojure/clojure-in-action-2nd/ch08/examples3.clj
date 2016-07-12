8318b0fd0469be0130b137fdab6de6e2fc2fa790
Marcelo dos Santos <mdssjc@gmail.com>
2016-07-12 20:54:56 -0300

    Exemplo de criação de objeto.

--- clojure/clojure-in-action-2nd/ch08/examples3.clj
+++ clojure/clojure-in-action-2nd/ch08/examples3.clj
@@ -4,3 +4,9 @@
       :name (name class-name))))
 (defmacro defclass [class-name]
   `(def ~class-name (new-class '~class-name)))
+
+(defclass Person)
+(Person :name)
+
+(def some-class Person)
+(some-class :name)
