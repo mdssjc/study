fcb82ff5042fc0182dc7e41a35aadb93a531d095
Marcelo dos Santos <mdssjc@gmail.com>
2016-07-21 22:42:27 -0300

    Removido os arquivos de exercícios sobre Clojure.

--- clojure/clojure-in-action-2nd/ch05/examples3.clj
+++
@@ -1,9 +0,0 @@
-(import 'java.awt.event.MouseAdapter)
-
-(proxy [MouseAdapter] []
-  (mousePressed [event]
-    (println "Hey!")))
-
-(reify java.io.FileFilter
-  (accept [this f]
-    (.isDirectory f)))
