1fb35c845c9085367829b3daab7a479b4a59fc1f
Marcelo dos Santos <mdssjc@gmail.com>
2016-07-23 12:32:13 -0300

    Alteração do equals.

--- head-first-labs/object-oriented-analysis-and-design/ricks-guitars/src/main/java/com/github/mdssjc/hfooad/ricksguitars/Instrument.java
+++ head-first-labs/object-oriented-analysis-and-design/ricks-guitars/src/main/java/com/github/mdssjc/hfooad/ricksguitars/Instrument.java
@@ -2,9 +2,11 @@ package com.github.mdssjc.hfooad.ricksguitars;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
 @AllArgsConstructor
+@EqualsAndHashCode(exclude = { "price", "spec" })
 public abstract class Instrument {
 
   private final String         serialNumber;
