bc29334d393fed2fcd303efab66780aa3d7ab6a8
Marcelo dos Santos <mdssjc@gmail.com>
2016-07-23 22:56:32 -0300

    Exercício 14.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises1/Ex14.java
@@ -0,0 +1,36 @@
+package com.github.mdssjc.algorithms.chapter1.exercises1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Exercício 14.
+ * 
+ * @author Marcelo dos Santos
+ *
+ */
+public class Ex14 {
+
+  public static void main(final String[] args) {
+    for (int i = 0; i < 1000; i += 2) {
+      final int lg = lg(i);
+      final double log = Math.log(i) / Math.log(2);
+
+      StdOut.println(i + " " + lg + " " + log);
+
+      if (lg > log && Double.isFinite(log)) {
+        throw new RuntimeException();
+      }
+    }
+  }
+
+  public static int lg(final int n) {
+    int count = 0;
+    int r = 2;
+
+    while (r <= n) {
+      count++;
+      r *= 2;
+    }
+    return count;
+  }
+}
