708fd7f3a13b0464c49fb69c8e6f85b0c34011df
Marcelo dos Santos <mdssjc@gmail.com>
2016-07-23 21:23:14 -0300

    Exercício 8.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises1/Ex8.java
@@ -0,0 +1,18 @@
+package com.github.mdssjc.algorithms.chapter1.exercises1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Exercício 8.
+ * 
+ * @author Marcelo dos Santos
+ *
+ */
+public class Ex8 {
+
+  public static void main(final String[] args) {
+    StdOut.println('b');
+    StdOut.println('b' + 'c');
+    StdOut.println((char) ('a' + 4));
+  }
+}
