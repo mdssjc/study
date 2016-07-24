7260cd8a14b32cb336189ee06b8bda800d27ebc7
Marcelo dos Santos <mdssjc@gmail.com>
2016-07-23 21:59:23 -0300

    Ordem das funções.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises1/Ex5.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises1/Ex5.java
@@ -10,13 +10,13 @@ import edu.princeton.cs.algs4.StdOut;
  */
 public class Ex5 {
 
-  public static boolean check(final double x, final double y) {
-    return (x > 0.0 && x < 1.0) && (y > 0.0 && y < 1.0);
-  }
-
   public static void main(final String[] args) {
     StdOut.println(check(1E-6, 1E-3));
     StdOut.println(check(0, 1));
     StdOut.println(check(0.5, 0.5));
+  }
+
+  public static boolean check(final double x, final double y) {
+    return (x > 0.0 && x < 1.0) && (y > 0.0 && y < 1.0);
   }
 }
