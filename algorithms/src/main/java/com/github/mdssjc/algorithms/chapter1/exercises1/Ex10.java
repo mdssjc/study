a462a2c1407089a8d99f458967dc178b118ffc13
Marcelo dos Santos <mdssjc@gmail.com>
2016-07-23 21:30:17 -0300

    Exercício 10.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises1/Ex10.java
@@ -0,0 +1,25 @@
+package com.github.mdssjc.algorithms.chapter1.exercises1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Exercício 10.
+ * 
+ * @author Marcelo dos Santos
+ *
+ */
+public class Ex10 {
+
+  public static void main(final String[] args) {
+    // final int[] a;
+    final int[] a = new int[10];
+
+    for (int i = 0; i < 10; i++) {
+      a[i] = i * i;
+    }
+
+    for (final int value : a) {
+      StdOut.println(value);
+    }
+  }
+}
