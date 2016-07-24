2146c96ea48f05562c9c2ee27716ff0fd3ac4be1
Marcelo dos Santos <mdssjc@gmail.com>
2016-07-23 23:22:06 -0300

    Melhoria da API.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/utils/MainUtils.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/utils/MainUtils.java
@@ -5,8 +5,21 @@ import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.function.Consumer;
 
+/**
+ * Utilitário para entrada de dados no main e classes de teste.
+ * 
+ * @author Marcelo dos Santos
+ *
+ */
 public class MainUtils {
 
+  /**
+   * Converte para String[][].
+   * 
+   * @param xs
+   *          Conjunto de valores
+   * @return String[][]
+   */
   public static <T> String[][] toXSS(final T... xs) {
     return Arrays.stream(xs)
                  .map(String::valueOf)
@@ -14,12 +27,27 @@ public class MainUtils {
                  .toArray(String[][]::new);
   }
 
+  /**
+   * Converte para String[]
+   * 
+   * @param xs
+   *          Conjunto de valores
+   * @return String[]
+   */
   public static <T> String[] toXS(final T... xs) {
     return Arrays.stream(xs)
                  .map(String::valueOf)
                  .toArray(String[]::new);
   }
 
+  /**
+   * Execução do ponto de entrada da classe.
+   * 
+   * @param method
+   *          Método a ser executado
+   * @param xs
+   *          Valores para o args
+   */
   public static <T> void execute(final Consumer<T> method, final T... xs) {
     if (xs.length == 0) {
       method.accept(null);
@@ -30,6 +58,14 @@ public class MainUtils {
     }
   }
 
+  /**
+   * Execução do main.
+   * 
+   * @param clazz
+   *          Classe selecionada
+   * @param inputs
+   *          Valores para o args
+   */
   public static void execute(final Class<?> clazz, final String[]... inputs) {
     try {
       final Method method = clazz.getMethod("main", String[].class);
