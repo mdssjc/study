040c70eb4e93f1000863658a10c721957b4883d8
Marcelo dos Santos <mdssjc@gmail.com>
2016-07-23 21:46:19 -0300

    Exercício 12.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises1/Ex12.java
@@ -0,0 +1,26 @@
+package com.github.mdssjc.algorithms.chapter1.exercises1;
+
+/**
+ * Exercício 12.
+ * 
+ * @author Marcelo dos Santos
+ *
+ */
+public class Ex12 {
+
+  public static void main(final String[] args) {
+    final int[] a = new int[10];
+    for (int i = 0; i < 10; i++) {
+      a[i] = 9 - i;
+    }
+    for (int i = 0; i < 10; i++) {
+      a[i] = a[a[i]];
+    }
+    for (int i = 0; i < 10; i++) {
+      System.out.println(i);
+    }
+    for (int i = 0; i < 10; i++) {
+      System.out.println(a[i]);
+    }
+  }
+}
