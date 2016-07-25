38152acd5cd16bf9a88a81dc14886d98331420b1
Marcelo dos Santos <mdssjc@gmail.com>
2016-07-25 19:27:47 -0300

    Execução de JavaScript.

--- hands-on/script-api/src/main/java/com/github/mdssjc/script/Main.java
+++ hands-on/script-api/src/main/java/com/github/mdssjc/script/Main.java
@@ -1,5 +1,10 @@
 package com.github.mdssjc.script;
 
+import javax.script.Bindings;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 /**
  * Hands-on sobre a Script API.
  * 
@@ -9,5 +14,18 @@ package com.github.mdssjc.script;
 public class Main {
 
   public static void main(final String[] args) {
+    final ScriptEngineManager factory = new ScriptEngineManager();
+    final ScriptEngine engine = factory.getEngineByName("Nashorn");
+
+    final Bindings bindings = engine.createBindings();
+    bindings.put("str", "Calling JavaScript");
+    bindings.put("engine", engine);
+
+    try {
+      engine.eval("print(str + ' from ' + engine.getClass().getSimpleName())",
+          bindings);
+    } catch (final ScriptException e) {
+      System.err.println(e.getMessage());
+    }
   }
 }
