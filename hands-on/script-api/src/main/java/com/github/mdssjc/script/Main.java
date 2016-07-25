1a9ef51e0f8c0e0a9e1ad26510b2037baa14da1f
Marcelo dos Santos <mdssjc@gmail.com>
2016-07-25 19:18:47 -0300

    Commit inicial.

---
+++ hands-on/script-api/src/main/java/com/github/mdssjc/script/Main.java
@@ -0,0 +1,13 @@
+package com.github.mdssjc.script;
+
+/**
+ * Hands-on sobre a Script API.
+ * 
+ * @author Marcelo dos Santos
+ *
+ */
+public class Main {
+
+  public static void main(final String[] args) {
+  }
+}
