36bbb66d4bc0dc6761b0d7d89f2b80febcda0ac1
Marcelo dos Santos <mdssjc@gmail.com>
2016-07-30 10:50:44 -0300

    Novo test drive.

---
+++ head-first-labs/object-oriented-analysis-and-design/ricks-guitars/src/test/java/com/github/mdssjc/hfooad/ricksguitars/FindInstrument.java
@@ -0,0 +1,50 @@
+package com.github.mdssjc.hfooad.ricksguitars;
+
+import java.util.Iterator;
+import java.util.function.Consumer;
+
+import com.github.mdssjc.hfooad.ricksguitars.types.Builder;
+import com.github.mdssjc.hfooad.ricksguitars.types.Wood;
+
+public class FindInstrument {
+
+  public static void main(final String[] args) {
+    final Inventory<Instrument, InstrumentSpec> inventory = new Inventory<>();
+    GuitarsTestDataBuilder.initializeInventory(inventory);
+
+    final InstrumentSpec clientSpec = new InstrumentSpec()
+      .addProperty("builder", Builder.FENDER)
+      .addProperty("backWood", Wood.MAPLE);
+
+    final Iterator<Instrument> matchingInstruments = inventory
+      .search(clientSpec);
+    if (matchingInstruments.hasNext()) {
+      System.out.println("You might like these instruments:");
+      while (matchingInstruments.hasNext()) {
+        matchingInstruments.forEachRemaining(formatMessage());
+      }
+    } else {
+      System.out.println("Sorry, we have nothing for you.");
+    }
+  }
+
+  private static Consumer<? super Instrument> formatMessage() {
+    return (instrument) -> {
+      final InstrumentSpec spec = instrument.getSpec();
+      System.out.println("We have a " + spec.getProperty("instrumentType")
+          + " with the following properties:");
+
+      final Iterator<String> properties = spec.getProperties();
+      while (properties.hasNext()) {
+        final String p = properties.next();
+        if (p.equals("instrumentType")) {
+          continue;
+        }
+        System.out.println(" " + p + ": " + spec.getProperty(p));
+      }
+      System.out
+        .println(" You can have this " + spec.getProperty("instrumentType")
+            + " for $" + instrument.getPrice() + "\n---");
+    };
+  }
+}
