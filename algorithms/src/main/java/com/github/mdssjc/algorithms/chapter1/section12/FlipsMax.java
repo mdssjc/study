c406a536d4e5aec789f68a17c2d7662390712ce2
Marcelo dos Santos <mdssjc@gmail.com>
2016-08-06 23:04:56 -0300

    FlipsMax Class.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section12/FlipsMax.java
@@ -0,0 +1,40 @@
+package com.github.mdssjc.algorithms.chapter1.section12;
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+/**
+ * FlipsMax Class.
+ * 
+ * @author Marcelo dos Santos
+ *
+ */
+public class FlipsMax {
+
+  public static Counter max(final Counter x, final Counter y) {
+    if (x.tally() > y.tally()) {
+      return x;
+    }
+    return y;
+  }
+
+  public static void main(final String[] args) {
+    final int T = Integer.parseInt(args[0]);
+    final Counter heads = new Counter("heads");
+    final Counter tails = new Counter("tails");
+
+    for (int t = 0; t < T; t++) {
+      if (StdRandom.bernoulli(0.5)) {
+        heads.increment();
+      } else {
+        tails.increment();
+      }
+    }
+
+    if (heads.tally() == tails.tally()) {
+      StdOut.println("Tie");
+    } else {
+      StdOut.println(max(heads, tails) + " wins");
+    }
+  }
+}
