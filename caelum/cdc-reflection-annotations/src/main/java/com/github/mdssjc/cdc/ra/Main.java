e855e4fc062d7f48876a934bcd972c741f5c4e3e
Marcelo dos Santos <mdssjc@gmail.com>
2016-08-11 19:19:21 -0300

    Refatoração do lambda.

--- caelum/cdc-reflection-annotations/src/main/java/com/github/mdssjc/cdc/ra/Main.java
+++ caelum/cdc-reflection-annotations/src/main/java/com/github/mdssjc/cdc/ra/Main.java
@@ -1,15 +1,20 @@
 package com.github.mdssjc.cdc.ra;
 
+import java.util.function.BiConsumer;
+
 public class Main {
 
   public static void main(final String[] args) {
+    final BiConsumer<String, Object> print = (k, v) -> System.out
+      .println(k + " = " + v);
+
     final Produto p = new Produto("Design Patterns", "Livro", 59.90,
         "Publicado pela Casa do Código");
     GeradorMapa.gerarMapa(p)
-      .forEach((k, v) -> System.out.println(k + " = " + v));
+      .forEach(print);
 
     final Telefone t = new Telefone("12", "TIM");
     GeradorMapa.gerarMapa(t)
-        .forEach((k, v) -> System.out.println(k + " = " + v));
+      .forEach(print);
   }
 }
