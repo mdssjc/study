7f812d47221c3a8fca2f3f035be2ae0934446f53
Marcelo dos Santos <mdssjc@gmail.com>
2016-08-13 14:58:51 -0300

    Interface requerida por Board.

--- head-first-labs/object-oriented-analysis-and-design/gsf/src/main/java/com/github/mdssjc/gsf/Tile.java
+++ head-first-labs/object-oriented-analysis-and-design/gsf/src/main/java/com/github/mdssjc/gsf/Tile.java
@@ -1,5 +1,21 @@
 package com.github.mdssjc.gsf;
 
+import java.util.List;
+
+import com.github.mdssjc.gsf.unit.Unit;
+
 public class Tile {
 
+  public List<Unit> getUnits() {
+    return null;
+  }
+
+  public void addUnit(final Unit unit) {
+  }
+
+  public void removeUnit(final Unit unit) {
+  }
+
+  public void removeUnits() {
+  }
 }
