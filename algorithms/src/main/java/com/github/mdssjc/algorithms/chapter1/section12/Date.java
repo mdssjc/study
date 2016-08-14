61648cf986b654779cbe1338ab194b9a8d4a0ec9
Marcelo dos Santos <mdssjc@gmail.com>
2016-08-14 20:17:41 -0300

    Date Interface.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section12/Date.java
@@ -0,0 +1,16 @@
+package com.github.mdssjc.algorithms.chapter1.section12;
+
+/**
+ * Date Interface.
+ * 
+ * @author Marcelo dos Santos
+ *
+ */
+public interface Date {
+
+  int month();
+
+  int day();
+
+  int year();
+}
