50a4435bceb58fbf9a82714a093442582c9a7624
Marcelo dos Santos <mdssjc@gmail.com>
2016-08-13 21:49:55 -0300

    Creative Exercício 30.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises11/CEx30.java
@@ -0,0 +1,35 @@
+package com.github.mdssjc.algorithms.chapter1.exercises11;
+
+import com.github.mdssjc.algorithms.chapter1.section11.Euclid;
+
+import edu.princeton.cs.algs4.StdArrayIO;
+
+/**
+ * Creative Exercício 30.
+ * <p>
+ * Array exercise.
+ * 
+ * @author Marcelo dos Santos
+ *
+ */
+public class CEx30 {
+
+  public static void main(final String[] args) {
+    final boolean[][] a = new boolean[10][10];
+
+    for (int i = 0; i < a.length; i++) {
+      for (int j = 0; j < a[i].length; j++) {
+        a[i][j] = isPrime(i, j);
+      }
+    }
+
+    StdArrayIO.print(a);
+  }
+
+  private static boolean isPrime(final int i, final int j) {
+    if (i <= 1 || j <= 1) {
+      return false;
+    }
+    return Euclid.gcd(i, j) == 1;
+  }
+}
