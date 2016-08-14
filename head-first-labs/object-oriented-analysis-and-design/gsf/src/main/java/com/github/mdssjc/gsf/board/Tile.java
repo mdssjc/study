923ee5f75a66b6b35c6008a8cb1a13533e5ec8b2
Marcelo dos Santos <mdssjc@gmail.com>
2016-08-14 10:38:19 -0300

    Implementação de Tile.

--- head-first-labs/object-oriented-analysis-and-design/gsf/src/main/java/com/github/mdssjc/gsf/board/Tile.java
+++ head-first-labs/object-oriented-analysis-and-design/gsf/src/main/java/com/github/mdssjc/gsf/board/Tile.java
@@ -1,21 +1,31 @@
 package com.github.mdssjc.gsf.board;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import com.github.mdssjc.gsf.unit.Unit;
 
 public class Tile {
 
+  private final List<Unit> units;
+
+  public Tile() {
+    this.units = new LinkedList<>();
+  }
+
   public List<Unit> getUnits() {
-    return null;
+    return this.units;
   }
 
-  public void addUnit(final Unit unit) {
+  protected void addUnit(final Unit unit) {
+    this.units.add(unit);
   }
 
-  public void removeUnit(final Unit unit) {
+  protected void removeUnit(final Unit unit) {
+    this.units.remove(unit);
   }
 
   public void removeUnits() {
+    this.units.clear();
   }
 }
