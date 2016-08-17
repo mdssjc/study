dfcb600fb26681beb6ac2d88d2582462093e9326
Marcelo dos Santos <mdssjc@gmail.com>
2016-08-17 19:11:27 -0300

    Referência estática de classe.

---
+++ caelum/cdc-reflection-annotations/src/main/java/com/github/mdssjc/cdc/ra/ch02/ReferenciaEstaticaClasse.java
@@ -0,0 +1,14 @@
+package com.github.mdssjc.cdc.ra.ch02;
+
+public class ReferenciaEstaticaClasse {
+
+  public static void main(final String[] args) {
+    final Class<String> classe = String.class;
+    System.out.println(classe.getName());
+    imprimeNomeClasse(Integer.class);
+  }
+
+  public static void imprimeNomeClasse(final Class<Integer> classe) {
+    System.out.println("Chamado o método com " + classe.getName());
+  }
+}
