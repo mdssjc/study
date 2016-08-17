bedbe7e5220d57c1a08c00c9153a036b77f8d239
Marcelo dos Santos <mdssjc@gmail.com>
2016-08-17 19:22:57 -0300

    Correção do tipo.

--- caelum/cdc-reflection-annotations/src/main/java/com/github/mdssjc/cdc/ra/ch02/RecuperandoPeloObjeto.java
+++ caelum/cdc-reflection-annotations/src/main/java/com/github/mdssjc/cdc/ra/ch02/RecuperandoPeloObjeto.java
@@ -3,7 +3,7 @@ package com.github.mdssjc.cdc.ra.ch02;
 public class RecuperandoPeloObjeto {
 
   public static void main(final String[] args) {
-    final Integer object = new Integer(100);
+    final Number object = new Integer(100);
     final Class<? extends Number> c = object.getClass();
     System.out.println(c.getName());
     // ...
