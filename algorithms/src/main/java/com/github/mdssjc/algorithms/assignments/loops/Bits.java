fadfe35d740f83a12e982c2ea9be9694465342d6
Marcelo dos Santos <mdssjc@gmail.com>
2016-08-22 00:00:37 -0300

    Uso do StdOut no lugar do System.out.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/assignments/loops/Bits.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/assignments/loops/Bits.java
@@ -3,6 +3,8 @@ package com.github.mdssjc.algorithms.assignments.loops;
 import com.github.mdssjc.algorithms.utils.Executor;
 import com.github.mdssjc.algorithms.utils.TestDrive;
 
+import edu.princeton.cs.algs4.StdOut;
+
 @TestDrive({ "0", "1", "2", "4" })
 @TestDrive({ "8", "16", "1000", "-23" })
 public class Bits {
@@ -13,14 +15,14 @@ public class Bits {
     int n = Integer.parseInt(args[0]);
 
     if (n < 0) {
-      System.out.println("Illegal input");
+      StdOut.println("Illegal input");
     } else {
       int count = 0;
       while (n > 0) {
         n /= 2;
         count++;
       }
-      System.out.println(count);
+      StdOut.println(count);
     }
   }
 }
