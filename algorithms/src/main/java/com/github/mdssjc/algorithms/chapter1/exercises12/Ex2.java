4b032b7db67dbc0d904b9ecdf5c54a790fe3f6b9
Marcelo dos Santos <mdssjc@gmail.com>
2016-08-21 22:15:22 -0300

    Exercício 2.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises12/Ex2.java
@@ -0,0 +1,40 @@
+package com.github.mdssjc.algorithms.chapter1.exercises12;
+
+import com.github.mdssjc.algorithms.utils.Executor;
+import com.github.mdssjc.algorithms.utils.TestDrive;
+
+import edu.princeton.cs.algs4.Interval1D;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Exercício 2.
+ * 
+ * @author Marcelo dos Santos
+ *
+ */
+@TestDrive(value = { "3" }, input = { "1", "2", "2", "3", "4", "5" })
+public class Ex2 {
+
+  public static void main(final String[] args) {
+    if (args.length == 0) {
+      Executor.execute(Ex2.class);
+      return;
+    }
+
+    final int N = Integer.parseInt(args[0]);
+
+    final Interval1D[] intervals = new Interval1D[N];
+    for (int i = 0; i < N; i++) {
+      intervals[i] = new Interval1D(StdIn.readDouble(), StdIn.readDouble());
+    }
+
+    for (int i = 0; i < intervals.length; i++) {
+      for (int j = i + 1; j < intervals.length; j++) {
+        if (intervals[i].intersects(intervals[j])) {
+          StdOut.println(intervals[i] + " " + intervals[j]);
+        }
+      }
+    }
+  }
+}
