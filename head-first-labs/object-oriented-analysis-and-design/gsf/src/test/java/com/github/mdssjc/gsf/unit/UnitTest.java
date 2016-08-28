df5bd1369b929c6fa6bd3e1637b6c65de0694717
Marcelo dos Santos <mdssjc@gmail.com>
2016-08-28 13:58:41 -0300

    Refatoração da criação do Unit.

--- head-first-labs/object-oriented-analysis-and-design/gsf/src/test/java/com/github/mdssjc/gsf/unit/UnitTest.java
+++ head-first-labs/object-oriented-analysis-and-design/gsf/src/test/java/com/github/mdssjc/gsf/unit/UnitTest.java
@@ -2,40 +2,45 @@ package com.github.mdssjc.gsf.unit;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class UnitTest {
 
+  private Unit unit;
+
+  @Before
+  public void initialize() {
+    this.unit = new Unit(0);
+  }
+
   @Test
   public void settingAndGettingACommonProperty() {
-    final Unit unit = new Unit(0);
     final String property = "type";
-    unit.setProperty(property, "infantry");
+    this.unit.setProperty(property, "infantry");
 
-    final Object output = unit.getProperty(property);
+    final Object output = this.unit.getProperty(property);
 
     assertEquals("infantry", output);
   }
 
   @Test
   public void settingAndGettingAUnitSpecificProperty() {
-    final Unit unit = new Unit(0);
     final String property = "hitPoints";
-    unit.setProperty(property, "25");
+    this.unit.setProperty(property, "25");
 
-    final Object output = unit.getProperty(property);
+    final Object output = this.unit.getProperty(property);
 
     assertEquals("25", output);
   }
 
   @Test
   public void changingAnExistingPropertysValue() {
-    final Unit unit = new Unit(0);
     final String property = "hitPoints";
-    unit.setProperty(property, "25");
+    this.unit.setProperty(property, "25");
 
-    unit.setProperty(property, "15");
-    final Object output = unit.getProperty(property);
+    this.unit.setProperty(property, "15");
+    final Object output = this.unit.getProperty(property);
 
     assertEquals("15", output);
   }
