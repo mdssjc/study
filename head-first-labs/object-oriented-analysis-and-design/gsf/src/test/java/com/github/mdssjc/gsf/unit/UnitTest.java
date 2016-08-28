85578f1fffcec7eadb4c7ee7bd8ea55ffca6cf50
Marcelo dos Santos <mdssjc@gmail.com>
2016-08-28 13:49:28 -0300

    Refatoração para os rótulos das propriedades.

--- head-first-labs/object-oriented-analysis-and-design/gsf/src/test/java/com/github/mdssjc/gsf/unit/UnitTest.java
+++ head-first-labs/object-oriented-analysis-and-design/gsf/src/test/java/com/github/mdssjc/gsf/unit/UnitTest.java
@@ -9,19 +9,21 @@ public class UnitTest {
   @Test
   public void setAndGetACommonProperty() {
     final Unit unit = new Unit(0);
-    unit.setProperty("type", "infantry");
+    final String property = "type";
+    unit.setProperty(property, "infantry");
 
-    final Object output = unit.getProperty("type");
+    final Object output = unit.getProperty(property);
 
     assertEquals("infantry", output);
   }
 
   @Test
   public void setAndGetASpecificProperty() {
-    Unit unit = new Unit(0);
-    unit.setProperty("hitPoints", "25");
+    final Unit unit = new Unit(0);
+    final String property = "hitPoints";
+    unit.setProperty(property, "25");
 
-    Object output = unit.getProperty("hitPoints");
+    final Object output = unit.getProperty(property);
 
     assertEquals("25", output);
   }
