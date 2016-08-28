418550ab270e28edbdc5277908eae2032af2418c
Marcelo dos Santos <mdssjc@gmail.com>
2016-08-27 23:04:01 -0300

    Exercício 5.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises12/Ex5.java
@@ -0,0 +1,19 @@
+package com.github.mdssjc.algorithms.chapter1.exercises12;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Exercício 5.
+ * 
+ * @author Marcelo dos Santos
+ *
+ */
+public class Ex5 {
+
+  public static void main(final String[] args) {
+    final String s = "Hello World";
+    s.toUpperCase();
+    s.substring(6, 11);
+    StdOut.println(s);
+  }
+}
