34746ab1e5cb2fae224cf1634f9015d6ae53ea96
Marcelo dos Santos <mdssjc@gmail.com>
2016-09-03 13:35:58 -0300

    Inclusão da classe de grupo de unidades.

---
+++ head-first-labs/object-oriented-analysis-and-design/gsf/src/main/java/com/github/mdssjc/gsf/unit/UnitGroup.java
@@ -0,0 +1,43 @@
+package com.github.mdssjc.gsf.unit;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class UnitGroup {
+
+  private final Map<Integer, Unit> units;
+
+  public UnitGroup(final List<Unit> unitList) {
+    this.units = new HashMap<>();
+    for (final Unit name : unitList) {
+      final Unit unit = name;
+      this.units.put(unit.getId(), unit);
+    }
+  }
+
+  public UnitGroup() {
+    this(new LinkedList<>());
+  }
+
+  public void addUnit(final Unit unit) {
+    this.units.put(unit.getId(), unit);
+  }
+
+  public void removeUnit(final int id) {
+    this.units.remove(id);
+  }
+
+  public void removeUnit(final Unit unit) {
+    removeUnit(unit.getId());
+  }
+
+  public Unit getUnit(final int id) {
+    return this.units.get(id);
+  }
+
+  public List<Unit> getUnits() {
+    return new LinkedList<>(this.units.values());
+  }
+}
---
+++ head-first-labs/object-oriented-analysis-and-design/gsf/src/test/java/com/github/mdssjc/gsf/unit/UnitGroupTest.java
@@ -0,0 +1,83 @@
+package com.github.mdssjc.gsf.unit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class UnitGroupTest {
+
+  @Test
+  public void CreatingANewUnitGroupFromAListOfUnits() {
+    UnitGroup unitGroup;
+    final List<Unit> units = Arrays.asList(new Unit(0), new Unit(1),
+        new Unit(2));
+
+    unitGroup = new UnitGroup(units);
+    final List<Unit> unitsOutput = unitGroup.getUnits();
+
+    assertEquals(units, unitsOutput);
+  }
+
+  @Test
+  public void AddingAUnitToAGroup() {
+    final UnitGroup unitGroup = new UnitGroup();
+    final Unit unit = new Unit(100);
+
+    unitGroup.addUnit(unit);
+    final Unit unitOutput = unitGroup.getUnit(100);
+
+    assertEquals(unit, unitOutput);
+  }
+
+  @Test
+  public void GettingAUnitByItsId() {
+    final UnitGroup unitGroup = new UnitGroup();
+    final int id = 100;
+
+    final Unit unit = new Unit(id);
+    unitGroup.addUnit(unit);
+    final Unit unitOutput = unitGroup.getUnit(id);
+
+    assertEquals(unit, unitOutput);
+  }
+
+  @Test
+  public void GettingAllTheUnitsInAGroup() {
+    final List<Unit> units = Arrays.asList(new Unit(0), new Unit(1),
+        new Unit(2));
+    final UnitGroup unitGroup = new UnitGroup(units);
+
+    final List<Unit> unitsOutput = unitGroup.getUnits();
+
+    assertEquals(units, unitsOutput);
+  }
+
+  @Test
+  public void RemovingAUnitByTheIdOfTheUnit() {
+    final UnitGroup unitGroup = new UnitGroup();
+    final int id = 100;
+
+    unitGroup.addUnit(new Unit(id));
+    unitGroup.removeUnit(id);
+
+    assertTrue(unitGroup.getUnits()
+      .isEmpty());
+  }
+
+  @Test
+  public void RemovingAUnitByTheUnitInstance() {
+    final UnitGroup unitGroup = new UnitGroup();
+    final Unit unit = new Unit(100);
+
+    unitGroup.addUnit(unit);
+    unitGroup.removeUnit(unit);
+
+    assertTrue(unitGroup.getUnits()
+      .isEmpty());
+  }
+
+}
