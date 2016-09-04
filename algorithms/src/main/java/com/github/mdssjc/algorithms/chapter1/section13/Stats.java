3cac5cbd473d69aff3fe105e25cfc7dd805d9364
Marcelo dos Santos <mdssjc@gmail.com>
2016-09-04 20:58:30 -0300

    Stats Class.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section13/Stats.java
@@ -0,0 +1,39 @@
+package com.github.mdssjc.algorithms.chapter1.section13;
+
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Stats Class.
+ * 
+ * @author Marcelo dos Santos
+ *
+ */
+public class Stats {
+
+  public static void main(final String[] args) {
+    final Bag<Double> numbers = new Bag<>();
+
+    while (!StdIn.isEmpty()) {
+      numbers.add(StdIn.readDouble());
+    }
+
+    final int N = numbers.size();
+    double sum = 0.0;
+    for (final double x : numbers) {
+      sum += x;
+    }
+
+    final double mean = sum / N;
+    sum = 0.0;
+    for (final double x : numbers) {
+      sum += (x - mean) * (x - mean);
+    }
+
+    final double std = Math.sqrt(sum / (N - 1));
+
+    StdOut.printf("Mean: %.2f\n", mean);
+    StdOut.printf("Std dev: %.2f\n", std);
+  }
+}
