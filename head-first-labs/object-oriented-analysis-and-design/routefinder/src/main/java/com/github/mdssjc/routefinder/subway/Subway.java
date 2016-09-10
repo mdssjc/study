c8297c2e6d3492081890849be046caec08e77998
Marcelo dos Santos <mdssjc@gmail.com>
2016-09-10 14:39:49 -0300

    Classe Subway.

---
+++ head-first-labs/object-oriented-analysis-and-design/routefinder/src/main/java/com/github/mdssjc/routefinder/subway/Subway.java
@@ -0,0 +1,41 @@
+package com.github.mdssjc.routefinder.subway;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Subway {
+
+  private final List<Station>    stations;
+  private final List<Connection> connections;
+
+  public Subway() {
+    this.stations = new LinkedList<>();
+    this.connections = new LinkedList<>();
+  }
+
+  public void addStation(final String stationName) {
+    if (!this.hasStation(stationName)) {
+      final Station station = new Station(stationName);
+      this.stations.add(station);
+    }
+  }
+
+  public boolean hasStation(final String stationName) {
+    return this.stations.contains(new Station(stationName));
+  }
+
+  public void addConnection(final String station1Name,
+      final String station2Name, final String lineName) {
+    if ((this.hasStation(station1Name)) && (this.hasStation(station2Name))) {
+      final Station station1 = new Station(station1Name);
+      final Station station2 = new Station(station2Name);
+      final Connection connection = new Connection(station1, station2,
+          lineName);
+      this.connections.add(connection);
+      this.connections
+        .add(new Connection(station2, station1, connection.getLineName()));
+    } else {
+      throw new RuntimeException("Invalid connection!");
+    }
+  }
+}
