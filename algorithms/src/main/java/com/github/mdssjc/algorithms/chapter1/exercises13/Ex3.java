43bde9c9b1d6ff9f9b4d915116272d4f1eabcb48
Marcelo dos Santos <mdssjc@gmail.com>
2016-10-11 19:31:37 -0300

    Exercício concluído.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises13/Ex3.java
@@ -0,0 +1,21 @@
+package com.github.mdssjc.algorithms.chapter1.exercises13;
+
+import com.github.mdssjc.algorithms.utils.Executor;
+import com.github.mdssjc.algorithms.utils.TestDrive;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Exercício 3.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+@TestDrive
+public class Ex3 {
+
+  public static void main(String[] args) {
+    Executor.execute(Ex3.class, args);
+
+    StdOut.println("a - c - d - e - h");
+  }
+}
