10c83b1d56beb294f26cc1dd7489913cebaf47fe
Marcelo dos Santos <mdssjc@gmail.com>
2016-10-12 21:37:38 -0300

    Exercício concluído.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises13/Ex6.java
@@ -0,0 +1,41 @@
+package com.github.mdssjc.algorithms.chapter1.exercises13;
+
+import com.github.mdssjc.algorithms.datastructure.queue.concrete.QueueLinkedList;
+import com.github.mdssjc.algorithms.datastructure.stack.concrete.StackLinkedList;
+import com.github.mdssjc.algorithms.utils.Executor;
+import com.github.mdssjc.algorithms.utils.TestDrive;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Exercício 6.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+@TestDrive( input = {"a b c d e f"} )
+public class Ex6 {
+
+  public static void main(String[] args) {
+    Executor.execute(Ex6.class, args);
+
+    final QueueLinkedList<String> q = new QueueLinkedList<>();
+    final StackLinkedList<String> stack = new StackLinkedList<>();
+
+    while (StdIn.hasNextLine()) {
+      q.enqueue(StdIn.readString());
+    }
+
+    while (!q.isEmpty()) {
+      stack.push(q.dequeue());
+    }
+
+    while (!stack.isEmpty()) {
+      q.enqueue(stack.pop());
+    }
+
+    for (String s : q) {
+      StdOut.print(s);
+    }
+  }
+}
