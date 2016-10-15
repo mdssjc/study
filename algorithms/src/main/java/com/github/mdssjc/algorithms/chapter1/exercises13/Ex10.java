295375a1fe189b972fd5da110b459e400cdd9947
Marcelo dos Santos <mdssjc@gmail.com>
2016-10-14 21:36:29 -0300

    Exercício concluído.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises13/Ex10.java
@@ -0,0 +1,22 @@
+package com.github.mdssjc.algorithms.chapter1.exercises13;
+
+import com.github.mdssjc.algorithms.utils.Executor;
+import com.github.mdssjc.algorithms.utils.TestDrive;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Exercício 10.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+@TestDrive( value = {"( ( 1 + 2 ) * ( ( 3 - 4 ) * ( 5 - 6 ) ) )"} )
+public class Ex10 {
+
+  public static void main(String[] args) {
+    Executor.execute(Ex10.class, args);
+
+    String postfix = InfixToPostfix.convert(args[0]);
+    StdOut.println(postfix);
+  }
+}
---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises13/InfixToPostfix.java
@@ -0,0 +1,29 @@
+package com.github.mdssjc.algorithms.chapter1.exercises13;
+
+import com.github.mdssjc.algorithms.datastructure.stack.concrete.StackLinkedList;
+
+/**
+ * Classe InfixToPostfix.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+public class InfixToPostfix {
+
+  public static String convert(final String infix) {
+    final StackLinkedList<String> ops = new StackLinkedList<>();
+    final StringBuilder sb = new StringBuilder();
+
+    for (String s : infix.split(" ")) {
+      if ("+".equals(s) || "-".equals(s) || "*".equals(s) || "/".equals(s)) {
+        ops.push(s);
+      } else if (")".equals(s)) {
+        sb.append(ops.pop() + ") ");
+      } else {
+        sb.append(s + " ");
+      }
+    }
+
+    return sb.toString();
+  }
+}
