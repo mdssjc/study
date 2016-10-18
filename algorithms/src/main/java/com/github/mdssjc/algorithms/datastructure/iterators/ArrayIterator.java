b179469e190b0269dcff340bef6002bafcbd156a
Marcelo dos Santos <mdssjc@gmail.com>
2016-10-18 21:19:25 -0200

    Iterator para Array (Sequêncial).

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/datastructure/iterators/ArrayIterator.java
@@ -0,0 +1,43 @@
+package com.github.mdssjc.algorithms.datastructure.iterators;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Iterador para Array.
+ *
+ * @param <T>
+ *     Tipo de Dado
+ * @author Marcelo dos Santos
+ */
+public class ArrayIterator<T> implements Iterator<T> {
+
+  private final T[] items;
+  private final int length;
+  private int index;
+
+  public ArrayIterator(final T[] items, final int length) {
+    this.items = items;
+    this.length = length;
+  }
+
+  @Override
+  public boolean hasNext() {
+    return this.index < this.length;
+  }
+
+  @Override
+  public T next() {
+    if (!hasNext()) {
+      throw new NoSuchElementException("No Such Element Exception");
+    }
+
+    return this.items[this.index++];
+  }
+
+  @Override
+  public void remove() {
+    throw new UnsupportedOperationException(
+        "Unsupported Operation Exception");
+  }
+}
