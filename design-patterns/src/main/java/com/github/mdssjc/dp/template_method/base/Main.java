cdcaa34addf05e4b3b0a0ca262a49c8fdaec91a1
Marcelo dos Santos <mdssjc@gmail.com>
2016-10-18 10:05:15 -0200

    Inspeção de código.

--- design-patterns/src/main/java/com/github/mdssjc/dp/template_method/base/Main.java
+++ design-patterns/src/main/java/com/github/mdssjc/dp/template_method/base/Main.java
@@ -14,9 +14,9 @@ import com.github.mdssjc.dp.template_method.application.ApplicationClassB;
  */
 public class Main {
 
-  public static void main(String[] args) {
-    ApplicationClassA appA = new ApplicationClassA();
-    ApplicationClassB appB = new ApplicationClassB();
+  public static void main(final String[] args) {
+    final ApplicationClassA appA = new ApplicationClassA();
+    final ApplicationClassB appB = new ApplicationClassB();
 
     appA.templateMethod();
     appB.templateMethod();
