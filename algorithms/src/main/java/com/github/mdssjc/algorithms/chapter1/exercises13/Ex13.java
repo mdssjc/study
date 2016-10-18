8525b01e4fc3e650a932c60b7a7c2f4cd89c92ba
Marcelo dos Santos <mdssjc@gmail.com>
2016-10-18 19:52:28 -0200

    Exercício concluído.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises13/Ex13.java
@@ -0,0 +1,21 @@
+package com.github.mdssjc.algorithms.chapter1.exercises13;
+
+import com.github.mdssjc.algorithms.utils.Executor;
+import com.github.mdssjc.algorithms.utils.TestDrive;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Exercício 13.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+@TestDrive
+public class Ex13 {
+
+  public static void main(final String[] args) {
+    Executor.execute(Ex13.class, args);
+
+    StdOut.println("b - c - d");
+  }
+}
