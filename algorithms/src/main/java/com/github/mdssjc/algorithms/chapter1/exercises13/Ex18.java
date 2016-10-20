b4ebe21b655bc78899b256d90d551e2b7191818b
Marcelo dos Santos <mdssjc@gmail.com>
2016-10-20 20:31:06 -0200

    Exercício concluído.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises13/Ex18.java
@@ -0,0 +1,21 @@
+package com.github.mdssjc.algorithms.chapter1.exercises13;
+
+import com.github.mdssjc.algorithms.utils.Executor;
+import com.github.mdssjc.algorithms.utils.TestDrive;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Exercício 18.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+@TestDrive
+public class Ex18 {
+
+  public static void main(final String[] args) {
+    Executor.execute(Ex18.class, args);
+
+    StdOut.println("Exclui da lista o nó imediatamente seguinte ao 'x'");
+  }
+}
