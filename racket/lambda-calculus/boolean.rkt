d24cf17ce6deca73592d26a4ae91512882f1322f
Marcelo dos Santos <mdssjc@gmail.com>
2016-10-22 11:45:45 -0200

    Implementação de operações booleanas.

---
+++ racket/lambda-calculus/boolean.rkt
@@ -0,0 +1,28 @@
+#lang racket/base
+
+(define T (lambda (a b) a))
+(define F (lambda (a b) b))
+
+(define NOT (λ (x) (x F T)))
+(define AND (λ (b1 b2) (b1 b2 F)))
+(define OR  (λ (b1 b2) (b1 T b2)))
+
+;; Output
+(println (T 1 0))
+(println (F 1 0))
+
+(println "NOT")
+(println (NOT T))
+(println (NOT F))
+
+(println "AND")
+(println (AND F F))
+(println (AND F T))
+(println (AND T F))
+(println (AND T T))
+
+(println "OR")
+(println (OR F F))
+(println (OR F T))
+(println (OR T F))
+(println (OR T T))
