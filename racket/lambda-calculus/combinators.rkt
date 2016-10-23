83f48d5bfd235c30b15871c16e03308db14f1d79
Marcelo dos Santos <mdssjc@gmail.com>
2016-10-23 14:23:12 -0200

    Continuação do estudo sobre combinators.

--- racket/lambda-calculus/combinators.rkt
+++ racket/lambda-calculus/combinators.rkt
@@ -1,26 +1,50 @@
-#lang racket/base
+#lang lazy
 
+;;
 ;; Combinators
-(define S (λ (x) (λ (y) (λ (z) ((x z) (y z))))))           ; Substitution
-(define K (λ (x) (λ (y) x)))                               ; Cancellator
-(define I (λ (x) x))                                       ; Identificator
-(define B (λ (x) (λ (y) (λ (z) ((x y) z)))))               ; Compositor
-(define C (λ (x) (λ (y) (λ (z) x z y))))                   ; Permutator
+;;
+;; author: Marcelo dos Santos
+;;
+;; S -> Substitution/Slider: λx[λy[λz[xz(yz)]]]
+(define S (λ (x) (λ (y) (λ (z) ((x z) (y z))))))
+;; k -> Killer/Cancellator: λx[λy[x]]
+(define K (λ (x) (λ (y) x)))
+;; I -> Identificator: λx[x]
+(define I (λ (x) x))
+;; B -> Compositor: λx[λy[λz[x(yz)]]]
+(define B (λ (x) (λ (y) (λ (z) (x (y z))))))
+;; C -> Permutator: λx[λy[λz[xzy]]]
+(define C (λ (x) (λ (y) (λ (z) ((x z) y)))))
+;; T -> True: λx[λy[x]]
 (define T (λ (x) (λ (y) x)))
+;; F -> False: λx[λy[y]]
 (define F (λ (x) (λ (y) y)))
-(define ω (λ (x) x x))
-(define Ω (λ () ω ω))
-(define Y (λ (x) (λ (y) (λ (z) (λ (u) (x (y z) (y u))))))) ; Distribution
-(define Θ (λ (x) (λ (y) (λ (z) (λ (u) (x (y u) (z u))))))) ; Distribution
+;; W -> Duplicator: λx[λy[xyy]]
+(define W (λ (x) (λ (y) ((x y) y))))
+;; ω -> λx[xx]
+(define ω (λ (x) (x x)))
+;; Ω -> ωω
+(define Ω (λ () (ω (ω))))
+;; Y -> Distribution: λf[(λx[f(xx)])(λx[f(xx)])]
+(define Y (λ (f) ((λ (x) (f (x x)))
+                  (λ (x) (f (x x))))))
+;; Θ -> Distribution: (λx[λf[f(xxf)]])(λx[λf[f(xxf)]])
+(define Θ (λ (x) (λ (y) (λ (z) (λ (u) (x (y u) (z u)))))))
 
 ;; Output
 (define multi (λ (x) (λ (y) (* x y))))
-(define double (λ (x) (* x x)))
+(define div (λ (x) (λ (y) (/ x y))))
+(define (double x) (* x x))
+(define (succ x) (+ x 1))
+(define factorial (Y (λ (fact) (λ (n) (if (zero? n) 1 (* n (fact (- n 1))))))))
 
 (((S multi) double) 5)
 ((K "A") "B")
 (I 1)
-(((B multi) 5) 2)
-
+(((B double) succ) 1)
+(((C div) 4) 2)
 ((T 1) 0)
 ((F 1) 0)
+((W multi) 5)
+
+(!! (map factorial '(1 2 4 8)))
