63f8ccbec967241affc091be13819116d98905ce
Marcelo dos Santos <mdssjc@gmail.com>
2016-11-05 19:52:03 -0200

    Classe Simulador.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section11/Simulator.java
@@ -0,0 +1,16 @@
+package com.github.mdssjc.algorithms.chapter1.section11;
+
+import com.github.mdssjc.algorithms.utils.Executor;
+
+/**
+ * Simulator Class.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+public class Simulator {
+
+  public static void main(final String[] args) {
+    Executor.execute(BinarySearch.class);
+  }
+}
