3d62ae83b42c619a600c6cbbebe3a400a936af25
Marcelo dos Santos <mdssjc@gmail.com>
2016-11-05 19:51:24 -0200

    Uso do Test Drive.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section11/BinarySearch.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section11/BinarySearch.java
@@ -1,21 +1,30 @@
 package com.github.mdssjc.algorithms.chapter1.section11;
 
-import java.util.Arrays;
-
+import com.github.mdssjc.algorithms.utils.Executor;
+import com.github.mdssjc.algorithms.utils.TestDrive;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.Arrays;
+
+/**
+ * BinarySearch Class.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+@TestDrive( value = "largeW.txt", valueFile = true, input = "largeT.txt", inputFile = true )
 public class BinarySearch {
 
-  public static int rank(int key, int[] a) {
+  public static int rank(final int key, final int[] a) {
     // Array must be sorted.
     int lo = 0;
     int hi = a.length - 1;
 
     while (lo <= hi) {
       // Key is in a[lo..hi] or not present.
-      int mid = lo + (hi - lo) / 2;
+      final int mid = lo + (hi - lo) / 2;
       if (key < a[mid]) {
         hi = mid - 1;
       } else if (key > a[mid]) {
@@ -27,14 +36,16 @@ public class BinarySearch {
     return -1;
   }
 
-  public static void main(String[] args) {
-    int[] whitelist = In.readInts(args[0]);
+  public static void main(final String[] args) {
+    Executor.execute(BinarySearch.class, args);
+
+    final int[] whitelist = In.readInts(args[0]);
 
     Arrays.sort(whitelist);
 
     while (!StdIn.isEmpty()) {
       // Read key, print if not in whitelist.
-      int key = StdIn.readInt();
+      final int key = StdIn.readInt();
       if (rank(key, whitelist) == -1) {
         StdOut.println(key);
       }
