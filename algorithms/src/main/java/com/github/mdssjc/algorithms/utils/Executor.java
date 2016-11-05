529c5df4e8d7cdb6d7039faff74b27a8628afff6
Marcelo dos Santos <mdssjc@gmail.com>
2016-11-05 19:52:53 -0200

    Implementação da leitura por arquivo da entrada de dados (input).

--- algorithms/src/main/java/com/github/mdssjc/algorithms/utils/Executor.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/utils/Executor.java
@@ -1,44 +1,49 @@
 package com.github.mdssjc.algorithms.utils;
 
-import java.io.ByteArrayInputStream;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 /**
  * Executor Class.
- * 
+ *
  * @author Marcelo dos Santos
  *
  */
 public class Executor {
 
+  public static final String RESOURCES_PATH = "src/main/resources/";
+
   public static void execute(final Class<?> clazz) {
     try {
       final Method method = clazz.getMethod("main", String[].class);
       final Annotation[] annotations = clazz
-        .getAnnotationsByType(TestDrive.class);
+          .getAnnotationsByType(TestDrive.class);
 
       for (final Annotation annotation : annotations) {
         final TestDrive td = (TestDrive) annotation;
         StdOut.printf("java %s %s%n", clazz.getSimpleName(),
-            Arrays.deepToString(td.value()));
+                      Arrays.deepToString(td.value()));
 
         final String[] value = td.value();
         if (td.valueFile()) {
-          value[0] = "src/main/resources/" + value[0];
+          value[0] = RESOURCES_PATH + value[0];
         }
 
         if (td.input().length > 0) {
           System.setIn(new ByteArrayInputStream(
-              Arrays.stream(td.input())
-                .collect(Collectors.joining(" "))
-                .getBytes()));
+              Arrays.stream(getInputValue(td))
+                    .collect(Collectors.joining(" "))
+                    .getBytes()));
 
           final Method resync = StdIn.class.getDeclaredMethod("resync");
           resync.setAccessible(true);
@@ -53,6 +58,18 @@ public class Executor {
       StdOut.println(exception.getMessage());
       StdOut.println(exception);
     }
+  }
+
+  private static String[] getInputValue(final TestDrive td) {
+    if (td.inputFile()) {
+      try {
+        return Files.readAllLines(Paths.get(RESOURCES_PATH + td.input()[0]))
+                    .toArray(new String[0]);
+      } catch (final IOException e) {
+        StdOut.println(e.getMessage());
+      }
+    }
+    return td.input();
   }
 
   public static void execute(final Class<?> clazz, final String label) {
