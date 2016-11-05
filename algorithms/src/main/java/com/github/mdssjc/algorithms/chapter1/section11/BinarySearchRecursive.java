6bb0bbe55bb889bfee062edc4edd77ec7513f2d4
Marcelo dos Santos <mdssjc@gmail.com>
2016-11-05 19:59:59 -0200

    Inclusão do TestDrive.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section11/BinarySearchRecursive.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section11/BinarySearchRecursive.java
@@ -1,24 +1,33 @@
 package com.github.mdssjc.algorithms.chapter1.section11;
 
-import java.util.Arrays;
-
+import com.github.mdssjc.algorithms.utils.Executor;
+import com.github.mdssjc.algorithms.utils.TestDrive;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.Arrays;
+
+/**
+ * BinarySearchRecursive Class.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+@TestDrive( value = "largeW.txt", valueFile = true, input = "largeT.txt", inputFile = true )
 public class BinarySearchRecursive {
 
-  public static int rank(int key, int[] a) {
+  public static int rank(final int key, final int[] a) {
     return rank(key, a, 0, a.length - 1);
   }
 
-  private static int rank(int key, int[] a, int lo, int hi) {
+  private static int rank(final int key, final int[] a, final int lo, final int hi) {
     // Index of key in a[], if present, is not smaller than lo and not larger
     // than hi.
     if (lo > hi) {
       return -1;
     }
-    int mid = lo + (hi - lo) / 2;
+    final int mid = lo + (hi - lo) / 2;
     if (key < a[mid]) {
       return rank(key, a, lo, mid - 1);
     } else if (key > a[mid]) {
@@ -28,13 +37,15 @@ public class BinarySearchRecursive {
     }
   }
 
-  public static void main(String[] args) {
-    int[] whitelist = In.readInts(args[0]);
+  public static void main(final String[] args) {
+    Executor.execute(BinarySearchRecursive.class, args);
+
+    final int[] whitelist = In.readInts(args[0]);
 
     Arrays.sort(whitelist);
 
     while (!StdIn.isEmpty()) {
-      int key = StdIn.readInt();
+      final int key = StdIn.readInt();
       if (rank(key, whitelist) == -1) {
         StdOut.println(key);
       }
