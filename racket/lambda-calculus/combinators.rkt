31f57f15df246d8284aa3f078c19611cacfdf46e
Marcelo dos Santos <mdssjc@gmail.com>
2016-11-05 11:06:44 -0200

    Legibilidade e transformação para módulo.

--- racket/lambda-calculus/combinators.rkt
+++ racket/lambda-calculus/combinators.rkt
@@ -5,29 +5,42 @@
 ;;
 ;; author: Marcelo dos Santos
 ;;
+(provide combinators)
+
 ;; S -> Substitution/Slider: λx[λy[λz[xz(yz)]]]
 (define S (λ (x) (λ (y) (λ (z) ((x z) (y z))))))
+
 ;; k -> Killer/Cancellator: λx[λy[x]]
 (define K (λ (x) (λ (y) x)))
+
 ;; I -> Identificator: λx[x]
 (define I (λ (x) x))
+
 ;; B -> Compositor: λx[λy[λz[x(yz)]]]
 (define B (λ (x) (λ (y) (λ (z) (x (y z))))))
+
 ;; C -> Permutator: λx[λy[λz[xzy]]]
 (define C (λ (x) (λ (y) (λ (z) ((x z) y)))))
+
 ;; T -> True: λx[λy[x]]
 (define T (λ (x) (λ (y) x)))
+
 ;; F -> False: λx[λy[y]]
 (define F (λ (x) (λ (y) y)))
+
 ;; W -> Duplicator: λx[λy[xyy]]
 (define W (λ (x) (λ (y) ((x y) y))))
+
 ;; ω -> λx[xx]
 (define ω (λ (x) (x x)))
+
 ;; Ω -> ωω
 (define Ω (λ () (ω (ω))))
+
 ;; Y -> Distribution: λf[(λx[f(xx)])(λx[f(xx)])]
 (define Y (λ (f) ((λ (x) (f (x x)))
                   (λ (x) (f (x x))))))
+
 ;; Θ -> Distribution: (λx[λf[f(xxf)]])(λx[λf[f(xxf)]])
 (define Θ (λ (x) (λ (y) (λ (z) (λ (u) (x (y u) (z u)))))))
 
