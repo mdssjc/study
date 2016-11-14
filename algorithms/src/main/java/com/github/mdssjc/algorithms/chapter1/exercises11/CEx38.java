16c12da602890959a119bde850fc185b835fc5f3
Marcelo dos Santos <mdssjc@gmail.com>
2016-11-13 23:05:16 -0200

    Binary search versus brute-force search.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises11/CEx38.java
@@ -0,0 +1,61 @@
+package com.github.mdssjc.algorithms.chapter1.exercises11;
+
+import com.github.mdssjc.algorithms.chapter1.section11.BinarySearch;
+import com.github.mdssjc.algorithms.utils.Executor;
+import com.github.mdssjc.algorithms.utils.TestDrive;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Stopwatch;
+
+import java.util.Arrays;
+
+/**
+ * Creative Exercise 38.
+ * <p>
+ * Binary search versus brute-force search.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+@TestDrive( value = {"largeW.txt", "largeT.txt"}, valueFile = true )
+public class CEx38 {
+
+  public static void main(final String[] args) {
+    Executor.execute(CEx38.class, args);
+
+    final int[] whitelist = In.readInts(args[0]);
+    final int[] inputs = In.readInts(args[1]);
+
+    Arrays.sort(whitelist);
+
+    StdOut.println("BinarySearch:");
+    final Stopwatch stopwatchA = new Stopwatch();
+    for (final int input : inputs) {
+      if (BinarySearch.rank(input, whitelist) == -1) {
+        StdOut.println(input);
+      }
+    }
+    final double elapsedTimeA = stopwatchA.elapsedTime();
+
+    StdOut.println("BruteForceSearch:");
+    final Stopwatch stopwatchB = new Stopwatch();
+    for (final int input : inputs) {
+      if (BruteForceSearch.rank(input, whitelist) == -1) {
+        StdOut.println(input);
+      }
+    }
+    final double elapsedTimeB = stopwatchB.elapsedTime();
+
+    StdOut.printf("A: %.2f | B: %.2f%n", elapsedTimeA, elapsedTimeB);
+  }
+}
+
+class BruteForceSearch {
+
+  public static int rank(final int key, final int[] a) {
+    for (int i = 0; i < a.length; i++) {
+      if (a[i] == key) return i;
+    }
+    return -1;
+  }
+}
