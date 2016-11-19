21d8b53b4d46996ad07b66a8c4671da2422eb059
Marcelo dos Santos <mdssjc@gmail.com>
2016-11-19 20:47:01 -0200

    Refatoração do exercício.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises12/Ex9.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises12/Ex9.java
@@ -1,43 +1,22 @@
 package com.github.mdssjc.algorithms.chapter1.exercises12;
 
-import java.util.Arrays;
-
 import com.github.mdssjc.algorithms.utils.Executor;
 import com.github.mdssjc.algorithms.utils.TestDrive;
-
 import edu.princeton.cs.algs4.Counter;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.Arrays;
+
 /**
- * Exercício 9.
- * 
+ * Exercise 9.
+ *
  * @author Marcelo dos Santos
  *
  */
-@TestDrive(value = { "intsEven10" }, valueFile = true, input = { "1", "0",
-    "3" })
+@TestDrive( value = "intsEven10", valueFile = true, input = {"1", "0", "3"} )
 public class Ex9 {
-
-  public static int rank(final int key, final int[] a, final Counter counter) {
-    int lo = 0;
-    int hi = a.length - 1;
-
-    while (lo <= hi) {
-      counter.increment();
-
-      final int mid = lo + (hi - lo) / 2;
-      if (key < a[mid]) {
-        hi = mid - 1;
-      } else if (key > a[mid]) {
-        lo = mid + 1;
-      } else {
-        return mid;
-      }
-    }
-    return -1;
-  }
 
   public static void main(final String[] args) {
     Executor.execute(Ex9.class, args);
@@ -56,5 +35,24 @@ public class Ex9 {
     }
 
     StdOut.println("Total of " + counter.tally() + " keys examined.");
+  }
+
+  public static int rank(final int key, final int[] a, final Counter counter) {
+    int lo = 0;
+    int hi = a.length - 1;
+
+    while (lo <= hi) {
+      counter.increment();
+
+      final int mid = lo + (hi - lo) / 2;
+      if (key < a[mid]) {
+        hi = mid - 1;
+      } else if (key > a[mid]) {
+        lo = mid + 1;
+      } else {
+        return mid;
+      }
+    }
+    return -1;
   }
 }
