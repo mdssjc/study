129387c0cbd8bab521bb8e3d062d2b6476c06482
Marcelo dos Santos <mdssjc@gmail.com>
2016-11-19 20:55:31 -0200

    Refatoração do exercício.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises12/Ex11.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises12/Ex11.java
@@ -3,18 +3,17 @@ package com.github.mdssjc.algorithms.chapter1.exercises12;
 import com.github.mdssjc.algorithms.chapter1.section12.Datable;
 import com.github.mdssjc.algorithms.utils.Executor;
 import com.github.mdssjc.algorithms.utils.TestDrive;
-
 import edu.princeton.cs.algs4.StdOut;
 
 /**
- * Exercício 11.
- * 
+ * Exercise 11.
+ *
  * @author Marcelo dos Santos
  *
  */
-@TestDrive({ "0", "31", "1999" })
-@TestDrive({ "12", "0", "1999" })
-@TestDrive({ "12", "31", "0" })
+@TestDrive( {"0", "31", "1999"} )
+@TestDrive( {"12", "0", "1999"} )
+@TestDrive( {"12", "31", "0"} )
 public class Ex11 implements Datable {
 
   private final int value;
