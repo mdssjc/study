cbb5d9a3286082495b1b162d6230ad6963647190
Marcelo dos Santos <mdssjc@gmail.com>
2016-11-19 20:41:47 -0200

    Padronização com o testdrive.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises12/Ex4.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises12/Ex4.java
@@ -1,16 +1,21 @@
 package com.github.mdssjc.algorithms.chapter1.exercises12;
 
+import com.github.mdssjc.algorithms.utils.Executor;
+import com.github.mdssjc.algorithms.utils.TestDrive;
 import edu.princeton.cs.algs4.StdOut;
 
 /**
  * Exercise 4.
- * 
+ *
  * @author Marcelo dos Santos
  *
  */
+@TestDrive
 public class Ex4 {
 
   public static void main(final String[] args) {
+    Executor.execute(Ex4.class, args);
+
     String string1 = "hello";
     final String string2 = string1;
     string1 = "world";
--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises12/Ex5.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises12/Ex5.java
@@ -1,5 +1,7 @@
 package com.github.mdssjc.algorithms.chapter1.exercises12;
 
+import com.github.mdssjc.algorithms.utils.Executor;
+import com.github.mdssjc.algorithms.utils.TestDrive;
 import edu.princeton.cs.algs4.StdOut;
 
 /**
@@ -8,9 +10,12 @@ import edu.princeton.cs.algs4.StdOut;
  * @author Marcelo dos Santos
  *
  */
+@TestDrive
 public class Ex5 {
 
   public static void main(final String[] args) {
+    Executor.execute(Ex5.class, args);
+
     final String s = "Hello World";
     s.toUpperCase();
     s.substring(6, 11);
