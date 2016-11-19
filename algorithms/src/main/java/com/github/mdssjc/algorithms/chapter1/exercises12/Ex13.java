0725b7404daca52e934c8d7005d9a33d8bb4ca0f
Marcelo dos Santos <mdssjc@gmail.com>
2016-11-19 21:02:43 -0200

    Refatoração do exercício.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises12/Ex13.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises12/Ex13.java
@@ -2,31 +2,49 @@ package com.github.mdssjc.algorithms.chapter1.exercises12;
 
 import com.github.mdssjc.algorithms.utils.Executor;
 import com.github.mdssjc.algorithms.utils.TestDrive;
-
 import edu.princeton.cs.algs4.Date;
 import edu.princeton.cs.algs4.StdOut;
 
 /**
- * Exercício 13 (Transaction).
- * 
+ * Exercise 13.
+ *
  * @author Marcelo dos Santos
  *
  */
-@TestDrive({ "Marcelo", "08/28/2016", "1520.55" })
-@TestDrive({ "Marcelo,08/28/2016,1520.55" })
+@TestDrive( {"Marcelo", "08/28/2016", "1520.55"} )
+@TestDrive( "Marcelo,08/28/2016,1520.55" )
 public class Ex13 {
 
+  public static void main(final String[] args) {
+    Executor.execute(Ex13.class, args);
+
+    final Transaction transaction;
+    if (args.length == 1) {
+      transaction = new Transaction(args[0]);
+    } else {
+      final String who = args[0];
+      final Date when = new Date(args[1]);
+      final double amount = Double.parseDouble(args[2]);
+      transaction = new Transaction(who, when, amount);
+    }
+
+    StdOut.println(transaction);
+  }
+}
+
+class Transaction {
+
   private final String who;
-  private final Date   when;
+  private final Date when;
   private final double amount;
 
-  public Ex13(final String who, final Date when, final double amount) {
+  public Transaction(final String who, final Date when, final double amount) {
     this.who = who;
     this.when = when;
     this.amount = amount;
   }
 
-  public Ex13(final String transaction) {
+  public Transaction(final String transaction) {
     final String[] xs = transaction.split(",");
     this.who = xs[0];
     this.when = new Date(xs[1]);
@@ -48,21 +66,5 @@ public class Ex13 {
   @Override
   public String toString() {
     return String.format("%s: %s %.2f", this.who, this.when, this.amount);
-  }
-
-  public static void main(final String[] args) {
-    Executor.execute(Ex13.class, args);
-
-    Ex13 ex13;
-    if (args.length == 1) {
-      ex13 = new Ex13(args[0]);
-    } else {
-      final String who = args[0];
-      final Date when = new Date(args[1]);
-      final double amount = Double.parseDouble(args[2]);
-      ex13 = new Ex13(who, when, amount);
-    }
-
-    StdOut.println(ex13);
   }
 }
