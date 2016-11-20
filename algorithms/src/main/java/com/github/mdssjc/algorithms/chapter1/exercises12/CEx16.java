c1a5c208472857c6e3514e79f12055394de225ff
Marcelo dos Santos <mdssjc@gmail.com>
2016-11-19 23:20:38 -0200

    Classe interna renomeada.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises12/CEx16.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises12/CEx16.java
@@ -7,7 +7,7 @@ import edu.princeton.cs.algs4.StdOut;
 /**
  * Creative Exercise 16.
  * <p>
- * Rational numbers.
+ * RationalCEx16 numbers.
  *
  * @author Marcelo dos Santos
  *
@@ -18,58 +18,58 @@ public class CEx16 {
   public static void main(final String[] args) {
     Executor.execute(CEx16.class, args);
 
-    Rational x;
-    Rational y;
-    Rational z;
+    RationalCEx16 x;
+    RationalCEx16 y;
+    RationalCEx16 z;
 
     // 1/2 + 1/3 = 5/6
-    x = new Rational(1, 2);
-    y = new Rational(1, 3);
+    x = new RationalCEx16(1, 2);
+    y = new RationalCEx16(1, 3);
     z = x.plus(y);
     StdOut.println(z);
 
     // 8/9 + 1/9 = 1
-    x = new Rational(8, 9);
-    y = new Rational(1, 9);
+    x = new RationalCEx16(8, 9);
+    y = new RationalCEx16(1, 9);
     z = x.plus(y);
     StdOut.println(z);
 
     //  4/17 * 7/3 = 28/51
-    x = new Rational(4, 17);
-    y = new Rational(7, 3);
+    x = new RationalCEx16(4, 17);
+    y = new RationalCEx16(7, 3);
     z = x.times(y);
     StdOut.println(z);
 
     // 203/16957 * 9299/5887 = 17/899
-    x = new Rational(203, 16957);
-    y = new Rational(9299, 5887);
+    x = new RationalCEx16(203, 16957);
+    y = new RationalCEx16(9299, 5887);
     z = x.times(y);
     StdOut.println(z);
 
     // 3/5 / 1/2 = 6/5
-    x = new Rational(3, 5);
-    y = new Rational(1, 2);
+    x = new RationalCEx16(3, 5);
+    y = new RationalCEx16(1, 2);
     z = x.dividedBy(y);
     StdOut.println(z);
 
     // 1/6 - -4/-8 = -1/3
-    x = new Rational(1, 6);
-    y = new Rational(-4, -8);
+    x = new RationalCEx16(1, 6);
+    y = new RationalCEx16(-4, -8);
     z = x.minus(y);
     StdOut.println(z);
 
     // 0/6 = 0
-    x = new Rational(0, 6);
+    x = new RationalCEx16(0, 6);
     StdOut.println(x);
   }
 }
 
-class Rational {
+class RationalCEx16 {
 
   private long numerator;
   private long denominator;
 
-  public Rational(final long numerator, final long denominator) {
+  public RationalCEx16(final long numerator, final long denominator) {
     if (denominator == 0) {
       throw new ArithmeticException("Denominator is zero");
     }
@@ -92,22 +92,22 @@ class Rational {
     return this.denominator;
   }
 
-  public Rational plus(final Rational b) {
+  public RationalCEx16 plus(final RationalCEx16 b) {
     final long numerator = (this.numerator * b.denominator) + (this.denominator * b.numerator);
     final long denominator = this.denominator * b.denominator;
-    return new Rational(numerator, denominator);
+    return new RationalCEx16(numerator, denominator);
   }
 
-  public Rational minus(final Rational b) {
-    return plus(new Rational(-b.numerator, b.denominator));
+  public RationalCEx16 minus(final RationalCEx16 b) {
+    return plus(new RationalCEx16(-b.numerator, b.denominator));
   }
 
-  public Rational times(final Rational b) {
-    return new Rational(this.numerator * b.numerator, this.denominator * b.denominator);
+  public RationalCEx16 times(final RationalCEx16 b) {
+    return new RationalCEx16(this.numerator * b.numerator, this.denominator * b.denominator);
   }
 
-  public Rational dividedBy(final Rational b) {
-    return times(new Rational(b.denominator, b.numerator));
+  public RationalCEx16 dividedBy(final RationalCEx16 b) {
+    return times(new RationalCEx16(b.denominator, b.numerator));
   }
 
   @Override
@@ -118,7 +118,7 @@ class Rational {
     if (o == null || getClass() != o.getClass()) {
       return false;
     }
-    final Rational rational = (Rational) o;
+    final RationalCEx16 rational = (RationalCEx16) o;
     if (this.numerator != rational.numerator) {
       return false;
     }
