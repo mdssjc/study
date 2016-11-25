26097d6d5543cff42769a258c9db01de7488e9c9
Marcelo dos Santos <mdssjc@gmail.com>
2016-11-25 12:07:15 -0200

    Substituição do if pelo switch.

--- head-first-labs/c/ch01/cards.c
+++ head-first-labs/c/ch01/cards.c
@@ -11,16 +11,31 @@ int main() {
   puts("Enter the card_name: ");
   scanf("%2s", card_name);
   int val = 0;
-  if (card_name[0] == 'K') {
-    val = 10;
-  } else if (card_name[0] == 'Q') {
-    val = 10;
-  } else if (card_name[0] == 'J') {
-    val = 10;
-  } else if (card_name[0] == 'A') {
+  /* if (card_name[0] == 'K') { */
+  /*   val = 10; */
+  /* } else if (card_name[0] == 'Q') { */
+  /*   val = 10; */
+  /* } else if (card_name[0] == 'J') { */
+  /*   val = 10; */
+  /* } else if (card_name[0] == 'A') { */
+  /*   val = 11; */
+  /* } else { */
+  /*   val = atoi(card_name); */
+  /* } */
+
+  switch (card_name[0]) {
+  case 'K':
+  case 'Q':
+  case 'J': {
+    val =  10;
+    break;
+  }
+  case 'A': {
     val = 11;
-  } else {
-    val = atoi(card_name);
+    break;
+  }
+  default:
+    val =  atoi(card_name);
   }
 
   if (val >= 3 && val <= 6) {
