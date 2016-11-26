8caf41cefdc167d5cf691f15dd4a81bfa7539ba7
Marcelo dos Santos <mdssjc@gmail.com>
2016-11-26 21:47:36 -0200

    Wget.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises11/WEx41.java
@@ -0,0 +1,40 @@
+package com.github.mdssjc.algorithms.chapter1.exercises11;
+
+import com.github.mdssjc.algorithms.utils.Executor;
+import com.github.mdssjc.algorithms.utils.TestDrive;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * Web Exercise 41.
+ * <p>
+ * Wget.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+@TestDrive( "http://introcs.cs.princeton.edu/data/codes.csv" )
+public class WEx41 {
+
+  public static void main(final String[] args) {
+    Executor.execute(WEx41.class, args);
+
+    final String url = args[0];
+    final String filename = url.substring(url.lastIndexOf('/') + 1);
+
+    try (Scanner scanner = new Scanner(new URL(url).openStream());
+         PrintStream printStream = new PrintStream(filename)) {
+      while (scanner.hasNext()) {
+        final String line = scanner.next();
+        StdOut.println(line);
+        printStream.println(line);
+      }
+    } catch (final IOException e) {
+      StdOut.println(e.getMessage());
+    }
+  }
+}
