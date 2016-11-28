62b9a8fa4b36b844585383a6e612b3fb0ced8f4c
Marcelo dos Santos <mdssjc@gmail.com>
2016-11-28 11:22:57 -0200

    Atualização com while.

--- head-first-labs/c/ch01/cards.c
+++ head-first-labs/c/ch01/cards.c
@@ -8,44 +8,58 @@
 
 int main() {
   char card_name[3];
-  puts("Enter the card_name: ");
-  scanf("%2s", card_name);
-  int val = 0;
-  /* if (card_name[0] == 'K') { */
-  /*   val = 10; */
-  /* } else if (card_name[0] == 'Q') { */
-  /*   val = 10; */
-  /* } else if (card_name[0] == 'J') { */
-  /*   val = 10; */
-  /* } else if (card_name[0] == 'A') { */
-  /*   val = 11; */
-  /* } else { */
-  /*   val = atoi(card_name); */
-  /* } */
+  int count = 0;
 
-  switch (card_name[0]) {
-  case 'K':
-  case 'Q':
-  case 'J': {
-    val =  10;
-    break;
-  }
-  case 'A': {
-    val = 11;
-    break;
-  }
-  default:
-    val =  atoi(card_name);
-  }
+  while (card_name[0] != 'X') {
+    puts("Enter the card_name: ");
+    scanf("%2s", card_name);
+    int val = 0;
+    /* if (card_name[0] == 'K') { */
+    /*   val = 10; */
+    /* } else if (card_name[0] == 'Q') { */
+    /*   val = 10; */
+    /* } else if (card_name[0] == 'J') { */
+    /*   val = 10; */
+    /* } else if (card_name[0] == 'A') { */
+    /*   val = 11; */
+    /* } else { */
+    /*   val = atoi(card_name); */
+    /* } */
 
-  if (val >= 3 && val <= 6) {
-    /* Check if the value is 3 to 6 */
-    puts("Count has gone up");
-  } else if (val == 10) {
-    /* Otherwise check if the card was 10, J, Q or K */
-    puts("Count has gone down");
-  }
+    switch (card_name[0]) {
+    case 'K':
+    case 'Q':
+    case 'J': {
+      val =  10;
+      break;
+    }
+    case 'A': {
+      val = 11;
+      break;
+    }
+    case 'X': {
+      continue;
+      break;
+    }
+    default:
+      val =  atoi(card_name);
+      if (val < 1 || val > 10) {
+        puts("I don't understand that value!");
+        continue;
+      }
+    }
 
-  /* printf("The card value is: %i\n", val); */
+    if (val > 2 && val < 7) {
+      /* Check if the value is 3 to 6 */
+      puts("Count has gone up");
+      count++;
+    } else if (val == 10) {
+      /* Otherwise check if the card was 10, J, Q or K */
+      puts("Count has gone down");
+      count--;
+    }
+    printf("Current count: %i\n", count);
+    /* printf("The card value is: %i\n", val); */
+  }
   return 0;
 }
