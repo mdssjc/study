056d4c36758e2c2666e23a485c01c77217729568
Marcelo dos Santos <mdssjc@gmail.com>
2016-11-30 10:41:56 -0200

    Complemento do exercício.

--- head-first-labs/c/ch02/monte.c
+++ head-first-labs/c/ch02/monte.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 int main() {
+  /* const char *cards = "JQK"; */
   char cards[] = "JQK";
   char a_card = cards[2];
+
   cards[2] = cards[1];
   cards[1] = cards[0];
   cards[0] = cards[2];
   cards[2] = cards[1];
   cards[1] = a_card;
+
   puts(cards);
   return 0;
 }
