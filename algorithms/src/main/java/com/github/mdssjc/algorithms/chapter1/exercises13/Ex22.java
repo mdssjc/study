c75bc944e893d84e4098be480496ffd1fe2f68c8
Marcelo dos Santos <mdssjc@gmail.com>
2016-12-10 20:49:47 -0200

    Exercise 22.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises13/Ex22.java
@@ -0,0 +1,21 @@
+package com.github.mdssjc.algorithms.chapter1.exercises13;
+
+import com.github.mdssjc.algorithms.utils.Executor;
+import com.github.mdssjc.algorithms.utils.TestDrive;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Exercise 22.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+@TestDrive
+public class Ex22 {
+
+  public static void main(final String[] args) {
+    Executor.execute(Ex22.class, args);
+
+    StdOut.println("Inserts node t immediately after node x.");
+  }
+}
