2b4773c3dc75e37860f62028ef2ddfe1755875de
Marcelo dos Santos <mdssjc@gmail.com>
2016-12-11 19:59:26 -0200

    Coesão do exercício.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises13/Ex20.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises13/Ex20.java
@@ -1,5 +1,7 @@
 package com.github.mdssjc.algorithms.chapter1.exercises13;
 
+import com.github.mdssjc.algorithms.datastructure.iterators.LinkedListIterator;
+import com.github.mdssjc.algorithms.datastructure.linkedlist.Node;
 import com.github.mdssjc.algorithms.utils.Executor;
 import com.github.mdssjc.algorithms.utils.TestDrive;
 import edu.princeton.cs.algs4.StdOut;
@@ -10,26 +12,44 @@ import edu.princeton.cs.algs4.StdOut;
  * @author Marcelo dos Santos
  *
  */
-@TestDrive( "0" )
-@TestDrive( "1" )
-@TestDrive( "2" )
-@TestDrive( "3" )
-@TestDrive( "4" )
+@TestDrive
 public class Ex20 {
 
   public static void main(final String[] args) {
     Executor.execute(Ex20.class, args);
 
-    final QueueEx20<String> queue = new QueueEx20<>();
-    queue.enqueue("A");
-    queue.enqueue("B");
-    queue.enqueue("C");
+    final Node<String> nodeRoot = new Node<>();
+    nodeRoot.item = "root";
+    final Node<String> nodeA = new Node<>();
+    nodeA.item = "node a";
+    nodeRoot.next = nodeA;
+    final Node<String> nodeB = new Node<>();
+    nodeB.item = "node b";
+    nodeA.next = nodeB;
+    final Node<String> nodeC = new Node<>();
+    nodeC.item = "node c";
+    nodeB.next = nodeC;
 
-    queue.delete(Integer.parseInt(args[0]));
+    final int k = 2;
 
-    for (final String s : queue) {
-      StdOut.print(queue.dequeue() + " ");
+    new LinkedListIterator<>(nodeRoot).forEachRemaining(StdOut::println);
+
+    delete(nodeRoot, k);
+
+    new LinkedListIterator<>(nodeRoot).forEachRemaining(StdOut::println);
+  }
+
+  private static void delete(final Node<String> node, final int k) {
+    int count = 0;
+
+    Node ref = node;
+    while (ref != null) {
+      count++;
+      if (count == k) {
+        ref.next = ref.next.next;
+        break;
+      }
+      ref = ref.next;
     }
-    StdOut.println();
   }
 }
