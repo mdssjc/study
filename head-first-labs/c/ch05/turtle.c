513ab7ea0c242bb5f01001c687bd74d636bdf3bf
Marcelo dos Santos <mdssjc@gmail.com>
2016-12-12 11:35:15 -0200

    Operador correto.

--- head-first-labs/c/ch05/turtle.c
+++ head-first-labs/c/ch05/turtle.c
@@ -7,8 +7,8 @@ typedef struct {
 } turtle;
 
 void happy_birthday(turtle *t) {
-  (*t).age = (*t).age + 1;
-  printf("Happy Birthday %s! You are now %i years old!\n", (*t).name, (*t).age);
+  t->age = t->age + 1;
+  printf("Happy Birthday %s! You are now %i years old!\n", t->name, t->age);
 }
 
 int main() {
