48f2ff9861e29ff84893bff8132afb5fc40ff52e
Marcelo dos Santos <mdssjc@gmail.com>
2016-12-14 12:09:05 -0200

    Versão dinâmica.

--- head-first-labs/c/ch06/tour.c
+++ head-first-labs/c/ch06/tour.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct island {
   char *name;
@@ -6,6 +8,25 @@ typedef struct island {
   char *closes;
   struct island *next;
 } island;
+
+island* create(char *name) {
+  island *i = malloc(sizeof(island));
+  i->name = strdup(name);
+  i->opens = "09:00";
+  i->closes = "17:00";
+  i->next = NULL;
+  return i;
+}
+
+void release(island *start) {
+  island *i = start;
+  island *next = NULL;
+  for (; i != NULL; i = next) {
+    next = i->next;
+    free(i->name);
+    free(i);
+  }
+}
 
 void display(island *start) {
   island *i = start;
@@ -15,18 +36,23 @@ void display(island *start) {
 }
 
 int main() {
-  island amity = {"Amity", "09:00", "17:00", NULL};
-  island craggy = {"Craggy", "09:00", "17:00", NULL};
-  island isla_nublar = {"Isla Nublar", "09:00", "17:00", NULL};
-  island skull = {"Skull", "09:00", "17:00", NULL};
-  island shutter = {"Shutter", "09:00", "17:00", NULL};
+  island *start = NULL;
+  island *i = NULL;
+  island *next = NULL;
+  char name[80];
 
-  amity.next = &craggy;
-  craggy.next = &isla_nublar;
-  isla_nublar.next = &skull;
-  skull.next = &shutter;
+  for(; fgets(name, 80, stdin) != NULL; i = next) {
+    next = create(name);
+    if (start == NULL){
+      start = next;
+    }
+    if (i != NULL) {
+      i->next = next;
+    }
+  }
 
-  display(&amity);
+  display(start);
+  release(start);
 
   return 0;
 }
