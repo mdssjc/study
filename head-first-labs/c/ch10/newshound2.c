34c8e23084f7f4f395aa5b18d14f623393c086cf
Marcelo dos Santos <mdssjc@gmail.com>
2016-12-20 14:07:00 -0200

    Programa Newshound 2.

---
+++ head-first-labs/c/ch10/newshound2.c
@@ -0,0 +1,34 @@
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void error(char *msg) {
+  fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+  exit(1);
+}
+
+int main(int argc, char *argv[]) {
+  char *phrase = argv[1];
+  char *vars[] = {"RSS_FEED=http://www.rollingstone.com/rss",  NULL};
+
+  FILE *f = fopen("stories.txt", "w");
+  if (!f) {
+    error("Can't open stories.txt");
+  }
+
+  pid_t pid =  fork();
+  if (pid == -1) {
+    error("Can't fork process");
+  }
+  if (!pid) {
+    if (dup2(fileno(f), 1) == -1) {
+      error("Can't redirect Standard Output");
+    }
+    if (execle("/usr/bin/python2", "/usr/bin/python2", "./rssgossip.py", phrase, NULL, vars) == -1) {
+      error("Can't run script");
+    }
+  }
+  return 0;
+}
