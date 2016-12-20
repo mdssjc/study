5980bd5885ecfd57987bfffd4bdc5f7eb85e9505
Marcelo dos Santos <mdssjc@gmail.com>
2016-12-20 14:33:28 -0200

    Wait PID.

--- head-first-labs/c/ch10/newshound2.c
+++ head-first-labs/c/ch10/newshound2.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 
 void error(char *msg) {
   fprintf(stderr, "%s: %s\n", msg, strerror(errno));
@@ -28,6 +29,10 @@ int main(int argc, char *argv[]) {
     }
     if (execle("/usr/bin/python2", "/usr/bin/python2", "./rssgossip.py", phrase, NULL, vars) == -1) {
       error("Can't run script");
+    }
+    int pid_status;
+    if (waitpid(pid, &pid_status, 0) == -1) {
+      error("Error waiting for child process");
     }
   }
   return 0;
