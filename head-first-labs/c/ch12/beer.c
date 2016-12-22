6e7a0f51d61d3f2a357641796da5de433fe4eb54
Marcelo dos Santos <mdssjc@gmail.com>
2016-12-22 14:06:09 -0200

    Com Mutex.

--- head-first-labs/c/ch12/beer.c
+++ head-first-labs/c/ch12/beer.c
@@ -11,12 +11,15 @@ void error(char *msg) {
 }
 
 int beers = 2000000;
-
+pthread_mutex_t beers_lock = PTHREAD_MUTEX_INITIALIZER;
 void* drink_lots(void *a) {
   int i;
   for (i = 0; i < 100000; i++) {
+    pthread_mutex_lock(&beers_lock);
     beers = beers - 1;
+    pthread_mutex_unlock(&beers_lock);
   }
+  printf("beers = %i\n", beers);
   return NULL;
 }
 
