b503770b201dacf266c54f841a7d5af6fbefcaa5
Marcelo dos Santos <mdssjc@gmail.com>
2017-01-07 21:20:33 -0200

    Tipo Quick Union.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section15/QuickUnion.java
@@ -0,0 +1,33 @@
+package com.github.mdssjc.algorithms.chapter1.section15;
+
+/**
+ * Quick Union.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+public class QuickUnion extends UnionFind {
+
+  public QuickUnion(final int n) {
+    super(n);
+  }
+
+  @Override
+  public void union(final int p, final int q) {
+    final int i = find(p);
+    final int j = find(q);
+    if (i == j) {
+      return;
+    }
+    this.id[i] = j;
+    this.count--;
+  }
+
+  @Override
+  public int find(int p) {
+    while (p != this.id[p]) {
+      p = this.id[p];
+    }
+    return p;
+  }
+}
