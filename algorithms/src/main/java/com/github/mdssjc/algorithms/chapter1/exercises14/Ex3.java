b2c8956967dddbf0a2d29f2c38a3e377160d518e
Marcelo dos Santos <mdssjc@gmail.com>
2017-01-16 11:15:59 -0200

    Correção dos Plots.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises14/Ex3.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises14/Ex3.java
@@ -8,8 +8,8 @@ import edu.princeton.cs.algs4.StdOut;
 
 /**
  * Exercise 3.
- *
- * TODO: corrigir e seleção entre plots.
+ * <p>
+ * TODO: Seleção dos plots via linha de comando.
  *
  * @author Marcelo dos Santos
  *
@@ -30,34 +30,68 @@ public class Ex3 {
     xs[0] = 0;
     ys[0] = 0.0;
 
-    for (int n = BEGIN, i = 1; n <= END; n += n, i++) {
+    for (int n = BEGIN, i = 0; n <= END; n += n, i++) {
+      System.out.println(i);
       xs[i] = n;
       ys[i] = DoublingTest.timeTrial(n);
-      plot(xs, ys, i);
-      StdOut.printf("%7d %5.1f\n", n, ys[i]);
+      //plot(xs, ys, i);
+      plotLog(xs, ys, i);
+      StdOut.printf("%7d %5.5f\n", n, ys[i]);
     }
 
     StdDraw.show();
   }
 
+  // standard plot
   private static void plot(final int[] xs, final double[] ys, final int last) {
+    if (last <= 2) {
+      return;
+    }
     StdDraw.clear();
 
-    StdDraw.setXscale(0, xs[last]);
-    StdDraw.setYscale(0, ys[last]);
+    StdDraw.setXscale(xs[0], xs[last]);
+    StdDraw.setYscale(ys[0], ys[last]);
 
     for (int i = 1; i <= last; i++) {
-      // standard plot
       final double x0 = xs[i - 1];
-      double y0 = ys[i - 1];
+      final double y0 = ys[i - 1];
       final double x1 = xs[i];
-      double y1 = ys[i];
-
-      // log-log plot
-      y0 = Math.log(x0) / Math.log(y0);
-      y1 = Math.log(x1) / Math.log(y1);
+      final double y1 = ys[i];
 
       StdDraw.line(x0, y0, x1, y1);
     }
+  }
+
+  // log-log plot
+  private static void plotLog(final int[] xs, final double[] ys, final int last) {
+    if (last <= 2) {
+      return;
+    }
+    StdDraw.clear();
+
+    double b = 0;
+    if (ys.length >= 2) {
+      b = lg(ys[last] / ys[last - 1]);
+    }
+    final double a = ys[last] / Math.pow(xs[last], b);
+    final double c = lg(a);
+
+    if (a > 0) {
+      StdDraw.setXscale(lg(xs[0]), lg(xs[last]));
+      StdDraw.setYscale(b * lg(xs[0]) + c, b * lg(xs[last]) + c);
+    }
+
+    for (int i = 1; i <= last; i++) {
+      final double x0 = lg(xs[i - 1]);
+      final double y0 = b * lg(xs[i - 1]) + c;
+      final double x1 = lg(xs[i]);
+      final double y1 = b * lg(xs[i]) + c;
+
+      StdDraw.line(x0, y0, x1, y1);
+    }
+  }
+
+  private static double lg(final double x) {
+    return Math.log10(x) / Math.log10(2);
   }
 }
