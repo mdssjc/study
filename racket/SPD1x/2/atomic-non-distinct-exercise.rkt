044f61c453a226085b3cc4943868d0cfa8c6c9b2
Marcelo dos Santos <mdssjc@gmail.com>
2017-01-17 19:43:04 -0200

    Prática de Atomic Non-Distinct.

---
+++ racket/SPD1x/2/atomic-non-distinct-exercise.rkt
@@ -0,0 +1,18 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname atomic-non-distinct-exercise) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; Problem:
+; Complete the following data definition that represents the name of
+; a course by choosing the best answer for each step in the HtDD recipe.
+
+
+;; CourseName is String
+;; interp. the name of a course
+(define CN1 "Systematic Program Design")
+
+#;
+(define (fn-for-course-name cn)
+  (... cn))
+
+;; Template rules used:
+;; - atomic non-distinct: String
