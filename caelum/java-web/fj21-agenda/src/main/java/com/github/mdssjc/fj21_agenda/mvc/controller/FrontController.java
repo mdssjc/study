7b7b41a2a4b83c9541b71dc84331d41bf3b9f611
Marcelo dos Santos <mdssjc@gmail.com>
2017-01-18 14:15:51 -0200

    Refatorado para novo pacote.

---
+++ caelum/java-web/fj21-agenda/src/main/java/com/github/mdssjc/fj21_agenda/mvc/controller/FrontController.java
@@ -0,0 +1,39 @@
+package com.github.mdssjc.fj21_agenda.mvc.controller;
+
+import com.github.mdssjc.fj21_agenda.mvc.command.LogicCommand;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
+
+@WebServlet( "/do" )
+public class FrontController extends HttpServlet {
+
+  private static final long serialVersionUID = 1L;
+
+  public FrontController() {
+    super();
+  }
+
+  @Override
+  protected void service(final HttpServletRequest request, final HttpServletResponse response)
+      throws ServletException, IOException {
+    final Optional<String> parameter = Optional.ofNullable(request.getParameter("cmd"));
+    final String className = "com.github.mdssjc.fj21_agenda.mvc.command." + parameter.orElse("NullLogic");
+
+    try {
+      final LogicCommand logic = (LogicCommand) Class.forName(className)
+                                                     .newInstance();
+      final String page = logic.execute(request, response);
+
+      request.getRequestDispatcher(page)
+             .forward(request, response);
+    } catch (final Exception e) {
+      throw new ServletException(e);
+    }
+  }
+}
