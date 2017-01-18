aebe8187ee2326895c71603919322c11c5a1d675
Marcelo dos Santos <mdssjc@gmail.com>
2017-01-18 14:41:02 -0200

    Refatoração da Persistence.

---
+++ caelum/java-web/fj21-agenda/src/main/java/com/github/mdssjc/fj21_agenda/persistence/DAO.java
@@ -0,0 +1,16 @@
+package com.github.mdssjc.fj21_agenda.persistence;
+
+import java.util.List;
+
+public interface DAO<T> {
+
+  void save(T type) throws DAOException;
+
+  T findById(long id) throws DAOException;
+
+  List<T> findAll() throws DAOException;
+
+  void set(T type) throws DAOException;
+
+  void delete(T type) throws DAOException;
+}
---
+++ caelum/java-web/fj21-agenda/src/main/java/com/github/mdssjc/fj21_agenda/persistence/DAOException.java
@@ -0,0 +1,20 @@
+package com.github.mdssjc.fj21_agenda.persistence;
+
+@SuppressWarnings( "serial" )
+public class DAOException extends Exception {
+
+  public DAOException() {
+  }
+
+  public DAOException(String message) {
+    super(message);
+  }
+
+  public DAOException(Throwable cause) {
+    super(cause);
+  }
+
+  public DAOException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
