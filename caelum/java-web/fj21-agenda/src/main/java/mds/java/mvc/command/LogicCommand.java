0005d867b887a6069df05b055a2d3073c65bd57c
Marcelo dos Santos <mdssjc@gmail.com>
2017-01-18 13:26:54 -0200

    Refatoração da estrutura do projeto.

--- caelum/java-web/fj21-agenda/src/main/java/mds/java/mvc/command/LogicCommand.java
+++
@@ -1,9 +0,0 @@
-package mds.java.mvc.command;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface LogicCommand {
-    String execute(HttpServletRequest request, HttpServletResponse response)
-            throws Exception;
-}
