4820568e0d1148b19e5a8e9a8d6899498ca6246d
Marcelo dos Santos <mdssjc@gmail.com>
2017-01-18 22:23:50 -0200

    Javadoc.

--- design-patterns/src/main/java/com/github/mdssjc/dp/command/Command.java
+++ design-patterns/src/main/java/com/github/mdssjc/dp/command/Command.java
@@ -3,10 +3,10 @@ package com.github.mdssjc.dp.command;
 /**
  * Interface Command.
  * <p>
- * - execute:
- * - undo:
- * - store:
- * - load:
+ * - execute;
+ * - undo;
+ * - store; e
+ * - load.
  *
  * @author Marcelo dos Santos
  *
