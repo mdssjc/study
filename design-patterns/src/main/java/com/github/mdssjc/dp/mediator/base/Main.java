a2a7cc980f4b752f3f061ea09ae18f622b623d88
Marcelo dos Santos <mdssjc@gmail.com>
2017-01-20 21:43:33 -0200

    Stub para o Mediator.

---
+++ design-patterns/src/main/java/com/github/mdssjc/dp/mediator/base/Main.java
@@ -0,0 +1,19 @@
+package com.github.mdssjc.dp.mediator.base;
+
+/**
+ * Test drive do padrão de projeto Mediator.
+ * <p>
+ * Design Pattern
+ * Behavioral - Mediator
+ *
+ * Ao cliente: impõe a política de baixo, invisível e habilitado
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+public class Main {
+
+  public static void main(final String[] args) {
+    // TODO: implementar o padrão
+  }
+}
