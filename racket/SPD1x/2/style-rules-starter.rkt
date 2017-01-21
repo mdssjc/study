8bfaaaf50de9c4630f8731287d40b9bb33ddb606
Marcelo dos Santos <mdssjc@gmail.com>
2017-01-21 15:06:07 -0200

    Problema - HtDD P5.

---
+++ racket/SPD1x/2/style-rules-starter.rkt
@@ -0,0 +1,35 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname style-rules-starter) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+;; style-rules-starter.rkt
+
+; PROBLEM:
+; 
+; You're redesigning the SeatNum data definition from lecture, and you're not
+; sure if you've done it correctly. When you ask a TA for feedback, she tells
+; you that you haven't followed our style rules and she asks you to re-format 
+; your data definition before she gives you feedback.
+; 
+; a) Why is it important to follow style rules?
+; 
+; ;  The code is a live documentation of the project, in which all stakeholders will use.
+; 
+; 
+; b) Fix the data definition below so that it follows our style rules. Be sure to 
+; consult the style rules page so that you make ALL the required changes, of which 
+; there are quite a number.
+
+
+;; SeatNum is Natural[1,32]
+;; interp. the number of a seat in a row, seats 1 and 32 are aisle seats
+(define SN1 1)
+(define SN2 16)
+(define SN3 32)
+
+#;
+(define (fn-for-seat-num sn)
+  (... sn))
+
+;; Template rules used:
+;; - atomic non-distinct: Natural[1,32]
