4e88ec10e721dd9631220751e22cdf2ba6f97a5c
Marcelo dos Santos <mdssjc@gmail.com>
2017-01-21 23:08:12 -0200

    Simulator para o Capítulo 2.1.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter2/section21/Simulator.java
@@ -0,0 +1,17 @@
+package com.github.mdssjc.algorithms.chapter2.section21;
+
+import com.github.mdssjc.algorithms.utils.Executor;
+
+/**
+ * Simulator Class.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+public class Simulator {
+
+  public static void main(final String[] args) {
+    Executor.execute(Date.class);
+    Executor.execute(SortCompare.class);
+  }
+}
