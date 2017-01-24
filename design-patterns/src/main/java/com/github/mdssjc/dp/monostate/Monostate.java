1ad1a186f3bb9414bbdf3e477ad9b80e8e60c290
Marcelo dos Santos <mdssjc@gmail.com>
2017-01-23 22:09:10 -0200

    Padrão de Projeto Monostate.

---
+++ design-patterns/src/main/java/com/github/mdssjc/dp/monostate/Monostate.java
@@ -0,0 +1,19 @@
+package com.github.mdssjc.dp.monostate;
+
+/**
+ * Implementação do Monostate.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+public class Monostate {
+  private static int x;
+
+  public int getX() {
+    return Monostate.x;
+  }
+
+  public void setX(final int x) {
+    Monostate.x = x;
+  }
+}
---
+++ design-patterns/src/main/java/com/github/mdssjc/dp/monostate/base/Main.java
@@ -0,0 +1,22 @@
+package com.github.mdssjc.dp.monostate.base;
+
+import com.github.mdssjc.dp.monostate.Monostate;
+
+/**
+ * Test drive do padrão de projeto Monostate.
+ * <p>
+ * Design Pattern
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+public class Main {
+  public static void main(final String[] args) {
+    final Monostate m1 = new Monostate();
+    final Monostate m2 = new Monostate();
+
+    m1.setX(10);
+
+    System.out.printf("m1: %d == m2: %d%n", m1.getX(), m2.getX());
+  }
+}
