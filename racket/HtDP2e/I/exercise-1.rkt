cc4a871be586d9f2818c0c09188028466f3bec27
Marcelo dos Santos <mdssjc@gmail.com>
2017-01-27 20:00:57 -0200

    Exercício 1: distância do ponto.

---
+++ racket/HtDP2e/I/exercise-1.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; Exercise 1
+
+(define x 3)
+(define y 4)
+
+(define (distance x y)
+  (sqrt (+ (sqr x) (sqr y))))
+
+(check-expect (distance x y) 5)
+(check-expect (distance 12 5) 13)
