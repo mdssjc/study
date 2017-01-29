eda41f6aa422d40b2b4ee798fe3d5774f171acb2
Marcelo dos Santos <mdssjc@gmail.com>
2017-01-29 21:11:13 -0200

    Exercício 3.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter2/exercises22/Ex3.java
@@ -0,0 +1,38 @@
+package com.github.mdssjc.algorithms.chapter2.exercises22;
+
+import com.github.mdssjc.algorithms.sort.Sort;
+import com.github.mdssjc.algorithms.utils.Executor;
+import com.github.mdssjc.algorithms.utils.TestDrive;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+/**
+ * Exercise 3.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+@TestDrive("M E R G E S O R T E X A M P L E")
+@TestDrive("E A S Y Q U E S T I O N")
+public class Ex3 {
+
+  public static void main(final String[] args) {
+    Executor.execute(Ex3.class, args);
+
+    final String[] a = args[0].split(" ");
+
+    final String[] index = IntStream.range(0, a.length)
+                                    .boxed()
+                                    .map(String::valueOf)
+                                    .toArray(String[]::new);
+
+    StdOut.printf("%2s %2s  %s%n", "i", "j",
+                  Arrays.deepToString(index));
+    final Sort merge = new MergeSortMonitor(MergeSortMonitor.TYPE.BOTTOM_UP,
+                                            MergeSortMonitor.MONITOR.M3);
+
+    merge.sort(a);
+  }
+}
