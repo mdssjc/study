5e976792cc4ff7654e716bf2fb8f0eb12f5ac617
Marcelo dos Santos <mdssjc@gmail.com>
2017-02-01 21:54:03 -0200

    Exercício 28.

---
+++ racket/HtDP2e/I/exercise-28.rkt
@@ -0,0 +1,21 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-28) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; Exercise 28
+
+(define (profit price)
+  (- (* (+ 120
+           (* (/ 15 0.1)
+              (- 5.0 price)))
+        price)
+     (+ 180
+        (* 0.04
+           (+ 120
+              (* (/ 15 0.1)
+                 (- 5.0 price)))))))
+
+(profit 1)
+(profit 2)
+(profit 3) ; <-
+(profit 4)
+(profit 5)
