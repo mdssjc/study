c93018795ec1e0891baca8c38092e3c713bb109a
Marcelo dos Santos <mdssjc@gmail.com>
2017-02-01 21:25:13 -0200

    Exercício 21.

---
+++ racket/HtDP2e/I/exercise-21.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-21) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; Exercise 21
+
+(define (ff a) (* 10 a))
+
+(ff (+ 1 1))
