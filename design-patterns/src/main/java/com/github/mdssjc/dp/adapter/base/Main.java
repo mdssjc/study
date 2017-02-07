5e9056771d99132a6742e3e464172f36b03f9a5b
Marcelo dos Santos <mdssjc@gmail.com>
2017-02-06 22:43:43 -0200

    Padrão de Projeto Adapter (Stub).

---
+++ design-patterns/src/main/java/com/github/mdssjc/dp/adapter/base/Main.java
@@ -0,0 +1,19 @@
+package com.github.mdssjc.dp.adapter.base;
+
+/**
+ * Test drive do padrão de projeto Adapter.
+ * <p>
+ * Design Pattern
+ * Structural - Adapter (Wrapper)
+ * <p>
+ * Adaptador do objeto
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+public class Main {
+
+  public static void main(final String[] args) {
+    // TODO: implementar o padrão
+  }
+}
