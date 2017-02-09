b64097bf1bdd79113ef345b49874163f9fdf83bd
Marcelo dos Santos <mdssjc@gmail.com>
2017-02-08 22:05:29 -0200

    Padrão de Projeto Proxy (Stub).

---
+++ design-patterns/src/main/java/com/github/mdssjc/dp/proxy/base/Main.java
@@ -0,0 +1,17 @@
+package com.github.mdssjc.dp.proxy.base;
+
+/**
+ * Test drive do padrão de projeto Proxy.
+ * <p>
+ * Design Pattern
+ * Structural - Proxy (Surrogate)
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+public class Main {
+
+  public static void main(final String[] args) {
+    // TODO: implementar o padrão
+  }
+}
