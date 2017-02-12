bbb96686acd95ebbab9aae8b2134dab636e86ff3
Marcelo dos Santos <mdssjc@gmail.com>
2017-02-11 23:56:24 -0200

    Refatorado para uso do Monitor.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter2/exercises22/Ex3.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter2/exercises22/Ex3.java
@@ -2,11 +2,8 @@ package com.github.mdssjc.algorithms.chapter2.exercises22;
 
 import com.github.mdssjc.algorithms.sort.Sort;
 import com.github.mdssjc.algorithms.utils.Executor;
+import com.github.mdssjc.algorithms.utils.Monitor;
 import com.github.mdssjc.algorithms.utils.TestDrive;
-import edu.princeton.cs.algs4.StdOut;
-
-import java.util.Arrays;
-import java.util.stream.IntStream;
 
 /**
  * Exercise 3.
@@ -23,16 +20,8 @@ public class Ex3 {
 
     final String[] a = args[0].split(" ");
 
-    final String[] index = IntStream.range(0, a.length)
-                                    .boxed()
-                                    .map(String::valueOf)
-                                    .toArray(String[]::new);
-
-    StdOut.printf("%2s %2s  %s%n", "i", "j",
-                  Arrays.deepToString(index));
-    final Sort merge = new MergeSortMonitor(MergeSortMonitor.TYPE.BOTTOM_UP,
-                                            MergeSortMonitor.MONITOR.M3);
-
+    final Monitor monitor = new Monitor(Monitor.MONITOR.M3);
+    final Sort merge = new MergeSortMonitor(MergeSortMonitor.TYPE.BOTTOM_UP, monitor);
     merge.sort(a);
   }
 }
