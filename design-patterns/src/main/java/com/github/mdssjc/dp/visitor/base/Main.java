0f07cac643a6c34f051317a60b5f0f3c73da1638
Marcelo dos Santos <mdssjc@gmail.com>
2017-02-14 22:03:05 -0200

    Execução com a versão acíclica.

--- design-patterns/src/main/java/com/github/mdssjc/dp/visitor/base/Main.java
+++ design-patterns/src/main/java/com/github/mdssjc/dp/visitor/base/Main.java
@@ -1,6 +1,12 @@
 package com.github.mdssjc.dp.visitor.base;
 
 import com.github.mdssjc.dp.visitor.Visitor;
+import com.github.mdssjc.dp.visitor.acyclic.AVisitor;
+import com.github.mdssjc.dp.visitor.acyclic.concrete.AConcreteVisitor1;
+import com.github.mdssjc.dp.visitor.acyclic.concrete.AConcreteVisitor2;
+import com.github.mdssjc.dp.visitor.acyclic.element.AElement;
+import com.github.mdssjc.dp.visitor.acyclic.element.concrete.AConcreteElementA;
+import com.github.mdssjc.dp.visitor.acyclic.element.concrete.AConcreteElementB;
 import com.github.mdssjc.dp.visitor.concrete.ConcreteVisitor1;
 import com.github.mdssjc.dp.visitor.concrete.ConcreteVisitor2;
 import com.github.mdssjc.dp.visitor.element.Element;
@@ -31,6 +37,16 @@ public class Main {
     for (final Element element : elements) {
       System.out.println(element.accept(visitor1));
       System.out.println(element.accept(visitor2));
+    }
+
+    // Versão Acíclica
+    final List<AElement> aElements = Arrays.asList(new AConcreteElementA(), new AConcreteElementB());
+
+    final AVisitor aVisitor1 = new AConcreteVisitor1();
+    final AVisitor aVisitor2 = new AConcreteVisitor2();
+    for (final AElement element : aElements) {
+      System.out.println(element.accept(aVisitor1));
+      System.out.println(element.accept(aVisitor2));
     }
 
     // Versão Funcional
