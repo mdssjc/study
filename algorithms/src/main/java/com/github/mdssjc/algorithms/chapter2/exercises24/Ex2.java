d930e4a73001f61aa636113f8076006f8eec3681
Marcelo dos Santos <mdssjc@gmail.com>
2017-02-25 20:51:17 -0300

    Exercise 2.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter2/exercises24/Ex2.java
@@ -0,0 +1,16 @@
+package com.github.mdssjc.algorithms.chapter2.exercises24;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Exercise 2.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+public class Ex2 {
+
+  public static void main(final String[] args) {
+    StdOut.println("Will need to update the maximum value from scratch after a remove-the-maximum operation.");
+  }
+}
