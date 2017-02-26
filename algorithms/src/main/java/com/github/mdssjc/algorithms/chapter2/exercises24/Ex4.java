9b70b5dc12a787f3fa63f589377e95d282e2e7e3
Marcelo dos Santos <mdssjc@gmail.com>
2017-02-26 00:22:34 -0300

    Exercise 4.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter2/exercises24/Ex4.java
@@ -0,0 +1,15 @@
+package com.github.mdssjc.algorithms.chapter2.exercises24;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Exercise 4.
+ *
+ * @author Marcelo dos Santos
+ */
+public class Ex4 {
+
+  public static void main(final String[] args) {
+    StdOut.println("Yes.");
+  }
+}
