4e3426a87f54b58cb290c8801d4a2986b7c0c671
Marcelo dos Santos <mdssjc@gmail.com>
2017-02-25 21:36:39 -0300

    Transferido as características para implementação com Array.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/datastructure/priority_queue/PriorityQueue.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/datastructure/priority_queue/PriorityQueue.java
@@ -38,38 +38,4 @@ public interface PriorityQueue<Key extends Comparable<Key>> {
    * @return número de chaves
    */
   int size();
-
-  /**
-   * Retorna a Priority Queue.
-   *
-   * @return a pq
-   */
-  Key[] getPq();
-
-  default boolean less(final int i, final int j) {
-    return getPq()[i].compareTo(getPq()[j]) < 0;
-  }
-
-  default void exch(final int i, final int j) {
-    final Key t = getPq()[i];
-    getPq()[i] = getPq()[j];
-    getPq()[j] = t;
-  }
-
-  default void swim(int k) {
-    while (k > 1 && less(k / 2, k)) {
-      exch(k / 2, k);
-      k = k / 2;
-    }
-  }
-
-  default void sink(int k) {
-    while (2 * k <= size()) {
-      int j = 2 * k;
-      if (j < size() && less(j, j + 1)) j++;
-      if (!less(k, j)) break;
-      exch(k, j);
-      k = j;
-    }
-  }
 }
