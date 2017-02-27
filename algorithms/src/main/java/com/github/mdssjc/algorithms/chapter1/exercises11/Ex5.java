dd633c450f0431c26a4ad22ca794b06863f2c105
Marcelo dos Santos <mdssjc@gmail.com>
2017-02-27 13:40:04 -0300

    Melhoria no Ex 1.1.5.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises11/Ex5.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises11/Ex5.java
@@ -16,12 +16,18 @@ public class Ex5 {
   public static void main(final String[] args) {
     Executor.execute(Ex5.class, args);
 
-    StdOut.println(check(1E-6, 1E-3));
-    StdOut.println(check(0, 1));
-    StdOut.println(check(0.5, 0.5));
+    StdOut.println(isBetween0and1(1E-6, 1E-3));
+    StdOut.println(isBetween0and1(0, 0));
+    StdOut.println(isBetween0and1(0, 1));
+    StdOut.println(isBetween0and1(0.5, 0.5));
+
+    StdOut.println(isBetween0and1(-0.1, 0.5));
+    StdOut.println(isBetween0and1(0.5, -0.1));
+    StdOut.println(isBetween0and1(1.1, 0.5));
+    StdOut.println(isBetween0and1(0.5, 1.1));
   }
 
-  private static boolean check(final double x, final double y) {
-    return (x > 0.0 && x < 1.0) && (y > 0.0 && y < 1.0);
+  private static boolean isBetween0and1(final double x, final double y) {
+    return (x >= 0.0) && (x <= 1.0) && (y >= 0.0) && (y <= 1.0);
   }
 }
