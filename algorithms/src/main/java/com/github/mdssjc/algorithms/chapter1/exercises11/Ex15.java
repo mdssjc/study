d105dd3071d4763d4e3f2562c18455f98ab6a647
Marcelo dos Santos <mdssjc@gmail.com>
2017-02-28 14:19:33 -0300

    Melhoria no Ex 1.1.15.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises11/Ex15.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises11/Ex15.java
@@ -4,35 +4,42 @@ import com.github.mdssjc.algorithms.utils.Executor;
 import com.github.mdssjc.algorithms.utils.TestDrive;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.Arrays;
+
 /**
  * Exercise 15.
- * 
+ *
  * @author Marcelo dos Santos
  *
  */
-@TestDrive
+@TestDrive("0 2 2 1 1 1")
 public class Ex15 {
 
   public static void main(final String[] args) {
     Executor.execute(Ex15.class, args);
 
-    final int[] a = { 0, 2, 2, 1, 1, 1 };
+    final int[] a = Arrays.stream(args[0].split(" "))
+                          .mapToInt(Integer::valueOf)
+                          .toArray();
+    final int[] h = histogram(a, a.length);
 
-    for (final int value : histogram(a, a.length)) {
-      StdOut.print(value + " ");
-    }
+    StdOut.println(Arrays.toString(h));
+
+    final int sum = Arrays.stream(h)
+                          .sum();
+    StdOut.printf("Sum: %d%nLength: %d%n", sum, a.length);
   }
 
-  private static int[] histogram(final int[] a, final int length) {
-    final int[] res = new int[length];
+  private static int[] histogram(final int[] a, final int m) {
+    final int[] result = new int[m];
 
-    for (int i = 0; i < res.length; i++) {
-      if (a[i] < 0 || a[i] > length - 1) {
+    for (int i = 0; i < result.length; i++) {
+      if (a[i] < 0 || a[i] > m - 1) {
         throw new IllegalArgumentException();
       }
-      res[a[i]]++;
+      result[a[i]]++;
     }
 
-    return res;
+    return result;
   }
 }
