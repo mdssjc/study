df6ecdb2018242db077308cb37f34ab99299016a
Marcelo dos Santos <mdssjc@gmail.com>
2017-03-01 14:21:43 -0300

    Melhoria no Ex 1.1.26.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises11/CEx26.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises11/CEx26.java
@@ -2,7 +2,7 @@ package com.github.mdssjc.algorithms.chapter1.exercises11;
 
 import com.github.mdssjc.algorithms.utils.Executor;
 import com.github.mdssjc.algorithms.utils.TestDrive;
-import edu.princeton.cs.algs4.StdArrayIO;
+import edu.princeton.cs.algs4.StdOut;
 
 import java.util.Arrays;
 
@@ -14,19 +14,23 @@ import java.util.Arrays;
  * @author Marcelo dos Santos
  *
  */
-@TestDrive
+@TestDrive({"3 1 8"})
 public class CEx26 {
 
   public static void main(final String[] args) {
     Executor.execute(CEx26.class, args);
 
-    final int[] vector = new int[]{3, 1, 8};
+    final int[] numbers = Arrays.stream(args[0].split(" "))
+                               .mapToInt(Integer::valueOf)
+                               .toArray();
 
-    final int[] sorted = sort(vector);
-    StdArrayIO.print(sorted);
+    StdOut.printf("Values: %s%n", Arrays.toString(numbers));
 
-    Arrays.sort(vector);
-    StdArrayIO.print(vector);
+    final int[] sorted = sort(numbers);
+    StdOut.printf("Sorted: %s%n", Arrays.toString(sorted));
+
+    Arrays.sort(numbers);
+    StdOut.printf("Reference: %s%n", Arrays.toString(numbers));
   }
 
   private static int[] sort(final int[] vector) {
@@ -35,16 +39,19 @@ public class CEx26 {
     int c = vector[2];
 
     int t;
+
     if (a > b) {
       t = a;
       a = b;
       b = t;
     }
+
     if (a > c) {
       t = a;
       a = c;
       c = t;
     }
+
     if (b > c) {
       t = b;
       b = c;
