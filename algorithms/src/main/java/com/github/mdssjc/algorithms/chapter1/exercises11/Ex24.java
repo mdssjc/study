4ed2c580047e3a548bd5bc2a7564d21eaa39ee63
Marcelo dos Santos <mdssjc@gmail.com>
2017-03-01 14:11:10 -0300

    Melhoria no Ex 1.1.24.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises11/Ex24.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises11/Ex24.java
@@ -10,17 +10,17 @@ import edu.princeton.cs.algs4.StdOut;
  * @author Marcelo dos Santos
  *
  */
-@TestDrive( {"105", "24"} )
-@TestDrive( {"1111111", "1234567"} )
+@TestDrive({"105", "24"})
+@TestDrive({"1111111", "1234567"})
 public class Ex24 {
 
   public static void main(final String[] args) {
     Executor.execute(Ex24.class, args);
 
-    final int arg1 = Integer.parseInt(args[0]);
-    final int arg2 = Integer.parseInt(args[1]);
+    final int p = Integer.parseInt(args[0]);
+    final int q = Integer.parseInt(args[1]);
 
-    StdOut.println("gcd: " + gcd(arg1, arg2));
+    StdOut.println("gcd: " + gcd(p, q));
   }
 
   private static int gcd(final int p, final int q) {
