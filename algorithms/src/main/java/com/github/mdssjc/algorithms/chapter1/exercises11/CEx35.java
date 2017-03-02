41807cbdea398219b03c09b64e39af39177c0121
Marcelo dos Santos <mdssjc@gmail.com>
2017-03-02 14:32:16 -0300

    Melhoria no Ex 1.1.35.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises11/CEx35.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises11/CEx35.java
@@ -31,12 +31,11 @@ public class CEx35 {
     }
 
     print(dist);
-
     run(SIDES, dist);
   }
 
   private static void run(final int sides, final double[] dist) {
-    final double[] frequencies = new double[2 * sides + 1];
+    final double[] frequency = new double[2 * sides + 1];
     boolean more = true;
     int n = 0;
 
@@ -44,20 +43,20 @@ public class CEx35 {
       final int dice1 = StdRandom.uniform(sides) + 1;
       final int dice2 = StdRandom.uniform(sides) + 1;
 
+      frequency[dice1 + dice2]++;
       n++;
-      frequencies[dice1 + dice2]++;
 
-      if (n % 10 == 0 && verify(distribution(frequencies, n), dist)) {
+      if (n % 10 == 0 && match(distribution(frequency, n), dist)) {
         more = false;
         StdOut.printf("%d times:%n", n);
-        print(distribution(frequencies, n));
+        print(distribution(frequency, n));
       }
     }
   }
 
-  private static boolean verify(final double[] distributions, final double[] dist) {
-    for (int i = 2; i < distributions.length; i++) {
-      final int value1 = (int) (distributions[i] * 1000.0);
+  private static boolean match(final double[] distribution, final double[] dist) {
+    for (int i = 2; i < distribution.length; i++) {
+      final int value1 = (int) (distribution[i] * 1000.0);
       final int value2 = (int) (dist[i] * 1000.0);
       if (value1 != value2) {
         return false;
@@ -66,17 +65,17 @@ public class CEx35 {
     return true;
   }
 
-  private static double[] distribution(final double[] frequencies, final int n) {
-    final double[] distributions = new double[frequencies.length];
-    for (int i = 2; i < frequencies.length; i++) {
-      distributions[i] = frequencies[i] / n;
+  private static double[] distribution(final double[] frequency, final int n) {
+    final double[] distribution = new double[frequency.length];
+    for (int i = 2; i < frequency.length; i++) {
+      distribution[i] = frequency[i] / n;
     }
-    return distributions;
+    return distribution;
   }
 
-  private static void print(final double[] distributions) {
-    for (int i = 2; i < distributions.length; i++) {
-      StdOut.printf("%d: %.3f%% ", i, distributions[i] - 0.0005);
+  private static void print(final double[] distribution) {
+    for (int i = 2; i < distribution.length; i++) {
+      StdOut.printf("%d: %.3f%% ", i, distribution[i] - 0.0005);
     }
     StdOut.println();
   }
