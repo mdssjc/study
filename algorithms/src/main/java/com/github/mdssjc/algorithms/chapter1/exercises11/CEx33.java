39212def6e71eaee10705c234572b7c2e2cebe91
Marcelo dos Santos <mdssjc@gmail.com>
2017-03-02 12:08:53 -0300

    Melhoria no Ex 1.1.33.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises11/CEx33.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises11/CEx33.java
@@ -3,6 +3,7 @@ package com.github.mdssjc.algorithms.chapter1.exercises11;
 import com.github.mdssjc.algorithms.utils.Executor;
 import com.github.mdssjc.algorithms.utils.TestDrive;
 import edu.princeton.cs.algs4.StdArrayIO;
+import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 /**
@@ -13,31 +14,37 @@ import edu.princeton.cs.algs4.StdOut;
  * @author Marcelo dos Santos
  *
  */
-@TestDrive
+@TestDrive(input = {"1 2 3 4 5 6 9 1 3 1 2 3"})
 public class CEx33 {
+
+  private static final int LENGTH = 3;
 
   public static void main(final String[] args) {
     Executor.execute(CEx33.class, args);
 
-    final double[][] matrix = {{1, 2, 3}, {4, 5, 6}, {9, 1, 3}};
-    final double[] vector = {1, 2, 3};
+    final double[][] matrix = new double[LENGTH][LENGTH];
+    final double[] vector = new double[LENGTH];
+
+    StdOut.printf("Enter values to [%1$dx%1$d] and [%1$d]%n", LENGTH);
+    while (!StdIn.isEmpty()) {
+      for (int i = 0; i < matrix.length; i++) {
+        for (int j = 0; j < matrix[i].length; j++) {
+          matrix[i][j] = StdIn.readDouble();
+        }
+      }
+
+      for (int i = 0; i < vector.length; i++) {
+        vector[i] = StdIn.readDouble();
+      }
+    }
 
     StdArrayIO.print(matrix);
     StdArrayIO.print(vector);
 
-    final double dot = Matrix.dot(vector, vector);
-    StdOut.println(dot);
-
-    final double[][] mult = Matrix.mult(matrix, matrix);
-    StdArrayIO.print(mult);
-
-    final double[][] transpose = Matrix.transpose(matrix);
-    StdArrayIO.print(transpose);
-
-    final double[] multmv = Matrix.mult(matrix, vector);
-    StdArrayIO.print(multmv);
-
-    final double[] multvm = Matrix.mult(vector, matrix);
-    StdArrayIO.print(multvm);
+    StdOut.println(Matrix.dot(vector, vector));
+    StdArrayIO.print(Matrix.mult(matrix, matrix));
+    StdArrayIO.print(Matrix.transpose(matrix));
+    StdArrayIO.print(Matrix.mult(matrix, vector));
+    StdArrayIO.print(Matrix.mult(vector, matrix));
   }
 }
