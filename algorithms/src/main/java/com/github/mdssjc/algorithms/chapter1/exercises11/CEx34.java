38c37ab716b92189649b9c8f88157734c145fcd6
Marcelo dos Santos <mdssjc@gmail.com>
2017-03-02 13:22:44 -0300

    Melhoria no Ex 1.1.34.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises11/CEx34.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises11/CEx34.java
@@ -19,15 +19,15 @@ public class CEx34 {
     Executor.execute(CEx34.class, args);
 
     StdOut.println("Array: ");
-    StdOut.println("Print the k th smallest value, for k less than 100.");
+    StdOut.println("Print the median of the numbers.");
+    StdOut.println("Print the percentage of numbers greater than the average.");
     StdOut.println("Print the N numbers in increasing order.");
     StdOut.println("Print the N numbers in random order.");
 
     StdOut.println("Filter: ");
     StdOut.println("Print the maximum and minimum numbers.");
-    StdOut.println("Print the median of the numbers.");
+    StdOut.println("Print the kth smallest value, for k less than 100.");
     StdOut.println("Print the sum of the squares of the numbers.");
     StdOut.println("Print the average of the N numbers.");
-    StdOut.println("Print the percentage of numbers greater than the average.");
   }
 }
