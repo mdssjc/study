48937275243f4e8c5f329ccf230bf19a7d10b917
Marcelo dos Santos <mdssjc@gmail.com>
2017-03-03 14:52:26 -0300

    Melhoria no Ex 1.1.40.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises11/WEx40.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises11/WEx40.java
@@ -5,6 +5,8 @@ import com.github.mdssjc.algorithms.utils.TestDrive;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
+import java.util.Arrays;
+
 /**
  * Web Exercise 40.
  * <p>
@@ -13,7 +15,7 @@ import edu.princeton.cs.algs4.StdRandom;
  * @author Marcelo dos Santos
  *
  */
-@TestDrive( "10" )
+@TestDrive("10")
 public class WEx40 {
 
   public static void main(final String[] args) {
@@ -24,19 +26,17 @@ public class WEx40 {
     final int[] items = new int[n];
     init(items);
 
-    print(items);
+    StdOut.println(Arrays.toString(items));
     sattoloCycle(items);
-    print(items);
+    StdOut.println(Arrays.toString(items));
   }
 
-  private static void sattoloCycle(final int[] items) {
-    int i = items.length;
-    while (i > 1) {
-      i--;
-      final int j = StdRandom.uniform(i);
-      final int swap = items[j];
-      items[j] = items[i - 1];
-      items[i - 1] = swap;
+  private static void sattoloCycle(final int[] a) {
+    for (int i = a.length; i > 1; i--) {
+      final int r = StdRandom.uniform(i);
+      final int swap = a[r];
+      a[r] = a[i - 1];
+      a[i - 1] = swap;
     }
   }
 
@@ -44,12 +44,5 @@ public class WEx40 {
     for (int i = 0; i < a.length; i++) {
       a[i] = StdRandom.uniform(a.length);
     }
-  }
-
-  private static void print(final int[] items) {
-    for (final int x : items) {
-      StdOut.print(x + " ");
-    }
-    StdOut.println();
   }
 }
