ae5b1ec6890d99d0b96b0a243f6b6a4d6ef70310
Marcelo dos Santos <mdssjc@gmail.com>
2017-03-05 20:24:59 -0300

    Classe de composição da Linked List Symbol Table.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/datastructure/linkedlist/NodeST.java
@@ -0,0 +1,21 @@
+package com.github.mdssjc.algorithms.datastructure.linkedlist;
+
+import lombok.Data;
+
+/**
+ * Classe de composição da Linked List Symbol Table.
+ *
+ * @author Marcelo dos Santos
+ *
+ * @param <Key>
+ *     O tipo de dado da chave
+ * @param <Value>
+ *     O tipo de dado do valor
+ */
+@Data
+public class NodeST<Key, Value> {
+
+  public Key key;
+  public Value value;
+  public Node next;
+}
