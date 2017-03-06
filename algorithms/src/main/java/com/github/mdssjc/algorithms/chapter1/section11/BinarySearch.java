e7c8808d9c5e21700ea98a2cac6425749824318a
Marcelo dos Santos <mdssjc@gmail.com>
2017-03-06 14:19:31 -0300

    Javadoc, formatação e substituição de método depreciado.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section11/BinarySearch.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section11/BinarySearch.java
@@ -9,22 +9,20 @@ import edu.princeton.cs.algs4.StdOut;
 import java.util.Arrays;
 
 /**
- * BinarySearch Class.
+ * Binary Search algorithm.
  *
  * @author Marcelo dos Santos
  *
  */
-@TestDrive( value = "largeW.txt", valueFile = true, input = "largeT.txt", inputFile = true )
-@TestDrive( value = "tinyW.txt", valueFile = true, input = "tinyT.txt", inputFile = true )
+@TestDrive(value = "largeW.txt", valueFile = true, input = "largeT.txt", inputFile = true)
+@TestDrive(value = "tinyW.txt", valueFile = true, input = "tinyT.txt", inputFile = true)
 public class BinarySearch {
 
   public static int rank(final int key, final int[] a) {
-    // Array must be sorted.
     int lo = 0;
     int hi = a.length - 1;
 
     while (lo <= hi) {
-      // Key is in a[lo..hi] or not present.
       final int mid = lo + (hi - lo) / 2;
       if (key < a[mid]) {
         hi = mid - 1;
@@ -40,12 +38,11 @@ public class BinarySearch {
   public static void main(final String[] args) {
     Executor.execute(BinarySearch.class, args);
 
-    final int[] whitelist = In.readInts(args[0]);
+    final int[] whitelist = new In(args[0]).readAllInts();
 
     Arrays.sort(whitelist);
 
     while (!StdIn.isEmpty()) {
-      // Read key, print if not in whitelist.
       final int key = StdIn.readInt();
       if (rank(key, whitelist) == -1) {
         StdOut.println(key);
