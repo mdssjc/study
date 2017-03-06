bc8deefcc5804ea5e5f869266982706ebb44cf49
Marcelo dos Santos <mdssjc@gmail.com>
2017-03-06 14:46:14 -0300

    Refatoração e documentação.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section11/Average.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section11/Average.java
@@ -6,26 +6,25 @@ import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 /**
- * Average Class.
+ * Average - Sample StdOut client.
  *
  * @author Marcelo dos Santos
  *
  */
-@TestDrive( input = "1.23 2.34 3.45 4.56" )
+@TestDrive(input = "1.23456 2.34567 3.45678 4.56789")
 public class Average {
 
   public static void main(final String[] args) {
     Executor.execute(Average.class, args);
 
-    // Average the numbers on StdIn.
     double sum = 0.0;
     int cnt = 0;
+
     while (!StdIn.isEmpty()) {
-      // Read a number and cumulate the sum.
       sum += StdIn.readDouble();
       cnt++;
     }
     final double avg = sum / cnt;
-    StdOut.printf("Average is %.5f\n", avg);
+    StdOut.printf("Average is %.5f%n", avg);
   }
 }
