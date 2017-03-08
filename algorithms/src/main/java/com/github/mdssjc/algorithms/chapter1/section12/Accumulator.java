0289cc38870c69d6e2ca4d9afae1a7d884ba8ade
Marcelo dos Santos <mdssjc@gmail.com>
2017-03-08 14:23:25 -0300

    Refatoração do Accumulator.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section12/Accumulator.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section12/Accumulator.java
@@ -2,13 +2,12 @@ package com.github.mdssjc.algorithms.chapter1.section12;
 
 import com.github.mdssjc.algorithms.utils.Executor;
 import com.github.mdssjc.algorithms.utils.TestDrive;
-
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
 /**
- * Accumulator Class.
- * 
+ * Accumulator data type.
+ *
  * @author Marcelo dos Santos
  *
  */
@@ -18,34 +17,34 @@ import edu.princeton.cs.algs4.StdRandom;
 public class Accumulator {
 
   private double total;
-  private int    N;
-
-  public void addDataValue(final double val) {
-    this.N++;
-    this.total += val;
-  }
-
-  public double mean() {
-    return this.total / this.N;
-  }
-
-  public int getN() {
-    return this.N;
-  }
-
-  @Override
-  public String toString() {
-    return "Mean (" + this.N + " values): " + String.format("%7.5f", mean());
-  }
+  private int n;
 
   public static void main(final String[] args) {
     Executor.execute(Accumulator.class, args);
 
-    final int T = Integer.parseInt(args[0]);
+    final int trails = Integer.parseInt(args[0]);
     final Accumulator a = new Accumulator();
-    for (int i = 0; i < T; i++) {
-      a.addDataValue(StdRandom.random());
+    for (int i = 0; i < trails; i++) {
+      a.addDataValue(StdRandom.uniform());
     }
     StdOut.println(a);
+  }
+
+  public void addDataValue(final double val) {
+    this.n++;
+    this.total += val;
+  }
+
+  public double mean() {
+    return this.total / this.n;
+  }
+
+  public int getN() {
+    return this.n;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("Mean (%d values): %7.5f", n, mean());
   }
 }
