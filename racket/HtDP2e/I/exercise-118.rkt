000f53d828ffaa5cd64bdffb6495260e22ed6685
Marcelo dos Santos <mdssjc@gmail.com>
2017-03-08 20:18:51 -0300

    Exercício 118.

---
+++ racket/HtDP2e/I/exercise-118.rkt
@@ -0,0 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-118) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; Exercise 118
+
+; (define (f x) x)
+; def -> variable variable
+;   variable (inner)
+
+; (define (f x) y)
+; def -> variable variable
+;   variable (outer)
+
+; (define (f x y) 3)
+; def -> variable variable variable
+;   value
