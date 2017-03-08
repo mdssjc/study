862ae59e6a855a1393383a6851d20a552cb22af0
Marcelo dos Santos <mdssjc@gmail.com>
2017-03-08 14:55:00 -0300

    Refatoração do Evaluate.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section13/Evaluate.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section13/Evaluate.java
@@ -7,15 +7,13 @@ import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 /**
- * Evaluate Class.
- * <p>
  * Dijkstra’s two-Stack Algorithm for expression evaluation.
  *
  * @author Marcelo dos Santos
  *
  */
-@TestDrive( input = "( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) )" )
-@TestDrive( input = "( ( 1 + sqrt ( 5.0 ) ) / 2.0 )" )
+@TestDrive(input = "( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) )")
+@TestDrive(input = "( ( 1 + sqrt ( 5.0 ) ) / 2.0 )")
 public class Evaluate {
 
   public static void main(final String[] args) {
@@ -24,42 +22,53 @@ public class Evaluate {
     final Stack<String> ops = new Stack<>();
     final Stack<Double> vals = new Stack<>();
 
-    while (!StdIn.isEmpty()) { // Read token, push if operator.
+    while (!StdIn.isEmpty()) {
       final String s = StdIn.readString();
-      if (s.equals("(")) {
-        ;
-      } else if (s.equals("+")) {
-        ops.push(s);
-      } else if (s.equals("-")) {
-        ops.push(s);
-      } else if (s.equals("*")) {
-        ops.push(s);
-      } else if (s.equals("/")) {
-        ops.push(s);
-      } else if (s.equals("sqrt")) {
-        ops.push(s);
-      } else if (s.equals(")")) { // Pop, evaluate, and push result if token is
-        // ")".
-        final String op = ops.pop();
-        double v = vals.pop();
-        if (op.equals("+")) {
-          v = vals.pop() + v;
-        } else if (op.equals("-")) {
-          v = vals.pop() - v;
-        } else if (op.equals("*")) {
-          v = vals.pop() * v;
-        } else if (op.equals("/")) {
-          v = vals.pop() / v;
-        } else if (op.equals("sqrt")) {
-          v = Math.sqrt(v);
-        }
-        vals.push(v);
-      } // Token not operator or paren: push double value.
-      else {
-        vals.push(Double.parseDouble(s));
+      switch (s) {
+        case "(":
+          break;
+        case "+":
+          ops.push(s);
+          break;
+        case "-":
+          ops.push(s);
+          break;
+        case "*":
+          ops.push(s);
+          break;
+        case "/":
+          ops.push(s);
+          break;
+        case "sqrt":
+          ops.push(s);
+          break;
+        case ")":
+          final String op = ops.pop();
+          double v = vals.pop();
+          switch (op) {
+            case "+":
+              v = vals.pop() + v;
+              break;
+            case "-":
+              v = vals.pop() - v;
+              break;
+            case "*":
+              v = vals.pop() * v;
+              break;
+            case "/":
+              v = vals.pop() / v;
+              break;
+            case "sqrt":
+              v = Math.sqrt(v);
+              break;
+          }
+          vals.push(v);
+          break;
+        default:
+          vals.push(Double.parseDouble(s));
+          break;
       }
     }
-
     StdOut.println(vals.pop());
   }
 }
