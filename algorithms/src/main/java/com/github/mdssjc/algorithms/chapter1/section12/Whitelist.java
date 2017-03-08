59499bd7a091c439a76746cbf65a88d95ac8dd1e
Marcelo dos Santos <mdssjc@gmail.com>
2017-03-08 14:32:47 -0300

    Refatoração do Whitelist.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section12/Whitelist.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section12/Whitelist.java
@@ -1,22 +1,27 @@
 package com.github.mdssjc.algorithms.chapter1.section12;
 
+import com.github.mdssjc.algorithms.utils.Executor;
+import com.github.mdssjc.algorithms.utils.TestDrive;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 /**
- * Whitelist Class - StaticSETofInts Client.
- * 
+ * StaticSETofInts client.
+ *
  * @author Marcelo dos Santos
  *
  */
+@TestDrive(value = "largeW.txt", valueFile = true, input = "largeT.txt", inputFile = true)
 public class Whitelist {
 
   public static void main(final String[] args) {
-    final int[] w = In.readInts(args[0]);
+    Executor.execute(Whitelist.class, args);
+
+    final int[] w = new In(args[0]).readAllInts();
     final StaticSETofInts set = new StaticSETofInts(w);
+
     while (!StdIn.isEmpty()) {
-      // Read key, print if not in whitelist.
       final int key = StdIn.readInt();
       if (!set.contains(key)) {
         StdOut.println(key);
