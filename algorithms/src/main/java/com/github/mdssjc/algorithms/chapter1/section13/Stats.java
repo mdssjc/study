f75f1d5e01fb2db79b9e5f3272fe9d043a4d2797
Marcelo dos Santos <mdssjc@gmail.com>
2017-03-08 14:46:32 -0300

    Refatoração do Stats.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section13/Stats.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section13/Stats.java
@@ -1,5 +1,6 @@
 package com.github.mdssjc.algorithms.chapter1.section13;
 
+import com.github.mdssjc.algorithms.utils.Executor;
 import com.github.mdssjc.algorithms.utils.TestDrive;
 import edu.princeton.cs.algs4.Bag;
 import edu.princeton.cs.algs4.StdIn;
@@ -11,31 +12,32 @@ import edu.princeton.cs.algs4.StdOut;
  * @author Marcelo dos Santos
  *
  */
-@TestDrive( input = "100 99 101 120 98 107 109 81 101 90" )
+@TestDrive(input = "100 99 101 120 98 107 109 81 101 90")
 public class Stats {
 
   public static void main(final String[] args) {
+    Executor.execute(Stats.class, args);
+
     final Bag<Double> numbers = new Bag<>();
 
     while (!StdIn.isEmpty()) {
       numbers.add(StdIn.readDouble());
     }
+    final int n = numbers.size();
 
-    final int N = numbers.size();
     double sum = 0.0;
     for (final double x : numbers) {
       sum += x;
     }
+    final double mean = sum / n;
 
-    final double mean = sum / N;
     sum = 0.0;
     for (final double x : numbers) {
       sum += (x - mean) * (x - mean);
     }
+    final double std = Math.sqrt(sum / (n - 1));
 
-    final double std = Math.sqrt(sum / (N - 1));
-
-    StdOut.printf("Mean: %.2f\n", mean);
-    StdOut.printf("Std dev: %.2f\n", std);
+    StdOut.printf("Mean: %.2f%n", mean);
+    StdOut.printf("Std dev: %.2f%n", std);
   }
 }
