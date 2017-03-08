be72ef5e2c5c15e934ec2809820b432b72cea838
Marcelo dos Santos <mdssjc@gmail.com>
2017-03-08 14:00:07 -0300

    Refatoração do Flips.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section12/Flips.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section12/Flips.java
@@ -6,23 +6,25 @@ import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
 /**
- * Flips Class.
+ * Counter client that simulates t coin flips.
  *
  * @author Marcelo dos Santos
  *
  */
-@TestDrive( "1000" )
+@TestDrive("10")
+@TestDrive("10")
+@TestDrive("1000000")
 public class Flips {
 
   public static void main(final String[] args) {
     Executor.execute(Flips.class, args);
 
-    final int T = Integer.parseInt(args[0]);
+    final int trails = Integer.parseInt(args[0]);
 
     final Counter heads = new Counter("heads");
     final Counter tails = new Counter("tails");
 
-    for (int t = 0; t < T; t++) {
+    for (int t = 0; t < trails; t++) {
       if (StdRandom.bernoulli(0.5)) {
         heads.increment();
       } else {
@@ -30,10 +32,7 @@ public class Flips {
       }
     }
 
-    StdOut.println(heads);
-    StdOut.println(tails);
-
     final int d = heads.tally() - tails.tally();
-    StdOut.println("delta: " + Math.abs(d));
+    StdOut.printf("%s%n%s%ndelta: %d%n", heads, tails, Math.abs(d));
   }
 }
