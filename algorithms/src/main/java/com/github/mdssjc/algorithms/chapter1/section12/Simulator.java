d7ff9137123030599b981d7c12a553a621052e6d
Marcelo dos Santos <mdssjc@gmail.com>
2017-03-08 14:35:41 -0300

    Inclusão dos Labels.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section12/Simulator.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section12/Simulator.java
@@ -4,20 +4,21 @@ import com.github.mdssjc.algorithms.utils.Executor;
 
 /**
  * Simulator Class.
- * 
+ *
  * @author Marcelo dos Santos
- * 
+ *
  */
 public class Simulator {
 
   public static void main(final String[] args) {
-    Executor.execute(Counter.class);
-    Executor.execute(Flips.class);
-    Executor.execute(FlipsMax.class);
-    Executor.execute(Rolls.class);
-    Executor.execute(BasicDate.class);
-    Executor.execute(SmallDate.class);
-    Executor.execute(Accumulator.class);
-    Executor.execute(VisualAccumulator.class);
+    Executor.execute(Flips.class, "Counter client that simulates t coin flips");
+    Executor.execute(FlipsMax.class, "Example of a static method with object arguments and return values");
+    Executor.execute(Rolls.class, "Counter client that simulates T rolls of a die");
+    Executor.execute(Counter.class, "Counter data type");
+    Executor.execute(BasicDate.class, "Basic Date: implementation");
+    Executor.execute(SmallDate.class, "Small Date: alternate implementation");
+    Executor.execute(Accumulator.class, "Accumulator data type");
+    Executor.execute(VisualAccumulator.class, "Visual Accumulator data type");
+    Executor.execute(Whitelist.class, "StaticSETofInts client");
   }
 }
