88c52d8edfbc8ede085419c894d2a22082f4df1e
Marcelo dos Santos <mdssjc@gmail.com>
2017-03-09 14:45:26 -0300

    Date client.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section12/Simulator.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section12/Simulator.java
@@ -21,6 +21,7 @@ public class Simulator {
     Executor.execute(VectorClient.class, "Euclidean vector client");
     Executor.execute(BasicDate.class, "Basic Date: implementation");
     Executor.execute(SmallDate.class, "Small Date: alternate implementation");
+    Executor.execute(DateClient.class, "Date client");
     Executor.execute(Accumulator.class, "Accumulator data type");
     Executor.execute(VisualAccumulator.class, "Visual Accumulator data type");
   }
