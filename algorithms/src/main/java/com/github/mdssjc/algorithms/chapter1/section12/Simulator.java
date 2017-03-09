8170c7077f580a3cfa7a0299a9940ef3dc2c977b
Marcelo dos Santos <mdssjc@gmail.com>
2017-03-09 14:48:00 -0300

    Transaction client.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section12/Simulator.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section12/Simulator.java
@@ -22,6 +22,7 @@ public class Simulator {
     Executor.execute(BasicDate.class, "Basic Date: implementation");
     Executor.execute(SmallDate.class, "Small Date: alternate implementation");
     Executor.execute(DateClient.class, "Date client");
+    Executor.execute(TransactionClient.class, "Transaction client");
     Executor.execute(Accumulator.class, "Accumulator data type");
     Executor.execute(VisualAccumulator.class, "Visual Accumulator data type");
   }
