1df763e403f8c4d60dc6711036db009aa88461b7
Marcelo dos Santos <mdssjc@gmail.com>
2017-03-09 15:05:13 -0300

    Interval2D client.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section12/Simulator.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section12/Simulator.java
@@ -25,6 +25,7 @@ public class Simulator {
     Executor.execute(TransactionClient.class, "Transaction client");
     Executor.execute(Point2DClient.class, "Point2D client");
     Executor.execute(Interval1DClient.class, "Interval1D client");
+    Executor.execute(Interval2DClient.class, "Interval2D client");
     Executor.execute(Accumulator.class, "Accumulator data type");
     Executor.execute(VisualAccumulator.class, "Visual Accumulator data type");
   }
