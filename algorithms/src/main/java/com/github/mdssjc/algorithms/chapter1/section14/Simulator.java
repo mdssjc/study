8e62764749c1eafd80f65c36b4ea8bf81bd72350
Marcelo dos Santos <mdssjc@gmail.com>
2017-03-10 14:13:35 -0300

    Refatoração do ThreeSumFast.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section14/Simulator.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section14/Simulator.java
@@ -15,7 +15,7 @@ public class Simulator {
     Executor.execute(Stopwatch.class, "Timer (wall time)");
     Executor.execute(DoublingTest.class, "Doubling test");
     Executor.execute(TwoSumFast.class, "Two sum fast");
-    Executor.execute(ThreeSumFast.class);
+    Executor.execute(ThreeSumFast.class, "Faster three sum");
     Executor.execute(DoublingRatio.class);
   }
 }
--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section14/ThreeSumFast.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section14/ThreeSumFast.java
@@ -9,37 +9,38 @@ import edu.princeton.cs.algs4.StdOut;
 import java.util.Arrays;
 
 /**
- * ThreeSumFast Class.
- * <p>
- * Count triples that sum to 0.
+ * Faster three sum.
  *
  * @author Marcelo dos Santos
  *
  */
-@TestDrive( value = "1Kints.txt", valueFile = true )
-@TestDrive( value = "2Kints.txt", valueFile = true )
-@TestDrive( value = "4Kints.txt", valueFile = true )
-@TestDrive( value = "8Kints.txt", valueFile = true )
+@TestDrive(value = "1Kints.txt", valueFile = true)
+@TestDrive(value = "2Kints.txt", valueFile = true)
+@TestDrive(value = "4Kints.txt", valueFile = true)
+@TestDrive(value = "8Kints.txt", valueFile = true)
+@TestDrive(value = "16Kints.txt", valueFile = true)
+@TestDrive(value = "32Kints.txt", valueFile = true)
+@TestDrive(value = "1Mints.txt", valueFile = true)
 public class ThreeSumFast {
-
-  public static int count(final int[] a) {
-    Arrays.sort(a);
-    final int n = a.length;
-    int cnt = 0;
-    for (int i = 0; i < n; i++) {
-      for (int j = i + 1; j < n; j++) {
-        if (BinarySearch.rank(-a[i]-a[j], a) > j) {
-          cnt++;
-        }
-      }
-    }
-    return cnt;
-  }
 
   public static void main(final String[] args) {
     Executor.execute(ThreeSumFast.class, args);
 
-    final int[] a = In.readInts(args[0]);
+    final int[] a = new In(args[0]).readAllInts();
     StdOut.println(count(a));
+  }
+
+  public static int count(final int[] a) {
+    Arrays.sort(a);
+    final int n = a.length;
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+      for (int j = i + 1; j < n; j++) {
+        if (BinarySearch.indexOf(a, -a[i] - a[j]) > j) {
+          count++;
+        }
+      }
+    }
+    return count;
   }
 }
