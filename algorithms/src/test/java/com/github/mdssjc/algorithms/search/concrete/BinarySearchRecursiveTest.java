93ea58931126e0c2d35e5706b10133368ade8793
Marcelo dos Santos <mdssjc@gmail.com>
2017-03-09 22:01:55 -0300

    Teste do Binary Search Recursive.

---
+++ algorithms/src/test/java/com/github/mdssjc/algorithms/search/concrete/BinarySearchRecursiveTest.java
@@ -0,0 +1,50 @@
+package com.github.mdssjc.algorithms.search.concrete;
+
+import com.github.mdssjc.algorithms.search.BinarySearch;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Teste do Binary Search Recursive.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+public class BinarySearchRecursiveTest {
+
+  private BinarySearch bs;
+
+  @Before
+  public void setup() {
+    this.bs = new BinarySearchRecursive();
+  }
+
+  @Test
+  public void buscaOValorNoVetor() {
+    final int[] vetor = {1, 3, 5, 7, 9};
+
+    final int result = this.bs.rank(5, vetor);
+
+    assertEquals(2, result);
+  }
+
+  @Test
+  public void buscaUmValorNoVetorVazio() {
+    final int[] vetor = new int[]{};
+
+    final int result = this.bs.rank(1, vetor);
+
+    assertEquals(-1, result);
+  }
+
+  @Test
+  public void buscaUmValorInexistenteNoVetor() {
+    final int[] vetor = {1, 3, 5, 7, 9};
+
+    final int result = this.bs.rank(2, vetor);
+
+    assertEquals(-1, result);
+  }
+}
