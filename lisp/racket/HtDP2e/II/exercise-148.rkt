dc5d870e95177f0c6772b44ea060e8f422c8c1d2
Marcelo dos Santos <mdssjc@gmail.com>
2017-03-20 20:12:04 -0300

    Exercício 148.

---
+++ lisp/racket/HtDP2e/II/exercise-148.rkt
@@ -0,0 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-148) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; Exercise 148
+
+; Yes, with empty list, the function has better expressiveness, however, it always executes an extra loop.
