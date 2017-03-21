e016c8b45c5e92fbabce2b245a24dcffe2690500
Marcelo dos Santos <mdssjc@gmail.com>
2017-03-21 20:08:54 -0300

    Exercício 150.

---
+++ lisp/racket/HtDP2e/II/exercise-150.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-150) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; Exercise 150
+
+; N -> Number
+; computes (+ n pi) without using +
+(check-within (add-to-pi 3) (+ 3 pi) 0.001)
+ 
+(define (add-to-pi n)
+  (cond [(zero? n) pi]
+        [else (add1 (add-to-pi (sub1 n)))]))
