7659f49a3222c5771d4a9573e3611111e88a570e
Marcelo dos Santos <mdssjc@gmail.com>
2017-03-23 20:50:30 -0300

    Exercício 157.

---
+++ lisp/racket/HtDP2e/II/exercise-157.rkt
@@ -0,0 +1,19 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-157) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; Exercise 157
+
+; change the height of the canvas to 220 pixels;
+; Yes.
+
+; change the width of the canvas to 30 pixels;
+; Yes.
+
+; change the x location of the line of shots to "somewhere to the left of the middle";
+; Yes.
+
+; change the background to a green rectangle; and
+; Yes.
+
+; change the rendering of shots to a red elongated rectangle.
+; Yes.
