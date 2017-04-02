c492d4310004bd132003e226f9a8ab0b01ace98a
Marcelo dos Santos <mdssjc@gmail.com>
2017-04-02 13:56:13 -0300

    Atualização das dependências.

--- lisp/clojure/WDC-2e/chapter-01/guestbook/project.clj
+++ lisp/clojure/WDC-2e/chapter-01/guestbook/project.clj
@@ -3,12 +3,14 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
 
-  :dependencies [[com.h2database/h2 "1.4.193"]
+  :dependencies [[bouncer "1.0.0"]
+                 [com.h2database/h2 "1.4.193"]
                  [compojure "1.5.2"]
                  [conman "0.6.3"]
                  [cprop "0.1.10"]
                  [funcool/struct "1.0.0"]
                  [luminus-immutant "0.2.3"]
+                 [luminus-log4j "0.1.3"]
                  [luminus-migrations "0.3.0"]
                  [luminus-nrepl "0.1.4"]
                  [luminus/ring-ttl-session "0.3.2"]
@@ -24,12 +26,15 @@
                  [org.webjars/font-awesome "4.7.0"]
                  [org.webjars/jquery "3.1.1"]
                  [org.webjars/webjars-locator-jboss-vfs "0.1.0"]
+                 [ring-middleware-format "0.7.0"]
                  [ring-webjars "0.1.1"]
                  [ring/ring-core "1.6.0-RC1"]
                  [ring/ring-defaults "0.2.3"]
+                 [selmer "1.0.4"]
                  [selmer "1.10.7"]
                  [yesql "0.5.3"]
-                 [yogthos/config "0.8"]]
+                 [yogthos/config "0.8"]
+                 [environ "1.1.0"]]
 
   :min-lein-version "2.0.0"
 
@@ -46,26 +51,26 @@
             [lein-immutant "2.1.0"]]
 
   :profiles
-  {:uberjar {:omit-source true
-             :aot :all
-             :uberjar-name "guestbook.jar"
-             :source-paths ["env/prod/clj"]
+  {:uberjar {:omit-source    true
+             :aot            :all
+             :uberjar-name   "guestbook.jar"
+             :source-paths   ["env/prod/clj"]
              :resource-paths ["env/prod/resources"]}
 
-   :dev           [:project/dev :profiles/dev]
-   :test          [:project/dev :project/test :profiles/test]
+   :dev  [:project/dev :profiles/dev]
+   :test [:project/dev :project/test :profiles/test]
 
-   :project/dev  {:dependencies [[prone "1.1.4"]
-                                 [ring/ring-mock "0.3.0"]
-                                 [ring/ring-devel "1.5.1"]
-                                 [pjstadig/humane-test-output "0.8.1"]]
-                  :plugins      [[com.jakemccrary/lein-test-refresh "0.18.1"]]
+   :project/dev {:dependencies [[prone "1.1.4"]
+                                [ring/ring-mock "0.3.0"]
+                                [ring/ring-devel "1.5.1"]
+                                [pjstadig/humane-test-output "0.8.1"]]
+                 :plugins      [[com.jakemccrary/lein-test-refresh "0.18.1"]]
 
-                  :source-paths ["env/dev/clj"]
-                  :resource-paths ["env/dev/resources"]
-                  :repl-options {:init-ns user}
-                  :injections [(require 'pjstadig.humane-test-output)
-                               (pjstadig.humane-test-output/activate!)]}
-   :project/test {:resource-paths ["env/test/resources"]}
-   :profiles/dev {}
+                 :source-paths   ["env/dev/clj" "test/clj"]
+                 :resource-paths ["env/dev/resources"]
+                 :repl-options   {:init-ns user}
+                 :injections     [(require 'pjstadig.humane-test-output)
+                                  (pjstadig.humane-test-output/activate!)]}
+   :project/test  {:resource-paths ["env/dev/resources" "env/test/resources"]}
+   :profiles/dev  {}
    :profiles/test {}})
