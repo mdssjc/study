47bca551969a1c8235e0cab4c811fcd8b525cd01
Marcelo dos Santos <mdssjc@gmail.com>
2017-04-02 14:30:38 -0300

    Bugfix.

--- lisp/clojure/WDC-2e/chapter-01/guestbook/test/clj/guestbook/test/db/core.clj
+++ lisp/clojure/WDC-2e/chapter-01/guestbook/test/clj/guestbook/test/db/core.clj
@@ -1,30 +1,34 @@
 (ns guestbook.test.db.core
-  (:require [guestbook.db.core :as db]
-            [guestbook.db.migrations :as migrations]
+  (:require [guestbook.db.core :refer [*db*] :as db]
+            [luminus-migrations.core :as migrations]
             [clojure.test :refer :all]
             [clojure.java.jdbc :as jdbc]
-            [config.core :refer [env]]
+            [guestbook.config :refer [env]]
             [mount.core :as mount]))
 
 (use-fixtures
   :once
   (fn [f]
-    (migrations/migrate ["migrate"])
+    (mount/start
+      #'guestbook.config/env
+      #'guestbook.db.core/*db*)
+    (migrations/migrate ["migrate"] (select-keys env [:database-url]))
     (f)))
 
 (deftest test-messages
-  (jdbc/with-db-transaction [t-conn db/conn]
+  (jdbc/with-db-transaction [t-conn *db*]
     (jdbc/db-set-rollback-only! t-conn)
     (let [timestamp (java.util.Date.)]
       (is (= 1 (db/save-message!
-                {:name      "Bob"
-                 :message   "Hello World"
-                 :timestamp timestamp}
-                {:connection t-conn})))
+                 t-conn
+                 {:name      "Bob"
+                  :message   "Hello, World"
+                  :timestamp timestamp}
+                 {:connection t-conn})))
       (is (=
            {:name      "Bob"
-            :message   "Hello World"
-            :timestamp timestamp})
-          (-> (db/get-messages {} {:connection t-conn})
-              first
-              (select-keys [:name :message :timestamp]))))))
+            :message   "Hello, World"
+            :timestamp timestamp}
+           (-> (db/get-messages t-conn {})
+                (first)
+                (select-keys [:name :message :timestamp])))))))
