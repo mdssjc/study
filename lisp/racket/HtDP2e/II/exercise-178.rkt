26612f6d62587d5737c604e7aa21c4e341d631d5
Marcelo dos Santos <mdssjc@gmail.com>
2017-04-04 19:57:59 -0300

    Exercício 178.

---
+++ lisp/racket/HtDP2e/II/exercise-178.rkt
@@ -0,0 +1,7 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-178) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; Exercise 178
+
+; Because its length is 1 (\t and \b are escape characters),
+; the same as the alphanumeric characters for (string-length k).
