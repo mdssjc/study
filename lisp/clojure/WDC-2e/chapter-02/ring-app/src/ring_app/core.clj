de1e3ca265b0a6163f8a297f8cf0607f3bcece9b
Marcelo dos Santos <mdssjc@gmail.com>
2017-04-04 23:10:33 -0300

    Projeto inicial.

---
+++ lisp/clojure/WDC-2e/chapter-02/ring-app/src/ring_app/core.clj
@@ -0,0 +1,22 @@
+(ns ring-app.core
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.util.response :as response]
+            [ring.middleware.reload :refer [wrap-reload]]))
+
+(defn handler [request]
+  (response/response
+   (str "<html><body> your IP is: "
+        (:remote-addr request)
+        "</body></html>")))
+
+(defn wrap-nocache [handler]
+  (fn [request]
+    (-> request
+        handler
+        (assoc-in [:headers "Pragma"] "no-cache"))))
+
+(defn -main []
+  (jetty/run-jetty
+   (-> handler var wrap-nocache wrap-reload)
+   {:port  3000
+    :join? false}))
