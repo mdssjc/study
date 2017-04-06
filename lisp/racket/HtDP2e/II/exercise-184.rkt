7a1628f7c07e9e9d3a1108a3acf263d735cec689
Marcelo dos Santos <mdssjc@gmail.com>
2017-04-06 19:10:58 -0300

    Exercício 184.

---
+++ lisp/racket/HtDP2e/II/exercise-184.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname exercise-184) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; Exercise 184
+
+(check-expect (list (string=? "a" "b") #false)
+              (list #false #false))
+
+(check-expect (list (+ 10 20) (* 10 20) (/ 10 20))
+              (list 30 200 0.5))
+
+(check-expect (list "dana" "jane" "mary" "laura")
+              (list "dana" "jane" "mary" "laura"))
