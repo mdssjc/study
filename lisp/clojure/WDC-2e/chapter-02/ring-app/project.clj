c420775481da01f7eebd43601d64d8f22437c366
Marcelo dos Santos <mdssjc@gmail.com>
2017-04-06 21:37:37 -0300

    Nova dependência.

--- lisp/clojure/WDC-2e/chapter-02/ring-app/project.clj
+++ lisp/clojure/WDC-2e/chapter-02/ring-app/project.clj
@@ -5,5 +5,6 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring "1.6.0-RC1"]
-                 [metosin/ring-http-response "0.8.2"]]
+                 [metosin/ring-http-response "0.8.2"]
+                 [ring-middleware-format "0.7.2"]]
   :main ring-app.core)
