8288d2b0a47476c9e11129bdb93bc87a9928fe87
Marcelo dos Santos <mdssjc@gmail.com>
2017-04-07 19:33:37 -0300

    Exercício 190.

---
+++ lisp/racket/HtDP2e/II/exercise-190.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname exercise-190) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; Exercise 190
+
+; List-of-1Strings
+; consumes a list of 1Strings lo1s and produces the list of all prefixes
+(check-expect (prefixes '()) '())
+(check-expect (prefixes (list "a" "b" "c")) (list "a" "b" "c" "d"))
+
+(define (prefixes lo1s)
+  (cond [(empty? lo1s) '()]
+        [else (append lo1s (list (int->string (add1 (string->int (first (reverse lo1s)))))))]))
