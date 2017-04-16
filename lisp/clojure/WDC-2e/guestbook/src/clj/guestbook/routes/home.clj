08e570c20e9f6875d27f4200226f7db8eb64ff53
Marcelo dos Santos <mdssjc@gmail.com>
2017-04-15 22:00:16 -0300

    Formulário com ClojureScript.

--- lisp/clojure/WDC-2e/guestbook/src/clj/guestbook/routes/home.clj
+++ lisp/clojure/WDC-2e/guestbook/src/clj/guestbook/routes/home.clj
@@ -1,11 +1,11 @@
 (ns guestbook.routes.home
   (:require [guestbook.layout :as layout]
             [compojure.core :refer [defroutes GET POST]]
-            [ring.util.http-response :as response]
             [clojure.java.io :as io]
             [guestbook.db.core :as db]
             [bouncer.core :as b]
-            [bouncer.validators :as v]))
+            [bouncer.validators :as v]
+            [ring.util.response :refer [response status]]))
 
 (defn home-page []
   (layout/render "home.html"))
@@ -19,17 +19,17 @@
 
 (defn save-message! [{:keys [params]}]
   (if-let [errors (validate-message params)]
-    (-> (response/found "/")
-        (assoc :flash (assoc params :errors errors)))
+    (-> {:errors errors} response (status 400))
     (do
       (db/save-message!
        (assoc params :timestamp (java.util.Date.)))
-      (response/found "/"))))
+      (response {:status :ok}))))
 
 (defn about-page []
   (layout/render "about.html"))
 
 (defroutes home-routes
   (GET "/" [] (home-page))
-  (POST "/message" request (save-message! request))
+  (POST "/messages" [] (response (db/get-messages)))
+  (POST "add-message" req (save-message! req))
   (GET "/about" [] (about-page)))
--- lisp/clojure/WDC-2e/guestbook/src/cljs/guestbook/core.cljs
+++ lisp/clojure/WDC-2e/guestbook/src/cljs/guestbook/core.cljs
@@ -1,37 +1,78 @@
 (ns guestbook.core
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require [reagent.core :as reagent :refer [atom]]
+            [ajax.core :refer [GET POST]]))
 
 (-> (.getElementById js/document "content")
    (.-innerHTML)
    (set! "Hello World!"))
 
 (defn home []
-  [:div.row
-   [:div.span12
-    [message-form]]])
+  (let [messages (atom nil)]
+    (get-messages messages)
+    (fn []
+      [:div
+       [:div.row
+        [:div.span12
+         [message-list messages]]]
+       [:div.row
+        [:div.span12
+         [message-form messages]]]])))
 
 (reagent/render
  [home]
  (.getElementById js/document "content"))
 
-(defn message-form []
-  (let [fields (atom {})]
+(defn errors-component [errors id]
+  (when-let [error (id @errors)]
+    [:div.alert.alert-danger (clojure.string/join error)]))
+
+(defn message-form [messages]
+  (let [fields (atom {})
+        errors (atom nil)]
     (fn []
       [:div.content
        [:div.form-group
-        [:p "name" (:name @fields)]
-        [:p "message" (:message @fields)]
+        [errors-component errors :name]
         [:p "Name:"
          [:input.form-control
-          {:type :text
-           :name :name
+          {:type      :text
+           :name      :name
            :on-change #(swap! fields assoc :name (-> % .-target .-value))
-           :value (:name @fields)}]]]
+           :value     (:name @fields)}]]]
+       [errors-component errors :message]
        [:p "Message:"
         [:textarea.form-control
-         {:rows 4
-          :cols 50
-          :name :message
+         {:rows      4
+          :cols      50
+          :name      :message
           :on-change #(swap! fields assoc :message (-> % .-target .-value))}
          (:message @fields)]]
-       [:input.btn.btn-primary {:type :submit :value "comment"}]])))
+       [:input.btn.btn-primary
+        {:type     :submit
+         :on-click #(send-message! fields errors)
+         :value    "comment"}]])))
+
+(defn send-message! [fields errors messages]
+  (POST "/add-message"
+        {:headers       {"Accept"       "application/transit+json"
+                         "x-csrf-token" (.-value (.getElementById js/document "token"))}
+         :params        @fields
+         :handler       #(do
+                           (reset! errors nil)
+                           (swap! messages conj (assoc @fields :timestamp (js/Date.))))
+         :error-handler #(do
+                           (.log js/console (str %))
+                           (reset! errors (get-in % [:response :errors])))}))
+(defn get-messages [messages]
+  (GET "/messages"
+       {:headers {"Accept" "application/transit+json"}
+        :handler #(reset! messages (vec %))}))
+
+(defn message-list [messages]
+  [:ul.content
+   (for [{:keys [timestamp message name]} @messages]
+     ^{:key timestamp}
+     [:li
+      [:time (.toLocaleString timestamp)]
+      [:p message]
+      [:p " - " name]])])
