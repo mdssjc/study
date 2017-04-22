f8c00aa7156053ab2c88f82db3ce783fbd19897a
Marcelo dos Santos <mdssjc@gmail.com>
2017-04-22 20:04:21 -0300

    Cria a tabela users.

--- lisp/clojure/WDC-2e/db-examples/src/db_examples/core.clj
+++ lisp/clojure/WDC-2e/db-examples/src/db_examples/core.clj
@@ -1,12 +1,14 @@
 (ns db-examples.core
   (:require [clojure.java.jdbc :as sql]))
 
-(def db {:subprotocol "postgresql"}
+(def db {:subprotocol "mysql"}
   :subname "//localhost:3306/reporting"
   :user "dummy"
   :password "dummy")
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn create-users-table! []
+  (sql/db-do-commands db
+                      (sql/create-table-ddl
+                       :users
+                       [:id "VARCHAR(32) PRIMARY KEY"]
+                       [:pass "VARCHAR(100)"])))
