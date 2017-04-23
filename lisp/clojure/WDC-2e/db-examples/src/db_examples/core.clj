50e9a89bd387c5d011c32f1b614d6d1659c6d99d
Marcelo dos Santos <mdssjc@gmail.com>
2017-04-22 21:24:39 -0300

    Correção da conexão, criação da tabela e query.

--- lisp/clojure/WDC-2e/db-examples/src/db_examples/core.clj
+++ lisp/clojure/WDC-2e/db-examples/src/db_examples/core.clj
@@ -1,14 +1,21 @@
 (ns db-examples.core
   (:require [clojure.java.jdbc :as sql]))
 
-(def db {:subprotocol "mysql"}
-  :subname "//localhost:3306/reporting"
-  :user "dummy"
-  :password "dummy")
+(def db {:subprotocol "mysql"
+         :subname "//localhost:3306/reporting"
+         :user "dummy"
+         :password "dummy"})
 
 (defn create-users-table! []
   (sql/db-do-commands db
                       (sql/create-table-ddl
                        :users
-                       [:id "VARCHAR(32) PRIMARY KEY"]
-                       [:pass "VARCHAR(100)"])))
+                       [[:id "VARCHAR(32) PRIMARY KEY"]
+                        [:pass "VARCHAR(100)"]])))
+
+;(create-users-table!)
+
+(defn get-user [id]
+  (first (sql/query db ["SELECT * FROM users WHERE id = ?" id])))
+
+(get-user "foo")
