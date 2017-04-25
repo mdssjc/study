b1cfaf316cf295cb5ae00743922e83b5cf4fa90b
Marcelo dos Santos <mdssjc@gmail.com>
2017-04-25 20:43:05 -0300

    Exercício 211.

---
+++ lisp/racket/HtDP2e/II/exercise-211.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname exercise-211) (read-case-sensitive #t) (teachpacks ((lib "itunes.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "itunes.rkt" "teachpack" "2htdp")) #f)))
+;; Exercise 211
+
+; List-of-strings -> List-of-strings
+; pick out all those Strings that occur in the dictionary
+(check-expect (in-dictionary (list "hello" "world")) (list "hello" "world"))
+(check-expect (in-dictionary (list "a" "car")) '())
+
+(define (in-dictionary los)
+  (cond [(empty? los) '()]
+        [(member? (first los) (list "hello" "world" "egg"))
+         (cons (first los) (in-dictionary (rest los)))]
+        [else (in-dictionary (rest los))]))
