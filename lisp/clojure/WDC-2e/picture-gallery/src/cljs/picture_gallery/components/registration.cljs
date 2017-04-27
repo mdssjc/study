9fb0dd737e26d951eb5b3a39686ef3a428feba7a
Marcelo dos Santos <mdssjc@gmail.com>
2017-04-27 15:21:25 -0300

    Parcial no registro de usuários.

---
+++ lisp/clojure/WDC-2e/picture-gallery/src/cljs/picture_gallery/components/registration.cljs
@@ -0,0 +1,45 @@
+(ns picture-gallery.components.registration
+  (:require [reagent.core :refer [atom]]
+            [reagent.session :as session]
+            [picture-gallery.components.common :as c]
+            [ajax.core :as ajax]
+            [picture-gallery.validation :refer [registration-errors]]))
+
+(defn register! [fields errors]
+  (reset! errors (registration-errors @fields))
+  (when-not @errors
+    (ajax/POST "/register"
+               {:params        @fields
+                :handler       #(do
+                                  (session/put! :identity (:id @fields))
+                                  (reset! fields {}))
+                :error-handler #(reset!
+                                 errors
+                                 {:server-error (get-in % [:response :message])})})))
+
+(defn registration-form []
+  (let [fields (atom {})
+        error  (atom nil)]
+    (fn []
+      [c/modal
+       [:div "Picture Gallery Registration"]
+       [:div
+        [:div.well.well-sm
+         [:strong "✱ required field"]]
+        [c/text-input "name" :id "enter a user name" fields]
+        [c/password-input "password" :pass "enter a password" fields]
+        [c/password-input "password" :pass-confirm "re-enter the password" fields]
+        (when-let [error (:server-error @error)]
+          [:div.alert.alert-danger error])]
+       [:div
+        [:button.btn.btn-primary
+         {:on-click #(register! fields error)}
+         "Register"]
+        [:button.btn.btn-danger
+         {:on-click #(session/remove! :modal)}
+         "Cancel"]]])))
+
+(defn registration-button []
+  [:a.btn
+   {:on-click #(session/put! :modal registration-form)}
+   "register"])
--- lisp/clojure/WDC-2e/picture-gallery/src/cljs/picture_gallery/core.cljs
+++ lisp/clojure/WDC-2e/picture-gallery/src/cljs/picture_gallery/core.cljs
@@ -1,10 +1,18 @@
-(ns picture-gallery.core)
+(ns picture-gallery.core
+  (:require [picture-gallery.components.common :as c]
+            [picture-gallery.components.registration :as reg])
+  (:import goog.History))
+
+(defn page []
+  [:div
+   [reg/registration-form-example]
+   [(pages (session/get :page))]])
 
 (defn mount-components []
-  (let [content (js/document.getElementById "app")]
-    (while (.hasChildNodes content)
-      (.removeChild content (.-lastChild content)))
-    (.appendChild content (js/document.createTextNode "Welcome to picture-gallery"))))
+  (r/render [#'navbar] (.getElementById js/document "navbar"))
+  (r/render [#'page] (.getElementById js/document "app")))
 
 (defn init! []
+  (load-interceptors!)
+  (hook-browser-navigation!)
   (mount-components))
