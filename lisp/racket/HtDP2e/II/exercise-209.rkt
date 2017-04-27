9c6f8f900d29956c59d9c2df314bf263b0a6272b
Marcelo dos Santos <mdssjc@gmail.com>
2017-04-26 22:11:16 -0300

    Remoção do add-on itunes.rkt.

--- lisp/racket/HtDP2e/II/exercise-209.rkt
+++ lisp/racket/HtDP2e/II/exercise-209.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname exercise-209) (read-case-sensitive #t) (teachpacks ((lib "itunes.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "itunes.rkt" "teachpack" "2htdp")) #f)))
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname exercise-209) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;; Exercise 209
 
 ; A Word is one of:
--- lisp/racket/HtDP2e/II/exercise-211.rkt
+++ lisp/racket/HtDP2e/II/exercise-211.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname exercise-211) (read-case-sensitive #t) (teachpacks ((lib "itunes.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "itunes.rkt" "teachpack" "2htdp")) #f)))
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname exercise-211) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;; Exercise 211
 
 ; List-of-strings -> List-of-strings
