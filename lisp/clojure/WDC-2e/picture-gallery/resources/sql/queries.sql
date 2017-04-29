3ae84b483a921a544a1943807c8b7a41d4b3151d
Marcelo dos Santos <mdssjc@gmail.com>
2017-04-29 13:59:31 -0300

    Configurações para a tabela 'files'.

--- lisp/clojure/WDC-2e/picture-gallery/resources/sql/queries.sql
+++ lisp/clojure/WDC-2e/picture-gallery/resources/sql/queries.sql
@@ -9,3 +9,7 @@ SELECT * FROM users WHERE id = :id
 -- :name delete-user! :! :n
 -- :doc delete a user given the id
 DELETE FROM users WHERE id = :id
+
+-- :name save-file! :! :n
+-- :doc saves a file to the database
+INSERT INTO files (owner, type, name, data) VALUES (:owner, :type, :name, :DATA)
