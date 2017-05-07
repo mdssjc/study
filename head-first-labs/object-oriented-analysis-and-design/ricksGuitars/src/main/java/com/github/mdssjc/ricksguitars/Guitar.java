b637c682b08b5187d3a6a1a7b792e9cb60293dd9
Marcelo dos Santos <mdssjc@gmail.com>
2017-05-07 13:25:48 -0300

    Correção da busca com Enumeração.

--- head-first-labs/object-oriented-analysis-and-design/ricksGuitars/src/main/java/com/github/mdssjc/ricksguitars/Guitar.java
+++ head-first-labs/object-oriented-analysis-and-design/ricksGuitars/src/main/java/com/github/mdssjc/ricksguitars/Guitar.java
@@ -9,9 +9,9 @@ public class Guitar {
 
   private String serialNumber;
   private double price;
-  private String builder;
+  private Builder builder;
   private String model;
-  private String type;
-  private String backWood;
-  private String topWood;
+  private Type type;
+  private Wood backWood;
+  private Wood topWood;
 }
