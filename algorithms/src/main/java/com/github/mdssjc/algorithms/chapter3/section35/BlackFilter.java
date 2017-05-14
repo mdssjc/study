d4d4de5f7d21c7936267e96c1c996bc83dff6274
Marcelo dos Santos <mdssjc@gmail.com>
2017-05-14 11:27:54 -0300

    BlackFilter Class.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter3/section35/BlackFilter.java
@@ -0,0 +1,34 @@
+package com.github.mdssjc.algorithms.chapter3.section35;
+
+import com.github.mdssjc.algorithms.utils.Executor;
+import com.github.mdssjc.algorithms.utils.TestDrive;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * BlackFilter Class.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+@TestDrive(value = "list.txt", valueFile = true, input = "tinyTale.txt", inputFile = true)
+public class BlackFilter {
+
+  public static void main(final String[] args) {
+    Executor.execute(BlackFilter.class, args);
+
+    final HashSET<String> set = new HashSET<>();
+
+    final In in = new In(args[0]);
+    while (!in.isEmpty()) {
+      set.add(in.readString());
+    }
+    while (!StdIn.isEmpty()) {
+      final String word = StdIn.readString();
+      if (!set.contains(word)) {
+        StdOut.println(word);
+      }
+    }
+  }
+}
