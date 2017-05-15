f79b9aba260d084c92d7004a26d4b66837bb779f
Marcelo dos Santos <mdssjc@gmail.com>
2017-05-15 14:21:53 -0300

    Caminho incorreto.

--- oracle/java-puzzle-ball/lesson-2/WeatherORama/src/main/java/com/github/mdssjc/weatherorama/DisplayElement.java
+++
@@ -1,6 +0,0 @@
-package com.github.mdssjc.weatherorama;
-
-public interface DisplayElement {
-
-  void display();
-}
--- oracle/java-puzzle-ball/lesson-2/WeatherORama/src/main/java/com/github/mdssjc/weatherorama/Observer.java
+++
@@ -1,6 +0,0 @@
-package com.github.mdssjc.weatherorama;
-
-public interface Observer {
-
-  void update(float temp, float humidity, float pressure);
-}
