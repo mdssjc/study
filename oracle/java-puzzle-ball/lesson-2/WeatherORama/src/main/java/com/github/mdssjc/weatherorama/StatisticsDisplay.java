887f35c81a42d0e41418ff7d8af1b935bae847d3
Marcelo dos Santos <mdssjc@gmail.com>
2017-05-14 21:57:10 -0300

    Displays disponíveis.

---
+++ oracle/java-puzzle-ball/lesson-2/WeatherORama/src/main/java/com/github/mdssjc/weatherorama/StatisticsDisplay.java
@@ -0,0 +1,35 @@
+package com.github.mdssjc.weatherorama;
+
+public class StatisticsDisplay implements Observer, DisplayElement {
+
+  private float maxTemp = 0.0f;
+  private float minTemp = 200;
+  private float tempSum = 0.0f;
+  private int numReadings;
+  private final WeatherData weatherData;
+
+  public StatisticsDisplay(WeatherData weatherData) {
+    this.weatherData = weatherData;
+    weatherData.registerObserver(this);
+  }
+
+  public void update(float temp, float humidity, float pressure) {
+    tempSum += temp;
+    numReadings++;
+
+    if (temp > maxTemp) {
+      maxTemp = temp;
+    }
+
+    if (temp < minTemp) {
+      minTemp = temp;
+    }
+
+    display();
+  }
+
+  public void display() {
+    System.out.printf("Avg/Max/Min temperature = %.1f/%.1f/%.1f%n",
+                      (tempSum / numReadings), maxTemp, minTemp);
+  }
+}
