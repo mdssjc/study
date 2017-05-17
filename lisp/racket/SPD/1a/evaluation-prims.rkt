9b0b7a15eed517d835d1d58e39c6680347b6720a
Marcelo dos Santos <mdssjc@gmail.com>
2017-05-17 19:56:14 -0300

    Correção na documentação.

--- lisp/racket/SPD/1a/evaluation-prims.rkt
+++ lisp/racket/SPD/1a/evaluation-prims.rkt
@@ -1,7 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname evaluation-prims-starter) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-;; evaluation-prims-starter.rkt
+;; evaluation-prims.rkt
 
 ;; PROBLEM:
 ;; BSL P2
