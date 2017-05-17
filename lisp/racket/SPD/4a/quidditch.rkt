b9b2bb4a1044f87b66813db6e5db26d06e926272
Marcelo dos Santos <mdssjc@gmail.com>
2017-05-17 19:04:34 -0300

    Documentação.

--- lisp/racket/SPD/4a/quidditch.rkt
+++ lisp/racket/SPD/4a/quidditch.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname quidditch-starter) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname quidditch) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;; quidditch-starter.rkt
 
 
@@ -9,7 +9,9 @@
 ;; Imagine that you are designing a program that will keep track of
 ;; your favorite Quidditch teams. (http://iqaquidditch.org/).
 ;;
-;; Design a data definition to represent a list of Quidditch teams. 
+;; (A) Design a data definition to represent a list of Quidditch teams.
+;; (B) Design a function that consumes ListOfString and produces true if 
+;; the list includes "UBC".
 ;;
 ;; Information:                     Data:
 ;;  - UBC                        -> "UBC"
@@ -41,12 +43,6 @@
 ;;  - atomic distinct: empty
 ;;  - compound: (cons String ListOfString)
 
-
-;; PROBLEM:
-;;
-;; We want to know whether your list of favorite Quidditch teams includes
-;; UBC! Design a function that consumes ListOfString and produces true if 
-;; the list includes "UBC".
 
 ;; ListOfString -> Boolean
 ;; produce true if los includes "UBC"
