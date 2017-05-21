8fe2de5993f83cea65101eb9f3dadb1a206ae489
Marcelo dos Santos <mdssjc@gmail.com>
2017-05-21 13:28:02 -0300

    Conversão de long.

--- head-first-labs/object-oriented-analysis-and-design/dogDoors/src/test/java/com/github/mdssjc/dogdoors/WhatTheDoorDoesStepsdef.java
+++ head-first-labs/object-oriented-analysis-and-design/dogDoors/src/test/java/com/github/mdssjc/dogdoors/WhatTheDoorDoesStepsdef.java
@@ -41,7 +41,7 @@ public class WhatTheDoorDoesStepsdef {
   @When("^The door shuts automatically\\.$")
   public void the_door_shuts_automatically() throws InterruptedException {
     while (this.door.isOpen()) {
-      Thread.sleep(100);
+      Thread.sleep(100L);
     }
   }
 
