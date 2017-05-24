c7ce43fadc87e7c3680478449e5cc5110dfb1687
Marcelo dos Santos <mdssjc@gmail.com>
2017-05-23 23:26:20 -0300

    Refatoração do Exercise 1.1.1.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises11/Ex1.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/exercises11/Ex1.java
@@ -16,8 +16,16 @@ public class Ex1 {
   public static void main(final String[] args) {
     Executor.execute(Ex1.class, args);
 
-    StdOut.println((0 + 15) / 2);
-    StdOut.println(2.0E-6 * 100000000.1);
-    StdOut.println(true && false || true && true);
+    StdOut.printf(
+        "a. ( 0 + 15 ) / 2 -> %d%n",
+        (0 + 15) / 2);
+
+    StdOut.printf(
+        "b. 2.0e-6 * 100000000.1 -> %f%n",
+        2.0E-6 * 100000000.1);
+
+    StdOut.printf(
+        "c. true && false || true && true -> %s%n",
+        true && false || true && true);
   }
 }
