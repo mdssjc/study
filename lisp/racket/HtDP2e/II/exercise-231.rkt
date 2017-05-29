fa04af9c8f6e1053eaef528b6ca5ae4cb3861313
Marcelo dos Santos <mdssjc@gmail.com>
2017-05-29 20:40:55 -0300

    Exercício 231.

---
+++ lisp/racket/HtDP2e/II/exercise-231.rkt
@@ -0,0 +1,19 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname exercise-231) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; Exercise 231
+
+'(1 "a" 2 #false 3 "c")
+(list 1 "a" 2 #false 3 "c")
+
+'()
+(list)
+
+'(("alan" 1000)
+  ("barb" 2000)
+  ("carl" 1500)
+  ("dawn" 2300))
+(list '("alan" 1000)
+      '("barb" 2000)
+      '("carl" 1500)
+      '("dawn" 2300))
