ec094ff9670f3b74a6e11f45169133df1613927d
Marcelo dos Santos <mdssjc@gmail.com>
2017-06-04 20:11:10 -0300

    Atualizado o Test Drive.

--- head-first-labs/design-patterns/Pizza/src/test/java/com/github/mdssjc/pizza/PizzaTestDrive.java
+++ head-first-labs/design-patterns/Pizza/src/test/java/com/github/mdssjc/pizza/PizzaTestDrive.java
@@ -3,13 +3,13 @@ package com.github.mdssjc.pizza;
 public class PizzaTestDrive {
 
   public static void main(final String[] args) {
-    final PizzaStore nyStore = new NYPizzaStore();
-    final PizzaStore chicagoStore = new ChicagoPizzaStore();
+    final PizzaStore nyPizzaStore = new NYPizzaStore();
+    final PizzaStore chicagoPizzaStore = new ChicagoPizzaStore();
 
-    Pizza pizza = nyStore.orderPizza("cheese");
+    Pizza pizza = nyPizzaStore.orderPizza("cheese");
     System.out.printf("Ethan ordered a %s%n%n", pizza.getName());
 
-    pizza = chicagoStore.orderPizza("cheese");
+    pizza = chicagoPizzaStore.orderPizza("cheese");
     System.out.printf("Joel ordered a %s%n%n", pizza.getName());
   }
 }
