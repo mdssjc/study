c85e0e453904d2ee9453fdf71f73234e2bbed2cf
Marcelo dos Santos <mdssjc@gmail.com>
2017-06-16 21:53:33 -0300

    Exercício 254.

---
+++ lisp/racket/HtDP2e/III/exercise-254.rkt
@@ -0,0 +1,18 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname exercise-254) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; Exercise 254
+
+
+; [List-of Number] (Number Number -> Boolean) -> [List-of Number]
+; !!!
+(define (sort-n lon f) empty)
+
+; [List-of String] (String String -> Boolean) -> [List-of String]
+; !!!
+(define (sort-s los f) empty)
+
+
+; [X] [List-of X] (X X -> Boolean) -> [List-of X]
+; !!!
+(define (sort-x lst f) empty)
