54b23f9f3853bb6027c776807771a815d758d634
Marcelo dos Santos <mdssjc@gmail.com>
2017-06-18 21:35:33 -0300

    Novo comando para Light.

---
+++ head-first-labs/design-patterns/Remote/src/main/java/com/github/mdssjc/remote/LightOffCommand.java
@@ -0,0 +1,15 @@
+package com.github.mdssjc.remote;
+
+public class LightOffCommand implements Command {
+
+  private final Light light;
+
+  public LightOffCommand(final Light light) {
+    this.light = light;
+  }
+
+  @Override
+  public void execute() {
+    this.light.off();
+  }
+}
