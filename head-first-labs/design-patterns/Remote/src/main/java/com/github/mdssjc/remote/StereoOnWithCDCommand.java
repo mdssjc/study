837a883dff573e09fde7e48a97e2fa1a269e3f41
Marcelo dos Santos <mdssjc@gmail.com>
2017-06-18 21:22:50 -0300

    Adição do Stereo.

---
+++ head-first-labs/design-patterns/Remote/src/main/java/com/github/mdssjc/remote/StereoOnWithCDCommand.java
@@ -0,0 +1,17 @@
+package com.github.mdssjc.remote;
+
+public class StereoOnWithCDCommand implements Command {
+
+  private final Stereo stereo;
+
+  public StereoOnWithCDCommand(final Stereo stereo) {
+    this.stereo = stereo;
+  }
+
+  @Override
+  public void execute() {
+    this.stereo.on();
+    this.stereo.setCD();
+    this.stereo.setVolume(11);
+  }
+}
