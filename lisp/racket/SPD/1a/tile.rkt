df07cd69107f3a35be9a69da95da3733eac14307
Marcelo dos Santos <mdssjc@gmail.com>
2017-06-26 19:47:47 -0300

    Documentação.

--- lisp/racket/SPD/1a/tile.rkt
+++ lisp/racket/SPD/1a/tile.rkt
@@ -3,6 +3,7 @@
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname tile-starter) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;; tile.rkt
 ;; BSL P3
+;; Create an image using image composition primitives.
 
 (require 2htdp/image)
 
@@ -12,7 +13,11 @@
 ;; Use the DrRacket square, beside and above functions to create an
 ;; image like this one:
 ;;
-;; [Tile Image]
+;; +--------+--------+
+;; |  Blue  | Yellow |
+;; +--------+--------+
+;; | Yellow |  Blue  |
+;; +--------+--------+
 ;;
 ;; If you prefer to be more creative feel free to do so. You can use
 ;; other DrRacket image functions to make a more interesting or more
