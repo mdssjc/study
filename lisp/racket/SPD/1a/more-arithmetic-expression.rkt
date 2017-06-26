307fb455cb7d82fe08e7558e9c66604dce8af89a
Marcelo dos Santos <mdssjc@gmail.com>
2017-06-26 19:37:23 -0300

    Documentação.

--- lisp/racket/SPD/1a/more-arithmetic-expression.rkt
+++ lisp/racket/SPD/1a/more-arithmetic-expression.rkt
@@ -3,6 +3,7 @@
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname more-arithmetic-expression-starter) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;; more-arithmetic-expression.rkt
 ;; BSL P1
+;; Write more arithmetic expressions.
 
 
 ;; PROBLEM:
@@ -10,8 +11,8 @@
 ;; Write two expressions that multiply the numbers 3, 5 and 7.
 ;; The first should take advantage of the fact that * can accept more
 ;; than 2 arguments.
-;; The second should build up the result by first multiplying 3 times 5
-;; and then multiply the result of that by 7.
+;; The second should build up the result by first multiplying 3 times
+;; 5 and then multiply the result of that by 7.
 
 (* 3 5 7)
 (* (* 3 5) 7)
