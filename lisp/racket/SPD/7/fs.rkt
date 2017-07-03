7758ffdab08c325c6abb1900b24058a199533589
Marcelo dos Santos <mdssjc@gmail.com>
2017-07-03 19:35:02 -0300

    Templating Mutual Recursion.

--- lisp/racket/SPD/7/fs.rkt
+++ lisp/racket/SPD/7/fs.rkt
@@ -25,6 +25,16 @@
 (define D5 (make-elt "D5" 0 (list F3)))
 (define D6 (make-elt "D6" 0 (list D4 D5)))
 
+(define (fn-for-element e)
+  (... (elt-name e)
+       (elt-data e)
+       (fn-for-loe (elt-subs e))))
+
+(define (fn-for-loe loe)
+  (cond [(empty? loe) (...)]
+        [else
+         (... (fn-for-element (first loe))
+              (fn-for-loe (rest loe)))]))
 
 ;; Functions:
 
