732664e76de3c94887d5dd46640917f68c6f2372
Marcelo dos Santos <mdssjc@gmail.com>
2017-07-05 23:14:32 -0300

    Implementação do Undirected Graph.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/datastructure/graph/concrete/UndirectedGraph.java
@@ -0,0 +1,60 @@
+package com.github.mdssjc.algorithms.datastructure.graph.concrete;
+
+import com.github.mdssjc.algorithms.datastructure.bag.Bag;
+import com.github.mdssjc.algorithms.datastructure.bag.concrete.BagLinkedList;
+import com.github.mdssjc.algorithms.datastructure.graph.Graph;
+import edu.princeton.cs.algs4.In;
+
+/**
+ * Implementação de Undirected Graph.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+public class UndirectedGraph implements Graph {
+
+  private final int v;
+  private int e;
+  private final Bag<Integer>[] adj;
+
+  public UndirectedGraph(final int v) {
+    this.v = v;
+    this.e = 0;
+    this.adj = new Bag[v];
+    for (int i = 0; i < this.v; i++) {
+      this.adj[i] = new BagLinkedList<>();
+    }
+  }
+
+  public UndirectedGraph(final In in) {
+    this(in.readInt());
+    final int e = in.readInt();
+    for (int i = 0; i < e; i++) {
+      final int v = in.readInt();
+      final int w = in.readInt();
+      addEdge(v, w);
+    }
+  }
+
+  @Override
+  public int v() {
+    return this.v;
+  }
+
+  @Override
+  public int e() {
+    return this.e;
+  }
+
+  @Override
+  public void addEdge(final int v, final int w) {
+    this.adj[v].add(w);
+    this.adj[w].add(v);
+    this.e++;
+  }
+
+  @Override
+  public Iterable<Integer> adj(final int v) {
+    return this.adj[v];
+  }
+}
