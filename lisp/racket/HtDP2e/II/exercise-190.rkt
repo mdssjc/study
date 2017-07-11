22a1c497bdade1a176be3583bf284a076b75ec89
Marcelo dos Santos <mdssjc@gmail.com>
2017-07-10 21:41:52 -0300

    Assinatura das funções.

--- lisp/racket/HtDP2e/II/exercise-190.rkt
+++ lisp/racket/HtDP2e/II/exercise-190.rkt
@@ -3,7 +3,7 @@
 #reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname exercise-190) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;; Exercise 190
 
-; List-of-1Strings
+; List-of-1Strings -> List-of-1Strings
 ; produces the list of all prefixes
 (check-expect (prefixes '()) '())
 (check-expect (prefixes (list "a" "b" "c")) (list "a" "b" "c" "d"))
@@ -13,7 +13,7 @@
         [else
          (append lo1s (list (int->string (add1 (string->int (first (reverse lo1s)))))))]))
 
-; List-of-1Strings
+; List-of-1Strings -> List-of-1Strings
 ; produces the list of all suffixes
 (check-expect (suffixes '()) '())
 (check-expect (suffixes (list "b" "c" "d")) (list "a" "b" "c" "d"))
