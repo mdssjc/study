baf4a9cc8b0e350749e607b193c8026553c0d5ec
Marcelo dos Santos <mdssjc@gmail.com>
2017-07-10 23:07:50 -0300

    Correção do exercício.

--- lisp/racket/HtDP2e/II/exercise-190.rkt
+++ lisp/racket/HtDP2e/II/exercise-190.rkt
@@ -3,22 +3,28 @@
 #reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname exercise-190) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;; Exercise 190
 
-; List-of-1Strings -> List-of-1Strings
+; List-of-1Strings -> List-of-List-of-1Strings
 ; produces the list of all prefixes
 (check-expect (prefixes '()) '())
-(check-expect (prefixes (list "a" "b" "c")) (list "a" "b" "c" "d"))
+(check-expect (prefixes (list "a")) (list (list "a")))
+(check-expect (prefixes (list "a" "b")) (list (list "a" "b") (list "a")))
+(check-expect (prefixes (list "a" "b" "c")) (list (list "a" "b" "c") (list "a" "b") (list "a")))
 
 (define (prefixes lo1s)
   (cond [(empty? lo1s) '()]
         [else
-         (append lo1s (list (int->string (add1 (string->int (first (reverse lo1s)))))))]))
+         (cons lo1s
+               (prefixes (reverse (rest (reverse lo1s)))))]))
 
-; List-of-1Strings -> List-of-1Strings
+; List-of-1Strings -> List-of-List-of-1Strings
 ; produces the list of all suffixes
 (check-expect (suffixes '()) '())
-(check-expect (suffixes (list "b" "c" "d")) (list "a" "b" "c" "d"))
+(check-expect (suffixes (list "a")) (list (list "a")))
+(check-expect (suffixes (list "a" "b")) (list (list "a" "b") (list "b")))
+(check-expect (suffixes (list "a" "b" "c")) (list (list "a" "b" "c") (list "b" "c") (list "c")))
 
 (define (suffixes lo1s)
   (cond [(empty? lo1s) '()]
         [else
-         (cons (int->string (sub1 (string->int (first lo1s)))) lo1s)]))
+         (cons lo1s
+               (suffixes (rest lo1s)))]))
