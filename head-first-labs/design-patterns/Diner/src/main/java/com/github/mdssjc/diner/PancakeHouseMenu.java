aaba4245edd2fd2f23f71dad7ff8c16758d846b2
Marcelo dos Santos <mdssjc@gmail.com>
2017-07-15 20:38:57 -0300

    Inclusão do PancakeHouseMenu.

---
+++ head-first-labs/design-patterns/Diner/src/main/java/com/github/mdssjc/diner/PancakeHouseMenu.java
@@ -0,0 +1,42 @@
+package com.github.mdssjc.diner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PancakeHouseMenu {
+
+  private final List<MenuItem> menuItems;
+
+  public PancakeHouseMenu() {
+    this.menuItems = new ArrayList<>();
+    initialize();
+  }
+
+  private void initialize() {
+    addItem("K&B’s Pancake Breakfast",
+            "Pancakes with scrambled eggs, and toast",
+            true,
+            2.99);
+    addItem("Regular Pancake Breakfast",
+            "Pancakes with fried eggs, sausage",
+            false,
+            2.99);
+    addItem("Blueberry Pancakes",
+            "Pancakes made with fresh blueberries",
+            true,
+            3.49);
+    addItem("Waffles",
+            "Waffles, with your choice of blueberries or strawberries",
+            true,
+            3.59);
+  }
+
+  public void addItem(final String name, final String description, final boolean vegetarian, final double price) {
+    this.menuItems.add(new MenuItem(name, description,
+                                    vegetarian, price));
+  }
+
+  public List<MenuItem> getMenuItems() {
+    return this.menuItems;
+  }
+}
