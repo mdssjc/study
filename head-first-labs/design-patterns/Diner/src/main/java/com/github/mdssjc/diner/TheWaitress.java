48fc671c1b101035c8aeb3dff79081a8282ce67e
Marcelo dos Santos <mdssjc@gmail.com>
2017-07-15 21:30:27 -0300

    Implementação parcial da Waitress.

---
+++ head-first-labs/design-patterns/Diner/src/main/java/com/github/mdssjc/diner/TheWaitress.java
@@ -0,0 +1,50 @@
+package com.github.mdssjc.diner;
+
+public class TheWaitress implements Waitress {
+
+  private final PancakeHouseMenu pancakeHouseMenu;
+  private final DinerMenu dinerMenu;
+
+  public TheWaitress(final PancakeHouseMenu pancakeHouseMenu,
+                     final DinerMenu dinerMenu) {
+    this.pancakeHouseMenu = pancakeHouseMenu;
+    this.dinerMenu = dinerMenu;
+  }
+
+  @Override
+  public void printMenu() {
+    final Iterator pancakeIterator = this.pancakeHouseMenu.createIterator();
+    final Iterator dinerIterator = this.dinerMenu.createIterator();
+
+    System.out.println("MENU\n----\nBREAKFAST");
+    printMenu(pancakeIterator);
+    System.out.println("\nLUNCH");
+    printMenu(dinerIterator);
+  }
+
+  private void printMenu(final Iterator<MenuItem> iterator) {
+    while (iterator.hasNext()) {
+      final MenuItem menuItem = iterator.next();
+      System.out.print(menuItem.getName() + ", ");
+      System.out.print(menuItem.getPrice() + " -- ");
+      System.out.println(menuItem.getDescription());
+    }
+  }
+
+  @Override
+  public void printBreakfastMenu() {
+  }
+
+  @Override
+  public void printLunchMenu() {
+  }
+
+  @Override
+  public void printVegetarianMenu() {
+  }
+
+  @Override
+  public boolean isItemVegetarian(final String name) {
+    return false;
+  }
+}
