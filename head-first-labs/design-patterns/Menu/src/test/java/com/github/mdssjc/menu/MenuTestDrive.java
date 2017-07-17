a75b811312d65bd591a5a6b128a398ad4a0115c7
Marcelo dos Santos <mdssjc@gmail.com>
2017-07-16 21:41:33 -0300

    Test drive com CafeMenu.

--- head-first-labs/design-patterns/Menu/src/test/java/com/github/mdssjc/menu/MenuTestDrive.java
+++ head-first-labs/design-patterns/Menu/src/test/java/com/github/mdssjc/menu/MenuTestDrive.java
@@ -5,8 +5,10 @@ public class MenuTestDrive {
   public static void main(final String[] args) {
     final Menu pancakeHouseMenu = new PancakeHouseMenu();
     final Menu dinerMenu = new DinerMenu();
+    final Menu cafeMenu = new CafeMenu();
 
-    final Waitress waitress = new TheWaitress(pancakeHouseMenu, dinerMenu);
+    final Waitress waitress = new TheWaitress(pancakeHouseMenu, dinerMenu,
+                                              cafeMenu);
 
     waitress.printMenu();
   }
