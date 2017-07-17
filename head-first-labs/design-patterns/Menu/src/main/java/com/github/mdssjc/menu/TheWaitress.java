3f8e972ce4c00cf79fccb1ad587361f04230baab
Marcelo dos Santos <mdssjc@gmail.com>
2017-07-16 21:41:17 -0300

    Recebe o CafeMenu.

--- head-first-labs/design-patterns/Menu/src/main/java/com/github/mdssjc/menu/TheWaitress.java
+++ head-first-labs/design-patterns/Menu/src/main/java/com/github/mdssjc/menu/TheWaitress.java
@@ -6,22 +6,27 @@ public class TheWaitress implements Waitress {
 
   private final Menu pancakeHouseMenu;
   private final Menu dinerMenu;
+  private final Menu cafeMenu;
 
   public TheWaitress(final Menu pancakeHouseMenu,
-                     final Menu dinerMenu) {
+                     final Menu dinerMenu, final Menu cafeMenu) {
     this.pancakeHouseMenu = pancakeHouseMenu;
     this.dinerMenu = dinerMenu;
+    this.cafeMenu = cafeMenu;
   }
 
   @Override
   public void printMenu() {
     final Iterator pancakeIterator = this.pancakeHouseMenu.createIterator();
     final Iterator dinerIterator = this.dinerMenu.createIterator();
+    final Iterator cafeIterator = this.cafeMenu.createIterator();
 
     System.out.println("MENU\n----\nBREAKFAST");
     printMenu(pancakeIterator);
     System.out.println("\nLUNCH");
     printMenu(dinerIterator);
+    System.out.println("\nDINNER");
+    printMenu(cafeIterator);
   }
 
   private void printMenu(final Iterator<MenuItem> iterator) {
