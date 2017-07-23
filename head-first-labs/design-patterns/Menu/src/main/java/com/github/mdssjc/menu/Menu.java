07475b252716f66d60545e080a4619da3807ca91
Marcelo dos Santos <mdssjc@gmail.com>
2017-07-22 21:41:56 -0300

    Melhoria com Composite.

--- head-first-labs/design-patterns/Menu/src/main/java/com/github/mdssjc/menu/Menu.java
+++ head-first-labs/design-patterns/Menu/src/main/java/com/github/mdssjc/menu/Menu.java
@@ -1,8 +1,50 @@
 package com.github.mdssjc.menu;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
-public interface Menu {
+public class Menu extends MenuComponent {
 
-  Iterator createIterator();
+  private final List<MenuComponent> menuComponents;
+  private final String name;
+  private final String description;
+
+  public Menu(final String name, final String description) {
+    this.name = name;
+    this.description = description;
+    this.menuComponents = new ArrayList<>();
+  }
+
+  public void add(final MenuComponent menuComponent) {
+    this.menuComponents.add(menuComponent);
+  }
+
+  public void remove(final MenuComponent menuComponent) {
+    this.menuComponents.remove(menuComponent);
+  }
+
+  public MenuComponent getChild(final int i) {
+    return this.menuComponents.get(i);
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public String getDescription() {
+    return this.description;
+  }
+
+  public void print() {
+    System.out.print("\n" + getName());
+    System.out.println(", " + getDescription());
+    System.out.println("---------------------");
+
+    final Iterator<MenuComponent> iterator = this.menuComponents.iterator();
+    while (iterator.hasNext()) {
+      final MenuComponent menuComponent = iterator.next();
+      menuComponent.print();
+    }
+  }
 }
--- head-first-labs/design-patterns/Menu/src/main/java/com/github/mdssjc/menu/MenuItem.java
+++ head-first-labs/design-patterns/Menu/src/main/java/com/github/mdssjc/menu/MenuItem.java
@@ -1,32 +1,47 @@
 package com.github.mdssjc.menu;
 
-public class MenuItem {
+public class MenuItem extends MenuComponent {
 
   private final String name;
   private final String description;
   private final boolean vegetarian;
   private final double price;
 
-  public MenuItem(final String name, final String description, final boolean vegetarian, final double price) {
+  public MenuItem(final String name, final String description,
+                  final boolean vegetarian, final double price) {
     this.name = name;
     this.description = description;
     this.vegetarian = vegetarian;
     this.price = price;
   }
 
+  @Override
   public String getName() {
     return this.name;
   }
 
+  @Override
   public String getDescription() {
     return this.description;
   }
 
+  @Override
   public double getPrice() {
     return this.price;
   }
 
+  @Override
   public boolean isVegetarian() {
     return this.vegetarian;
+  }
+
+  @Override
+  public void print() {
+    System.out.println(" " + getName());
+    if (isVegetarian()) {
+      System.out.println("(v)");
+    }
+    System.out.println(", " + getPrice());
+    System.out.println(" -- " + getDescription());
   }
 }
