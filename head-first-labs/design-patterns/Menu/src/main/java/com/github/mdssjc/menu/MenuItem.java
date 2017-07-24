56f9e404185a0f8807d6f7dbe9d589b5d55010d5
Marcelo dos Santos <mdssjc@gmail.com>
2017-07-23 21:43:02 -0300

    Correção dos espaços.

--- head-first-labs/design-patterns/Menu/src/main/java/com/github/mdssjc/menu/MenuItem.java
+++ head-first-labs/design-patterns/Menu/src/main/java/com/github/mdssjc/menu/MenuItem.java
@@ -39,12 +39,12 @@ public class MenuItem extends MenuComponent {
 
   @Override
   public void print() {
-    System.out.print(" " + getName());
+    System.out.print("  " + getName());
     if (isVegetarian()) {
       System.out.print("(v)");
     }
     System.out.println(", " + getPrice());
-    System.out.println(" -- " + getDescription());
+    System.out.println("     -- " + getDescription());
   }
 
   @Override
