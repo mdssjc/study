d53a85e353b349388f684bbb2b8417079e962828
Marcelo dos Santos <mdssjc@gmail.com>
2017-07-29 22:54:12 -0300

    Test drive adicionado.

---
+++ head-first-labs/design-patterns/Gumball/src/test/java/com/github/mdssjc/gumball/GumballMachineTestDrive.java
@@ -0,0 +1,39 @@
+package com.github.mdssjc.gumball;
+
+public class GumballMachineTestDrive {
+
+  public static void main(final String[] args) {
+    final GumballMachine gumballMachine = new GumballMachine(5);
+
+    System.out.println(gumballMachine);
+
+    gumballMachine.insertQuarter();
+    gumballMachine.turnCrank();
+
+    System.out.println(gumballMachine);
+
+    gumballMachine.insertQuarter();
+    gumballMachine.ejectQuarter();
+    gumballMachine.turnCrank();
+
+    System.out.println(gumballMachine);
+
+    gumballMachine.insertQuarter();
+    gumballMachine.turnCrank();
+    gumballMachine.insertQuarter();
+    gumballMachine.turnCrank();
+    gumballMachine.ejectQuarter();
+
+    System.out.println(gumballMachine);
+
+    gumballMachine.insertQuarter();
+    gumballMachine.insertQuarter();
+    gumballMachine.turnCrank();
+    gumballMachine.insertQuarter();
+    gumballMachine.turnCrank();
+    gumballMachine.insertQuarter();
+    gumballMachine.turnCrank();
+
+    System.out.println(gumballMachine);
+  }
+}
