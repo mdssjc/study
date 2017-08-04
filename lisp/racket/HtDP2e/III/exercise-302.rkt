b340c2b05ad1a51a7e46918ae5da9e3a2a7bf08c
Marcelo dos Santos <mdssjc@gmail.com>
2017-08-03 22:13:31 -0300

    Exercício 302.

---
+++ lisp/racket/HtDP2e/III/exercise-302.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname exercise-302) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; Exercise 302
+
+
+; x is used here before its definition
+;(define x (cons 1 x))
