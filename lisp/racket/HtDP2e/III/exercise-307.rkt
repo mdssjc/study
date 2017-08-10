ef182778c9a1684192c3c60afc384a49a77f9b17
Marcelo dos Santos <mdssjc@gmail.com>
2017-08-09 23:02:50 -0300

    Exercício 307 (Parcial).

---
+++ lisp/racket/HtDP2e/III/exercise-307.rkt
@@ -0,0 +1,32 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname exercise-307) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; Exercise 307
+
+(require 2htdp/abstraction)
+
+
+; String [List-of String] -> String
+; retrieves the first name on the latter that is equal to, or an extension of, the former
+(check-expect (find-name "John" empty) false)
+(check-expect (find-name "John" (list "Marie" "Paul")) false)
+(check-expect (find-name "John" (list "Marie" "Paul" "John")) "John")
+(check-expect (find-name "John" (list "Marie" "Paul" "Jones")) "Jones")
+
+(define (find-name n lon)
+  (for/string ([l n])
+    (if (string=? n "John1")
+        n
+        false)))
+
+; Number [List-of String] -> Boolean
+; ensures that no name on some list exceeds some given width
+(check-expect (exceed-width? 5 empty) false)
+(check-expect (exceed-width? 5 (list "alf")) false)
+(check-expect (exceed-width? 5 (list "alf" "marie")) false)
+(check-expect (exceed-width? 5 (list "alf" "marie" "johnson")) true)
+
+(define (exceed-width? n los)
+  (local ((define (larger-n? s)
+            (> (string-length s) n)))
+    (ormap larger-n? los)))
