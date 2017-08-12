1a2214884d71e2629fa7bf185b98b3667cc57efe
Marcelo dos Santos <mdssjc@gmail.com>
2017-08-12 13:43:22 -0300

    Refatoração da classe.

--- head-first-labs/object-oriented-analysis-and-design/routeFinder/src/main/java/com/github/mdssjc/routefinder/subway/Station.java
+++ head-first-labs/object-oriented-analysis-and-design/routeFinder/src/main/java/com/github/mdssjc/routefinder/subway/Station.java
@@ -1,26 +1,18 @@
 package com.github.mdssjc.routefinder.subway;
 
+import lombok.Data;
+
+@Data
 public class Station {
 
   private final String name;
-
-  public Station(final String name) {
-    this.name = name;
-  }
-
-  public String getName() {
-    return this.name;
-  }
 
   @Override
   public boolean equals(final Object obj) {
     if (this == obj) {
       return true;
     }
-    if (obj == null) {
-      return false;
-    }
-    if (getClass() != obj.getClass()) {
+    if (!(obj instanceof Station)) {
       return false;
     }
     final Station other = (Station) obj;
@@ -37,6 +29,6 @@ public class Station {
   @Override
   public int hashCode() {
     return this.name.toLowerCase()
-      .hashCode();
+                    .hashCode();
   }
 }
