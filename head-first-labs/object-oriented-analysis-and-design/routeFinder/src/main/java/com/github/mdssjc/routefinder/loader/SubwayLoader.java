9e836126b8a9eb76b55dd50489fc7c62efeb9023
Marcelo dos Santos <mdssjc@gmail.com>
2017-08-12 15:03:33 -0300

    Refatoração da classe.

--- head-first-labs/object-oriented-analysis-and-design/routeFinder/src/main/java/com/github/mdssjc/routefinder/loader/SubwayLoader.java
+++ head-first-labs/object-oriented-analysis-and-design/routeFinder/src/main/java/com/github/mdssjc/routefinder/loader/SubwayLoader.java
@@ -1,11 +1,11 @@
 package com.github.mdssjc.routefinder.loader;
 
+import com.github.mdssjc.routefinder.subway.Subway;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-
-import com.github.mdssjc.routefinder.subway.Subway;
 
 public class SubwayLoader {
 
@@ -16,11 +16,10 @@ public class SubwayLoader {
   }
 
   public Subway loadFromFile(final File subwayFile) throws IOException {
-    try (final BufferedReader reader = new BufferedReader(
-        new FileReader(subwayFile))) {
+    try (final BufferedReader reader = new BufferedReader(new FileReader(subwayFile))) {
       loadStations(this.subway, reader);
       String lineName = reader.readLine();
-      while ((lineName != null) && (lineName.length() > 0)) {
+      while ((lineName != null) && (!lineName.isEmpty())) {
         loadLine(this.subway, reader, lineName);
         lineName = reader.readLine();
       }
@@ -28,21 +27,19 @@ public class SubwayLoader {
     }
   }
 
-  private static void loadStations(final Subway subway,
-      final BufferedReader reader) throws IOException {
+  private static void loadStations(final Subway subway, final BufferedReader reader) throws IOException {
     String currentLine;
     currentLine = reader.readLine();
-    while (currentLine.length() > 0) {
+    while (!currentLine.isEmpty()) {
       subway.addStation(currentLine);
       currentLine = reader.readLine();
     }
   }
 
-  private static void loadLine(final Subway subway, final BufferedReader reader,
-      final String lineName) throws IOException {
+  private static void loadLine(final Subway subway, final BufferedReader reader, final String lineName) throws IOException {
     String station1Name = reader.readLine();
     String station2Name = reader.readLine();
-    while ((station2Name != null) && (station2Name.length() > 0)) {
+    while ((station2Name != null) && (!station2Name.isEmpty())) {
       subway.addConnection(station1Name, station2Name, lineName);
       station1Name = station2Name;
       station2Name = reader.readLine();
