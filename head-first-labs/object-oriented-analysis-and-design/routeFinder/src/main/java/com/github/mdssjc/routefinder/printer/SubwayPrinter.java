1b3851572788b3b9834541f78dda3a9c1262b070
Marcelo dos Santos <mdssjc@gmail.com>
2017-08-12 15:56:45 -0300

    Refatoração da classe.

--- head-first-labs/object-oriented-analysis-and-design/routeFinder/src/main/java/com/github/mdssjc/routefinder/printer/SubwayPrinter.java
+++ head-first-labs/object-oriented-analysis-and-design/routeFinder/src/main/java/com/github/mdssjc/routefinder/printer/SubwayPrinter.java
@@ -1,10 +1,10 @@
 package com.github.mdssjc.routefinder.printer;
 
+import com.github.mdssjc.routefinder.subway.Connection;
+
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.List;
-
-import com.github.mdssjc.routefinder.subway.Connection;
 
 public class SubwayPrinter {
 
@@ -19,31 +19,28 @@ public class SubwayPrinter {
     String currentLine = connection.getLineName();
     String previousLine = currentLine;
     this.out.println("Start out at " + connection.getStation1()
-      .getName() + ".");
-    this.out.println("Get on the " + currentLine + " heading towards "
-        + connection.getStation2()
-          .getName()
-        + ".");
+                                                 .getName() + ".");
+    this.out.println(
+        "Get on the " + currentLine + " heading towards " + connection.getStation2()
+                                                                      .getName() + ".");
 
     for (int i = 1; i < route.size(); i++) {
       connection = route.get(i);
       currentLine = connection.getLineName();
       if (currentLine.equals(previousLine)) {
         this.out.println(" Continue past " + connection.getStation1()
-          .getName() + "...");
+                                                       .getName() + "...");
       } else {
         this.out.println("When you get to " + connection.getStation1()
-          .getName() + ", get off the " + previousLine + ".");
-        this.out
-          .println("Switch over to the " + currentLine + ", heading towards "
-              + connection.getStation2()
-                .getName()
-              + ".");
+                                                        .getName() + ", get off the " + previousLine + ".");
+        this.out.println(
+            "Switch over to the " + currentLine + ", heading towards " + connection.getStation2()
+                                                                                   .getName() + ".");
         previousLine = currentLine;
       }
     }
 
     this.out.println("Get off at " + connection.getStation2()
-      .getName() + " and enjoy yourself!");
+                                               .getName() + " and enjoy yourself!");
   }
 }
