71b9a67e4ce72457169c39c92db192ce07cd509d
Marcelo dos Santos <mdssjc@gmail.com>
2017-08-12 15:12:27 -0300

    Refatoração da classe.

--- head-first-labs/object-oriented-analysis-and-design/routeFinder/src/test/java/com/github/mdssjc/routefinder/LoadTester.java
+++ head-first-labs/object-oriented-analysis-and-design/routeFinder/src/test/java/com/github/mdssjc/routefinder/LoadTester.java
@@ -1,9 +1,9 @@
 package com.github.mdssjc.routefinder;
 
-import java.io.File;
-
 import com.github.mdssjc.routefinder.loader.SubwayLoader;
 import com.github.mdssjc.routefinder.subway.Subway;
+
+import java.io.File;
 
 public class LoadTester {
 
@@ -11,9 +11,8 @@ public class LoadTester {
     try {
       final SubwayLoader loader = new SubwayLoader();
       final String file = ClassLoader.getSystemResource("ObjectvilleSubway.txt")
-        .getPath();
-      final Subway objectville = loader
-        .loadFromFile(new File(file));
+                                     .getPath();
+      final Subway objectville = loader.loadFromFile(new File(file));
 
       System.out.println("Testing stations...");
       if (objectville.hasStation("DRY Drive") &&
@@ -24,14 +23,15 @@ public class LoadTester {
         System.out.println("...station test FAILED.");
         System.exit(-1);
       }
+
       System.out.println("\nTesting connections...");
       if (objectville.hasConnection("DRY Drive",
-          "Head First Theater", "Meyer Line") &&
+                                    "Head First Theater", "Meyer Line") &&
           objectville.hasConnection("Weather-O-Rama, Inc.",
-              "XHTML Expressway", "Wirfs-Brock Line")
-          &&
+                                    "XHTML Expressway", "Wirfs-Brock Line") &&
+
           objectville.hasConnection("Head First Theater",
-              "Infinite Circle", "Rumbaugh Line")) {
+                                    "Infinite Circle", "Rumbaugh Line")) {
         System.out.println("...connections test passed successfully.");
       } else {
         System.out.println("...connections test FAILED.");
