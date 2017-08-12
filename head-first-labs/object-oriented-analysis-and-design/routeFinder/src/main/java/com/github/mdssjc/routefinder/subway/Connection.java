0d336582bcd12f4a97dda2c56dbbeef3fb0e41fc
Marcelo dos Santos <mdssjc@gmail.com>
2017-08-12 13:47:03 -0300

    Refatoração da classe.

--- head-first-labs/object-oriented-analysis-and-design/routeFinder/src/main/java/com/github/mdssjc/routefinder/subway/Connection.java
+++ head-first-labs/object-oriented-analysis-and-design/routeFinder/src/main/java/com/github/mdssjc/routefinder/subway/Connection.java
@@ -1,27 +1,11 @@
 package com.github.mdssjc.routefinder.subway;
 
+import lombok.Data;
+
+@Data
 public class Connection {
 
   private final Station station1;
   private final Station station2;
-  private final String  lineName;
-
-  public Connection(final Station station1, final Station station2,
-      final String lineName) {
-    this.station1 = station1;
-    this.station2 = station2;
-    this.lineName = lineName;
-  }
-
-  public Station getStation1() {
-    return this.station1;
-  }
-
-  public Station getStation2() {
-    return this.station2;
-  }
-
-  public String getLineName() {
-    return this.lineName;
-  }
+  private final String lineName;
 }
