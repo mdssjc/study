ed904d2bd5602420be4b80d238aefba481e13fed
Marcelo dos Santos <mdssjc@gmail.com>
2017-08-12 22:02:05 -0300

    Test drive com RMI.

--- head-first-labs/design-patterns/Gumball/src/test/java/com/github/mdssjc/gumball/GumballMachineTestDrive.java
+++ head-first-labs/design-patterns/Gumball/src/test/java/com/github/mdssjc/gumball/GumballMachineTestDrive.java
@@ -1,50 +1,61 @@
 package com.github.mdssjc.gumball;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.util.logging.Logger;
+
 public class GumballMachineTestDrive {
 
   public static void main(final String[] args) {
-    int count;
+    final int count;
 
     if (args.length < 2) {
       System.out.println("GumballMachine <name> <inventory>");
       System.exit(1);
     }
 
-    count = Integer.parseInt(args[1]);
-    final GumballMachine gumballMachine = new GumballMachine(args[0], count);
-    final GumballMonitor monitor = new GumballMonitor(gumballMachine);
+    try {
+      count = Integer.parseInt(args[1]);
+      final GumballMachine gumballMachine = new GumballMachine(args[0], count);
+      Naming.rebind("//" + args[0] + "/gumballmachine", gumballMachine);
+      final GumballMonitor monitor = new GumballMonitor(gumballMachine);
 
-    System.out.println(gumballMachine);
+      System.out.println(gumballMachine);
 
-    gumballMachine.insertQuarter();
-    gumballMachine.turnCrank();
+      gumballMachine.insertQuarter();
+      gumballMachine.turnCrank();
 
-    System.out.println(gumballMachine);
+      System.out.println(gumballMachine);
 
-    gumballMachine.insertQuarter();
-    gumballMachine.ejectQuarter();
-    gumballMachine.turnCrank();
+      gumballMachine.insertQuarter();
+      gumballMachine.ejectQuarter();
+      gumballMachine.turnCrank();
 
-    System.out.println(gumballMachine);
+      System.out.println(gumballMachine);
 
-    gumballMachine.insertQuarter();
-    gumballMachine.turnCrank();
-    gumballMachine.insertQuarter();
-    gumballMachine.turnCrank();
-    gumballMachine.ejectQuarter();
+      gumballMachine.insertQuarter();
+      gumballMachine.turnCrank();
+      gumballMachine.insertQuarter();
+      gumballMachine.turnCrank();
+      gumballMachine.ejectQuarter();
 
-    System.out.println(gumballMachine);
+      System.out.println(gumballMachine);
 
-    gumballMachine.insertQuarter();
-    gumballMachine.insertQuarter();
-    gumballMachine.turnCrank();
-    gumballMachine.insertQuarter();
-    gumballMachine.turnCrank();
-    gumballMachine.insertQuarter();
-    gumballMachine.turnCrank();
+      gumballMachine.insertQuarter();
+      gumballMachine.insertQuarter();
+      gumballMachine.turnCrank();
+      gumballMachine.insertQuarter();
+      gumballMachine.turnCrank();
+      gumballMachine.insertQuarter();
+      gumballMachine.turnCrank();
 
-    System.out.println(gumballMachine);
+      System.out.println(gumballMachine);
 
-    monitor.report();
+      monitor.report();
+    } catch (RemoteException | MalformedURLException e) {
+      Logger.getGlobal()
+            .info(e.getMessage());
+    }
   }
 }
---
+++ head-first-labs/design-patterns/Gumball/src/test/java/com/github/mdssjc/gumball/GumballMonitorTestDrive.java
@@ -0,0 +1,35 @@
+package com.github.mdssjc.gumball;
+
+import java.rmi.Naming;
+import java.util.logging.Logger;
+
+public class GumballMonitorTestDrive {
+
+  public static void main(final String[] args) {
+//    final String[] location = {
+//        "rmi://127.0.0.1/gumballmachine",
+//        "rmi://santafe.mightygumball.com/gumballmachine",
+//        "rmi://boulder.mightygumball.com/gumballmachine",
+//        "rmi://seattle.mightygumball.com/gumballmachine"};
+
+    final String[] location = {"rmi://127.0.0.1/gumballmachine"};
+
+    final GumballMonitor[] monitor = new GumballMonitor[location.length];
+
+    for (int i = 0; i < location.length; i++) {
+      try {
+        final GumballMachineRemote machine =
+            (GumballMachineRemote) Naming.lookup(location[i]);
+        monitor[i] = new GumballMonitor(machine);
+        System.out.println(monitor[i]);
+      } catch (final Exception e) {
+        Logger.getGlobal()
+              .info(e.getMessage());
+      }
+    }
+
+    for (final GumballMonitor m : monitor) {
+      m.report();
+    }
+  }
+}
