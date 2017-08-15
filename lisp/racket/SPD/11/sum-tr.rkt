6fcec4d15cb40de31893da55483a8752da70c470
Marcelo dos Santos <mdssjc@gmail.com>
2017-08-15 18:57:11 -0300

    Conclusão do exercício sobre Tail Recursion.

--- lisp/racket/SPD/11/sum-tr.rkt
+++ lisp/racket/SPD/11/sum-tr.rkt
@@ -17,8 +17,25 @@
 (check-expect (sum empty) 0)
 (check-expect (sum (list 2 4 5)) 11)
 
+#;
 (define (sum lon)
   (cond [(empty? lon) 0]
         [else
          (+ (first lon)
             (sum (rest lon)))]))
+
+(define (sum lon0)
+  ;; acc: Number; the sum of the elements of lon0 seen so far
+  ;; (sum (list 2 4 5))
+  ;; (sum (list 2 4 5)  0)
+  ;; (sum (list   4 5)  2)
+  ;; (sum (list     5)  6)
+  ;; (sum (list      ) 11)
+  (local ((define (sum lon acc)
+            (cond [(empty? lon) acc]
+                  [else
+                   (sum (rest lon)
+                        (+ acc (first lon)))])))
+    (sum lon0 0)))
+
+(sum (list 1 2 3 4 5))
