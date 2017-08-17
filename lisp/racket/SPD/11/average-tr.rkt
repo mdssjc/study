bfe4c40c19a5b21e94886499172b55da3505b50f
Marcelo dos Santos <mdssjc@gmail.com>
2017-08-17 20:14:04 -0300

    Accumulators P4 - Average Tail Recursive.

---
+++ lisp/racket/SPD/11/average-tr.rkt
@@ -0,0 +1,32 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname average-tr) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; average.rkt
+;; Accumulators P4 - Average Tail Recursive
+;; Design a tail-recursive function that produces the average of
+;; numbers in a list.
+
+
+;; PROBLEM:
+;;
+;; Design a function called average that consumes (listof Number) and
+;; produces the average of the numbers in the list.
+
+;; (listof Number) -> Number
+;; produces the average of the numbers in the list
+(check-expect (average empty) 0)
+(check-expect (average (list 2)) 2)
+(check-expect (average (list 1 2 3)) 2)
+(check-expect (average (list 4 8 2)) (/ (+ 4 8 2) 3))
+
+;(define (average lon) 0) ; Stub
+
+(define (average lon0)
+  (local ((define len (length lon0))
+          (define (average lon sum acc)
+            (cond [(empty? lon) 0]
+                  [else
+                   (if (= len acc)
+                       (/ (+ (first lon) sum) acc)
+                       (average (rest lon) (+ (first lon) sum) (add1 acc)))])))
+    (average lon0 0 1)))
