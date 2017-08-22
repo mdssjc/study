1ce5b39a62b4dddb029dd896a2c2148c03746746
Marcelo dos Santos <mdssjc@gmail.com>
2017-08-22 19:47:09 -0300

    Constructing Cyclic Data, Pt. 1.

---
+++ lisp/racket/SPD/12/graphs-v2.rkt
@@ -0,0 +1,50 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname graphs-v2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; graphs-v2.rkt
+
+
+;; PROBLEM:
+;;
+;; Imagine you are suddenly transported into a mysterious house, in which all you can
+;; see is the name of the room you are in, and any doors that lead OUT of the room.
+;; One of the things that makes the house so mysterious is that the doors only go in
+;; one direction. You can't see the doors that lead into the room.
+;;
+;; Here are some examples of such a house:
+;;
+;;   A -> B
+;;
+;;   A -> B -> A
+;;
+;;   A -> B -> C -> A
+;;
+;;   A -> B D
+;;   B -> C E
+;;   C -> B
+;;   D -> E
+;;   E -> F A
+;;   F
+;;
+;; In computer science, we refer to such an information structure as a directed graph.
+;; Like trees, in directed graphs the arrows have direction. But in a graph it is
+;; possible to go in circles, as in the second example above. It is also possible for
+;; two arrows to lead into a single node, as in the fourth example.
+;;
+;;
+;; Design a data definition to represent such houses. Also provide example data for
+;; the four houses above.
+
+(define-struct room (name exits))
+;; Room is (make-room String (listof Room))
+;; interp. the room's name, and list of rooms that the exits lead to
+
+(define H1 (make-room "A" (list (make-room "B" empty))))
+
+(define H2
+  (shared ((-0- (make-room "A" (list (make-room "B" (list -0-))))))
+    -0-))
+
+(define H3
+  (shared ((-0- (make-room "A" (list (make-room "B" (list (make-room "C" (list -0-))))))))
+    -0-))
