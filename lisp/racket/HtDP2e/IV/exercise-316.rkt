8a93148752050412ec390851c98b882ba7c1be70
Marcelo dos Santos <mdssjc@gmail.com>
2017-08-21 22:32:07 -0300

    Exercício 316.

---
+++ lisp/racket/HtDP2e/IV/exercise-316.rkt
@@ -0,0 +1,35 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname exercise-316) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; Exercise 316
+
+
+;; ====================
+;; Data definitions:
+
+; An S-expr is one of:
+; - Atom
+; - SL
+ 
+; An SL is one of:
+; - '()
+; - (cons S-expr SL)
+	
+; An Atom is one of:
+; - Number
+; - String
+; - Symbol
+
+
+;; ====================
+;; Functions:
+
+; Atom -> Boolean
+; predicates if it's an atom
+(check-expect (atom? #true) #false)
+(check-expect (atom? 1) #true)
+(check-expect (atom? "abc") #true)
+(check-expect (atom? 'sb) #true)
+
+(define (atom? a)
+  (or (number? a) (string? a) (symbol? a)))
