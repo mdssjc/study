a74b4fe6378129e9ed7e56b8f3dc6a5370f78a1c
Marcelo dos Santos <mdssjc@gmail.com>
2017-08-23 14:43:23 -0300

    Interface do Model.

---
+++ head-first-labs/design-patterns/DJView/src/main/java/com/github/mdssjc/djview/BeatModelInterface.java
@@ -0,0 +1,22 @@
+package com.github.mdssjc.djview;
+
+public interface BeatModelInterface {
+
+  void initialize();
+
+  void on();
+
+  void off();
+
+  int getBPM();
+
+  void setBPM(int bpm);
+
+  void registerObserver(BeatObserver o);
+
+  void removeObserver(BeatObserver o);
+
+  void registerObserver(BPMObserver o);
+
+  void removeObserver(BPMObserver o);
+}
