8060716913b9aee0bcbcc0093ae61a815a8ce2da
Marcelo dos Santos <mdssjc@gmail.com>
2017-08-24 14:38:10 -0300

    Melhorias contra null.

--- head-first-labs/design-patterns/DJView/src/main/java/com/github/mdssjc/djview/DJView.java
+++ head-first-labs/design-patterns/DJView/src/main/java/com/github/mdssjc/djview/DJView.java
@@ -128,16 +128,24 @@ public class DJView implements ActionListener, BeatObserver, BPMObserver {
   }
 
   public void updateBPM() {
-    final int bpm = this.model.getBPM();
-    if (bpm == 0) {
-      this.bpmOutputLabel.setText("offline");
-    } else {
-      this.bpmOutputLabel.setText("Current BPM: " + this.model.getBPM());
+    if (this.model != null) {
+      final int bpm = this.model.getBPM();
+      if (bpm == 0) {
+        if (this.bpmOutputLabel != null) {
+          this.bpmOutputLabel.setText("offline");
+        }
+      } else {
+        if (this.bpmOutputLabel != null) {
+          this.bpmOutputLabel.setText("Current BPM: " + this.model.getBPM());
+        }
+      }
     }
   }
 
   public void updateBeat() {
-    this.beatBar.setValue(100);
+    if (this.beatBar != null) {
+      this.beatBar.setValue(100);
+    }
   }
 
   @Override
