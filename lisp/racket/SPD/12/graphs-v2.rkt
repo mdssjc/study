163b1070dcc3812f65f5d0546c7cc99028e92752
Marcelo dos Santos <mdssjc@gmail.com>
2017-08-23 21:28:00 -0300

    Problem: reachable?.

--- lisp/racket/SPD/12/graphs-v2.rkt
+++ lisp/racket/SPD/12/graphs-v2.rkt
@@ -68,6 +68,7 @@
 ;; template: structural recursion, encapsulate w/ local, tail-recursive w/ worklist,
 ;;           context-preserving accumulator what rooms have we already visited
 
+#;
 (define (fn-for-house r0)
   ;; todo is (listof Room); a worklist accumulator
   ;; visited is (listof String); context preserving accumulator, names of rooms already visited
@@ -78,6 +79,47 @@
                             (cons (room-name r) visited)))) ; (... (room-name r))
           (define (fn-for-lor todo visited)
             (cond [(empty? todo) (...)]
+                  [else
+                   (fn-for-room (first todo)
+                                (rest todo)
+                                visited)]))]
+    (fn-for-room r0 empty empty)))
+
+;; PROBLEM:
+;;
+;; Design a function that consumes a Room and a room name, and
+;; produces true if it is possible to reach a room with the given name
+;; starting at the given room. For example:
+;;
+;;   (reachable? H1 "A") produces true
+;;   (reachable? H1 "B") produces true
+;;   (reachable? H1 "C") produces false
+;;   (reachable? H4 "F") produces true
+;;
+;; But note that if you defined H4F to be the room named F in the H4
+;; house then (reachable? H4F "A") would produce false because it is
+;; not possible to get to A from F in that house.
+
+;; Room String -> Boolean
+;; produce true if starting at r0 it is possible to reach a room named rn
+
+(check-expect (reachable? H1 "A") true)
+(check-expect (reachable? H1 "B") true)
+(check-expect (reachable? H1 "C") false)
+(check-expect (reachable? (first (room-exits H1)) "A") false)
+(check-expect (reachable? H4 "F") true)
+
+(define (reachable? r0 rn)
+  ;; todo is (listof Room); a worklist accumulator
+  ;; visited is (listof String); context preserving accumulator, names of rooms already visited
+  (local [(define (fn-for-room r todo visited)
+            (cond [(string=? (room-name r) rn) true]
+                  [(member (room-name r) visited) (fn-for-lor todo visited)]
+                  [else
+                   (fn-for-lor (append (room-exits r) todo)
+                               (cons (room-name r) visited))])) ; (... (room-name r))
+          (define (fn-for-lor todo visited)
+            (cond [(empty? todo) false]
                   [else
                    (fn-for-room (first todo)
                                 (rest todo)
