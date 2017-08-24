7d8b7156001c5b862dfab3e5406c798716ba2e01
Marcelo dos Santos <mdssjc@gmail.com>
2017-08-23 21:11:53 -0300

    Templating.

--- lisp/racket/SPD/12/graphs-v2.rkt
+++ lisp/racket/SPD/12/graphs-v2.rkt
@@ -64,3 +64,22 @@
            (-E- (make-room "E" (list -F- -A-)))
            (-F- (make-room "F" (list))))
     -A-))
+
+;; template: structural recursion, encapsulate w/ local, tail-recursive w/ worklist,
+;;           context-preserving accumulator what rooms have we already visited
+
+(define (fn-for-house r0)
+  ;; todo is (listof Room); a worklist accumulator
+  ;; visited is (listof String); context preserving accumulator, names of rooms already visited
+  (local [(define (fn-for-room r todo visited)
+            (if (member (room-name r) visited)
+                (fn-for-lor todo visited)
+                (fn-for-lor (append (room-exits r) todo)
+                            (cons (room-name r) visited)))) ; (... (room-name r))
+          (define (fn-for-lor todo visited)
+            (cond [(empty? todo) (...)]
+                  [else
+                   (fn-for-room (first todo)
+                                (rest todo)
+                                visited)]))]
+    (fn-for-room r0 empty empty)))
