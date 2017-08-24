776e90cb0e2b9a783d776ff8f8ad80fa877a102b
Marcelo dos Santos <mdssjc@gmail.com>
2017-08-24 13:54:02 -0300

    Controller da aplicação.

---
+++ head-first-labs/design-patterns/DJView/src/main/java/com/github/mdssjc/djview/ControllerInterface.java
@@ -0,0 +1,14 @@
+package com.github.mdssjc.djview;
+
+public interface ControllerInterface {
+
+  void start();
+
+  void stop();
+
+  void increaseBPM();
+
+  void decreaseBPM();
+
+  void setBPM(int bpm);
+}
