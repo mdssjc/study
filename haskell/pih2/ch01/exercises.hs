6ffa08e6bac20b9b2adbb17cd393040c709ff7ec
Marcelo dos Santos <mdssjc@gmail.com>
2017-09-03 18:42:20 -0300

    Exercise 1.3.

--- haskell/pih2/ch01/exercises.hs
+++ haskell/pih2/ch01/exercises.hs
@@ -6,3 +6,8 @@ double x = 2 * x
 sum' :: Num a => [a] -> a
 sum' []     = 0
 sum' (x:xs) = x + sum' xs
+
+-- 1.3
+product' :: Num a => [a] -> a
+product' []     = 1
+product' (x:xs) = x * product' xs
