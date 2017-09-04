8db91f13e916961a6a7700e8590202226bfdab38
Marcelo dos Santos <mdssjc@gmail.com>
2017-09-03 22:25:31 -0300

    Previne o processamento do formulário.

--- javascript/jscangaceiro/client/app/index.js
+++ javascript/jscangaceiro/client/app/index.js
@@ -13,13 +13,14 @@ var tbody = document.querySelector('table tbody');
 document.querySelector('.form')
     .addEventListener('submit',
                       function(event) {
+                          event.preventDefault();
+
                           var tr = document.createElement('tr');
                           campos.forEach(function(campo) {
                               var td = document.createElement('td');
                               td.textContent = campo.value;
                               tr.appendChild(td);
                           });
-
 
                           var tdVolume = document.createElement('td');
                           tdVolume.textContext = campos[1].value * campos[2].value;
