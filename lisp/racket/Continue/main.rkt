32e30785a7c7c0f23b546e2ed259dc40a31786c0
Marcelo dos Santos <mdssjc@gmail.com>
2017-09-05 22:15:10 -0300

    Basic Blog.

--- lisp/racket/Continue/main.rkt
+++ lisp/racket/Continue/main.rkt
@@ -1,6 +1,19 @@
 #lang web-server/insta
 
 
+;; =================
+;; Data definitions:
+
+(struct post (title body))
+(define P1 (post "New release" "The new version..."))
+(define P2 (post "Summary #3" "02/05 - all tests are ok."))
+(define P3 (post "Courses" "HTDP - new course in..."))
+(define BLOG (list (post "First Post!"
+                         "Hey, this is my first post!")))
+
+
+;; ==========
+;; Functions:
 (define (start request)
   (response/xexpr
    '(html
