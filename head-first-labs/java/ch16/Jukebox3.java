41f4cc79d198c6c33673c5c5ecb2a0628a69cf86
Marcelo dos Santos <mdssjc@gmail.com>
2017-09-07 13:55:01 -0300

    Capítulo 16.

---
+++ head-first-labs/java/ch16/Jukebox3.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Jukebox3 {
+
+  ArrayList<Song> songList = new ArrayList<>();
+
+  public static void main(String[] args) {
+    new Jukebox3().go();
+  }
+
+  public void go() {
+    getSongs();
+    System.out.println(songList);
+    Collections.sort(songList);
+    System.out.println(songList);
+  }
+
+  void getSongs() {
+    try {
+      File file = new File("SongList.txt");
+      BufferedReader reader = new BufferedReader(new FileReader(file));
+      String line;
+      while ((line = reader.readLine()) != null) {
+        addSong(line);
+      }
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  void addSong(String lineToParse) {
+    String[] tokens = lineToParse.split("/");
+    Song nextSong = new Song(tokens[0], tokens[1], tokens[2], tokens[3]);
+    songList.add(nextSong);
+  }
+}
