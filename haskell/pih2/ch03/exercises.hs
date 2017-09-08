93fff44d57eb9f2d5d86c0ffc4bbc5977e02d9dc
Marcelo dos Santos <mdssjc@gmail.com>
2017-09-07 21:33:26 -0300

    Exercise 3.1.

---
+++ haskell/pih2/ch03/exercises.hs
@@ -0,0 +1,6 @@
+-- 3.1
+x1 = ['a','b','c']              -- x1 :: [Char]
+x2 = ('a','b','c')              -- x2 :: (Char, Char, Char)
+x3 = [(False,'O'),(True,'1')]   -- x3 :: [(Bool, Char)]
+x4 = ([False,True],['o','1'])   -- x4 :: ([Bool], [Char])
+x5 = [tail,init,reverse]        -- x5 :: [[a] -> [a]]
