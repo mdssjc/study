abff699f2d5bc904b077a75c9ad46483aeecc851
Marcelo dos Santos <mdssjc@gmail.com>
2017-09-08 20:12:06 -0300

    Associando métodos do controller às ações do usuário.

--- javascript/jscangaceiro/client/app/controllers/NegociacaoController.js
+++ javascript/jscangaceiro/client/app/controllers/NegociacaoController.js
@@ -5,12 +5,14 @@ class NegociacaoController {
   adiciona(event) {
     event.preventDefault();
 
-    let inputData = document.querySelector('#data');
-    let inputQuantidade = document.querySelector('#quantidade');
-    let inputValor = document.querySelector('#valor');
+    let $ = document.querySelector.bind(document);
+
+    let inputData = $('#data');
+    let inputQuantidade = $('#quantidade');
+    let inputValor = $('#valor');
 
     console.log(inputData.value);
-    console.log(inputQuantidade.value);
-    console.log(inputValor.value);
+    console.log(parseInt(inputQuantidade.value));
+    console.log(parseFloat(inputValor.value));
   }
 }
