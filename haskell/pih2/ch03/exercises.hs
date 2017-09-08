e01761ac0565ae7281ee7c1cb1e4ca1f9d5c3dce
Marcelo dos Santos <mdssjc@gmail.com>
2017-09-07 21:45:09 -0300

    Exercise 3.3.

--- haskell/pih2/ch03/exercises.hs
+++ haskell/pih2/ch03/exercises.hs
@@ -20,3 +20,22 @@ copy a = (a,a)
 
 apply :: (a -> b) -> a -> b
 apply f a = f a
+
+-- 3.3
+second :: [a] -> a
+second xs = head(tail xs)
+
+swap :: (a, b) -> (b, a)
+swap (x,y) = (y,x)
+
+pair' :: a -> b -> (a, b)
+pair' x y = (x,y)
+
+double' :: Num a => a -> a
+double' x = x*2
+
+palindrome :: Eq a => [a] -> Bool
+palindrome xs = reverse xs == xs
+
+twice :: (a -> a) -> a -> a
+twice f x = f(f x)
