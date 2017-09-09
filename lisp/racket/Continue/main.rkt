5e83bc305cf39d585e542f71dd36e34f9db40fde
Marcelo dos Santos <mdssjc@gmail.com>
2017-09-08 23:17:20 -0300

    Rendering HTML: render-post.

--- lisp/racket/Continue/main.rkt
+++ lisp/racket/Continue/main.rkt
@@ -15,8 +15,23 @@
 ;; ==========
 ;; Functions:
 
+; render-greeting: string -> response
+; consumes a name, and produces a dynamic response
+(define (render-greeting a-name)
+  (response/xexpr
+   `(html (head (title "Welcome"))
+          (body (p ,(string-append "Hello " a-name))))))
+
+; render-post: post -> xexpr/c
+; consumes a post and produces an X-expression representing that content
+(define (render-post p)
+  `(div ((class "post"))
+        ,(post-title p)
+        (p ,(post-body p))))
+
 (define (start request)
   (response/xexpr
-   '(html
+   `(html
      (head (title "My Blog"))
-     (body (h1 "Under construction")))))
+     (body (h1 "Under construction")
+           ,(render-post (post "First post!" "This is a first post."))))))
