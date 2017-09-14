52382beed509ff87ad3fccdd3f626a6516698663
Marcelo dos Santos <mdssjc@gmail.com>
2017-09-13 22:23:51 -0300

    Exercise 4.2.

--- haskell/pih2/ch04/exercises.hs
+++ haskell/pih2/ch04/exercises.hs
@@ -2,3 +2,9 @@
 halve :: [a] -> ([a], [a])
 halve xs = (take n xs, drop n xs)
   where n = length xs `div` 2
+
+-- 4.2
+third :: [a] -> a
+--third xs = head (tail (tail xs)) -- a. head and tail
+--third xs = xs !! 2               -- b. list indexing !!
+third (_:_:x:_) = x                -- c. pattern matching
