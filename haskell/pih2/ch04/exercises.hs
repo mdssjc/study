4b19db99496bd8733b1f0f4f6c0abf197eb55b6e
Marcelo dos Santos <mdssjc@gmail.com>
2017-09-14 22:32:15 -0300

    Exercise 4.3.

--- haskell/pih2/ch04/exercises.hs
+++ haskell/pih2/ch04/exercises.hs
@@ -8,3 +8,14 @@ third :: [a] -> a
 --third xs = head (tail (tail xs)) -- a. head and tail
 --third xs = xs !! 2               -- b. list indexing !!
 third (_:_:x:_) = x                -- c. pattern matching
+
+-- 4.3
+safetail :: [a] -> [a]
+-- safetail xs = if null xs        -- a. conditional expression
+--               then []
+--               else tail xs
+-- safetail xs                     -- b. guarded expression
+--   | null xs = []
+--   | otherwise = tail xs
+safetail [] = []                   -- c. pattern matching
+safetail xs = tail xs
