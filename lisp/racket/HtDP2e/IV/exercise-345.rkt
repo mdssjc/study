90d7435addf2610248bcde89a62d63eaffb094db
Marcelo dos Santos <mdssjc@gmail.com>
2017-09-18 23:20:55 -0300

    Exercício 345.

---
+++ lisp/racket/HtDP2e/IV/exercise-345.rkt
@@ -0,0 +1,29 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname exercise-345) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; Exercise 345
+
+
+;; ====================
+;; Data definitions:
+
+(define-struct add [left right])
+; An Add is a structure:
+;   (make-add Number Number)
+; interpretation (make-add l r) specifies an addition expression
+;  l: is the left operand; and
+;  r: is the right operand
+
+(define-struct mul [left right])
+; An Mul is a structure:
+;   (make-mul Number Number)
+; interpretation (make-mul l r) specifies an multiplication expression
+;  l: is the left operand; and
+;  r: is the right operand
+
+(define E1 (make-add 10 -10))
+(define E2 (make-add (make-mul 20 3) 33))
+(define E3 (make-add (make-mul (make-mul 2 3)) (make-mul 3.14 (make-mul -1 -9))))
+; (+ -1 2)
+; (+ (* -2 -3) 33)
+; (* (+ 1 (* 2 3)) 3.14)
