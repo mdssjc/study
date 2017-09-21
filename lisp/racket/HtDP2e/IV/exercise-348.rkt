22622385fd12863840cae78d6a3348b884e8d5b0
Marcelo dos Santos <mdssjc@gmail.com>
2017-09-20 23:01:35 -0300

    Exercício 348.

---
+++ lisp/racket/HtDP2e/IV/exercise-348.rkt
@@ -0,0 +1,63 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname exercise-348) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; Exercise 348
+
+
+;; ====================
+;; Data definitions:
+
+(define-struct and2 [left right])
+; An And is a structure:
+;   (make-and Boolean Boolean)
+; interpretation (make-and2 l r) specifies an AND operation
+;  l: is the left operand; and
+;  r: is the right operand
+
+(define-struct or2 [left right])
+; An Or is a structure:
+;   (make-or Boolean Boolean)
+; interpretation (make-or2 l r) specifies an OR operation
+;  l: is the left operand; and
+;  r: is the right operand
+
+(define-struct not2 [operand])
+; A Not is a structure:
+;   (make-not Boolean)
+; interpretation (make-not2 o) specifies a NOT operation
+;  o: is an operand
+
+; A BSL-Boolean is one of:
+;  - #false
+;  - #true
+;  - (make-and2 BSL-Boolean BSL-Boolean)
+;  - (make-or2  BSL-Boolean BSL-Boolean)
+;  - (make-not2 BSL-Boolean)
+; interpretation class of values to which a representation of a BSL boolean expression can evaluate
+
+
+;; ====================
+;; Functions:
+
+; BSL-Boolean -> Boolean
+; computes their values
+(check-expect (eval-bool-expression #false) #false)
+(check-expect (eval-bool-expression #true) #true)
+(check-expect (eval-bool-expression (make-and2 #true #true)) #true)
+(check-expect (eval-bool-expression (make-and2 #true #false)) #false)
+(check-expect (eval-bool-expression (make-or2 #true #false)) #true)
+(check-expect (eval-bool-expression (make-or2 #false #false)) #false)
+(check-expect (eval-bool-expression (make-not2 #false)) #true)
+(check-expect (eval-bool-expression (make-not2 #true)) #false)
+(check-expect (eval-bool-expression (make-not2 (make-and2 #true (make-or2 #false #true)))) #false)
+
+(define (eval-bool-expression b)
+  (cond [(boolean? b) b]
+        [(and2? b)
+         (and (eval-bool-expression (and2-left b))
+              (eval-bool-expression (and2-right b)))]
+        [(or2? b)
+         (or (eval-bool-expression (or2-left b))
+             (eval-bool-expression (or2-right b)))]
+        [(not2? b)
+         (not (eval-bool-expression (not2-operand b)))]))
