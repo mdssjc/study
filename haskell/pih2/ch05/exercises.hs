1ab0bc4c7a9c6bf5942436c2375cfd669981cbfd
Marcelo dos Santos <mdssjc@gmail.com>
2017-09-21 21:59:50 -0300

    Exercise 5.4.

--- haskell/pih2/ch05/exercises.hs
+++ haskell/pih2/ch05/exercises.hs
@@ -8,3 +8,7 @@ grid x y = [(a, b) | a <- [0..x], b <- [0..y]]
 -- 5.3
 square :: Int -> [(Int, Int)]
 square x = [(a, b) | a <- [0..x], b <- [0..x], a /= b]
+
+-- 5.4
+replicate' :: Int -> a -> [a]
+replicate' n t = [t | _ <- [1..n]]
