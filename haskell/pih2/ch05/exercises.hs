2880ca8593b409f86f2f6ddb4e6bf2b525ee8997
Marcelo dos Santos <mdssjc@gmail.com>
2017-09-21 21:57:54 -0300

    Exercise 5.3.

--- haskell/pih2/ch05/exercises.hs
+++ haskell/pih2/ch05/exercises.hs
@@ -4,3 +4,7 @@ res = [x | x <- [12,22..1002]]
 -- 5.2
 grid :: Int -> Int -> [(Int, Int)]
 grid x y = [(a, b) | a <- [0..x], b <- [0..y]]
+
+-- 5.3
+square :: Int -> [(Int, Int)]
+square x = [(a, b) | a <- [0..x], b <- [0..x], a /= b]
