fd5c24cd4689e9e0b1a0c10f2be539e0dd138dc4
Marcelo dos Santos <mdssjc@gmail.com>
2017-09-24 19:56:52 -0300

    Nosso Proxy ainda não está 100%.

--- javascript/jscangaceiro/client/app/util/ProxyFactory.js
+++ javascript/jscangaceiro/client/app/util/ProxyFactory.js
@@ -5,8 +5,7 @@ class ProxyFactory {
   static create(objeto, props, armadilha) {
     return new Proxy(objeto, {
       get(target, prop, receiver) {
-        if (typeof (target[prop]) == typeof (Function) &&
-          props.includes(prop)) {
+        if (ProxyFactory._ehFuncao(target[prop]) && props.includes(prop)) {
           return function () {
             console.log(`"${prop}" disparou a armadilha`);
             target[prop].apply(target, arguments);
@@ -15,7 +14,21 @@ class ProxyFactory {
         } else {
           return target[prop];
         }
+      },
+
+      set(target, prop, value, receiver) {
+        const updated = Reflect.set(target, prop, value);
+
+        if (props.includes(prop)) {
+          armadilha(target);
+        }
+
+        return updated;
       }
     })
+  }
+
+  static _ehFuncao(fn) {
+    return typeof (fn) == typeof (Function);
   }
 }
