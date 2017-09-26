427a5423600414e9a84cc313fa1429866c12f841
Marcelo dos Santos <mdssjc@gmail.com>
2017-09-26 15:42:01 -0300

    Refatoração.

--- caelum/java-lab/fj22-argentum/src/main/java/com/github/mdssjc/argentum/testes/GeradorAleatorioDeXML.java
+++ caelum/java-lab/fj22-argentum/src/main/java/com/github/mdssjc/argentum/testes/GeradorAleatorioDeXML.java
@@ -1,5 +1,9 @@
 package com.github.mdssjc.argentum.testes;
 
+import com.github.mdssjc.argentum.modelo.Negociacao;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
@@ -8,47 +12,42 @@ import java.util.Calendar;
 import java.util.List;
 import java.util.Random;
 
-import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.io.xml.DomDriver;
-
-import com.github.mdssjc.argentum.modelo.Negociacao;
-
 public class GeradorAleatorioDeXML {
 
-    public static void main(String[] args) throws IOException {
-        Calendar data = Calendar.getInstance();
-        Random random = new Random(123);
-        List<Negociacao> negociacoes = new ArrayList<Negociacao>();
+  public static void main(final String[] args) throws IOException {
+    Calendar data = Calendar.getInstance();
+    final Random random = new Random(123);
+    final List<Negociacao> negociacoes = new ArrayList<>();
 
-        double valor = 40;
-        int quantidade = 1000;
+    double valor = 40;
+    int quantidade = 1000;
 
-        for (int dias = 0; dias < 30; dias++) {
-            int quantidadeNegociacoesDoDia = random.nextInt(20);
+    for (int dias = 0; dias < 30; dias++) {
+      final int quantidadeNegociacoesDoDia = random.nextInt(20);
 
-            for (int negociacao = 0; negociacao < quantidadeNegociacoesDoDia; negociacao++) {
+      for (int negociacao = 0; negociacao < quantidadeNegociacoesDoDia; negociacao++) {
 
-                // no máximo sobe ou cai R$1,00 e nao baixa além de R$5,00
-                valor += (random.nextInt(200) - 100) / 100.0;
-                if (valor < 5.0) {
-                    valor = 5.0;
-                }
-
-                // quantidade: entre 500 e 1500
-                quantidade += 1000 - random.nextInt(500);
-
-                Negociacao n = new Negociacao(valor, quantidade, data);
-                negociacoes.add(n);
-            }
-            data = (Calendar) data.clone();
-            data.add(Calendar.DAY_OF_YEAR, 1);
+        // no máximo sobe ou cai R$1,00 e nao baixa além de R$5,00
+        valor += (random.nextInt(200) - 100) / 100.0;
+        if (valor < 5.0) {
+          valor = 5.0;
         }
 
-        XStream stream = new XStream(new DomDriver());
-        stream.alias("negociacao", Negociacao.class);
-        stream.setMode(XStream.NO_REFERENCES);
+        // quantidade: entre 500 e 1500
+        quantidade += 1000 - random.nextInt(500);
 
-        PrintStream out = new PrintStream(new File("negociacao.xml"));
-        out.println(stream.toXML(negociacoes));
+        final Negociacao n = new Negociacao(valor, quantidade, data);
+        negociacoes.add(n);
+      }
+      data = (Calendar) data.clone();
+      data.add(Calendar.DAY_OF_YEAR, 1);
     }
+
+    final XStream stream = new XStream(new DomDriver());
+    stream.alias("negociacao", Negociacao.class);
+    stream.setMode(XStream.NO_REFERENCES);
+
+    final PrintStream out = new PrintStream(new File("negociacao.xml"));
+    out.println(stream.toXML(negociacoes));
+  }
 }
