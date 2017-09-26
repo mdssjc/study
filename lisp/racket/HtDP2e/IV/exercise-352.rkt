44db5236adf6c3322ff0de7e9b8e31709958474b
Marcelo dos Santos <mdssjc@gmail.com>
2017-09-25 23:40:36 -0300

    Exercício 352.

---
+++ lisp/racket/HtDP2e/IV/exercise-352.rkt
@@ -0,0 +1,52 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname exercise-352) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; Exercise 352
+
+
+;; ====================
+;; Data definitions:
+
+(define-struct add [left right])
+; An Add is a structure:
+;   (make-add Number Number)
+; interpretation (make-add l r) specifies an addition expression
+;  l: is the left operand; and
+;  r: is the right operand
+
+(define-struct mul [left right])
+; An Mul is a structure:
+;   (make-mul Number Number)
+; interpretation (make-mul l r) specifies an multiplication expression
+;  l: is the left operand; and
+;  r: is the right operand
+
+; A BSL-var-expr is one of: 
+;  - Number
+;  - Symbol 
+;  - (make-add BSL-var-expr BSL-var-expr)
+;  - (make-mul BSL-var-expr BSL-var-expr)
+
+
+;; ====================
+;; Functions:
+
+; BSL-var-expr Symbol Number -> BSL-var-expr
+; produces a BSL-var-expr like ex with all occurrences of x replaced by v
+(check-expect (subst 1 'a 2) 1)
+(check-expect (subst 'a 'a 2) 2)
+(check-expect (subst 'b 'a 2) 'b)
+(check-expect (subst (make-add 'a 'b) 'a 2) (make-add 2 'b))
+(check-expect (subst (make-add 'a 'a) 'a 2) (make-add 2 2))
+(check-expect (subst (make-mul 'a 'b) 'a 3) (make-mul 3 'b))
+(check-expect (subst (make-mul 'a 'a) 'a 3) (make-mul 3 3))
+(check-expect (subst (make-mul 'a (make-add 'b 'c)) 'b 3) (make-mul 'a (make-add 3 'c)))
+
+(define (subst ex x v)
+  (cond [(number? ex) ex]
+        [(symbol? ex)
+         (if (symbol=? ex x) v ex)]
+        [(add? ex)
+         (make-add (subst (add-left ex) x v) (subst (add-right ex) x v))]
+        [(mul? ex)
+         (make-mul (subst (mul-left ex) x v) (subst (mul-right ex) x v))]))
