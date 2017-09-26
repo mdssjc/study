a554637450f4f89dce93ca43bd365611a8a1014f
Marcelo dos Santos <mdssjc@gmail.com>
2017-09-26 15:46:16 -0300

    Refatoração do teste.

--- caelum/java-lab/fj22-argentum/src/test/java/com/github/mdssjc/argentum/modelo/CandlestickFactoryTest.java
+++ caelum/java-lab/fj22-argentum/src/test/java/com/github/mdssjc/argentum/modelo/CandlestickFactoryTest.java
@@ -1,6 +1,7 @@
 package com.github.mdssjc.argentum.modelo;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -108,5 +109,70 @@ public class CandlestickFactoryTest {
     assertEquals(39.8, candle.getFechamento(), 0.00001);
     assertFalse(candle.isAlta());
     assertTrue(candle.isBaixa());
+  }
+
+  @Test
+  public void paraNegociacoesDeTresDiasDistintosGeraTresCandles() {
+    final Negociacao negociacao1 = new Negociacao(40.5, 100, this.hoje);
+    final Negociacao negociacao2 = new Negociacao(45.0, 100, this.hoje);
+    final Negociacao negociacao3 = new Negociacao(39.8, 100, this.hoje);
+    final Negociacao negociacao4 = new Negociacao(42.3, 100, this.hoje);
+
+    final Calendar amanha = (Calendar) this.hoje.clone();
+    amanha.add(Calendar.DAY_OF_MONTH, 1);
+
+    final Negociacao negociacao5 = new Negociacao(48.8, 100, amanha);
+    final Negociacao negociacao6 = new Negociacao(49.3, 100, amanha);
+
+    final Calendar depois = (Calendar) amanha.clone();
+    depois.add(Calendar.DAY_OF_MONTH, 1);
+
+    final Negociacao negociacao7 = new Negociacao(51.8, 100, depois);
+    final Negociacao negociacao8 = new Negociacao(52.3, 100, depois);
+
+    final List<Negociacao> negociacoes = Arrays.asList(negociacao1, negociacao2,
+                                                       negociacao3, negociacao4,
+                                                       negociacao5,
+                                                       negociacao6, negociacao7,
+                                                       negociacao8);
+
+    final CandlestickFactory fabrica = new CandlestickFactory();
+    final List<Candlestick> candles = fabrica.constroiCandles(negociacoes);
+
+    assertEquals(3, candles.size());
+    assertEquals(40.5, candles.get(0)
+                              .getAbertura(), 0.00001);
+    assertEquals(42.3, candles.get(0)
+                              .getFechamento(), 0.00001);
+    assertEquals(48.8, candles.get(1)
+                              .getAbertura(), 0.00001);
+    assertEquals(49.3, candles.get(1)
+                              .getFechamento(), 0.00001);
+    assertEquals(51.8, candles.get(2)
+                              .getAbertura(), 0.00001);
+    assertEquals(52.3, candles.get(2)
+                              .getFechamento(), 0.00001);
+  }
+
+  @Test(expected = IllegalStateException.class)
+  @Ignore("Devido a funcionalidade de ordenação da lista on demand")
+  public void naoPermiteConstruirCandlesComNegociacoesForaDeOrdem() {
+    final Calendar hoje = Calendar.getInstance();
+    final Negociacao negociacao1 = new Negociacao(40.5, 100, hoje);
+
+    final Calendar amanha = (Calendar) hoje.clone();
+    amanha.add(Calendar.DAY_OF_MONTH, 1);
+    final Negociacao negociacao2 = new Negociacao(48.8, 100, amanha);
+
+
+    final Calendar depois = (Calendar) amanha.clone();
+    depois.add(Calendar.DAY_OF_MONTH, 2);
+    final Negociacao negociacao3 = new Negociacao(52.3, 100, depois);
+
+    final List<Negociacao> negociacoes = Arrays.asList(negociacao3, negociacao1,
+                                                       negociacao2);
+
+    final CandlestickFactory fabrica = new CandlestickFactory();
+    fabrica.constroiCandles(negociacoes);
   }
 }
