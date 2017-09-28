5c69626b636023e7e4dc25fe9ad81e1ad18b23ce
Marcelo dos Santos <mdssjc@gmail.com>
2017-09-28 19:43:22 -0300

    Criando promises.

--- javascript/jscangaceiro/client/app/domain/negociacao/NegociacaoService.js
+++ javascript/jscangaceiro/client/app/domain/negociacao/NegociacaoService.js
@@ -2,23 +2,23 @@
 
 class NegociacaoService {
 
-  obtemNegociacoesDaSemana(cb) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'negociacoes/semana');
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState == 4) {
-        if (xhr.status == 200) {
-          const negociacoes = JSON
-            .parse(xhr.responseText)
-            .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor));
-
-          cb(null, negociacoes);
-        } else {
-          console.log(xhr.responseText);
-          cb('Não foi possível obter as negociações da semana', null);
+  obtemNegociacoesDaSemana() {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', 'negociacoes/semana');
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState == 4) {
+          if (xhr.status == 200) {
+            const negociacoes = JSON
+              .parse(xhr.responseText)
+              .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor));
+            resolve(negociacoes);
+          } else {
+            reject('Não foi possível obter as negociações da semana');
+          }
         }
-      }
-    };
-    xhr.send();
+      };
+      xhr.send();
+    })
   }
 }
