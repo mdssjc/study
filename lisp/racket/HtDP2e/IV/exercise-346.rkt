3e5b8068b11d235003edf3f76ba2b9cd6778d0fd
Marcelo dos Santos <mdssjc@gmail.com>
2017-10-01 22:27:43 -0300

    Correção dos tipos nos parâmetros.

--- lisp/racket/HtDP2e/IV/exercise-346.rkt
+++ lisp/racket/HtDP2e/IV/exercise-346.rkt
@@ -9,14 +9,14 @@
 
 (define-struct add [left right])
 ; An Add is a structure:
-;   (make-add Number Number)
+;   (make-add BSL-expr BSL-expr)
 ; interpretation (make-add l r) specifies an addition expression
 ;  l: is the left operand; and
 ;  r: is the right operand
 
 (define-struct mul [left right])
 ; A Mul is a structure:
-;   (make-mul Number Number)
+;   (make-mul BSL-expr BSL-expr)
 ; interpretation (make-mul l r) specifies a multiplication expression
 ;  l: is the left operand; and
 ;  r: is the right operand
