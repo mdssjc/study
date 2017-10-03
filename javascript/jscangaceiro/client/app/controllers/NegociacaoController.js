bb276a4938143b91eabd4bda90b7c9711ca2b727
Marcelo dos Santos <mdssjc@gmail.com>
2017-10-03 19:50:58 -0300

    As funções filter() e some().

--- javascript/jscangaceiro/client/app/controllers/NegociacaoController.js
+++ javascript/jscangaceiro/client/app/controllers/NegociacaoController.js
@@ -63,7 +63,12 @@ class NegociacaoController {
     this._service
       .obtemNegociacoesDoPeriodo()
       .then(negociacoes => {
-        negociacoes.forEach(negociacao => this._negociacoes.adiciona(negociacao));
+        negociacoes
+          .filter(novaNegociacao =>
+            !this._negociacoes
+              .paraArray()
+              .some(negociacaoExistente => novaNegociacao.equals(negociacaoExistente)))
+          .forEach(negociacao => this._negociacoes.adiciona(negociacao));
         this._mensagem.texto = 'Negociações do período importadas com sucesso';
       }).catch(err => this._mensagem.texto = err);
   }
