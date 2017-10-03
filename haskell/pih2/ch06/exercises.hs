ee90b6943c8bc066c5ac0a5827e32f63ddd9d062
Marcelo dos Santos <mdssjc@gmail.com>
2017-10-02 21:16:49 -0300

    Exercise 6.5.

--- haskell/pih2/ch06/exercises.hs
+++ haskell/pih2/ch06/exercises.hs
@@ -20,3 +20,17 @@ euclid :: Int -> Int -> Int
 euclid a b
   | a == b = a
   | otherwise = if (a < b) then euclid a (b - a) else euclid (a - b) b
+
+-- 6.5
+length' :: [a] -> Int
+length' [] = 0
+length' (_:xs) = 1 + length' xs
+
+drop' :: Int -> [a] -> [a]
+drop' 0 xs = xs
+drop' _ [] = []
+drop' n (_:xs) = drop (n - 1) xs
+
+init' :: [a] -> [a]
+init' [_] = []
+init' (x:xs) = x : init' xs
