af8ced46dab5b52850e6db7563d588ddebb51ef6
Marcelo dos Santos <mdssjc@gmail.com>
2017-10-09 19:49:16 -0300

    Criando stores.

--- javascript/jscangaceiro/client/app/util/ConnectionFactory.js
+++ javascript/jscangaceiro/client/app/util/ConnectionFactory.js
@@ -10,15 +10,27 @@ class ConnectionFactory {
 
   static getConnection() {
     return new Promise((resolve, reject) => {
-      const openRequest = indexedDB.open('jscangaceiro', 2);
+      const openRequest = indexedDB.open('jscangaceiro', 3);
 
       openRequest.onupgradeneeded = e => {
-        stores.forEach(store => {
-
-        });
+        ConnectionFactory._createStores(e.target.result);
       };
-      openRequest.onsuccess = e => { };
-      openRequest.onerror = e => { };
+      openRequest.onsuccess = e => {
+        resolve(e.target.result);
+      };
+      openRequest.onerror = e => {
+        console.log(e.target.error);
+        reject(e.target.error.name);
+      };
     });
   };
+
+  static _createStores(connection) {
+    stores.forEach(store => {
+      if (connection.objectStoreNames.contains(store)) {
+        connection.deleteObjectStore(store);
+      }
+      connection.createObjectStore(store, { autoIncrement: true });
+    });
+  }
 }
