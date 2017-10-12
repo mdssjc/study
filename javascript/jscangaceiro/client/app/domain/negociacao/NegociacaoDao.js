44716a26b28f5f8dd663b4fd9d4a871be11b31b3
Marcelo dos Santos <mdssjc@gmail.com>
2017-10-12 19:56:45 -0300

    Criando nosso DAO de negociações.

---
+++ javascript/jscangaceiro/client/app/domain/negociacao/NegociacaoDao.js
@@ -0,0 +1,21 @@
+// client/app/domain/negociacao/NegociacaoDao.js
+
+class NegociacaoDao {
+
+    constructor(connection) {
+        this._connection = connection;
+        this._store = 'negociacoes';
+    }
+
+    adiciona(negociacao) {
+        return new Promise((resolve, reject) => {
+
+        });
+    }
+
+    listaTodos() {
+        return new Promise((resolve, reject) => {
+
+        });
+    }
+}
