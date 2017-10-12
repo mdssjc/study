47648c4a37a0a6ffebac74cb642586369c4229c3
Marcelo dos Santos <mdssjc@gmail.com>
2017-10-12 20:08:30 -0300

    Implementando a lógica de inclusão.

--- javascript/jscangaceiro/client/app/domain/negociacao/NegociacaoDao.js
+++ javascript/jscangaceiro/client/app/domain/negociacao/NegociacaoDao.js
@@ -2,20 +2,29 @@
 
 class NegociacaoDao {
 
-    constructor(connection) {
-        this._connection = connection;
-        this._store = 'negociacoes';
-    }
+  constructor(connection) {
+    this._connection = connection;
+    this._store = 'negociacoes';
+  }
 
-    adiciona(negociacao) {
-        return new Promise((resolve, reject) => {
+  adiciona(negociacao) {
+    return new Promise((resolve, reject) => {
+      const request = this._connection
+        .transaction([this._store], 'readwrite')
+        .objectStore(this._store)
+        .add(negociacao);
 
-        });
-    }
+      request.onsuccess = e => resolve();
+      request.onerror = e => {
+        console.log(e.target.error);
+        reject('Não foi possível salvar a negociação');
+      };
+    });
+  }
 
-    listaTodos() {
-        return new Promise((resolve, reject) => {
+  listaTodos() {
+    return new Promise((resolve, reject) => {
 
-        });
-    }
+    });
+  }
 }
