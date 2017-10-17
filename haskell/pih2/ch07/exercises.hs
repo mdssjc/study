dc48afc3bcce5bbd564429f6fbb10f5e0252d758
Marcelo dos Santos <mdssjc@gmail.com>
2017-10-16 22:19:24 -0200

    Exercise 7.2.

--- haskell/pih2/ch07/exercises.hs
+++ haskell/pih2/ch07/exercises.hs
@@ -1,3 +1,22 @@
 -- 7.1
 hofmf :: (a -> b) -> (a -> Bool) -> [a] -> [b]
 hofmf f p xs = map f $ filter p xs
+
+-- 7.2
+all' :: (a -> Bool) -> [a] -> Bool
+all' p xs = and $ map p xs
+
+any' :: (a -> Bool) -> [a] -> Bool
+any' p xs = or $ map p xs
+
+takeWhile' :: (a -> Bool) -> [a] -> [a]
+takeWhile' _ [] = []
+takeWhile' p (x:xs)
+  | p x = x : takeWhile' p xs
+  | otherwise = []
+
+dropWhile' :: (a -> Bool) -> [a] -> [a]
+dropWhile' _ [] = []
+dropWhile' p (x:xs)
+  | p x = dropWhile' p xs
+  | otherwise = x:xs
