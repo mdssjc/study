76e5c460b51c04e63af6aa24475a0016d07fec2b
Marcelo dos Santos <mdssjc@gmail.com>
2017-10-21 18:38:16 -0200

    Ambiente.

--- c/byol/parsing.c
+++ c/byol/parsing.c
@@ -47,6 +47,12 @@ struct lval {
   lval** cell;
 };
 
+struct lenv {
+  int count;
+  char** syms;
+  lval** vals;
+};
+
 lval* lval_num(long x) {
   lval* v = malloc(sizeof(lval));
   v->type = LVAL_NUM;
@@ -117,6 +123,24 @@ lval* lval_add(lval* v, lval* x) {
   return v;
 }
 
+lenv* lenv_new(void) {
+  lenv* e = malloc(sizeof(lenv));
+  e->count = 0;
+  e->syms = NULL;
+  e->vals = NULL;
+  return e;
+}
+
+void lenv_del(lenv* e) {
+  for (int i = 0; i < e->count; i++) {
+    free(e->syms[i]);
+    lval_del(e->vals[i]);
+  }
+  free(e->syms);
+  free(e->vals);
+  free(e);
+}
+
 lval* lval_pop(lval* v, int i) {
   lval* x = v->cell[i];
 
@@ -158,6 +182,33 @@ lval* lval_copy(lval* v) {
   }
 
   return x;
+}
+
+lval* lenv_get(lenv* e, lval* k) {
+  for (int i = 0; i < e->count; i++) {
+    if (strcmp(e->syms[i], k->sym) == 0) {
+      return lval_copy(e->vals[i]);
+    }
+  }
+  return lval_err("unbound symbol!");
+}
+
+void lenv_put(lenv* e, lval* k, lval* v) {
+  for (int i = 0; i < e->count; i++) {
+    if (strcmp(e->syms[i], k->sym) == 0) {
+      lval_del(e->vals[i]);
+      e->vals[i] = lval_copy(v);
+      return;
+    }
+  }
+
+  e->count++;
+  e->vals = realloc(e->vals, sizeof(lval*) * e->count);
+  e->syms = realloc(e->syms, sizeof(char*) * e->count);
+
+  e->vals[e->count-1] = lval_copy(v);
+  e->syms[e->count-1] = malloc(strlen(k->sym)+1);
+  strcpy(e->syms[e->count-1], k->sym);
 }
 
 void lval_print(lval* v);
