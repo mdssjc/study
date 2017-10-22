9089b317091265301d5afa4452c1d62580160878
Marcelo dos Santos <mdssjc@gmail.com>
2017-10-22 14:29:28 -0200

    Atualização do Selenium.

--- caelum/java-lab/fj22-argentum/src/test/java/aceitacao/GeraGraficoTest.java
+++ caelum/java-lab/fj22-argentum/src/test/java/aceitacao/GeraGraficoTest.java
@@ -5,37 +5,38 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class GeraGraficoTest {
 
-    private static final String URL = "http://localhost:8080/fj22-argentum/index.xhtml";
+  private static final String URL = "http://localhost:8080/argentum/index.xhtml";
+  private WebDriver driver;
 
-    private WebDriver           driver;
+  @Before
+  public void setUp() {
+    System.setProperty("webdriver.gecko.driver",
+                       "/home/mdssjc/java/geckodriver");
+    driver = new FirefoxDriver();
+    driver.get(URL);
+  }
 
-    @Before
-    public void setUp() {
-        driver = new FirefoxDriver();
-    }
+  @After
+  public void tearDown() {
+    driver.close();
+  }
 
-    @After
-    public void tearDown() {
-        driver.close();
-    }
+  @Test
+  public void testeAoGerarGraficoSemTituloUmaMensagemEhApresentada() {
+    WebElement titulo = driver.findElement(By.id("dadosGrafico:titulo"));
+    titulo.sendKeys("");
+    titulo.sendKeys(Keys.ENTER);
 
-    @Test
-    public void testeAoGerarGraficoSemTituloUmaMensagemEhApresentada() {
-        driver.navigate()
-              .to(URL);
-        WebElement titulo = driver.findElement(By.id("dadosGrafico:titulo"));
-        titulo.sendKeys("");
-        titulo.submit();
+    boolean existeMensagem = driver.getPageSource()
+                                   .contains("Erro de validação");
 
-        boolean existeMensagem = driver.getPageSource()
-                                       .contains("Erro de validação");
-
-        Assert.assertTrue(existeMensagem);
-    }
+    Assert.assertTrue(existeMensagem);
+  }
 }
