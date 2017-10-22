ddd68b1030d954b17c62636eb181c505ae1edc43
Marcelo dos Santos <mdssjc@gmail.com>
2017-10-22 13:28:45 -0200

    Refatoração.

--- caelum/java-lab/fj22-argentum/src/main/java/com/github/mdssjc/argentum/testes/GeradorDeSerie.java
+++ caelum/java-lab/fj22-argentum/src/main/java/com/github/mdssjc/argentum/testes/GeradorDeSerie.java
@@ -1,27 +1,26 @@
 package com.github.mdssjc.argentum.testes;
 
+import com.github.mdssjc.argentum.modelo.Candlestick;
+import com.github.mdssjc.argentum.modelo.SerieTemporal;
+
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
-import com.github.mdssjc.argentum.modelo.Candle;
-import com.github.mdssjc.argentum.modelo.SerieTemporal;
-
 public class GeradorDeSerie {
 
-    /**
-     * Serve para ajudar a fazer os testes.
-     * 
-     * Recebe uma sequência de valores e cria candles com abertura, fechamento,
-     * minimo e maximo iguais, mil de volume e data de hoje. Finalmente, devolve
-     * tais candles encapsuladas em uma Serie Temporal.
-     **/
-    public static SerieTemporal criaSerie(double... valores) {
-        List<Candle> candles = new ArrayList<Candle>();
-        for (double d : valores) {
-            candles.add(
-                    new Candle(d, d, d, d, 1000, Calendar.getInstance()));
-        }
-        return new SerieTemporal(candles);
+  /**
+   * Serve para ajudar a fazer os testes.
+   * <p>
+   * Recebe uma sequência de valores e cria candles com abertura, fechamento,
+   * minimo e maximo iguais, mil de volume e data de hoje. Finalmente, devolve
+   * tais candles encapsuladas em uma Serie Temporal.
+   **/
+  public static SerieTemporal criaSerie(final double... valores) {
+    final List<Candlestick> candles = new ArrayList<>();
+    for (final double d : valores) {
+      candles.add(new Candlestick(d, d, d, d, 1000, Calendar.getInstance()));
     }
+    return new SerieTemporal(candles);
+  }
 }
