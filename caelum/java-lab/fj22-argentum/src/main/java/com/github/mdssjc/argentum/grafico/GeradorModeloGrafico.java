41e5bcb51c54b8ba18081e6ac86167b4514a1615
Marcelo dos Santos <mdssjc@gmail.com>
2017-10-22 12:19:55 -0200

    Formatação.

--- caelum/java-lab/fj22-argentum/src/main/java/com/github/mdssjc/argentum/grafico/GeradorModeloGrafico.java
+++ caelum/java-lab/fj22-argentum/src/main/java/com/github/mdssjc/argentum/grafico/GeradorModeloGrafico.java
@@ -1,42 +1,40 @@
 package com.github.mdssjc.argentum.grafico;
 
 import com.github.mdssjc.argentum.indicadores.Indicador;
+import com.github.mdssjc.argentum.modelo.SerieTemporal;
 import org.primefaces.model.chart.ChartModel;
 import org.primefaces.model.chart.LineChartModel;
 import org.primefaces.model.chart.LineChartSeries;
 
-import com.github.mdssjc.argentum.modelo.SerieTemporal;
-
 public class GeradorModeloGrafico {
 
-    private SerieTemporal  serie;
-    private int            comeco;
-    private int            fim;
-    private LineChartModel modeloGrafico;
-    private String         tituloGrafico;
+  private final SerieTemporal serie;
+  private final int comeco;
+  private final int fim;
+  private final LineChartModel modeloGrafico;
+  private final String tituloGrafico;
 
-    public GeradorModeloGrafico(SerieTemporal serie, int comeco, int fim,
-            String tituloGrafico) {
-        this.serie = serie;
-        this.comeco = comeco;
-        this.fim = fim;
-        this.tituloGrafico = tituloGrafico;
-        this.modeloGrafico = new LineChartModel();
+  public GeradorModeloGrafico(final SerieTemporal serie, final int comeco, final int fim, final String tituloGrafico) {
+    this.serie = serie;
+    this.comeco = comeco;
+    this.fim = fim;
+    this.tituloGrafico = tituloGrafico;
+    this.modeloGrafico = new LineChartModel();
+  }
+
+  public void plotaIndicador(final Indicador indicador) {
+    final LineChartSeries chartSerie = new LineChartSeries(indicador.toString());
+
+    for (int i = this.comeco; i <= this.fim; i++) {
+      final double valor = indicador.calcula(i, this.serie);
+      chartSerie.set(i, valor);
     }
+    this.modeloGrafico.addSeries(chartSerie);
+    this.modeloGrafico.setLegendPosition("w");
+    this.modeloGrafico.setTitle(this.tituloGrafico);
+  }
 
-    public void plotaIndicador(Indicador indicador) {
-        LineChartSeries chartSerie = new LineChartSeries(indicador.toString());
-
-        for (int i = comeco; i <= fim; i++) {
-            double valor = indicador.calcula(i, serie);
-            chartSerie.set(i, valor);
-        }
-        this.modeloGrafico.addSeries(chartSerie);
-        this.modeloGrafico.setLegendPosition("w");
-        this.modeloGrafico.setTitle(tituloGrafico);
-    }
-
-    public ChartModel getModeloGrafico() {
-        return this.modeloGrafico;
-    }
+  public ChartModel getModeloGrafico() {
+    return this.modeloGrafico;
+  }
 }
