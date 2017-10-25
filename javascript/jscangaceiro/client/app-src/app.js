39923caa708b6edf87c59635fa83ed2ac8e8a5db
Marcelo dos Santos <mdssjc@gmail.com>
2017-10-25 19:46:27 -0200

    Configurando uma requisição com API Fetch.

--- javascript/jscangaceiro/client/app-src/app.js
+++ javascript/jscangaceiro/client/app-src/app.js
@@ -1,6 +1,7 @@
 // client/app-src/app.js
 
 import { NegociacaoController } from './controllers/NegociacaoController.js';
+import { Negociacao } from './domain/index.js';
 
 const controller = new NegociacaoController();
 const $ = document.querySelector.bind(document);
@@ -13,3 +14,19 @@ $('#botao-apaga')
 
 $('#botao-importa')
   .addEventListener('click', controller.importaNegociacoes.bind(controller));
+
+const negociacao = new Negociacao(new Date(), 1, 200);
+const headers = new Headers();
+headers.set('Content-Type', 'application/json');
+
+const body = JSON.stringify(negociacao);
+const method = 'POST';
+
+const config = {
+  method,
+  headers,
+  body
+};
+
+fetch('/negociacoes', config)
+  .then(() => console.log('Dado enviado com sucesso'));
