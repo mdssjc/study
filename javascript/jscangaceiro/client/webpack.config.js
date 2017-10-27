5d6c4913388e223ac0b94d760541a66a815f23df
Marcelo dos Santos <mdssjc@gmail.com>
2017-10-27 19:53:45 -0200

    Preparando o build de produção.

--- javascript/jscangaceiro/client/webpack.config.js
+++ javascript/jscangaceiro/client/webpack.config.js
@@ -1,6 +1,13 @@
 // client/webpack.config.js
 
 const path = require('path');
+const babiliPlugin = require('babili-webpack-plugin');
+
+let plugins = [];
+
+if (process.env.NODE_ENV == 'production') {
+  plugins.push(new babiliPlugin());
+}
 
 module.exports = {
   entry: './app-src/app.js',
@@ -18,5 +25,6 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins
 };
