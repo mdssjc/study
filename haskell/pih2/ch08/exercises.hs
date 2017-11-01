269fb994eaec48ed4d1e22ea1099d8b68e19c8e4
Marcelo dos Santos <mdssjc@gmail.com>
2017-11-01 21:54:55 -0200

    Exercise 8.2.

--- haskell/pih2/ch08/exercises.hs
+++ haskell/pih2/ch08/exercises.hs
@@ -10,3 +10,16 @@ mult :: Nat -> Nat -> Nat
 mult Zero n = Zero
 mult m Zero = Zero
 mult m (Succ n) = if n == Zero then m else mult (add m m) n
+
+-- 8.2
+data Tree a = Leaf a | Node (Tree a) a (Tree a)
+
+t :: Tree Int
+t = Node (Node (Leaf 1) 3 (Leaf 4)) 5 (Node (Leaf 6) 7 (Leaf 9))
+
+occurs :: Ord a => a -> Tree a -> Bool
+occurs x (Leaf y) = x == y
+occurs x (Node l y r) = case compare x y of
+                          EQ -> True
+                          LT -> occurs x l
+                          GT -> occurs x r
