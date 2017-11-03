a8e987330f88353f3f16fc199496c4ab1a7e1dca
Marcelo dos Santos <mdssjc@gmail.com>
2017-11-03 15:29:19 -0200

    Comentários.

--- c/byol/parsing.c
+++ c/byol/parsing.c
@@ -834,7 +834,9 @@ lval* lval_read(mpc_ast_t* t) {
     if (strcmp(t->children[i]->contents, ")") == 0) { continue; }
     if (strcmp(t->children[i]->contents, "}") == 0) { continue; }
     if (strcmp(t->children[i]->contents, "{") == 0) { continue; }
-    if (strcmp(t->children[i]->tag,  "regex") == 0) { continue; }
+    if (strcmp(t->children[i]->tag, "regex") == 0) { continue; }
+    if (strstr(t->children[i]->tag, "comment")) { continue; }
+
     x = lval_add(x, lval_read(t->children[i]));
   }
 
@@ -844,25 +846,28 @@ lval* lval_read(mpc_ast_t* t) {
 /* Main */
 
 int main(int argc, char** argv) {
-  mpc_parser_t* Number = mpc_new("number");
-  mpc_parser_t* Symbol = mpc_new("symbol");
-  mpc_parser_t* String = mpc_new("string");
-  mpc_parser_t* Sexpr  = mpc_new("sexpr");
-  mpc_parser_t* Qexpr  = mpc_new("qexpr");
-  mpc_parser_t* Expr   = mpc_new("expr");
-  mpc_parser_t* Lispy  = mpc_new("lispy");
+  mpc_parser_t* Number  = mpc_new("number");
+  mpc_parser_t* Symbol  = mpc_new("symbol");
+  mpc_parser_t* String  = mpc_new("string");
+  mpc_parser_t* Comment = mpc_new("comment");
+  mpc_parser_t* Sexpr   = mpc_new("sexpr");
+  mpc_parser_t* Qexpr   = mpc_new("qexpr");
+  mpc_parser_t* Expr    = mpc_new("expr");
+  mpc_parser_t* Lispy   = mpc_new("lispy");
 
   mpca_lang(MPCA_LANG_DEFAULT,
-    "                                                                \
-      number : /-?[0-9]+/ ;                                          \
-      symbol : /[a-zA-Z0-9_+\\-*\\/\\\\=<>!&]+/ ;                    \
-      string : /\"(\\\\.|[^\"])*\"/ ;                                \
-      sexpr  : '(' <expr>* ')' ;                                     \
-      qexpr  : '{' <expr>* '}' ;                                     \
-      expr   : <number> | <symbol> | <string> | <sexpr> | <qexpr> ;  \
-      lispy  : /^/ <expr>* /$/ ;                                     \
+    "                                             \
+      number : /-?[0-9]+/ ;                       \
+      symbol : /[a-zA-Z0-9_+\\-*\\/\\\\=<>!&]+/ ; \
+      string : /\"(\\\\.|[^\"])*\"/ ;             \
+      comment : /;[^\\r\\n]*/ ;                   \
+      sexpr  : '(' <expr>* ')' ;                  \
+      qexpr  : '{' <expr>* '}' ;                  \
+      expr   : <number>  | <symbol> | <string>    \
+             | <comment> | <sexpr> | <qexpr> ;    \
+      lispy  : /^/ <expr>* /$/ ;                  \
     ",
-    Number, Symbol, String, Sexpr, Qexpr, Expr, Lispy);
+    Number, Symbol, String, Comment, Sexpr, Qexpr, Expr, Lispy);
 
   puts("Lispy Version 0.0.0.0.8");
   puts("Press Ctrl+c to Exit\n");
@@ -890,7 +895,7 @@ int main(int argc, char** argv) {
 
   lenv_del(e);
 
-  mpc_cleanup(7, Number, Symbol, String, Sexpr, Qexpr, Expr, Lispy);
+  mpc_cleanup(8, Number, Symbol, String, Comment, Sexpr, Qexpr, Expr, Lispy);
 
   return 0;
 }
