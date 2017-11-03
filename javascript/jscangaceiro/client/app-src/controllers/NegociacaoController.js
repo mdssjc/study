7f3a9c4139e6d5b3725b7d3b5c1846c5aa7c94ae
Marcelo dos Santos <mdssjc@gmail.com>
2017-11-03 19:48:44 -0200

    System.import vs import.

--- javascript/jscangaceiro/client/app-src/controllers/NegociacaoController.js
+++ javascript/jscangaceiro/client/app-src/controllers/NegociacaoController.js
@@ -81,7 +81,7 @@ export class NegociacaoController {
   @debounce(1500)
   async importaNegociacoes() {
     try {
-      const { NegociacaoService } = await System.import('../domain/negociacao/NegociacaoService');
+      const { NegociacaoService } = await import('../domain/negociacao/NegociacaoService');
       const service = new NegociacaoService();
 
       const negociacoes = await service.obtemNegociacoesDoPeriodo();
