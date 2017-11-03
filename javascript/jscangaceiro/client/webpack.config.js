9095b83c6e542a397a5122ee2e59ba424517ca65
Marcelo dos Santos <mdssjc@gmail.com>
2017-11-03 19:22:20 -0200

    Otimizando o build com scope hoisting.

--- javascript/jscangaceiro/client/webpack.config.js
+++ javascript/jscangaceiro/client/webpack.config.js
@@ -20,6 +20,7 @@ plugins.push(
 );
 
 if (process.env.NODE_ENV == 'production') {
+  plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
   plugins.push(new babiliPlugin());
   plugins.push(new optimizeCSSAssetsPlugin({
     cssProcessor: require('cssnano'),
