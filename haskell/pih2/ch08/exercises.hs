1713fe910e428205acfab3585e481202c22e6e3d
Marcelo dos Santos <mdssjc@gmail.com>
2017-11-06 20:04:18 -0200

    Exercise 8.7.

--- haskell/pih2/ch08/exercises.hs
+++ haskell/pih2/ch08/exercises.hs
@@ -61,3 +61,14 @@ eval = folde id (+)
 
 size :: Expr -> Int
 size = folde (\_ -> 1) (+)
+
+-- 8.7
+-- instance Eq a => Eq (Maybe a) where
+-- (==) Nothing Nothing   = True
+-- (==) (Just x) (Just y) = x == y
+-- (==) _ _               = False
+
+-- instance Eq a => Eq [a] where
+-- (==) [] []         = True
+-- (==) (x:xs) (y:ys) = x == y && xs == ys
+-- (==) _ _           = False
