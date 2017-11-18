85a36fe679a8f59e1df486a0679196285ab59a7c
Marcelo dos Santos <mdssjc@gmail.com>
2017-11-18 20:42:42 -0200

    Exercise 12.2.

--- haskell/pih2/ch12/exercises.hs
+++ haskell/pih2/ch12/exercises.hs
@@ -5,3 +5,8 @@ data Tree a = Leaf | Node (Tree a) a (Tree a)
 instance Functor Tree where
   fmap f Leaf = Leaf
   fmap f (Node l x r) = Node (fmap f l) (f x) (fmap f r)
+
+-- 12.2
+-- instance Functor ((->) a) where
+-- fmap :: (b -> c) -> (a -> b) -> (a -> c)
+--   fmap = (.)
