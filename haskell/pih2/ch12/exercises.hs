6c85a6612b8ccb0fda1dd45904f25789badb9c44
Marcelo dos Santos <mdssjc@gmail.com>
2017-11-18 20:36:23 -0200

    Exercise 12.1.

---
+++ haskell/pih2/ch12/exercises.hs
@@ -0,0 +1,7 @@
+-- 12.1
+data Tree a = Leaf | Node (Tree a) a (Tree a)
+  deriving Show
+
+instance Functor Tree where
+  fmap f Leaf = Leaf
+  fmap f (Node l x r) = Node (fmap f l) (f x) (fmap f r)
