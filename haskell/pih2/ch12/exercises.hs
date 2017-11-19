a17103f0c3586e71de589d73f7db9796fcede5a0
Marcelo dos Santos <mdssjc@gmail.com>
2017-11-19 20:17:27 -0200

    Exercise 12.8.

--- haskell/pih2/ch12/exercises.hs
+++ haskell/pih2/ch12/exercises.hs
@@ -72,3 +72,27 @@ instance Monad Expr where
   Val x   >>= _ = Val x
   Var x   >>= f = f x
   Add x y >>= f = Add (x >>= f) (y >>= f)
+
+-- 12.8
+type State = Int
+newtype ST a = S(State -> (a, State))
+
+app :: ST a -> State -> (a, State)
+app (S st) x = st x
+
+instance Functor ST where
+  -- fmap :: (a -> b) -> ST a -> ST b
+  fmap g st = do s <- st
+                 return $ g s
+
+instance Applicative ST where
+  -- pure :: a -> ST a
+  pure x = S(\s -> (x,s))
+  -- (<*>) :: ST (a -> b) -> ST a -> ST b
+  stf <*> stx = do f <- stf
+                   x <- stx
+                   return $ f x
+
+instance Monad ST where
+  -- (>>=) :: ST a -> (a -> ST b) -> ST b
+  st >>= f = S(\s -> let (x,s') = app st s in app (f x) s')
