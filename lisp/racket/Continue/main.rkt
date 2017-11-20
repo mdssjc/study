5ccfb1ea4481717c38727724a464975848a4f6f8
Marcelo dos Santos <mdssjc@gmail.com>
2017-11-19 22:31:29 -0200

    6 Advanced Control Flow.

--- lisp/racket/Continue/main.rkt
+++ lisp/racket/Continue/main.rkt
@@ -18,51 +18,51 @@
 ;; ==========
 ;; Functions:
 
-; start: request -> response
-; produces a page that displays all of the web content
+; start: request -> doesn't return
+; consumes a request and produces a page that displays all of the
+; web content.
 (define (start request)
-  (define a-blog
-    (cond [(can-parse-post? (request-bindings request))
-           (cons (parse-post (request-bindings request))
-                 BLOG)]
-          [else
-           BLOG]))
-  (render-blog-page a-blog request))
-
-; can-parse-post?: bindings -> boolean
-; produces true if bindings contains values for 'title and 'body
-(define (can-parse-post? bindings)
-  (and (exists-binding? 'title bindings)
-       (exists-binding? 'body  bindings)))
+  (render-blog-page BLOG request))
 
 ; parse-post: bindings -> post
-; produces a post out of the bindings
+; extracts a post out of the bindings.
 (define (parse-post bindings)
   (post (extract-binding/single 'title bindings)
-        (extract-binding/single 'body  bindings)))
+        (extract-binding/single 'body bindings)))
 
-; render-blog-page: blog request -> response
-; produces an HTML page of the content of the blog
+; render-blog-page: blog request -> doesn't return
+; consumes a blog and a request, and produces an HTML page
+; of the content of the blog.
 (define (render-blog-page a-blog request)
-  (response/xexpr
-   `(html (head (title "My Blog"))
-          (body
-           (h1 "My Blog")
-           ,(render-posts a-blog)
-           (form
-            (input ((name "title")))
-            (input ((name "body")))
-            (input ((type "submit"))))))))
+  (define (response-generator embed/url)
+    (response/xexpr
+     `(html (head (title "My Blog"))
+            (body
+             (h1 "My Blog")
+             ,(render-posts a-blog)
+             (form ((action
+                     ,(embed/url insert-post-handler)))
+                   (input ((name "title")))
+                   (input ((name "body")))
+                   (input ((type "submit"))))))))
+ 
+  (define (insert-post-handler request)
+    (render-blog-page
+     (cons (parse-post (request-bindings request))
+           a-blog)
+     request))
+  (send/suspend/dispatch response-generator))
 
 ; render-post: post -> xexpr
-; produces an xexpr fragment of the post
+; consumes a post, produces an xexpr fragment of the post.
 (define (render-post a-post)
   `(div ((class "post"))
         ,(post-title a-post)
         (p ,(post-body a-post))))
 
 ; render-posts: blog -> xexpr
-; produces an xexpr fragment of all its posts
+; consumes a blog, produces an xexpr fragment
+; of all its posts.
 (define (render-posts a-blog)
   `(div ((class "posts"))
         ,@(map render-post a-blog)))
