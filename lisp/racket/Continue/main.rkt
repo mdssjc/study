f3267cfb83e564de85e1eba1d3527b2f2ebd0122
Marcelo dos Santos <mdssjc@gmail.com>
2017-11-20 22:53:58 -0200

    10 Adding a Back Button.

--- lisp/racket/Continue/main.rkt
+++ lisp/racket/Continue/main.rkt
@@ -80,8 +80,9 @@
   (send/suspend/dispatch response-generator))
 
 ; render-post-detail-page: post request -> doesn't return
-; consumes a post and request, and produces a detail page
-; of the post. The user will be able to insert new comments.
+; consumes a post and produces a detail page of the post.
+; The user will be able to either insert new comments
+; or go back to render-blog-page.
 (define (render-post-detail-page a-post request)
   (define (response-generator embed/url)
     (response/xexpr
@@ -95,15 +96,49 @@
              (form ((action
                      ,(embed/url insert-comment-handler)))
                    (input ((name "comment")))
-                   (input ((type "submit"))))))))
+                   (input ((type "submit"))))
+             (a ((href ,(embed/url back-handler)))
+                "Back to the blog")))))
 
   (define (parse-comment bindings)
     (extract-binding/single 'comment bindings))
 
-  (define (insert-comment-handler a-request)
-    (post-insert-comment!
-     a-post (parse-comment (request-bindings a-request)))
-    (render-post-detail-page a-post a-request))
+  (define (insert-comment-handler request)
+    (render-confirm-add-comment-page
+     (parse-comment (request-bindings request))
+     a-post
+     request))
+
+  (define (back-handler request)
+    (render-blog-page request))
+  (send/suspend/dispatch response-generator))
+
+; render-confirm-add-comment-page: comment post request -> doesn't return
+; consumes a comment that we intend to add to a post, as well
+; as the request. If the user follows through, adds a comment
+; and goes back to the display page. Otherwise, goes back to
+; the detail page of the post.
+(define (render-confirm-add-comment-page a-comment a-post request)
+  (define (response-generator embed/url)
+    (response/xexpr
+     `(html (head (title "Add a Comment"))
+            (body
+             (h1 "Add a Comment")
+             "The comment: " (div (p ,a-comment))
+             "will be added to "
+             (div ,(post-title a-post))
+
+             (p (a ((href ,(embed/url yes-handler)))
+                   "Yes, add the comment."))
+             (p (a ((href ,(embed/url cancel-handler)))
+                   "No, I changed my mind!"))))))
+
+  (define (yes-handler request)
+    (post-insert-comment! a-post a-comment)
+    (render-post-detail-page a-post request))
+
+  (define (cancel-handler request)
+    (render-post-detail-page a-post request))
   (send/suspend/dispatch response-generator))
 
 ; render-post: post (handler -> string) -> xexpr
