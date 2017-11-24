8c9dfde6094241cee193259a335a8009395d03d4
Marcelo dos Santos <mdssjc@gmail.com>
2017-11-24 20:13:12 -0200

    Exercise 14.4.

--- haskell/pih2/ch14/exercises.hs
+++ haskell/pih2/ch14/exercises.hs
@@ -16,14 +16,49 @@
 
 -- 14.3
 -- instance Foldable Maybe where
+--   --fold :: Monoid a => Maybe a -> a
+--   fold Nothing  = mempty
+--   fold (Just x) = x
+
 --   --foldMap :: Monoid b => (a -> b) -> Maybe a -> b
---   foldMap _ Nothing = mempty
+--   foldMap _ Nothing  = mempty
 --   foldMap g (Just x) = g x
 
---   --foldr   :: (a -> b -> b) -> b -> Maybe a -> b
+--   --foldr :: (a -> b -> b) -> b -> Maybe a -> b
 --   foldr _ v _ Nothing = v
---   foldr g v (Just x) = g x v
+--   foldr g v (Just x)  = g x v
 
---   --foldl   :: (a -> b -> a) -> a -> Maybe b -> a
---   foldl _ v Nothing = v
+--   --foldl :: (a -> b -> a) -> a -> Maybe b -> a
+--   foldl _ v Nothing  = v
 --   foldl g v (Just x) = g v x
+
+-- instance Traversable Maybe where
+--   -- traverse :: Applicative f => (a -> f b) -> Maybe a -> f (Maybe b)
+--   traverse _ Nothing  = pure Nothing
+--   traverse g (Just x) = fmap Just (g x)
+
+-- 14.4
+-- data Tree a = Leaf | Node (Tree a) a (Tree a)
+--             deriving Show
+
+-- instance Foldable Tree where
+--   --fold :: Monoid a => Tree a -> a
+--   fold Leaf         = mempty
+--   fold (Node l v r) = fold l `mappend` v `mappend` fold r
+
+--   --foldMap :: Monoid b => (a -> b) -> Tree a -> b
+--   foldMap _ Leaf         = mempty
+--   foldMap g (Node l v r) = foldMap g l `mappend` g v `mappend` foldMap g r
+
+--   --foldr :: (a -> b -> b) -> b -> Tree a -> b
+--   foldr g a Leaf         = a
+--   foldr g a (Node l v r) = foldr g (foldr g (g v a) r) l
+
+--   --foldl :: (a -> b -> a) -> a -> Tree b -> a
+--   foldl _ a Leaf         = a
+--   foldl g a (Node l v r) = foldl g (foldl g (g a v) r) r
+
+-- instance Traversable Maybe where
+--   -- traverse :: Applicative f => (a -> f b) -> Tree a -> f (Tree b)
+--   traverse _ Leaf         = pure Leaf
+--   traverse g (Node l v r) = fmap Node (traverse g l) (g x) (traverse g r)
