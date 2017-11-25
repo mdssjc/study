eef1f8552c67e701704029b28b4affd67d788aef
Marcelo dos Santos <mdssjc@gmail.com>
2017-11-25 19:40:45 -0200

    Exercise 15.2.

--- haskell/pih2/ch15/exercises.hs
+++ haskell/pih2/ch15/exercises.hs
@@ -3,3 +3,17 @@
 -- (1+2) * (2+3): innermost
 -- fst(1+2, 2+3): both
 -- (\x -> 1+x)(2*3): both
+
+-- 15.2
+-- outermost
+-- fst(1+2, 2+3)
+-- 1+2
+-- 3
+
+-- innermost
+-- fst(1+2, 2+3)
+-- fst(3, 2+3)
+-- fst(3, 5)
+-- 3
+
+-- Outermost is preferable.
