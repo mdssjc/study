c29a61ee026e268c2b678ffdb2c46b76c7135603
Marcelo dos Santos <mdssjc@gmail.com>
2017-11-27 22:49:58 -0200

    11 Continuations.

--- lisp/racket/More/serve.rkt
+++ lisp/racket/More/serve.rkt
@@ -1,7 +1,8 @@
 #lang racket
 
 (require xml
-         net/url)
+         net/url
+         racket/control)
 
 
 ;; ==========
@@ -44,7 +45,7 @@
     ; Discard the rest of the header (up to blank line):
     (regexp-match #rx"(\r\n|^)\r\n" in)
     ; Dispatch:
-    (let ([xexpr (dispatch (list-ref req 1))])
+    (let ([xexpr (prompt (dispatch (list-ref req 1)))])
       ; Send reply:
       (display "HTTP/1.0 200 Okay\r\n" out)
       (display "Server: k\r\nContent-Type: text/html\r\n\r\n" out)
@@ -85,10 +86,49 @@
   (define n (string->number (cdr (assq 'number query))))
   `(html (body ,@(for/list ([i (in-range n)]) " hello"))))
 
+(define (sum query) (build-request-page "First number:" "/one" ""))
+
+(define (one query)
+  (build-request-page "Second number:"
+                      "/two"
+                      (cdr (assq 'number query))))
+
+(define (two query)
+  (let ([n (string->number (cdr (assq 'hidden query)))]
+        [m (string->number (cdr (assq 'number query)))])
+    `(html (body "The sum is " ,(number->string (+ m n))))))
+
+(define (send/suspend mk-page)
+  (let/cc k
+    (define tag (format "k~a" (current-inexact-milliseconds)))
+    (hash-set! dispatch-table tag k)
+    (abort (mk-page (string-append "/" tag)))))
+
+(define (get-number label)
+  (define query
+    ; Generate a URL for the current computation:
+    (send/suspend
+     ; Receive the computation-as-URL here:
+     (lambda (k-url)
+       ; Generate the query-page result for this connection.
+       ; Send the query result to the saved-computation URL:
+       (build-request-page label k-url ""))))
+  ; We arrive here later, in a new connection
+  (string->number (cdr (assq 'number query))))
+
+(define (sum2 query)
+  (define m (get-number "First number:"))
+  (define n (get-number "Second number:"))
+  `(html (body "The sum is " ,(number->string (+ m n)))))
+
 
 (hash-set! dispatch-table "hello" (lambda (query) `(html (body "Hello, World!"))))
 (hash-set! dispatch-table "many"  many)
 (hash-set! dispatch-table "reply" reply)
+(hash-set! dispatch-table "sum"   sum)
+(hash-set! dispatch-table "one"   one)
+(hash-set! dispatch-table "two"   two)
+(hash-set! dispatch-table "sum2"  sum2)
 
 ;(enter! "serve.rkt")
 (go)
