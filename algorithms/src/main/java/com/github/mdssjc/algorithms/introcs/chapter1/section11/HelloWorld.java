d1d579e40be1f228fc5f8d61323b61fefd845ac4
Marcelo dos Santos <mdssjc@gmail.com>
2018-01-03 22:47:03 -0200

    Program 1.1.1 Hello, World.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/section11/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.github.mdssjc.algorithms.introcs.chapter1.section11;
+
+import com.github.mdssjc.algorithms.utils.Executor;
+import com.github.mdssjc.algorithms.utils.TestDrive;
+
+/**
+ * Program 1.1.1 Hello, World.
+ *
+ * @author Marcelo dos Santos
+ */
+@TestDrive
+public class HelloWorld {
+
+  public static void main(final String[] args) {
+    Executor.execute(HelloWorld.class, args);
+
+    // Prints "Hello, World" in the terminal window.
+    System.out.println("Hello, World");
+  }
+}
