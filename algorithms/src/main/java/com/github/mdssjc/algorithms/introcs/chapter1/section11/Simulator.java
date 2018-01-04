d43efa3dd420a73381663a136a815faf4ef29929
Marcelo dos Santos <mdssjc@gmail.com>
2018-01-03 22:47:36 -0200

    Simulador adicionado.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/section11/Simulator.java
@@ -0,0 +1,16 @@
+package com.github.mdssjc.algorithms.introcs.chapter1.section11;
+
+import com.github.mdssjc.algorithms.utils.Executor;
+
+/**
+ * Simulator Class.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+public class Simulator {
+
+  public static void main(final String[] args) {
+    Executor.execute(HelloWorld.class, "Program 1.1.1 Hello, World");
+  }
+}
