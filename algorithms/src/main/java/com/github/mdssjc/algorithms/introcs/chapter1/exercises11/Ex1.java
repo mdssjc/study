9b6b6c31f7923540e39d70cedb393af67012a46f
Marcelo dos Santos <mdssjc@gmail.com>
2018-01-04 23:13:15 -0200

    Exercise 1.1.1.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/exercises11/Ex1.java
@@ -0,0 +1,22 @@
+package com.github.mdssjc.algorithms.introcs.chapter1.exercises11;
+
+import com.github.mdssjc.algorithms.utils.Executor;
+import com.github.mdssjc.algorithms.utils.TestDrive;
+
+/**
+ * Exercise 1.1.1.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+@TestDrive
+public class Ex1 {
+
+  public static void main(final String[] args) {
+    Executor.execute(Ex1.class, args);
+
+    for (int i = 0; i < 10; i++) {
+      System.out.println("Hello, World");
+    }
+  }
+}
