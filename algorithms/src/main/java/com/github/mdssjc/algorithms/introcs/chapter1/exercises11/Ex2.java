caf602c0a7354a9df40c7c8092a140301db8ef83
Marcelo dos Santos <mdssjc@gmail.com>
2018-01-04 23:13:28 -0200

    Exercise 1.1.2.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/exercises11/Ex2.java
@@ -0,0 +1,23 @@
+package com.github.mdssjc.algorithms.introcs.chapter1.exercises11;
+
+import com.github.mdssjc.algorithms.utils.Executor;
+import com.github.mdssjc.algorithms.utils.TestDrive;
+
+/**
+ * Exercise 1.1.2.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+@TestDrive
+public class Ex2 {
+
+  public static void main(final String[] args) {
+    Executor.execute(Ex2.class, args);
+
+    System.out.println("a. Erro: o método main não foi encontrado na classe HelloWorld");
+    System.out.println("b. Erro: o método main não é static na classe HelloWorld");
+    System.out.println("c. Error: invalid method declaration");
+    System.out.println("d. Error: <identifier> expected");
+  }
+}
