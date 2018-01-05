9f382b185b50a52aac78e35729887b9a80f3a6d2
Marcelo dos Santos <mdssjc@gmail.com>
2018-01-04 22:54:00 -0200

    Program 1.1.2 Using a command-line argument.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/section11/UseArgument.java
@@ -0,0 +1,23 @@
+package com.github.mdssjc.algorithms.introcs.chapter1.section11;
+
+import com.github.mdssjc.algorithms.utils.Executor;
+import com.github.mdssjc.algorithms.utils.TestDrive;
+
+/**
+ * Program 1.1.2 Using a command-line argument.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+@TestDrive("Alice")
+@TestDrive("Bob")
+public class UseArgument {
+
+  public static void main(String[] args) {
+    Executor.execute(UseArgument.class, args);
+
+    System.out.print("Hi, ");
+    System.out.print(args[0]);
+    System.out.println(". How are you?");
+  }
+}
