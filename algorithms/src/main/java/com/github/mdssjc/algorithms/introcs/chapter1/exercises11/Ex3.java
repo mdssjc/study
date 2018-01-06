5029cd0657a88f94bb9ad967d9d8f5a30877b8c2
Marcelo dos Santos <mdssjc@gmail.com>
2018-01-05 23:07:20 -0200

    Exercise 1.1.3.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/exercises11/Ex3.java
@@ -0,0 +1,23 @@
+package com.github.mdssjc.algorithms.introcs.chapter1.exercises11;
+
+import com.github.mdssjc.algorithms.utils.Executor;
+import com.github.mdssjc.algorithms.utils.TestDrive;
+
+/**
+ * Exercise 1.1.3.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+@TestDrive
+public class Ex3 {
+
+  public static void main(final String[] args) {
+    Executor.execute(Ex3.class, args);
+
+    System.out.println("a. error: <identifier> expected");
+    System.out.println("b. error: <identifier> expected; error: invalid method declaration; return type required");
+    System.out.println("c. error: cannot find symbol");
+    System.out.println("d. compile");
+  }
+}
