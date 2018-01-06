8c01877b1fbc37af356e86537c0d165646fc1839
Marcelo dos Santos <mdssjc@gmail.com>
2018-01-05 23:14:26 -0200

    Exercise 1.1.4.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/exercises11/Ex4.java
@@ -0,0 +1,22 @@
+package com.github.mdssjc.algorithms.introcs.chapter1.exercises11;
+
+import com.github.mdssjc.algorithms.utils.Executor;
+import com.github.mdssjc.algorithms.utils.TestDrive;
+
+/**
+ * Exercise 1.1.4.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+@TestDrive
+public class Ex4 {
+
+  public static void main(final String[] args) {
+    Executor.execute(Ex4.class, args);
+
+  //  System.out.println("Hello,
+  //                      World");
+    System.out.println("Compile error");
+  }
+}
