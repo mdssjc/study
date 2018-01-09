14ce35f1562f9f41aad512a42614adc2a59f80c6
Marcelo dos Santos <mdssjc@gmail.com>
2018-01-08 22:58:45 -0200

    Refatoração com final.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/section12/IntOps.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/section12/IntOps.java
@@ -13,14 +13,14 @@ import com.github.mdssjc.algorithms.utils.TestDrive;
 @TestDrive({"1234", "99"})
 public class IntOps {
 
-  public static void main(String[] args) {
+  public static void main(final String[] args) {
     Executor.execute(IntOps.class, args);
 
-    int a = Integer.parseInt(args[0]);
-    int b = Integer.parseInt(args[1]);
-    int p = a * b;
-    int q = a / b;
-    int r = a % b;
+    final int a = Integer.parseInt(args[0]);
+    final int b = Integer.parseInt(args[1]);
+    final int p = a * b;
+    final int q = a / b;
+    final int r = a % b;
     System.out.println(a + " * " + b + " = " + p);
     System.out.println(a + " / " + b + " = " + q);
     System.out.println(a + " % " + b + " = " + r);
