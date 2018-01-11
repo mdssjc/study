13f1e80d4b10be71547cd39adce237d83fdc8168
Marcelo dos Santos <mdssjc@gmail.com>
2018-01-11 19:29:36 -0200

    Placeholder para o padrão de projeto: Abstract Factory.

---
+++ design-patterns/src/main/java/com/github/mdssjc/design_patterns/abstract_factory/Main.java
@@ -0,0 +1,19 @@
+package com.github.mdssjc.design_patterns.abstract_factory;
+
+/**
+ * Padrão de projeto: Abstract Factory.
+ * <p>
+ * Design Pattern
+ * Object Creational / Construction - Abstract Factory (Kit)
+ * <p>
+ * O padrão Abstract Factory prove uma interface para criar uma família de
+ * objetos relacionados ou dependentes sem especificar suas classes concretas.
+ *
+ * @author Marcelo dos Santos
+ */
+public class Main {
+
+  public static void main(final String[] args) {
+
+  }
+}
