dff762579929a161518809551da0d4466bc66294
Marcelo dos Santos <mdssjc@gmail.com>
2018-01-11 22:46:42 -0200

    1.1 - The Arithmetic of Numbers.

---
+++ lisp/racket/HtDP2e/I/1-Arithmetic.rkt
@@ -0,0 +1,18 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname 1-Arithmetic) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; I - Fixed-Size Data
+;; 1 - Arithmetic
+
+;; 1.1 - The Arithmetic of Numbers
+
+;; Exercise 1
+
+(define x 3)
+(define y 4)
+
+(define (distance x y)
+  (sqrt (+ (sqr x) (sqr y))))
+
+(check-expect (distance x y)   5)
+(check-expect (distance 12 5) 13)
