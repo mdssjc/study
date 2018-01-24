f7e7e602cf774886b73bfcaca843ac53131179f8
Marcelo dos Santos <mdssjc@gmail.com>
2018-01-23 22:47:46 -0200

    Novo título: 3 - How to Design Programs; Exercise 33.

---
+++ lisp/racket/HtDP2e/I/3-How to Design Programs.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname 3-How to Design Programs) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; 3-How to Design Programs.rkt
+;; I - Fixed-Size Data
+;; 3 - How to Design Programs
+
+(require 2htdp/image)
+(require 2htdp/batch-io)
+
+
+;; Exercise 33
+
+"The Year 2000 problem is also known as the Y2K problem, the Millennium bug, the Y2K bug, or Y2K."
