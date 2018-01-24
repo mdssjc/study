a343c4c092275e398355a3bf07c10e07fae70f63
Marcelo dos Santos <mdssjc@gmail.com>
2018-01-23 22:53:09 -0200

    Correção do valor esperado na comparação do teste.

--- lisp/racket/HtDP2e/I/2-Functions and Programs.rkt
+++ lisp/racket/HtDP2e/I/2-Functions and Programs.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname 2-Functions and Programs) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |2-Functions and Programs|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;; 2-Functions and Programs.rkt
 ;; I - Fixed-Size Data
 ;; 2 - Functions and Programs
@@ -149,7 +149,7 @@
 
 ;; Exercise 26
 
-(check-expect (string-insert "helloworld" 6) "helloorld")
+(check-expect (string-insert "helloworld" 6) "hellow_orld")
 
 
 
