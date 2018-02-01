bf443fa72188225f5e57aba6160d6d3a4481d034
Marcelo dos Santos <mdssjc@gmail.com>
2018-02-01 15:35:39 -0200

    Exercise 1.2.12.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/exercises12/Ex12.java
@@ -0,0 +1,20 @@
+package com.github.mdssjc.algorithms.introcs.chapter1.exercises12;
+
+import com.github.mdssjc.algorithms.utils.Executor;
+import com.github.mdssjc.algorithms.utils.TestDrive;
+
+/**
+ * Exercise 1.2.12.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+@TestDrive
+public class Ex12 {
+
+  public static void main(final String[] args) {
+    Executor.execute(Ex12.class, args);
+
+    System.out.println("Error: 'java: cannot find symbol' - sqrt.");
+  }
+}
