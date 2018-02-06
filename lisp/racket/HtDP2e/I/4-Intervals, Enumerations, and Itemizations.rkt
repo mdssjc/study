5da4db02373a494d80f4c00fce27aeae98f7bb6f
Marcelo dos Santos <mdssjc@gmail.com>
2018-02-05 23:00:26 -0200

    Novo arquivo: 4 - Intervals, Enumerations, and Itemizations.

---
+++ lisp/racket/HtDP2e/I/4-Intervals, Enumerations, and Itemizations.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |4-Intervals, Enumerations, and Itemizations|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; 4-Intervals, Enumerations, and Itemizations.rkt
+;; I - Fixed-Size Data
+;; 4 - Intervals, Enumerations, and Itemizations
+
+(require 2htdp/image)
+(require 2htdp/universe)
