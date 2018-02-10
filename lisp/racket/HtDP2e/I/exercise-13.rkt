c8e0f7a8ff470428ab29186ee9023193bd3fbfa8
Marcelo dos Santos <mdssjc@gmail.com>
2018-02-10 12:13:49 -0200

    Arquivos removidos.

--- lisp/racket/HtDP2e/I/exercise-13.rkt
+++
@@ -1,9 +0,0 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-13) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-;; Exercise 13
-
-(define (string-first s)
-  (substring s 0 1))
-
-(check-expect (string-first "abc") "a")
--- lisp/racket/HtDP2e/I/exercise-7.rkt
+++
@@ -1,9 +0,0 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-7) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-;; Exercise 7
-
-(define sunny #true)
-(define friday #false)
-
-(check-expect (or (not sunny) friday) #false)
