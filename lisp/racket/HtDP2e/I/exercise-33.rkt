f50d1476849fc8168ddd8fd243e52ff201fcc1ca
Marcelo dos Santos <mdssjc@gmail.com>
2018-02-11 12:05:44 -0200

    Arquivos removidos.

--- lisp/racket/HtDP2e/I/exercise-33.rkt
+++
@@ -1,6 +0,0 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-33) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-;; Exercise 33
-
-"The Year 2000 problem is also known as the Y2K problem, the Millennium bug, the Y2K bug, or Y2K."
