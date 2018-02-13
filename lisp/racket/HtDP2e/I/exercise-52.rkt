9138af6fa691b6146d9ebf79edcb2cdb6096d229
Marcelo dos Santos <mdssjc@gmail.com>
2018-02-13 13:26:27 -0200

    Arquivos removidos.

--- lisp/racket/HtDP2e/I/exercise-52.rkt
+++
@@ -1,9 +0,0 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-52) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-;; Exercise 52
-
-"1: 3 4 5"
-"2: 4 5"
-"3: 3 4"
-"4: 4"
--- lisp/racket/HtDP2e/I/exercise-55.rkt
+++
@@ -1,9 +0,0 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-55) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-;; Exercise 55
-
-; LRCD -> Image
-; produces a rocket at height h
-(define (draw h)
-  (place-image ROCKET 10 (- h CENTER) BACKG))
