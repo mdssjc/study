9ed96b96654e2e71251b960ed904c345f9a82c7c
Marcelo dos Santos <mdssjc@gmail.com>
2018-02-15 19:21:34 -0200

    Documentação do padrão.

--- design-patterns/src/main/java/com/github/mdssjc/design_patterns/structural/bridge/Main.java
+++ design-patterns/src/main/java/com/github/mdssjc/design_patterns/structural/bridge/Main.java
@@ -8,6 +8,14 @@ package com.github.mdssjc.design_patterns.structural.bridge;
  * <p>
  * O padrão Bridge desacopla uma abstração da sua implementação, de modo que as
  * duas possam variar independentemente.
+ * <p>
+ * Abstração (Abstraction)        -> Interface.
+ * Implementação (Implementation) -> Plataforma (Platform).
+ * <p>
+ * Conceitos: abstração/plataforma (abstraction/platform);
+ *            domínio/infraestrutura (domain/infrastructure);
+ *            front-end/back-end; ou
+ *            interface/implementação (interface/implementation).
  *
  * @author Marcelo dos Santos
  *
@@ -15,8 +23,10 @@ package com.github.mdssjc.design_patterns.structural.bridge;
 public class Main {
 
   public static void main(final String[] args) {
-    final Abstraction abstraction1 = new RefinedAbstraction(new ConcreteImplementorA());
-    final Abstraction abstraction2 = new RefinedAbstraction(new ConcreteImplementorB());
+    final Abstraction abstraction1 = new RefinedAbstraction(
+        new ConcreteImplementorA());
+    final Abstraction abstraction2 = new RefinedAbstraction(
+        new ConcreteImplementorB());
 
     System.out.println(abstraction1.operation());
     System.out.println(abstraction2.operation());
