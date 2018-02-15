9847802f862d85b76c3a5ed07945725b3852c15a
Marcelo dos Santos <mdssjc@gmail.com>
2018-02-14 23:35:33 -0200

    Exercises 63 and 64.

--- lisp/racket/HtDP2e/I/5-Adding Structure.rkt
+++ lisp/racket/HtDP2e/I/5-Adding Structure.rkt
@@ -7,3 +7,101 @@
 
 (require 2htdp/image)
 (require 2htdp/universe)
+
+
+;; 5.1 - From Positions to posn Structures
+
+
+
+;; 5.2 - Computing with posns
+
+
+
+;; 5.3 - Programming with posn
+
+;; =================
+;; Functions:
+
+; computes the distance of ap to the origin
+(check-expect (distance-to-0 (make-posn 0 5))  5)
+(check-expect (distance-to-0 (make-posn 7 0))  7)
+(check-expect (distance-to-0 (make-posn 3 4))  5)
+(check-expect (distance-to-0 (make-posn 8 6))  10)
+(check-expect (distance-to-0 (make-posn 5 12)) 13)
+
+(define (distance-to-0 ap)
+  (sqrt
+   (+ (sqr (posn-x ap))
+      (sqr (posn-y ap)))))
+
+;; Exercise 63
+
+(distance-to-0 (make-posn 3 4))
+(sqrt
+ (+ (sqr (posn-x (make-posn 3 4)))
+    (sqr (posn-y (make-posn 3 4)))))
+(sqrt
+ (+ (sqr 3)
+    (sqr (posn-y (make-posn 3 4)))))
+(sqrt
+ (+ 9
+    (sqr (posn-y (make-posn 3 4)))))
+(sqrt
+ (+ 9
+    (sqr 4)))
+(sqrt
+ (+ 9
+    16))
+(sqrt 25)
+5
+
+(distance-to-0 (make-posn 6 (* 2 4)))
+(distance-to-0 (make-posn 6 8))
+(sqrt
+ (+ (sqr (posn-x (make-posn 6 8)))
+    (sqr (posn-y (make-posn 6 8)))))
+(sqrt
+ (+ (sqr 6)
+    (sqr (posn-y (make-posn 6 8)))))
+(sqrt
+ (+ 36
+    (sqr (posn-y (make-posn 6 8)))))
+(sqrt
+ (+ 36
+    (sqr 8)))
+(sqrt
+ (+ 36
+    64))
+(sqrt 100)
+10
+
+(+ (distance-to-0 (make-posn 12 5)) 10)
+(+ (sqrt
+    (+ (sqr (posn-x (make-posn 12 5)))
+       (sqr (posn-y (make-posn 12 5))))) 10)
+(+ (sqrt
+    (+ (sqr 12)
+       (sqr (posn-y (make-posn 12 5))))) 10)
+(+ (sqrt
+    (+ 144
+       (sqr (posn-y (make-posn 12 5))))) 10)
+(+ (sqrt
+    (+ 144
+       (sqr 5))) 10)
+(+ (sqrt
+    (+ 144
+       25)) 10)
+(+ (sqrt 169) 10)
+(+ 13 10)
+23
+
+;; Exercise 64
+
+;; =================
+;; Functions:
+
+;; measures the Manhattan distance of the given posn to the origin
+(check-expect (manhattan-distance (make-posn 3 4)) 5)
+
+(define (manhattan-distance p)
+  (distance-to-0 p))
