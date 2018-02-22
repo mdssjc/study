8a8f93468f68e726e0c91ecddbce23fed195bc37
Marcelo dos Santos <mdssjc@gmail.com>
2018-02-21 21:49:03 -0300

    Program 2.2.5 Plotting data values in an array.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter2/section22/StdStats.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter2/section22/StdStats.java
@@ -2,10 +2,12 @@ package com.github.mdssjc.algorithms.introcs.chapter2.section22;
 
 import com.github.mdssjc.algorithms.utils.Executor;
 import com.github.mdssjc.algorithms.utils.TestDrive;
+import edu.princeton.cs.introcs.StdDraw;
 import edu.princeton.cs.introcs.StdOut;
 
 /**
  * Program 2.2.4 Data analysis library.
+ * Program 2.2.5 Plotting data values in an array.
  *
  * @author Marcelo dos Santos
  *
@@ -54,13 +56,48 @@ public class StdStats {
     return Math.sqrt(var(a));
   }
 
+  public static void plotPoints(final double[] a) {
+    final int n = a.length;
+    StdDraw.setXscale(-1, n);
+    StdDraw.setPenRadius(1 / (3.0 * n));
+    for (int i = 0; i < n; i++) {
+      StdDraw.point(i, a[i]);
+    }
+  }
+
+  public static void plotLines(final double[] a) {
+    final int n = a.length;
+    StdDraw.setXscale(-1, n);
+    StdDraw.setPenRadius();
+    for (int i = 1; i < n; i++) {
+      StdDraw.line(i - 1, a[i - 1], i, a[i]);
+    }
+  }
+
+  public static void plotBars(final double[] a) {
+    final int n = a.length;
+    StdDraw.setXscale(-1, n);
+    for (int i = 0; i < n; i++) {
+      StdDraw.filledRectangle(i, a[i] / 2, 0.25, a[i] / 2);
+    }
+  }
+
   public static void main(final String[] args) {
     Executor.execute(StdStats.class, args);
 
-    final double[] a = StdArrayIO.readDouble1D();
+    double[] a = StdArrayIO.readDouble1D();
     StdOut.printf("     min %7.3f%n", min(a));
     StdOut.printf("    mean %7.3f%n", mean(a));
     StdOut.printf("     max %7.3f%n", max(a));
     StdOut.printf(" std dev %7.3f%n", stddev(a));
+
+    final int n = 20;
+    a = new double[n];
+    for (int i = 0; i < n; i++) {
+      a[i] = 1.0 / (i + 1);
+      plotPoints(a);
+      plotLines(a);
+      plotBars(a);
+    }
   }
 }
