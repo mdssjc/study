6ade26521f6ceba876d2b5a1fbfcf57fbf047836
Marcelo dos Santos <mdssjc@gmail.com>
2018-02-25 21:20:43 -0300

    Novo arquivo: 6 - Itemizations and Structures.

---
+++ lisp/racket/HtDP2e/I/6-Itemizations and Structures.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |6-Itemizations and Structures|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; 6-Itemizations and Structures.rkt
+;; I - Fixed-Size Data
+;; 6 Itemizations and Structures
+
+(require 2htdp/image)
+(require 2htdp/universe)
