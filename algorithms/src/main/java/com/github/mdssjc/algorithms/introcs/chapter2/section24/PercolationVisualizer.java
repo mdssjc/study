52c5642e8a0a0707b6e7a4ab2d85dde0ebe3533e
Marcelo dos Santos <mdssjc@gmail.com>
2018-03-06 21:42:45 -0300

    Program 2.4.3 Visualization client.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter2/section24/PercolationVisualizer.java
@@ -0,0 +1,34 @@
+package com.github.mdssjc.algorithms.introcs.chapter2.section24;
+
+import com.github.mdssjc.algorithms.utils.Executor;
+import com.github.mdssjc.algorithms.utils.TestDrive;
+import edu.princeton.cs.introcs.StdDraw;
+
+/**
+ * Program 2.4.3 Visualization client.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+@TestDrive({"20", "0.9"})
+@TestDrive({"20", "0.95"})
+public class PercolationVisualizer {
+
+  public static void main(final String[] args) {
+    Executor.execute(PercolationVisualizer.class, args);
+
+    final int n = Integer.parseInt(args[0]);
+    final double p = Double.parseDouble(args[1]);
+    StdDraw.enableDoubleBuffering();
+
+    final boolean[][] isOpen = Percolation.random(n, p);
+    StdDraw.setPenColor(StdDraw.BLACK);
+    Percolation.show(isOpen, false);
+
+    StdDraw.setPenColor(StdDraw.BOOK_BLUE);
+    final boolean[][] isFull = Percolation.flow(isOpen);
+    Percolation.show(isFull, true);
+
+    StdDraw.show();
+  }
+}
