c1f0cf7badc1b5ee0139301e332c11029c22239b
Marcelo dos Santos <mdssjc@gmail.com>
2018-03-07 22:47:42 -0300

    BSL Tests.

--- lisp/racket/HtDP2e/I/Intermezzo 1.rkt
+++ lisp/racket/HtDP2e/I/Intermezzo 1.rkt
@@ -256,5 +256,29 @@
 ; (ball-x (make-posn 1 2))
 ; error, ball-x expects a ball
 
-(ball-speed-y 5)
+; (ball-speed-y 5)
 ; error, ball-x expects a ball
+
+
+
+;; BSL Tests
+
+;; Exercise 128
+
+(check-expect 3 4)
+
+(check-member-of "green" "red" "yellow" "grey")
+
+(check-within (make-posn #i1.0 #i1.1)
+              (make-posn #i0.9 #i1.2)
+              0.01)
+
+(check-range #i0.9 #i0.6 #i0.8)
+
+(check-error (/ 1 1))
+
+(check-random (make-posn (random 3) (random 9))
+              (make-posn (random 9) (random 3)))
+
+; (check-satisfied 4 odd?)
+(check-satisfied 4 even?)
