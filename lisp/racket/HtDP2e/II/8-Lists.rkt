c04d8bde6ed869e65ae9b710a3b5c38a106afd54
Marcelo dos Santos <mdssjc@gmail.com>
2018-03-09 23:04:31 -0300

    8.1 - Creating Lists.

--- lisp/racket/HtDP2e/II/8-Lists.rkt
+++ lisp/racket/HtDP2e/II/8-Lists.rkt
@@ -7,3 +7,60 @@
 
 (require 2htdp/image)
 (require 2htdp/universe)
+
+
+
+;; 8.1 - Creating Lists
+
+;; Exercise 129
+
+; a list of celestial bodies
+(cons "Mercury"
+      (cons "Venus"
+            (cons "Earth"
+                  (cons "Mars"
+                        (cons "Jupiter"
+                              (cons "Saturn"
+                                    (cons "Uranus"
+                                          (cons "Neptune" '()))))))))
+
+; a list of items for a meal
+(cons "steak"
+      (cons "frenck fries"
+            (cons "beans"
+                  (cons "bread"
+                        (cons "water"
+                              (cons "brie cheese"
+                                    (cons "ice cream" '())))))))
+
+; a list of colors
+(cons "white"
+      (cons "black"
+            (cons "red"
+                  (cons "green"
+                        (cons "blue" '())))))
+
+;; Exercise 130
+
+; A List-of-names is one of:
+; - '()
+; - (cons String List-of-names)
+; interpretation a list of invitees, by last name
+(cons "Findler"
+      (cons "Flatt"
+            (cons "Felleisen"
+                  (cons "Krishnamurthi"
+                        (cons "dos Santos" '())))))
+
+(cons "1" (cons "2" '())) ; "1" and "2" are strings, match with List-of-names
+(cons 2 '())              ; 2 is a number, doesn't match with List-of-names
+
+;; Exercise 131
+
+; A List-of-booleans is one of:
+; - '()
+; - (cons Boolean List-of-booleans)
+; interpretation a list of boolean values
+(cons #true
+      (cons #false
+            (cons #true '())))
