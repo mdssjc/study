df0ab1b25c3f28eaefd3a2d33f5ef9370eee2a3e
Marcelo dos Santos <mdssjc@gmail.com>
2018-03-10 23:39:11 -0300

    8.4 - Computing with Lists.

--- lisp/racket/HtDP2e/II/8-Lists.rkt
+++ lisp/racket/HtDP2e/II/8-Lists.rkt
@@ -139,3 +139,184 @@
     [else
      (or (string=? (first los) s)
          (contains? s (rest los)))]))
+
+
+
+;; 8.4 - Computing with Lists
+
+;; Exercise 135
+
+(contains-flatt?.v2 (cons "Flatt" (cons "C" '())))
+(cond [(empty? (cons "Flatt" (cons "C" '()))) #false]
+      [(string=? (first (cons "Flatt" (cons "C" '()))) "Flatt") #true]
+      [else (contains-flatt?.v2 (rest (cons "Flatt" (cons "C" '()))))])
+(cond [#false #false]
+      [(string=? (first (cons "Flatt" (cons "C" '()))) "Flatt") #true]
+      [else (contains-flatt?.v2 (rest (cons "Flatt" (cons "C" '()))))])
+(cond [(string=? (first (cons "Flatt" (cons "C" '()))) "Flatt") #true]
+      [else (contains-flatt?.v2 (rest (cons "Flatt" (cons "C" '()))))])
+(cond [(string=? "Flatt" "Flatt") #true]
+      [else (contains-flatt?.v2 (rest (cons "Flatt" (cons "C" '()))))])
+(cond [#true #true]
+      [else (contains-flatt?.v2 (rest (cons "Flatt" (cons "C" '()))))])
+#true
+
+; ---
+
+(contains-flatt?.v2 (cons "A" (cons "Flatt" (cons "C" '()))))
+(cond [(empty? (cons "A" (cons "Flatt" (cons "C" '())))) #false]
+      [(string=? (first (cons "A" (cons "Flatt" (cons "C" '())))) "Flatt") #true]
+      [else (contains-flatt?.v2 (rest (cons "A" (cons "Flatt" (cons "C" '())))))])
+(cond [#false #false]
+      [(string=? (first (cons "A" (cons "Flatt" (cons "C" '())))) "Flatt") #true]
+      [else (contains-flatt?.v2 (rest (cons "A" (cons "Flatt" (cons "C" '())))))])
+(cond [(string=? (first (cons "A" (cons "Flatt" (cons "C" '())))) "Flatt") #true]
+      [else (contains-flatt?.v2 (rest (cons "A" (cons "Flatt" (cons "C" '())))))])
+(cond [(string=? "A" "Flatt") #true]
+      [else (contains-flatt?.v2 (rest (cons "A" (cons "Flatt" (cons "C" '())))))])
+(cond [#false #true]
+      [else (contains-flatt?.v2 (rest (cons "A" (cons "Flatt" (cons "C" '())))))])
+(cond [else (contains-flatt?.v2 (rest (cons "A" (cons "Flatt" (cons "C" '())))))])
+(contains-flatt?.v2 (rest (cons "A" (cons "Flatt" (cons "C" '())))))
+(contains-flatt?.v2 (cons "Flatt" (cons "C" '())))
+(cond [(empty? (cons "Flatt" (cons "C" '()))) #false]
+      [(string=? (first (cons "Flatt" (cons "C" '()))) "Flatt") #true]
+      [else (contains-flatt?.v2 (rest (cons "Flatt" (cons "C" '()))))])
+(cond [#false #false]
+      [(string=? (first (cons "Flatt" (cons "C" '()))) "Flatt") #true]
+      [else (contains-flatt?.v2 (rest (cons "Flatt" (cons "C" '()))))])
+(cond [(string=? (first (cons "Flatt" (cons "C" '()))) "Flatt") #true]
+      [else (contains-flatt?.v2 (rest (cons "Flatt" (cons "C" '()))))])
+(cond [(string=? "Flatt" "Flatt") #true]
+      [else (contains-flatt?.v2 (rest (cons "Flatt" (cons "C" '()))))])
+(cond [#true #true]
+      [else (contains-flatt?.v2 (rest (cons "Flatt" (cons "C" '()))))])
+#true
+
+; ---
+
+(contains-flatt?.v2 (cons "A" (cons "B" (cons "C" '()))))
+(cond [(empty? (cons "A" (cons "B" (cons "C" '())))) #false]
+      [(string=? (first (cons "A" (cons "B" (cons "C" '())))) "Flatt") #true]
+      [else (contains-flatt?.v2 (rest (cons "A" (cons "B" (cons "C" '())))))])
+(cond [#false #false]
+      [(string=? (first (cons "A" (cons "B" (cons "C" '())))) "Flatt") #true]
+      [else (contains-flatt?.v2 (rest (cons "A" (cons "B" (cons "C" '())))))])
+(cond [(string=? (first (cons "A" (cons "B" (cons "C" '())))) "Flatt") #true]
+      [else (contains-flatt?.v2 (rest (cons "A" (cons "B" (cons "C" '())))))])
+(cond [(string=? "A" "Flatt") #true]
+      [else (contains-flatt?.v2 (rest (cons "A" (cons "B" (cons "C" '())))))])
+(cond [#false #true]
+      [else (contains-flatt?.v2 (rest (cons "A" (cons "B" (cons "C" '())))))])
+(cond [else (contains-flatt?.v2 (rest (cons "A" (cons "B" (cons "C" '())))))])
+(contains-flatt?.v2 (rest (cons "A" (cons "B" (cons "C" '())))))
+(contains-flatt?.v2 (cons "B" (cons "C" '())))
+(cond [(empty? (cons "B" (cons "C" '()))) #false]
+      [(string=? (first (cons "B" (cons "C" '()))) "Flatt") #true]
+      [else (contains-flatt?.v2 (rest (cons "B" (cons "C" '()))))])
+(cond [#false #false]
+      [(string=? (first (cons "B" (cons "C" '()))) "Flatt") #true]
+      [else (contains-flatt?.v2 (rest (cons "B" (cons "C" '()))))])
+(cond [(string=? (first (cons "B" (cons "C" '()))) "Flatt") #true]
+      [else (contains-flatt?.v2 (rest (cons "B" (cons "C" '()))))])
+(cond [(string=? "B" "Flatt") #true]
+      [else (contains-flatt?.v2 (rest (cons "B" (cons "C" '()))))])
+(cond [#false #true]
+      [else (contains-flatt?.v2 (rest (cons "B" (cons "C" '()))))])
+(cond [else (contains-flatt?.v2 (rest (cons "B" (cons "C" '()))))])
+(contains-flatt?.v2 (rest (cons "B" (cons "C" '()))))
+(contains-flatt?.v2 (cons "C" '()))
+(cond [(empty? (cons "C" '())) #false]
+      [(string=? (first (cons "C" '())) "Flatt") #true]
+      [else (contains-flatt?.v2 (rest (cons "C" '())))])
+(cond [#false #false]
+      [(string=? (first (cons "C" '())) "Flatt") #true]
+      [else (contains-flatt?.v2 (rest (cons "C" '())))])
+(cond [(string=? (first (cons "C" '())) "Flatt") #true]
+      [else (contains-flatt?.v2 (rest (cons "C" '())))])
+(cond [(string=? "C" "Flatt") #true]
+      [else (contains-flatt?.v2 (rest (cons "C" '())))])
+(cond [#false #true]
+      [else (contains-flatt?.v2 (rest (cons "C" '())))])
+(cond [else (contains-flatt?.v2 (rest (cons "C" '())))])
+(contains-flatt?.v2 (rest (cons "C" '())))
+(contains-flatt?.v2 '())
+(cond [(empty? '()) #false]
+      [(string=? (first '()) "Flatt") #true]
+      [else (contains-flatt?.v2 (rest '()))])
+(contains-flatt?.v2 '())
+(cond [#true #false]
+      [(string=? (first '()) "Flatt") #true]
+      [else (contains-flatt?.v2 (rest '()))])
+#false
+
+;; Exercise 136
+
+
+;; =================
+;; Data definitions:
+
+(define-struct pair [left right])
+; A ConsPair is a structure:
+;   (make-pair Any Any)
+
+; A ConsOrEmpty is one of:
+; - '()
+; - (make-pair Any ConsOrEmpty)
+; interpretation ConsOrEmpty is the class of all lists
+
+
+;; =================
+;; Functions:
+
+; Any Any -> ConsOrEmpty
+(define (our-cons a-value a-list)
+  (cond
+    [(empty? a-list) (make-pair a-value a-list)]
+    [(pair?  a-list) (make-pair a-value a-list)]
+    [else (error "cons: second argument ...")]))
+
+; ConsOrEmpty -> Any
+; extracts the left part of the given pair
+(check-error  (our-first '()) "our-first: ...")
+(check-expect (our-first (our-cons "a" '())) "a")
+
+(define (our-first a-list)
+  (if (empty? a-list)
+      (error 'our-first "...")
+      (pair-left a-list)))
+
+; ConsOrEmpty -> Any
+; extracts the right part of the given pair
+(check-error  (our-rest '()) "our-rest: ...")
+(check-expect (our-rest (our-cons "a" '())) '())
+
+(define (our-rest a-list)
+  (if (empty? a-list)
+      (error 'our-rest "...")
+      (pair-right a-list)))
+
+
+(our-first (our-cons "a" '()))
+(our-first (make-pair "a" '()))
+(if (empty? (make-pair "a" '()))
+    (error 'our-first "...")
+    (pair-left (make-pair "a" '())))
+(if #false
+    (error 'our-first "...")
+    (pair-left (make-pair "a" '())))
+(pair-left (make-pair "a" '()))
+"a"
+
+; ---
+
+(our-rest (our-cons "a" '()))
+(our-rest (make-pair "a" '()))
+(if (empty? (make-pair "a" '()))
+    (error 'our-rest "...")
+    (pair-right (make-pair "a" '())))
+(if #false
+    (error 'our-rest "...")
+    (pair-right (make-pair "a" '())))
+(pair-right (make-pair "a" '()))
+'()
