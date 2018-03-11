a5c923c7477083dd0bdb1b7df81a3595cbf9ff67
Marcelo dos Santos <mdssjc@gmail.com>
2018-03-10 23:10:52 -0300

    8.3 - Programming with Lists.

--- lisp/racket/HtDP2e/II/8-Lists.rkt
+++ lisp/racket/HtDP2e/II/8-Lists.rkt
@@ -67,3 +67,75 @@
 
 
 ;; 8.2 - What Is '(), What Is cons
+
+
+
+;; 8.3 - Programming with Lists
+
+
+;; =================
+;; Constants:
+
+(define NAMES1 '())
+(define NAMES2 (cons "Find" '()))
+(define NAMES3 (cons "Flatt" '()))
+(define NAMES4 (cons "X" (cons "Y" (cons "Z" '()))))
+(define NAMES5 (cons "A" (cons "Flatt" (cons "C" '()))))
+(define NAMES6 (cons "Fagan" (cons "Findler" (cons "Fisler" (cons "Flanagan" (cons "Flatt" (cons "Felleisen" (cons "Friedman" '()))))))))
+
+
+;; =================
+;; Functions:
+
+;; Exercise 132
+
+; List-of-names -> Boolean
+; determines whether "Flatt" occurs on alon
+(check-expect (contains-flatt? NAMES1) #false)
+(check-expect (contains-flatt? NAMES2) #false)
+(check-expect (contains-flatt? NAMES3) #true)
+(check-expect (contains-flatt? NAMES4) #false)
+(check-expect (contains-flatt? NAMES5) #true)
+(check-expect (contains-flatt? NAMES6) #true)
+
+(define (contains-flatt? alon)
+  (cond
+    [(empty? alon) #false]
+    [(cons?  alon)
+     (or (string=? (first alon) "Flatt")
+         (contains-flatt? (rest alon)))]))
+
+;; Exercise 133
+
+; List-of-names -> Boolean
+; determines whether "Flatt" occurs on alon
+(check-expect (contains-flatt?.v2 NAMES1) (contains-flatt? NAMES1))
+(check-expect (contains-flatt?.v2 NAMES2) (contains-flatt? NAMES2))
+(check-expect (contains-flatt?.v2 NAMES3) (contains-flatt? NAMES3))
+(check-expect (contains-flatt?.v2 NAMES4) (contains-flatt? NAMES4))
+(check-expect (contains-flatt?.v2 NAMES5) (contains-flatt? NAMES5))
+(check-expect (contains-flatt?.v2 NAMES6) (contains-flatt? NAMES6))
+
+(define (contains-flatt?.v2 alon)
+  (cond
+    [(empty? alon) #false]
+    [(string=? (first alon) "Flatt") #true]
+    [else (contains-flatt?.v2 (rest alon))]))
+
+;; Exercise 134
+
+; String List-of-Strings -> Boolean
+; determines whether some given string occurs on a given list of strings
+(check-expect (contains? "Flatt" NAMES1) #false)
+(check-expect (contains? "Flatt" NAMES2) #false)
+(check-expect (contains? "Flatt" NAMES3) #true)
+(check-expect (contains? "Flatt" NAMES4) #false)
+(check-expect (contains? "Flatt" NAMES5) #true)
+(check-expect (contains? "Flatt" NAMES6) #true)
+
+(define (contains? s los)
+  (cond
+    [(empty? los) #false]
+    [else
+     (or (string=? (first los) s)
+         (contains? s (rest los)))]))
