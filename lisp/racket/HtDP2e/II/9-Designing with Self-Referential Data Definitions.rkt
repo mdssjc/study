6d29122fd19648467f3a2f092bd3e816235018cb
Marcelo dos Santos <mdssjc@gmail.com>
2018-03-11 22:37:37 -0300

    Placeholder para 9 - Designing with Self-Referential Data Definitions.

---
+++ lisp/racket/HtDP2e/II/9-Designing with Self-Referential Data Definitions.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |9-Designing with Self-Referential Data Definitions|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; 9-Designing with Self-Referential Data Definitions.rkt
+;; II - Arbitrarily Large Data
+;; 9 - Designing with Self-Referential Data Definitions
+
+(require 2htdp/image)
+(require 2htdp/universe)
