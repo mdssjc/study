3145f8d3b1ce013d5573ccecdacdd3e41362a5a9
Marcelo dos Santos <mdssjc@gmail.com>
2018-03-12 22:34:05 -0300

    Arquivos removidos.

--- lisp/racket/HtDP2e/II/exercise-137.rkt
+++
@@ -1,6 +0,0 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-137) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-;; Exercise 137
-
-; Yes, both have a cond for base and recursive conditions, with selectors for components.
