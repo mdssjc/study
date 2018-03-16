a7f38a93a4daf6398be37b1d0bbb03c0ad0bb477
Marcelo dos Santos <mdssjc@gmail.com>
2018-03-15 22:15:22 -0300

    Program 3.1.9 Splitting a file.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter3/section31/Split.java
@@ -0,0 +1,38 @@
+package com.github.mdssjc.algorithms.introcs.chapter3.section31;
+
+import com.github.mdssjc.algorithms.utils.Executor;
+import com.github.mdssjc.algorithms.utils.TestDrive;
+import edu.princeton.cs.introcs.In;
+import edu.princeton.cs.introcs.Out;
+
+/**
+ * Program 3.1.9 Splitting a file.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+@TestDrive(value = {"DJIA", "4"}, valueFile = true)
+public class Split {
+
+  public static void main(final String[] args) {
+    Executor.execute(Split.class, args);
+
+    final String name = args[0];
+    final int n = Integer.parseInt(args[1]);
+    final String delimiter = ",";
+
+    final Out[] out = new Out[n];
+    for (int i = 0; i < n; i++) {
+      out[i] = new Out(name + i + ".txt");
+    }
+
+    final In in = new In(name + ".csv");
+    while (in.hasNextLine()) {
+      final String line = in.readLine();
+      final String[] fields = line.split(delimiter);
+      for (int i = 0; i < n; i++) {
+        out[i].println(fields[i]);
+      }
+    }
+  }
+}
