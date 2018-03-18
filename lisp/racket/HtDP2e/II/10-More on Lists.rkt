48d122394552369f4d705cd57c1249a801eb1368
Marcelo dos Santos <mdssjc@gmail.com>
2018-03-17 23:02:24 -0300

    10 - More on Lists.

---
+++ lisp/racket/HtDP2e/II/10-More on Lists.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |10-More on Lists|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; 10-More on Lists.rkt
+;; II - Arbitrarily Large Data
+;; 10 - More on Lists
+
+(require 2htdp/image)
+(require 2htdp/universe)
