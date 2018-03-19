0ca24b6d9a7d448b0f49e4ca224ba24ab7482fa0
Marcelo dos Santos <mdssjc@gmail.com>
2018-03-18 23:43:34 -0300

    10.2 - Structures in Lists.

--- lisp/racket/HtDP2e/II/10-More on Lists.rkt
+++ lisp/racket/HtDP2e/II/10-More on Lists.rkt
@@ -136,3 +136,191 @@
   (cond [(empty? los) '()]
         [else (cons (string-replace (first los) old new)
                     (substitute new old (rest los)))]))
+
+
+
+;; 10.2 - Structures in Lists
+
+;; Exercise 166
+
+
+;; =================
+;; Data definitions:
+
+(define-struct work [employee number rate hours])
+; A (piece of) Work is a structure:
+;   (make-work String Number Number Number)
+; interpretation (make-work n num r h) combines the name
+; with the number num, pay rate r and the number of hours h
+
+(define-struct pay-check (number name amount))
+; A Pay-check is a structure:
+;  (make-pay-check Number String Number)
+; interpretation (make-pay-check num n a) contains
+; the employee's number num, name n and an amount a
+
+; A Low (short for list of works) is one of:
+; - '()
+; - (cons Work Low)
+; interpretation an instance of Low represents the
+; hours worked for a number of employees
+
+; A Lop is one of:
+; - '()
+; - (cons Pay-check Lop)
+; interpretation an instance of Lop represents the
+; employee's name and an amount
+
+
+;; =================
+;; Functions:
+
+; Low -> Lop
+; consumes a list of work records and computes a list of pay checks
+(check-expect (wage*.v3 '()) '())
+(check-expect (wage*.v3 (cons (make-work "Robby" 1 11.95 39) '()))
+              (cons (make-pay-check 1 "Robby" (* 11.95 39)) '()))
+(check-expect (wage*.v3 (cons (make-work "Matthew" 1 12.95 45)
+                              (cons (make-work "Robby" 2 11.95 39) '())))
+              (cons (make-pay-check 1 "Matthew" (* 12.95 45))
+                    (cons (make-pay-check 2 "Robby" (* 11.95 39)) '())))
+
+(define (wage*.v3 an-low)
+  (cond [(empty? an-low) '()]
+        [(cons? an-low) (cons (create-paycheck (first an-low))
+                              (wage*.v3 (rest an-low)))]))
+
+; Work -> Pay-check
+; creates a paycheck for the given work w
+(define (create-paycheck w)
+  (make-pay-check (work-number w)
+                  (work-employee w)
+                  (wage.v3 w)))
+
+; Work -> Number
+; computes the wage for the given work record w
+(define (wage.v3 w)
+  (* (work-rate w) (work-hours w)))
+
+;; Exercise 167
+
+
+;; =================
+;; Data definitions:
+
+; A Lop is one of:
+; - '()
+; - (cons Posn Lop)
+; interpretation an instance of Lop represents a Posn list
+
+
+;; =================
+;; Functions:
+
+; Lop -> Number
+; consumes a list of Posns and produces the sum of all of its x-coordinates
+(check-expect (sum '()) 0)
+(check-expect (sum (cons (make-posn 11 20) '())) 11)
+(check-expect (sum (cons (make-posn 11 20) (cons (make-posn 48 66) '()))) (+ 11 48))
+
+(define (sum lop)
+  (cond [(empty? lop) 0]
+        [else (+ (posn-x (first lop))
+                 (sum (rest lop)))]))
+
+;; Exercise 168
+
+
+;; =================
+;; Functions:
+
+; Lop -> Lop
+; produces lists of Posns with (make-posn x (+ y 1)) - known as translation
+(check-expect (translate '()) '())
+(check-expect (translate (cons (make-posn 10 10) '()))
+              (cons (make-posn 10 11) '()))
+(check-expect (translate (cons (make-posn 10 10) (cons (make-posn 20 20) '())))
+              (cons (make-posn 10 11) (cons (make-posn 20 21) '())))
+
+(define (translate lop)
+  (cond [(empty? lop) '()]
+        [else (cons (make-posn (posn-x (first lop))
+                               (add1 (posn-y (first lop))))
+              (translate (rest lop)))]))
+
+;; Exercise 169
+
+
+;; =================
+;; Functions:
+
+; Lop -> Lop
+; produces lists of Posns whose x-coordinates are between 0 and 100
+; and whose y-coordinates are between 0 and 200
+(check-expect (legal '()) '())
+(check-expect (legal (cons (make-posn -1  100)  '())) '())
+(check-expect (legal (cons (make-posn 0   100)  '())) (cons (make-posn 0 100)   '()))
+(check-expect (legal (cons (make-posn 50  100)  '())) (cons (make-posn 50 100)  '()))
+(check-expect (legal (cons (make-posn 100 100)  '())) (cons (make-posn 100 100) '()))
+(check-expect (legal (cons (make-posn 101 100)  '())) '())
+(check-expect (legal (cons (make-posn 50  -1)   '())) '())
+(check-expect (legal (cons (make-posn 50  0)    '())) (cons (make-posn 50 0)   '()))
+(check-expect (legal (cons (make-posn 50  100)  '())) (cons (make-posn 50 100) '()))
+(check-expect (legal (cons (make-posn 50  200)  '())) (cons (make-posn 50 200) '()))
+(check-expect (legal (cons (make-posn 50  201)  '())) '())
+(check-expect (legal (cons (make-posn -1  100)
+                           (cons (make-posn 50 100)
+                                 (cons (make-posn 50 -1)
+                                       (cons (make-posn 50 100) '())))))
+              (cons (make-posn 50 100) (cons (make-posn 50 100) '())))
+
+(define (legal lop)
+  (cond [(empty? lop) '()]
+        [(< (posn-x (first lop)) 0)   (legal (rest lop))]
+        [(> (posn-x (first lop)) 100) (legal (rest lop))]
+        [(< (posn-y (first lop)) 0)   (legal (rest lop))]
+        [(> (posn-y (first lop)) 200) (legal (rest lop))]
+        [else (cons (first lop)
+                    (legal (rest lop)))]))
+
+;; Exercise 170
+
+
+;; =================
+;; Data definitions:
+
+(define-struct phone [area switch four])
+; A Phone is a structure:
+;   (make-phone Three Three Four)
+; A Three is a Number between 100 and 999.
+; A Four is a Number between 1000 and 9999.
+(define P1 (make-phone 713 123 4567))
+(define P2 (make-phone 281 123 4567))
+(define P3 (make-phone 123 123 4567))
+
+; A Lop is one of:
+; - '()
+; - (cons Phone Lop)
+; interpretation an instance of Lop represents a Phone list
+
+
+;; =================
+;; Functions:
+
+; Lop -> Lop
+; replaces all occurrence of area code 713 with 281
+(check-expect (replace* '()) '())
+(check-expect (replace* (cons P1 '())) (cons P2 '()))
+(check-expect (replace* (cons P1 (cons P3 '()))) (cons P2 (cons P3 '())))
+
+(define (replace* lop)
+  (cond [(empty? lop) '()]
+        [else (cons (replace (first lop))
+                    (replace* (rest lop)))]))
+
+; Phone -> Phone
+; replaces the area code 713 with 281
+(define (replace p)
+  (cond [(= (phone-area p) 713)
+         (make-phone 281 (phone-switch p) (phone-four p))]
+        [else p]))
