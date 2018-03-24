5a19d8c22964ae59a714ce66b38cd12c833a8209
Marcelo dos Santos <mdssjc@gmail.com>
2018-03-23 22:06:01 -0300

    Configurado para Beginning Student with List Abbreviations.

--- lisp/racket/HtDP2e/II/11-Design by Composition.rkt
+++ lisp/racket/HtDP2e/II/11-Design by Composition.rkt
@@ -1,9 +1,9 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname |11-Design by Composition|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname |11-Design by Composition|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;; 11-Design by Composition.rkt
 ;; II - Arbitrarily Large Data
-;; 11 Design by Composition
+;; 11 - Design by Composition
 
 (require 2htdp/image)
 (require 2htdp/universe)
