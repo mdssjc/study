3f44b92b3f70be1fa051c3d543e69b39724b0071
Marcelo dos Santos <mdssjc@gmail.com>
2018-03-26 23:26:27 -0300

    Até o exercício 191.

--- lisp/racket/HtDP2e/II/11-Design by Composition.rkt
+++ lisp/racket/HtDP2e/II/11-Design by Composition.rkt
@@ -346,3 +346,89 @@
   (cond [(empty? lo1s) '()]
         [else (cons lo1s
                     (suffixes (rest lo1s)))]))
+
+
+
+;; 11.4 - Auxiliary Functions that Generalize
+
+;; Exercise 191
+
+
+;; =================
+;; Constants:
+
+; a plain background image
+(define MT (empty-scene 50 50))
+
+
+;; =================
+;; Data definitions:
+
+; A Polygon is one of:
+; - (list Posn Posn Posn)
+; - (cons Posn Polygon)
+(define triangle-p (list (make-posn 20 10)
+                         (make-posn 20 20)
+                         (make-posn 30 20)))
+(define square-p   (list (make-posn 10 10)
+                         (make-posn 20 10)
+                         (make-posn 20 20)
+                         (make-posn 10 20)))
+
+; An NELoP is one of:
+; - (cons Posn '())
+; - (cons Posn NELoP)
+
+
+;; =================
+;; Functions:
+
+; Image Polygon -> Image
+; renders the given polygon p into img
+(check-expect (render-poly MT triangle-p) (scene+line
+                                           (scene+line
+                                            (scene+line MT 20 10 20 20 "red")
+                                            20 20 30 20 "red")
+                                           30 20 20 10 "red"))
+(check-expect (render-poly MT square-p)
+              (scene+line (connect-dots MT square-p) 10 20 10 10 "red"))
+
+(define (render-poly img p)
+  (cond [(empty? (rest (rest (rest p))))
+         (render-line
+          (render-line
+           (render-line MT (first p) (second p))
+           (second p) (third p))
+          (third p) (first p))]
+        [else
+         (render-line (render-poly MT (rest p))
+                      (first p)
+                      (second p))]))
+
+; Image NELoP -> Image
+; connects the dots in p by rendering lines in img
+(check-expect (connect-dots MT triangle-p)
+              (scene+line (scene+line MT 20 10 20 20 "red") 20 20 30 20 "red"))
+
+(define (connect-dots img p)
+  (cond [(empty? (rest p)) img]
+        [else (render-line (connect-dots img (rest p))
+                           (first p)
+                           (second p))]))
+
+;; ; Image Polygon -> Image
+;; ; adds an image of p to img
+;; (define (render-polygon img p)
+;;   (render-line (connect-dots img p)
+;;                (first p)
+;;                (last p)))
+
+; Image Posn Posn -> Image
+; renders a line from p to q into img
+(check-expect (render-line MT (make-posn 2 10) (make-posn 20 20))
+              (scene+line MT 2 10 20 20 "red"))
+
+(define (render-line img p q)
+  (scene+line img
+              (posn-x p) (posn-y p) (posn-x q) (posn-y q)
+              "red"))
