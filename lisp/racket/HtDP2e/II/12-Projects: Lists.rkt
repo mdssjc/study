f4fa468efd0e8f4792390884618948065992dedd
Marcelo dos Santos <mdssjc@gmail.com>
2018-03-29 22:21:25 -0300

    Exercício 195.

--- lisp/racket/HtDP2e/II/12-Projects: Lists.rkt
+++ lisp/racket/HtDP2e/II/12-Projects: Lists.rkt
@@ -7,3 +7,52 @@
 
 (require 2htdp/image)
 (require 2htdp/universe)
+(require 2htdp/batch-io)
+
+
+;; 12.1 - Real-World Data: Dictionaries
+
+
+;; =================
+;; Constants:
+
+; On OS X: /usr/share/dict/words
+; On LINUX: /usr/share/dict/words or /var/lib/dict/words
+; On WINDOWS: borrow the word file from your Linux friend
+(define LOCATION "/usr/share/dict/words")
+
+
+;; =================
+;; Data definitions:
+
+; A Letter is one of the following 1Strings:
+; - "a"
+; - ...
+; - "z"
+; or, equivalently, a member? of this list:
+(define LETTERS (explode "abcdefghijklmnopqrstuvwxyz"))
+
+; A Dictionary is a List-of-strings.
+(define AS-LIST (read-lines LOCATION))
+(define DICTIONARY-AS-LIST (list "alfa" "eco" "bravo" "erlang" "charlie" "zulu"))
+
+
+;; =================
+;; Functions:
+
+;; Exercise 195
+
+; Dictionary Letter -> Number
+; counts how many words in the given Dictionary d start with the given Letter l
+(check-expect (starts-with# '() "e") 0)
+(check-expect (starts-with# DICTIONARY-AS-LIST "e") 2)
+(check-expect (starts-with# DICTIONARY-AS-LIST "z") 1)
+
+(define (starts-with# d l)
+  (cond [(empty? d) 0]
+        [else (+ (if (string=? (string-ith (first d) 0) l) 1 0)
+                 (starts-with# (rest d) l))]))
+
+
+(starts-with# AS-LIST "e")              ; 4006
+(starts-with# AS-LIST "z")              ; 190
