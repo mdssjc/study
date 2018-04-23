7898f0af781fcbea83d0759ed30ccb5448ef369c
Marcelo dos Santos <mdssjc@gmail.com>
2018-04-23 18:20:29 -0300

    Homework 5.3.

--- hands-on/M101J/src/main/resources/aggregate.js
+++ hands-on/M101J/src/main/resources/aggregate.js
@@ -39,3 +39,31 @@ db.zips.aggregate([
         }
     }
 ])
+
+db.grades.aggregate([
+    {
+        $unwind: "$scores"
+    },
+    {
+        $match: {
+            "scores.type": {"$in": ["exam", "homework"]}
+        }
+    },
+    {
+        $group: {
+            _id: {student_id: "$student_id", class_id :"$class_id"},
+            average: {"$avg": "$scores.score"}
+        }
+    },
+    {
+        $group: {
+            _id: "$_id.class_id",
+            average: {"$avg": "$average"}
+        }
+    },
+    {
+        $sort: {
+            average: -1
+        }
+    }
+])
