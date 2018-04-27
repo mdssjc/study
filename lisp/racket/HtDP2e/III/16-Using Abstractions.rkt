66ebd7f63d0bbfb9d47c4e2f30555ddf7d45fa04
Marcelo dos Santos <mdssjc@gmail.com>
2018-04-26 23:09:22 -0300

    16.5 - Using Abstractions, by Example.

--- lisp/racket/HtDP2e/III/16-Using Abstractions.rkt
+++ lisp/racket/HtDP2e/III/16-Using Abstractions.rkt
@@ -573,3 +573,88 @@
                      f
                      g)))
 (result.v2 2)
+
+
+
+;; 16.5 - Using Abstractions, by Example
+
+
+;; =================
+;; Functions:
+
+; [List-of Posn] -> [List-of Posn]
+; adds 3 to each x-coordinate on the given list
+(check-expect (add-3-to-all (list (make-posn 3 1) (make-posn 0 0)))
+              (list (make-posn 6 1) (make-posn 3 0)))
+
+#;
+(define (add-3-to-all lop) '())
+
+#;
+(define (add-3-to-all lop)
+  (local (; Posn -> Posn
+          ; ...
+          (define (fp p)
+            ... p ...))
+    (map fp lop)))
+
+(define (add-3-to-all lop)
+  (local (; Posn -> Posn
+          ; adds 3 to the x-coordinate of p
+          (define (add-3-to-1 p)
+            (make-posn (+ (posn-x p) 3) (posn-y p))))
+    (map add-3-to-1 lop)))
+
+; [List-of Posn] -> [List-of Posn]
+; eliminates Posns whose y-coordinate is > 100
+(check-expect (keep-good (list (make-posn 0 110) (make-posn 0 60)))
+              (list (make-posn 0 60)))
+
+#;
+(define (keep-good lop) '())
+
+#;
+(define (keep-good lop)
+  (local (; Posn -> Boolean
+          ; should this Posn stay on the list
+          (define (good? p) #true))
+    (filter good? lop)))
+
+(define (keep-good lop)
+  (local (; Posn -> Posn
+          ; should this Posn stay on the list
+          (define (good? p)
+            (not (> (posn-y p) 100))))
+    (filter good? lop)))
+
+; Posn Posn Number -> Boolean
+; is the distance between p and q less than d
+(define (close-to p q d)
+  (<= (sqrt (+ (sqr (- (posn-x q) (posn-x p)))
+               (sqr (- (posn-y q) (posn-y p)))))
+      d))
+
+; [List-of Posn] Posn -> Boolean
+; is any Posn on lop close to pt
+(check-expect (close? (list (make-posn 47 54) (make-posn 0 60)) (make-posn 50 50))
+              #true)
+
+#;
+(define (close? lop pt) #false)
+
+#;
+(define (close? lop pt)
+  (local (; Posn -> Boolean
+          ; ...
+          (define (is-one-close? p)
+            ...))
+    (ormap close-to? lop)))
+
+(define (close? lop pt)
+  (local (; Posn -> Boolean
+          ; is one shot close to pt
+          (define (is-one-close? p)
+            (close-to p pt CLOSENESS)))
+    (ormap is-one-close? lop)))
+
+(define CLOSENESS 5) ; in terms of pixels
