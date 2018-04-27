d529718ad978070246d009abf4a8fd79bed1e2b7
Marcelo dos Santos <mdssjc@gmail.com>
2018-04-27 20:50:44 -0300

    Program 4.2.6 Mergesort.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter4/section42/Merge.java
@@ -0,0 +1,58 @@
+package com.github.mdssjc.algorithms.introcs.chapter4.section42;
+
+import com.github.mdssjc.algorithms.utils.Executor;
+import com.github.mdssjc.algorithms.utils.TestDrive;
+import edu.princeton.cs.introcs.StdIn;
+import edu.princeton.cs.introcs.StdOut;
+
+/**
+ * Program 4.2.6 Mergesort.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+@TestDrive(input = "8words.txt", inputFile = true)
+@TestDrive(input = "TomSawyer.txt", inputFile = true)
+public class Merge {
+
+  public static void sort(final Comparable[] a) {
+    final Comparable[] aux = new Comparable[a.length];
+    sort(a, aux, 0, a.length);
+  }
+
+  private static void sort(final Comparable[] a, final Comparable[] aux,
+                           final int lo, final int hi) {
+    if (hi - lo <= 1) {
+      return;
+    }
+    final int mid = lo + (hi - lo) / 2;
+    sort(a, aux, lo, mid);
+    sort(a, aux, mid, hi);
+    int i = lo, j = mid;
+    for (int k = lo; k < hi; k++) {
+      if (i == mid) {
+        aux[k] = a[j++];
+      } else if (j == hi) {
+        aux[k] = a[i++];
+      } else if (a[j].compareTo(a[i]) < 0) {
+        aux[k] = a[j++];
+      } else {
+        aux[k] = a[i++];
+      }
+    }
+    for (int k = lo; k < hi; k++) {
+      a[k] = aux[k];
+    }
+  }
+
+  public static void main(final String[] args) {
+    Executor.execute(Merge.class, args);
+
+    final String[] a = StdIn.readAllStrings();
+    sort(a);
+    for (int i = 0; i < a.length; i++) {
+      StdOut.print(a[i] + " ");
+    }
+    StdOut.println();
+  }
+}
