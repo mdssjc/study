238327e5a7789b1df2e662081926c8c459393d66
Marcelo dos Santos <mdssjc@gmail.com>
2018-05-22 22:13:04 -0300

    Placeholder para o 20 - Iterative Refinement.

---
+++ lisp/racket/HtDP2e/IV/20-Iterative Refinement.rkt
@@ -0,0 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |20-Iterative Refinement|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; 20-Iterative Refinement.rkt
+;; IV - Intertwined Data
+;; 20 - Iterative Refinement
