f439bfff60ca78919214d0429f1d9c00143b85dd
Marcelo dos Santos <mdssjc@gmail.com>
2018-05-30 20:33:12 -0300

    Atualização: Program 1.2.2 Integer multiplication and division.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/section12/IntOps.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/section12/IntOps.java
@@ -5,6 +5,25 @@ import com.github.mdssjc.algorithms.utils.TestDrive;
 
 /**
  * Program 1.2.2 Integer multiplication and division.
+ * <p>
+ * Compilation:  javac IntOps.java
+ * Execution:    java IntOps a b
+ * <p>
+ * Illustrates the integer operations a + b, a * b, a / b, and a % b.
+ * <p>
+ * % java IntOps 1234 99
+ * 1234 + 99 = 1333
+ * 1234 * 99 = 122166
+ * 1234 / 99 = 12
+ * 1234 % 99 = 46
+ * 1234 = 12 * 99 + 46
+ * <p>
+ * % java IntOps 10 -3
+ * 10 + -3 = 7
+ * 10 * -3 = -30
+ * 10 / -3 = -3
+ * 10 % -3 = 1
+ * 10 = -3 * -3 + 1
  *
  * @author Marcelo dos Santos
  *
@@ -17,12 +36,15 @@ public class IntOps {
 
     final int a = Integer.parseInt(args[0]);
     final int b = Integer.parseInt(args[1]);
-    final int p = a * b;
-    final int q = a / b;
-    final int r = a % b;
-    System.out.println(a + " * " + b + " = " + p);
-    System.out.println(a + " / " + b + " = " + q);
-    System.out.println(a + " % " + b + " = " + r);
-    System.out.println(a + " = " + q + " * " + b + " + " + r);
+    final int sum = a + b;
+    final int prod = a * b;
+    final int quot = a / b;
+    final int rem = a % b;
+
+    System.out.println(a + " + " + b + " = " + sum);
+    System.out.println(a + " * " + b + " = " + prod);
+    System.out.println(a + " / " + b + " = " + quot);
+    System.out.println(a + " % " + b + " = " + rem);
+    System.out.println(a + " = " + quot + " * " + b + " + " + rem);
   }
 }
