f68fe5e4e9ea1d1d4e8fec9e7e3c50028c5602c9
Marcelo dos Santos <mdssjc@gmail.com>
2018-05-30 20:41:45 -0300

    Atualização: Program 1.2.5 Casting to get a random integer.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/section12/RandomInt.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/section12/RandomInt.java
@@ -5,6 +5,24 @@ import com.github.mdssjc.algorithms.utils.TestDrive;
 
 /**
  * Program 1.2.5 Casting to get a random integer.
+ * <p>
+ * Compilation:  javac RandomInt.java
+ * Execution:    java RandomInt N
+ * <p>
+ * Prints a pseudo-random integer between 0 and N-1.
+ * Illustrate an explicit type conversion (cast) from double to int.
+ * <p>
+ * % java RandomInt 6
+ * Your random integer is: 5
+ * <p>
+ * % java RandomInt 6
+ * Your random integer is: 0
+ * <p>
+ * % java RandomInt 1000
+ * Your random integer is: 129
+ * <p>
+ * % java RandomInt 1000
+ * Your random integer is: 333
  *
  * @author Marcelo dos Santos
  *
@@ -18,8 +36,11 @@ public class RandomInt {
     Executor.execute(RandomInt.class, args);
 
     final int n = Integer.parseInt(args[0]);
+
     final double r = Math.random();
+
     final int value = (int) (r * n);
+
     System.out.println(value);
   }
 }
