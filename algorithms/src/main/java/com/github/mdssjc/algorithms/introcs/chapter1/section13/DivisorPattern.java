b6fb16e6835824666985caad291dc6065eee3e75
Marcelo dos Santos <mdssjc@gmail.com>
2018-05-31 20:07:23 -0300

    Atualização: Program 1.3.4 Your first nested loops.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/section13/DivisorPattern.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/section13/DivisorPattern.java
@@ -5,24 +5,54 @@ import com.github.mdssjc.algorithms.utils.TestDrive;
 
 /**
  * Program 1.3.4 Your first nested loops.
+ * <p>
+ * Compilation:  javac DivisorPattern.java
+ * Execution:    java DivisorPattern n
+ * <p>
+ * Prints a table where entry (i, j) is a '* ' if i divides j
+ * or j divides i and '. ' otherwise.
+ * <p>
+ * <p>
+ * % java DivisorPattern 20
+ * * * * * * * * * * * * * * * * * * * * * 1
+ * * *   *   *   *   *   *   *   *   *   * 2
+ * *   *     *     *     *     *     *     3
+ * * *   *       *       *       *       * 4
+ * *       *         *         *         * 5
+ * * * *     *           *           *     6
+ * *           *             *             7
+ * * *   *       *               *         8
+ * *   *           *                 *     9
+ * * *     *         *                   * 10
+ * *                   *                   11
+ * * * * *   *           *                 12
+ * *                       *               13
+ * * *         *             *             14
+ * *   *   *                   *           15
+ * * *   *       *               *         16
+ * *                               *       17
+ * * * *     *     *                 *     18
+ * *                                   *   19
+ * * *   * *         *                   * 20
  *
  * @author Marcelo dos Santos
  *
  */
-@TestDrive("3")
-@TestDrive("12")
+@TestDrive("20")
 public class DivisorPattern {
 
   public static void main(final String[] args) {
     Executor.execute(DivisorPattern.class, args);
 
-    final int n = Integer.parseInt(args[0]);
-    for (int i = 1; i <= n; i++) {
-      for (int j = 1; j <= n; j++) {
-        if ((i % j == 0) || (j % i == 0)) {
+    final var n = Integer.parseInt(args[0]);
+
+    for (var i = 1; i <= n; i++) {
+      for (var j = 1; j <= n; j++) {
+        if (i % j == 0 || j % i == 0) {
           System.out.print("* ");
-        } else {
-          System.out.print(" ");
+        }
+        else {
+          System.out.print("  ");
         }
       }
       System.out.println(i);
