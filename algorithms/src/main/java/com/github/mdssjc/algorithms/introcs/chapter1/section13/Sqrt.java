0bf9138aa518661e4a7039c5e5eef76a7e49b312
Marcelo dos Santos <mdssjc@gmail.com>
2018-05-31 20:41:20 -0300

    Atualização: Program 1.3.6 Newton’s method.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/section13/Sqrt.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/section13/Sqrt.java
@@ -5,23 +5,70 @@ import com.github.mdssjc.algorithms.utils.TestDrive;
 
 /**
  * Program 1.3.6 Newton’s method.
+ * <p>
+ * Compilation:  javac Sqrt.java
+ * Execution:    java Sqrt c
+ * <p>
+ * Computes the square root of a nonnegative number c using
+ * Newton's method:
+ * - initialize t = c
+ * - replace t with the average of c/t and t
+ * - repeat until desired accuracy reached
+ * <p>
+ * % java Sqrt 2
+ * 1.414213562373095
+ * <p>
+ * % java Sqrt 1000000
+ * 1000.0
+ * <p>
+ * % java Sqrt 0.4
+ * 0.6324555320336759
+ * <p>
+ * % java Sqrt 1048575
+ * 1023.9995117186336
+ * <p>
+ * % java Sqrt 16664444
+ * 4082.2106756021303
+ * <p>
+ * % java Sqrt 0
+ * 0.0
+ * <p>
+ * % java Sqrt 1e-50
+ * 9.999999999999999E-26
+ * <p>
+ * <p>
+ * Remarks
+ * ----------
+ * - using Math.abs() is required if c < 1
+ * <p>
+ * <p>
+ * Known bugs
+ * ----------
+ * - goes into an infinite loop if the input is negative
  *
  * @author Marcelo dos Santos
  *
  */
 @TestDrive("2.0")
-@TestDrive("2544545")
+@TestDrive("1000000")
+@TestDrive("0.4")
+@TestDrive("1048575")
+@TestDrive("16664444")
+@TestDrive("0")
+@TestDrive("1e-50")
 public class Sqrt {
 
   public static void main(final String[] args) {
     Executor.execute(Sqrt.class, args);
 
-    final double c = Double.parseDouble(args[0]);
-    final double EPSILON = 1e-15;
-    double t = c;
-    while (Math.abs(t - c / t) > EPSILON * t) {
+    final var c = Double.parseDouble(args[0]);
+    final var epsilon = 1e-15;
+    var t = c;
+
+    while (Math.abs(t - c / t) > epsilon * t) {
       t = (c / t + t) / 2.0;
     }
+
     System.out.println(t);
   }
 }
