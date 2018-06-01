605f3ea6080429de59226d6c588baca61aec080b
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-01 20:57:14 -0300

    Atualização: Program 1.4.3 Sieve of Eratosthenes.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/section14/PrimeSieve.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/section14/PrimeSieve.java
@@ -5,12 +5,50 @@ import com.github.mdssjc.algorithms.utils.TestDrive;
 
 /**
  * Program 1.4.3 Sieve of Eratosthenes.
+ * <p>
+ * Compilation:  javac PrimeSieve.java
+ * Execution:    java -Xmx1100m PrimeSieve n
+ * <p>
+ * Computes the number of primes less than or equal to n using
+ * the Sieve of Eratosthenes.
+ * <p>
+ * % java PrimeSieve 25
+ * The number of primes <= 25 is 9
+ * <p>
+ * % java PrimeSieve 100
+ * The number of primes <= 100 is 25
+ * <p>
+ * % java -Xmx100m PrimeSieve 100000000
+ * The number of primes <= 100000000 is 5761455
+ * <p>
+ * % java PrimeSieve -Xmx1100m 1000000000
+ * The number of primes <= 1000000000 is 50847534
+ * <p>
+ * <p>
+ * The 110MB and 1100MB is the amount of memory you want to allocate
+ * to the program. If your computer has less, make this number smaller,
+ * but it may prevent you from solving the problem for very large
+ * values of n.
+ * <p>
+ * <p>
+ *                  n     Primes <= n
+ *  ---------------------------------
+ *                 10               4
+ *                100              25
+ *              1,000             168
+ *             10,000           1,229
+ *            100,000           9,592
+ *          1,000,000          78,498
+ *         10,000,000         664,579
+ *        100,000,000       5,761,455
+ *      1,000,000,000      50,847,534
  *
  * @author Marcelo dos Santos
  *
  */
 @TestDrive("25")
 @TestDrive("100")
+@TestDrive("100000000")  // configure o -Xmx#### na execução
 @TestDrive("1000000000") // configure o -Xmx#### na execução
 public class PrimeSieve {
 
@@ -23,10 +61,11 @@ public class PrimeSieve {
     for (int i = 2; i <= n; i++) {
       isPrime[i] = true;
     }
-    for (int i = 2; i <= n / i; i++) {
-      if (isPrime[i]) {
-        for (int j = i; j <= n / i; j++) {
-          isPrime[i * j] = false;
+
+    for (int factor = 2; factor * factor <= n; factor++) {
+      if (isPrime[factor]) {
+        for (int j = factor; factor * j <= n; j++) {
+          isPrime[factor * j] = false;
         }
       }
     }
@@ -37,6 +76,6 @@ public class PrimeSieve {
         primes++;
       }
     }
-    System.out.println(primes);
+    System.out.println("The number of primes <= " + n + " is " + primes);
   }
 }
