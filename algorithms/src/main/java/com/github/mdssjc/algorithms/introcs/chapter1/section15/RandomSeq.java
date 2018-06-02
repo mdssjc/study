9b8b9a303e440b3d358d891fc351793671327bab
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-01 21:11:56 -0300

    Atualização: Program 1.5.1 Generating a random sequence.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/section15/RandomSeq.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/section15/RandomSeq.java
@@ -5,18 +5,31 @@ import com.github.mdssjc.algorithms.utils.TestDrive;
 
 /**
  * Program 1.5.1 Generating a random sequence.
+ * <p>
+ * Compilation:  javac RandomSeq.java
+ * Execution:    java RandomSeq n
+ * <p>
+ * Prints n random real numbers between 0 and 1.
+ * <p>
+ * % java RandomSeq 5
+ * 0.1654760343787165
+ * 0.6212262060326124
+ * 0.631755596883274
+ * 0.4165639935584283
+ * 0.4603525361488371
  *
  * @author Marcelo dos Santos
  *
  */
-@TestDrive("1000000")
+@TestDrive("5")
 public class RandomSeq {
 
   public static void main(final String[] args) {
     Executor.execute(RandomSeq.class, args);
 
-    final int n = Integer.parseInt(args[0]);
-    for (int i = 0; i < n; i++) {
+    final var n = Integer.parseInt(args[0]);
+
+    for (var i = 0; i < n; i++) {
       System.out.println(Math.random());
     }
   }
