55bee2ba65f9ec3d54875fe02a3f9c212e6883b3
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-01 21:25:32 -0300

    Remoção do ponto final.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/section14/Simulator.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/section14/Simulator.java
@@ -11,9 +11,9 @@ import com.github.mdssjc.algorithms.utils.Executor;
 public class Simulator {
 
   public static void main(final String[] args) {
-    Executor.execute(Sample.class, "Program 1.4.1 Sampling without replacement.");
-    Executor.execute(CouponCollector.class, "Program 1.4.2 Coupon collector simulation.");
-    Executor.execute(PrimeSieve.class, "Program 1.4.3 Sieve of Eratosthenes.");
-    Executor.execute(SelfAvoidingWalk.class, "Program 1.4.4 Self-avoiding random walks.");
+    Executor.execute(Sample.class, "Program 1.4.1 Sampling without replacement");
+    Executor.execute(CouponCollector.class, "Program 1.4.2 Coupon collector simulation");
+    Executor.execute(PrimeSieve.class, "Program 1.4.3 Sieve of Eratosthenes");
+    Executor.execute(SelfAvoidingWalk.class, "Program 1.4.4 Self-avoiding random walks");
   }
 }
--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/section15/Simulator.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/section15/Simulator.java
@@ -11,12 +11,12 @@ import com.github.mdssjc.algorithms.utils.Executor;
 public class Simulator {
 
   public static void main(final String[] args) {
-    Executor.execute(RandomSeq.class, "Program 1.5.1 Generating a random sequence.");
-    Executor.execute(TwentyQuestions.class, "Program 1.5.2 Interactive user input.");
-    Executor.execute(Average.class, "Program 1.5.3 Averaging a stream of numbers.");
-    Executor.execute(RangeFilter.class, "Program 1.5.4 A simple filter.");
-    Executor.execute(PlotFilter.class, "Program 1.5.5 Standard input-to-drawing filter.");
-    Executor.execute(BouncingBall.class, "Program 1.5.6 Bouncing ball.");
-    Executor.execute(PlayThatTune.class, "Program 1.5.7 Digital signal processing.");
+    Executor.execute(RandomSeq.class, "Program 1.5.1 Generating a random sequence");
+    Executor.execute(TwentyQuestions.class, "Program 1.5.2 Interactive user input");
+    Executor.execute(Average.class, "Program 1.5.3 Averaging a stream of numbers");
+    Executor.execute(RangeFilter.class, "Program 1.5.4 A simple filter");
+    Executor.execute(PlotFilter.class, "Program 1.5.5 Standard input-to-drawing filter");
+    Executor.execute(BouncingBall.class, "Program 1.5.6 Bouncing ball");
+    Executor.execute(PlayThatTune.class, "Program 1.5.7 Digital signal processing");
   }
 }
