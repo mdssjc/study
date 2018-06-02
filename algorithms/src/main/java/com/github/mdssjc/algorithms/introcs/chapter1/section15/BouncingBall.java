c13a6085dcf787e268f8d70652d299564a2463d8
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-01 21:19:32 -0300

    Atualização: Program 1.5.6 Bouncing ball.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/section15/BouncingBall.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/section15/BouncingBall.java
@@ -6,6 +6,14 @@ import edu.princeton.cs.introcs.StdDraw;
 
 /**
  * Program 1.5.6 Bouncing ball.
+ * <p>
+ * Compilation:  javac BouncingBall.java
+ * Execution:    java BouncingBall
+ * Dependencies: StdDraw.java
+ * <p>
+ * Implementation of a 2-d bouncing ball in the box from (-1, -1) to (1, 1).
+ * <p>
+ * % java BouncingBall
  *
  * @author Marcelo dos Santos
  *
@@ -20,22 +28,30 @@ public class BouncingBall {
     StdDraw.setYscale(-1.0, 1.0);
     StdDraw.enableDoubleBuffering();
 
-    double rx = 0.480, ry = 0.860;
-    double vx = 0.015, vy = 0.023;
-    final double radius = 0.05;
+    var rx = 0.480;
+    var ry = 0.860;
+    var vx = 0.015;
+    var vy = 0.023;
+    final var radius = 0.05;
 
     while (true) {
-      if (Math.abs(rx + vx) + radius > 1.0) {
+      if (Math.abs(rx + vx) > 1.0 - radius) {
         vx = -vx;
       }
-      if (Math.abs(ry + vy) + radius > 1.0) {
+      if (Math.abs(ry + vy) > 1.0 - radius) {
         vy = -vy;
       }
+
       rx += vx;
       ry += vy;
-      StdDraw.clear();
+
+      StdDraw.clear(StdDraw.LIGHT_GRAY);
+
+      StdDraw.setPenColor(StdDraw.BLACK);
       StdDraw.filledCircle(rx, ry, radius);
+
       StdDraw.show();
+
       StdDraw.pause(20);
     }
   }
