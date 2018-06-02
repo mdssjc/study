4044ab3ecbd0bf358c4124c5a14b358371b2e512
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-01 21:14:42 -0300

    Atualização: Program 1.5.2 Interactive user input.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/section15/TwentyQuestions.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/section15/TwentyQuestions.java
@@ -7,6 +7,24 @@ import edu.princeton.cs.introcs.StdOut;
 
 /**
  * Program 1.5.2 Interactive user input.
+ * <p>
+ * Compilation:  javac TwentyQuestions.java
+ * Execution:    java TwentyQuestions
+ * Dependencies  StdIn.java
+ * <p>
+ * % java TwentyQuestions
+ * I'm thinking of a number between 1 and 1,000,000
+ * What's your guess? 500000
+ * Too high
+ * What's your guess? 250000
+ * Too low
+ * What's your guess? 375000
+ * Too high
+ * What's your guess? 312500
+ * Too high
+ * What's your guess? 300500
+ * Too low
+ * ...
  *
  * @author Marcelo dos Santos
  *
@@ -17,21 +35,19 @@ public class TwentyQuestions {
   public static void main(final String[] args) {
     Executor.execute(TwentyQuestions.class, args);
 
-    final int secret = 1 + (int) (Math.random() * 1000000);
+    final var secret = 1 + (int) (Math.random() * 1000000);
+
     StdOut.print("I'm thinking of a number ");
     StdOut.println("between 1 and 1,000,000");
-
-    int guess = 0;
+    var guess = 0;
     while (guess != secret) {
       StdOut.print("What's your guess? ");
       guess = StdIn.readInt();
       if (guess == secret) {
         StdOut.println("You win!");
-      }
-      if (guess < secret) {
+      } else if (guess < secret) {
         StdOut.println("Too low ");
-      }
-      if (guess > secret) {
+      } else if (guess > secret) {
         StdOut.println("Too high");
       }
     }
