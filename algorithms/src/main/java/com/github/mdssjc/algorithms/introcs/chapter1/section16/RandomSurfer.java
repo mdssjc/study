81a034fe5ff63af7fd6f715c97426d24bfdf2a91
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-01 21:22:55 -0300

    Atualização: Program 1.6.2 Simulating a random surfer.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/section16/RandomSurfer.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter1/section16/RandomSurfer.java
@@ -7,18 +7,17 @@ import edu.princeton.cs.introcs.StdOut;
 
 /**
  * Program 1.6.2 Simulating a random surfer.
+ * <p>
+ * Compilation:  javac RandomSurfer.java
+ * Execution:    java RandomSurfer trials
+ * Data files:   https://introcs.cs.princeton.edu/16pagerank/tiny.txt
+ *               https://introcs.cs.princeton.edu/16pagerank/medium.txt
+ * <p>
+ * % java Transition < tiny.txt | java RandomSurfer 1000000
+ * 0.27297 0.26583 0.14598 0.24729 0.06793
  *
  * @author Marcelo dos Santos
- *
  */
-@TestDrive(value = "100", input = {
-    "5", "5",
-    "0.02000", "0.92000", "0.02000", "0.02000", "0.02000",
-    "0.02000", "0.02000", "0.38000", "0.38000", "0.20000",
-    "0.02000", "0.02000", "0.02000", "0.92000", "0.02000",
-    "0.92000", "0.02000", "0.02000", "0.02000", "0.02000",
-    "0.47000", "0.02000", "0.47000", "0.02000", "0.02000"
-})
 @TestDrive(value = "1000000", input = {
     "5", "5",
     "0.02000", "0.92000", "0.02000", "0.02000", "0.02000",
@@ -32,23 +31,29 @@ public class RandomSurfer {
   public static void main(final String[] args) {
     Executor.execute(RandomSurfer.class, args);
 
-    final int trials = Integer.parseInt(args[0]);
-    final int n = StdIn.readInt();
-    StdIn.readInt();
+    final var trials = Integer.parseInt(args[0]);
+    final var m = StdIn.readInt();
+    final var n = StdIn.readInt();
+    if (m != n) {
+      StdOut.println("m does not equal n");
+      return;
+    }
 
-    final double[][] p = new double[n][n];
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
+    final var p = new double[n][n];
+    for (var i = 0; i < n; i++) {
+      for (var j = 0; j < n; j++) {
         p[i][j] = StdIn.readDouble();
       }
     }
 
-    int page = 0;
-    final int[] freq = new int[n];
-    for (int t = 0; t < trials; t++) {
-      final double r = Math.random();
-      double sum = 0.0;
-      for (int j = 0; j < n; j++) {
+    final var freq = new int[n];
+
+    var page = 0;
+
+    for (var t = 0; t < trials; t++) {
+      final var r = Math.random();
+      var sum = 0.0;
+      for (var j = 0; j < n; j++) {
         sum += p[page][j];
         if (r < sum) {
           page = j;
@@ -58,7 +63,7 @@ public class RandomSurfer {
       freq[page]++;
     }
 
-    for (int i = 0; i < n; i++) {
+    for (var i = 0; i < n; i++) {
       StdOut.printf("%8.5f", (double) freq[i] / trials);
     }
     StdOut.println();
