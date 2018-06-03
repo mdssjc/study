7c33e22aaa43cab72d8e928f71ac36059a6b37d2
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-03 19:36:44 -0300

    Atualização: Program 2.1.1 Harmonic numbers (revisited).

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter2/section21/Harmonic.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter2/section21/Harmonic.java
@@ -6,17 +6,28 @@ import edu.princeton.cs.introcs.StdOut;
 
 /**
  * Program 2.1.1 Harmonic numbers (revisited).
+ * <p>
+ * Compilation:  javac Harmonic.java
+ * Execution:    java Harmonic n
+ * <p>
+ * Prints the nth harmonic number: 1/1 + 1/2 + ... + 1/n.
+ * <p>
+ * % java Harmonic 10
+ * 2.9289682539682538
+ * <p>
+ * % java Harmonic 10000
+ * 9.787606036044348
  *
  * @author Marcelo dos Santos
  *
  */
-@TestDrive({"1", "2", "4"})
-@TestDrive({"10", "100", "1000", "10000"})
+@TestDrive("10")
+@TestDrive("10000")
 public class Harmonic {
 
   public static double harmonic(final int n) {
-    double sum = 0.0;
-    for (int i = 1; i <= n; i++) {
+    var sum = 0.0;
+    for (var i = 1; i <= n; i++) {
       sum += 1.0 / i;
     }
     return sum;
@@ -25,9 +36,9 @@ public class Harmonic {
   public static void main(final String[] args) {
     Executor.execute(Harmonic.class, args);
 
-    for (int i = 0; i < args.length; i++) {
-      final int arg = Integer.parseInt(args[i]);
-      final double value = harmonic(arg);
+    for (var i = 0; i < args.length; i++) {
+      final var arg = Integer.parseInt(args[i]);
+      final var value = harmonic(arg);
       StdOut.println(value);
     }
   }
