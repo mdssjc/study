72036cc0cbafcac58c5ccb60649b58ae715fb06b
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-06 20:18:05 -0300

    Atualização: Program 2.4.1 Percolation scaffolding e Program 2.4.5 Percolation detection.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter2/section24/Percolation.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter2/section24/Percolation.java
@@ -8,25 +8,79 @@ import edu.princeton.cs.introcs.StdOut;
 import edu.princeton.cs.introcs.StdRandom;
 
 /**
+ * Program 2.4.1 Percolation scaffolding.
  * Program 2.4.5 Percolation detection.
+ * <p>
+ * Compilation:  javac Percolation.java
+ * Execution:    java Percolation < input.txt
+ * Dependencies: StdArrayIO.java StdDraw.java StdOut.java
+ * Data files:   http://www.cs.princeton.edu/introcs/24percolation/test5.txt
+ *               http://www.cs.princeton.edu/introcs/24percolation/test8.txt
+ *               http://www.cs.princeton.edu/introcs/24percolation/testD.txt
+ *               http://www.cs.princeton.edu/introcs/24percolation/testV.txt
+ *               http://www.cs.princeton.edu/introcs/24percolation/testT.txt
+ *               http://www.cs.princeton.edu/introcs/24percolation/testF.txt
+ *               http://www.cs.princeton.edu/introcs/24percolation/testTiny.txt
+ * <p>
+ * % more test5.txt
+ * 5 5
+ * 0 1 1 0 1
+ * 0 0 1 1 1
+ * 1 1 0 1 1
+ * 1 0 0 0 1
+ * 0 1 1 1 1
+ * <p>
+ * % java Percolation < test5.txt
+ * 5 5
+ * 0 1 1 0 1
+ * 0 0 1 1 1
+ * 0 0 0 1 1
+ * 0 0 0 0 1
+ * 0 1 1 1 1
+ * true
+ * <p>
+ * % more testD.txt
+ * 8 8
+ * 0 0 0 1 1 1 0 1
+ * 1 1 1 0 0 1 1 1
+ * 1 0 1 0 0 1 0 0
+ * 1 0 1 1 1 1 0 1
+ * 1 0 0 1 0 1 0 0
+ * 1 1 0 1 0 0 1 0
+ * 0 1 1 0 0 1 1 1
+ * 0 0 1 0 0 0 0 0
+ * <p>
+ * % java Percolation < testD.txt
+ * 8 8
+ * 0 0 0 1 1 1 0 1
+ * 1 1 1 0 0 1 1 1
+ * 1 0 1 0 0 1 0 0
+ * 1 0 1 1 1 1 0 0
+ * 1 0 0 1 0 1 0 0
+ * 1 1 0 1 0 0 0 0
+ * 0 1 1 0 0 0 0 0
+ * 0 0 1 0 0 0 0 0
+ * true
  *
  * @author Marcelo dos Santos
  *
  */
-@TestDrive(input = "test8.txt", inputFile = true)
+@TestDrive(input = "test5.txt", inputFile = true)
+@TestDrive(input = "testD.txt", inputFile = true)
 public class Percolation {
 
   public static boolean[][] flow(final boolean[][] isOpen) {
-    final int n = isOpen.length;
-    final boolean[][] isFull = new boolean[n][n];
-    for (int j = 0; j < n; j++) {
+    final var n = isOpen.length;
+    final var isFull = new boolean[n][n];
+    for (var j = 0; j < n; j++) {
       flow(isOpen, isFull, 0, j);
     }
     return isFull;
   }
 
   public static void flow(final boolean[][] isOpen, final boolean[][] isFull, final int i, final int j) {
-    final int n = isFull.length;
+    final var n = isOpen.length;
+
     if (i < 0 || i >= n) {
       return;
     }
@@ -39,7 +93,9 @@ public class Percolation {
     if (isFull[i][j]) {
       return;
     }
+
     isFull[i][j] = true;
+
     flow(isOpen, isFull, i + 1, j);
     flow(isOpen, isFull, i, j + 1);
     flow(isOpen, isFull, i, j - 1);
@@ -47,9 +103,9 @@ public class Percolation {
   }
 
   public static boolean percolates(final boolean[][] isOpen) {
-    final boolean[][] isFull = flow(isOpen);
-    final int n = isOpen.length;
-    for (int j = 0; j < n; j++) {
+    final var n = isOpen.length;
+    final var isFull = flow(isOpen);
+    for (var j = 0; j < n; j++) {
       if (isFull[n - 1][j]) {
         return true;
       }
@@ -57,20 +113,12 @@ public class Percolation {
     return false;
   }
 
-  public static void main(final String[] args) {
-    Executor.execute(Percolation.class, args);
-
-    final boolean[][] isOpen = StdArrayIO.readBoolean2D();
-    StdArrayIO.print(flow(isOpen));
-    StdOut.println(percolates(isOpen));
-  }
-
   public static void show(final boolean[][] a, final boolean which) {
-    final int n = a.length;
+    final var n = a.length;
     StdDraw.setXscale(-1, n);
     StdDraw.setYscale(-1, n);
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
+    for (var i = 0; i < n; i++) {
+      for (var j = 0; j < n; j++) {
         if (a[i][j] == which) {
           StdDraw.filledSquare(j, n - i - 1, 0.5);
         }
@@ -79,12 +127,20 @@ public class Percolation {
   }
 
   public static boolean[][] random(final int n, final double p) {
-    final boolean[][] a = new boolean[n][n];
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
+    final var a = new boolean[n][n];
+    for (var i = 0; i < n; i++) {
+      for (var j = 0; j < n; j++) {
         a[i][j] = StdRandom.bernoulli(p);
       }
     }
     return a;
+  }
+
+  public static void main(final String[] args) {
+    Executor.execute(Percolation.class, args);
+
+    final var isOpen = StdArrayIO.readBoolean2D();
+    StdArrayIO.print(flow(isOpen));
+    StdOut.println(percolates(isOpen));
   }
 }
