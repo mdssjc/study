02144b9d3cc8ed01f2f10a4dfbb55bdd20ebe1bf
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-06 20:44:25 -0300

    Atualização: Program 2.4.2 Vertical percolation detection.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter2/section24/VerticalPercolation.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter2/section24/VerticalPercolation.java
@@ -9,31 +9,80 @@ import edu.princeton.cs.introcs.StdRandom;
 
 /**
  * Program 2.4.2 Vertical percolation detection.
+ * <p>
+ * Compilation:  javac VerticalPercolation.java
+ * Execution:    java VerticalPercolation < input.txt
+ * Dependencies: StdArrayIO.java StdOut.java
+ * Data files:   http://www.cs.princeton.edu/introcs/24percolation/test5.txt
+ *               http://www.cs.princeton.edu/introcs/24percolation/test8.txt
+ *               http://www.cs.princeton.edu/introcs/24percolation/testD.txt
+ *               http://www.cs.princeton.edu/introcs/24percolation/testV.txt
+ *               http://www.cs.princeton.edu/introcs/24percolation/testT.txt
+ *               http://www.cs.princeton.edu/introcs/24percolation/testF.txt
+ *               http://www.cs.princeton.edu/introcs/24percolation/testTiny.txt
+ *               https://introcs.cs.princeton.edu/24percolation/testV.txt
+ * <p>
+ * % java VerticalPercolation < test5.txt
+ * 5 5
+ * 0 1 1 0 1
+ * 0 0 1 0 1
+ * 0 0 0 0 1
+ * 0 0 0 0 1
+ * 0 0 0 0 1
+ * true
+ * <p>
+ * % java VerticalPercolation < testD.txt
+ * 8 8
+ * 0 0 0 1 1 1 0 1
+ * 0 0 0 0 0 1 0 1
+ * 0 0 0 0 0 1 0 0
+ * 0 0 0 0 0 1 0 0
+ * 0 0 0 0 0 1 0 0
+ * 0 0 0 0 0 0 0 0
+ * 0 0 0 0 0 0 0 0
+ * 0 0 0 0 0 0 0 0
+ * false
+ * <p>
+ * % java VerticalPercolation < testV.txt
+ * 8 8
+ * 0 0 0 1 1 1 0 1
+ * 0 0 0 0 0 1 0 1
+ * 0 0 0 0 0 1 0 0
+ * 0 0 0 0 0 1 0 0
+ * 0 0 0 0 0 1 0 0
+ * 0 0 0 0 0 1 0 0
+ * 0 0 0 0 0 1 0 0
+ * 0 0 0 0 0 1 0 0
+ * true
  *
  * @author Marcelo dos Santos
  *
  */
 @TestDrive(input = "test5.txt", inputFile = true)
+@TestDrive(input = "testV.txt", inputFile = true)
 public class VerticalPercolation {
 
   public static boolean[][] flow(final boolean[][] isOpen) {
-    final int n = isOpen.length;
-    final boolean[][] isFull = new boolean[n][n];
-    for (int j = 0; j < n; j++) {
+    final var n = isOpen.length;
+    final var isFull = new boolean[n][n];
+
+    for (var j = 0; j < n; j++) {
       isFull[0][j] = isOpen[0][j];
     }
-    for (int i = 1; i < n; i++) {
-      for (int j = 0; j < n; j++) {
+
+    for (var i = 1; i < n; i++) {
+      for (var j = 0; j < n; j++) {
         isFull[i][j] = isOpen[i][j] && isFull[i - 1][j];
       }
     }
+
     return isFull;
   }
 
   public static boolean percolates(final boolean[][] isOpen) {
-    final boolean[][] isFull = flow(isOpen);
-    final int n = isOpen.length;
-    for (int j = 0; j < n; j++) {
+    final var n = isOpen.length;
+    final var isFull = flow(isOpen);
+    for (var j = 0; j < n; j++) {
       if (isFull[n - 1][j]) {
         return true;
       }
@@ -41,20 +90,12 @@ public class VerticalPercolation {
     return false;
   }
 
-  public static void main(final String[] args) {
-    Executor.execute(VerticalPercolation.class, args);
-
-    final boolean[][] isOpen = StdArrayIO.readBoolean2D();
-    StdArrayIO.print(flow(isOpen));
-    StdOut.println(percolates(isOpen));
-  }
-
   public static void show(final boolean[][] a, final boolean which) {
-    final int n = a.length;
+    final var n = a.length;
     StdDraw.setXscale(-1, n);
     StdDraw.setYscale(-1, n);
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
+    for (var i = 0; i < n; i++) {
+      for (var j = 0; j < n; j++) {
         if (a[i][j] == which) {
           StdDraw.filledSquare(j, n - i - 1, 0.5);
         }
@@ -63,12 +104,20 @@ public class VerticalPercolation {
   }
 
   public static boolean[][] random(final int n, final double p) {
-    final boolean[][] a = new boolean[n][n];
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
+    final var a = new boolean[n][n];
+    for (var i = 0; i < n; i++) {
+      for (var j = 0; j < n; j++) {
         a[i][j] = StdRandom.bernoulli(p);
       }
     }
     return a;
+  }
+
+  public static void main(final String[] args) {
+    Executor.execute(VerticalPercolation.class, args);
+
+    final var isOpen = StdArrayIO.readBoolean2D();
+    StdArrayIO.print(flow(isOpen));
+    StdOut.println(percolates(isOpen));
   }
 }
