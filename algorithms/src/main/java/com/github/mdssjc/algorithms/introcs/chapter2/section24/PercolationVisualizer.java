15143a68df5a57c440fd73e14a3c5f56f38f6123
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-06 20:46:22 -0300

    Atualização: Program 2.4.3 Visualization client.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter2/section24/PercolationVisualizer.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter2/section24/PercolationVisualizer.java
@@ -6,29 +6,44 @@ import edu.princeton.cs.introcs.StdDraw;
 
 /**
  * Program 2.4.3 Visualization client.
+ * <p>
+ * Compilation:  javac PercolationVisualizer.java
+ * Execution:    java PercolationVisualizer n p trials
+ * Dependencies: Percolation.java StdDraw.java
+ * <p>
+ * Repeatedly generated n-by-n boolean matrices, where each site
+ * is true with probability p. Plot the results using standard
+ * graphics.
+ * <p>
+ * % java PercolationVisualizer 20 0.35 10
+ * <p>
+ * % java PercolationVisualizer 20 0.45 10
  *
  * @author Marcelo dos Santos
  *
  */
-@TestDrive({"20", "0.9"})
-@TestDrive({"20", "0.95"})
+@TestDrive({"20", "0.35", "10"})
+@TestDrive({"20", "0.45", "10"})
 public class PercolationVisualizer {
 
   public static void main(final String[] args) {
     Executor.execute(PercolationVisualizer.class, args);
 
-    final int n = Integer.parseInt(args[0]);
-    final double p = Double.parseDouble(args[1]);
+    final var n = Integer.parseInt(args[0]);
+    final var p = Double.parseDouble(args[1]);
+    final var trials = Integer.parseInt(args[2]);
+
     StdDraw.enableDoubleBuffering();
-
-    final boolean[][] isOpen = Percolation.random(n, p);
-    StdDraw.setPenColor(StdDraw.BLACK);
-    Percolation.show(isOpen, false);
-
-    StdDraw.setPenColor(StdDraw.BOOK_BLUE);
-    final boolean[][] isFull = Percolation.flow(isOpen);
-    Percolation.show(isFull, true);
-
-    StdDraw.show();
+    for (var t = 0; t < trials; t++) {
+      final var open = Percolation.random(n, p);
+      StdDraw.clear();
+      StdDraw.setPenColor(StdDraw.BLACK);
+      Percolation.show(open, false);
+      StdDraw.setPenColor(StdDraw.GRAY);
+      final var full = Percolation.flow(open);
+      Percolation.show(full, true);
+      StdDraw.show();
+      StdDraw.pause(1000);
+    }
   }
 }
