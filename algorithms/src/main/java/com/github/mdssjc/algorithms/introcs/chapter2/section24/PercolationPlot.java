9b921b76040ff3df9f48065e97032e410e1237a5
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-06 20:49:39 -0300

    Atualização: Program 2.4.6 Adaptive plot client.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter2/section24/PercolationPlot.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter2/section24/PercolationPlot.java
@@ -6,6 +6,15 @@ import edu.princeton.cs.introcs.StdDraw;
 
 /**
  * Program 2.4.6 Adaptive plot client.
+ * <p>
+ * Compilation:  javac PercolationPlot.java
+ * Execution:    java PercolationPlot n
+ * <p>
+ * This recursive program draws a plot of the percolation probability
+ * (experimental observation) against the site vacancy probability
+ * (control variable).
+ * <p>
+ * % java PercolationPlot n
  *
  * @author Marcelo dos Santos
  *
@@ -15,12 +24,12 @@ import edu.princeton.cs.introcs.StdDraw;
 public class PercolationPlot {
 
   public static void curve(final int n, final double x0, final double y0, final double x1, final double y1) {
-    final double gap = 0.01;
-    final double err = 0.0025;
-    final int trials = 10000;
-    final double xm = (x0 + x1) / 2;
-    final double ym = (y0 + y1) / 2;
-    final double fxm = PercolationProbability.estimate(n, xm, trials);
+    final var gap = 0.01;
+    final var err = 0.0025;
+    final var trials = 10000;
+    final var xm = (x0 + x1) / 2;
+    final var ym = (y0 + y1) / 2;
+    final var fxm = PercolationProbability.evaluate(n, xm, trials);
     if (x1 - x0 < gap || Math.abs(ym - fxm) < err) {
       StdDraw.line(x0, y0, x1, y1);
       return;
@@ -33,7 +42,8 @@ public class PercolationPlot {
   public static void main(final String[] args) {
     Executor.execute(PercolationPlot.class, args);
 
-    final int n = Integer.parseInt(args[0]);
+    StdDraw.setScale(-0.05, 1.05);
+    final var n = Integer.parseInt(args[0]);
     curve(n, 0.0, 0.0, 1.0, 1.0);
   }
 }
