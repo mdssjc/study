b61cc7e3a81ef49cf32274118a464db4a6775c80
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-05 21:00:38 -0300

    Atualização: Program 2.3.3 Gray code.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter2/section23/Beckett.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter2/section23/Beckett.java
@@ -6,6 +6,28 @@ import edu.princeton.cs.introcs.StdOut;
 
 /**
  * Program 2.3.3 Gray code.
+ * <p>
+ * Compilation:  javac Beckett.java
+ * Execution:    java Beckett n
+ * <p>
+ * Prints instructions for a Beckett play with n actors.
+ * <p>
+ * % java Beckett 1
+ * enter 1
+ * <p>
+ * % java Beckett 2
+ * enter 1
+ * enter 2
+ * exit  1
+ * <p>
+ * % java Beckett 3
+ * enter 1
+ * enter 2
+ * exit  1
+ * enter 3
+ * enter 1
+ * exit  2
+ * exit  1
  *
  * @author Marcelo dos Santos
  *
@@ -13,18 +35,17 @@ import edu.princeton.cs.introcs.StdOut;
 @TestDrive("1")
 @TestDrive("2")
 @TestDrive("3")
-@TestDrive("4")
 public class Beckett {
 
-  public static void moves(final int n, final boolean enter) {
+  public static void moves(final int n, final boolean forward) {
     if (n == 0) {
       return;
     }
     moves(n - 1, true);
-    if (enter) {
+    if (forward) {
       StdOut.println("enter " + n);
     } else {
-      StdOut.println("exit " + n);
+      StdOut.println("exit  " + n);
     }
     moves(n - 1, false);
   }
@@ -32,7 +53,7 @@ public class Beckett {
   public static void main(final String[] args) {
     Executor.execute(Beckett.class, args);
 
-    final int n = Integer.parseInt(args[0]);
+    final var n = Integer.parseInt(args[0]);
     moves(n, true);
   }
 }
