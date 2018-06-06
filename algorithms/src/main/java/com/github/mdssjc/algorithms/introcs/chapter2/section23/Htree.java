eb2e8ec0b719b98ef9171a06c3ed0c5fb109271a
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-05 21:02:43 -0300

    Atualização: Program 2.3.4 Recursive graphics.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter2/section23/Htree.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter2/section23/Htree.java
@@ -6,39 +6,57 @@ import edu.princeton.cs.introcs.StdDraw;
 
 /**
  * Program 2.3.4 Recursive graphics.
+ * <p>
+ * Compilation:  javac Htree.java
+ * Execution:    java Htree n
+ * Dependencies: StdDraw.java
+ * <p>
+ * Plot an order n H-tree.
+ * <p>
+ * % java Htree 5
  *
  * @author Marcelo dos Santos
  *
  */
-@TestDrive("3")
-@TestDrive("4")
 @TestDrive("5")
 public class Htree {
 
-  public static void draw(final int n, final double size, final double x, final double y) {
+  public static void drawH(final double x, final double y, final double size) {
+    final var x0 = x - size / 2;
+    final var x1 = x + size / 2;
+    final var y0 = y - size / 2;
+    final var y1 = y + size / 2;
+
+    StdDraw.line(x0, y0, x0, y1);
+    StdDraw.line(x1, y0, x1, y1);
+    StdDraw.line(x0,  y, x1,  y);
+  }
+
+  public static void draw(final int n, final double x, final double y, final double size) {
     if (n == 0) {
       return;
     }
+    drawH(x, y, size);
 
-    final double x0 = x - size / 2;
-    final double x1 = x + size / 2;
-    final double y0 = y - size / 2;
-    final double y1 = y + size / 2;
+    final var x0 = x - size / 2;
+    final var x1 = x + size / 2;
+    final var y0 = y - size / 2;
+    final var y1 = y + size / 2;
 
-    StdDraw.line(x0, y, x1, y);
-    StdDraw.line(x0, y0, x0, y1);
-    StdDraw.line(x1, y0, x1, y1);
-
-    draw(n - 1, size / 2, x0, y0);
-    draw(n - 1, size / 2, x0, y1);
-    draw(n - 1, size / 2, x1, y0);
-    draw(n - 1, size / 2, x1, y1);
+    draw(n-1, x0, y0, size/2);
+    draw(n-1, x0, y1, size/2);
+    draw(n-1, x1, y0, size/2);
+    draw(n-1, x1, y1, size/2);
   }
 
   public static void main(final String[] args) {
     Executor.execute(Htree.class, args);
 
-    final int n = Integer.parseInt(args[0]);
-    draw(n, 0.5, 0.5, 0.5);
+    final var n = Integer.parseInt(args[0]);
+
+    final var x = 0.5;
+    final var y = 0.5;
+    final var size = 0.5;
+    draw(n, x, y, size);
   }
 }
