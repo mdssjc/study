d1792bc7de133a62e7fed958c79641455f2a0b73
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-07 20:58:59 -0300

    Atualização: Program 3.1.5 Image scaling.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter3/section31/Scale.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter3/section31/Scale.java
@@ -6,30 +6,40 @@ import edu.princeton.cs.introcs.Picture;
 
 /**
  * Program 3.1.5 Image scaling.
+ * <p>
+ * Compilation:  javac Scale.java
+ * Execution:    java Scale filename w h
+ * Data files:   http://www.cs.princeton.edu/introcs/31datatype/mandrill.jpg
+ * <p>
+ * Scales an image to w-by-h and displays both the original
+ * and the scaled version to the screen.
+ * <p>
+ * % java Scale mandrill.jpg 200 300
  *
  * @author Marcelo dos Santos
  *
  */
-@TestDrive(value = {"mandrill.jpg", "800", "800"}, valueFile = true)
-@TestDrive(value = {"mandrill.jpg", "600", "300"}, valueFile = true)
-@TestDrive(value = {"mandrill.jpg", "200", "400"}, valueFile = true)
-@TestDrive(value = {"mandrill.jpg", "200", "200"}, valueFile = true)
+@TestDrive(value = {"mandrill.jpg", "200", "300"}, valueFile = true)
 public class Scale {
 
   public static void main(final String[] args) {
     Executor.execute(Scale.class, args);
 
-    final int w = Integer.parseInt(args[1]);
-    final int h = Integer.parseInt(args[2]);
-    final Picture source = new Picture(args[0]);
-    final Picture target = new Picture(w, h);
-    for (int colT = 0; colT < w; colT++) {
-      for (int rowT = 0; rowT < h; rowT++) {
-        final int colS = colT * source.width() / w;
-        final int rowS = rowT * source.height() / h;
-        target.set(colT, rowT, source.get(colS, rowS));
+    final var filename = args[0];
+    final var width = Integer.parseInt(args[1]);
+    final var height = Integer.parseInt(args[2]);
+    final var source = new Picture(filename);
+    final var target = new Picture(width, height);
+
+    for (var targetCol = 0; targetCol < width; targetCol++) {
+      for (var targetRow = 0; targetRow < height; targetRow++) {
+        final var sourceCol = targetCol * source.width() / width;
+        final var sourceRow = targetRow * source.height() / height;
+        final var color = source.get(sourceCol, sourceRow);
+        target.set(targetCol, targetRow, color);
       }
     }
+
     source.show();
     target.show();
   }
