374ecda2c2d007f5ffa418baef4b4437b255b078
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-07 20:59:27 -0300

    Atualização: Program 3.1.7 Concatenating files.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter3/section31/Cat.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter3/section31/Cat.java
@@ -7,9 +7,32 @@ import edu.princeton.cs.introcs.Out;
 
 /**
  * Program 3.1.7 Concatenating files.
+ * <p>
+ * Compilation:  javac Cat.java
+ * Execution:    java Cat file1.txt file2.txt file3.txt output.txt
+ * Dependencies: In.java
+ * Data files:   http://www.cs.princeton.edu/introcs/31datatype/in1.jpg
+ *               http://www.cs.princeton.edu/introcs/31datatype/in2.jpg
+ * <p>
+ * Reads in text files specified as the first command-line
+ * parameters, concatenates them, and writes the result to
+ * filename specified as the last command line parameter.
+ * <p>
+ * % more in1.txt
+ * This is
+ * <p>
+ * % more in2.txt
+ * a tiny
+ * test.
+ * <p>
+ * % java Cat in1.txt in2.txt out.txt
+ * <p>
+ * % more out.txt .
+ * This is
+ * a tiny
+ * test.
  *
  * @author Marcelo dos Santos
- *
  */
 @TestDrive(value = {"in1.txt", "in2.txt", "out.txt"}, valueFile = true)
 public class Cat {
@@ -17,11 +40,13 @@ public class Cat {
   public static void main(final String[] args) {
     Executor.execute(Cat.class, args);
 
-    final Out out = new Out(args[args.length - 1]);
-    for (int i = 0; i < args.length - 1; i++) {
-      final In in = new In(args[i]);
-      final String s = in.readAll();
+    final var out = new Out(args[args.length - 1]);
+    for (var i = 0; i < args.length - 1; i++) {
+      final var in = new In(args[i]);
+      final var s = in.readAll();
       out.println(s);
+      in.close();
     }
+    out.close();
   }
 }
