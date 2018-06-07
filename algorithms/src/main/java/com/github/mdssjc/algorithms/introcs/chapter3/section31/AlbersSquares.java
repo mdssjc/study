4155eb7b39332ae433458077fff0171986263069
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-07 20:57:55 -0300

    Atualização: Program 3.1.2 Albers squares.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter3/section31/AlbersSquares.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter3/section31/AlbersSquares.java
@@ -8,33 +8,47 @@ import java.awt.*;
 
 /**
  * Program 3.1.2 Albers squares.
+ * <p>
+ * Compilation:  javac AlbersSquares.java
+ * Execution:    java AlbersSquares r1 g1 b1 r2 g2 b2
+ * Dependencies: StdDraw.java
+ * <p>
+ * This program displays the colors entered in RGB format
+ * on the command line in the familiar format developed
+ * in the 1960s by the color theorist Josef Albers that
+ * revolutionized the way that people think about color.
+ * <p>
+ * % java AlbersSquares 0 174 239  147 149 252
  *
  * @author Marcelo dos Santos
  *
  */
-@TestDrive({"9", "90", "166", "100", "100", "100"})
+@TestDrive({"0", "174", "239", "147", "149", "252"})
 public class AlbersSquares {
 
   public static void main(final String[] args) {
     Executor.execute(AlbersSquares.class, args);
 
-    final int r1 = Integer.parseInt(args[0]);
-    final int g1 = Integer.parseInt(args[1]);
-    final int b1 = Integer.parseInt(args[2]);
-    final Color c1 = new Color(r1, g1, b1);
+    StdDraw.setCanvasSize(800, 800);
 
-    final int r2 = Integer.parseInt(args[3]);
-    final int g2 = Integer.parseInt(args[4]);
-    final int b2 = Integer.parseInt(args[5]);
-    final Color c2 = new Color(r2, g2, b2);
+    final var r1 = Integer.parseInt(args[0]);
+    final var g1 = Integer.parseInt(args[1]);
+    final var b1 = Integer.parseInt(args[2]);
+    final var c1 = new Color(r1, g1, b1);
+
+    final var r2 = Integer.parseInt(args[3]);
+    final var g2 = Integer.parseInt(args[4]);
+    final var b2 = Integer.parseInt(args[5]);
+    final var c2 = new Color(r2, g2, b2);
 
     StdDraw.setPenColor(c1);
-    StdDraw.filledSquare(.25, 0.5, 0.2);
+    StdDraw.filledSquare(0.25, 0.5, 0.2);
     StdDraw.setPenColor(c2);
-    StdDraw.filledSquare(.25, 0.5, 0.1);
+    StdDraw.filledSquare(0.25, 0.5, 0.1);
+
     StdDraw.setPenColor(c2);
-    StdDraw.filledSquare(.75, 0.5, 0.2);
+    StdDraw.filledSquare(0.75, 0.5, 0.2);
     StdDraw.setPenColor(c1);
-    StdDraw.filledSquare(.75, 0.5, 0.1);
+    StdDraw.filledSquare(0.75, 0.5, 0.1);
   }
 }
