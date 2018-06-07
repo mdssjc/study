a7a82b557a91c854252da7ac6d0ebef1e5077055
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-07 20:58:07 -0300

    Atualização: Program 3.1.3 Luminance library.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter3/section31/Luminance.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter3/section31/Luminance.java
@@ -8,25 +8,36 @@ import java.awt.*;
 
 /**
  * Program 3.1.3 Luminance library.
+ * <p>
+ * Compilation:  javac Luminance.java
+ * Execution:    java Luminance r1 g1 b1 r2 g2 b2
+ * <p>
+ * Library for dealing with monochrome luminance.
+ * Uses the NTSC formula Y = 0.299*r + 0.587*g + 0.114*b.
+ * <p>
+ * % java Luminance 0 0 0 0 0 255
  *
  * @author Marcelo dos Santos
  *
  */
-@TestDrive({"232", "232", "232", "0", "0", "0"})
-@TestDrive({"9", "90", "166", "232", "232", "232"})
-@TestDrive({"9", "90", "166", "0", "0", "0"})
+@TestDrive({"0", "0", "0", "0", "0", "255"})
 public class Luminance {
 
+  @Deprecated
+  public static double lum(final Color color) {
+    return intensity(color);
+  }
+
   public static double intensity(final Color color) {
-    final int r = color.getRed();
-    final int g = color.getGreen();
-    final int b = color.getBlue();
+    final var r = color.getRed();
+    final var g = color.getGreen();
+    final var b = color.getBlue();
     return 0.299 * r + 0.587 * g + 0.114 * b;
   }
 
   public static Color toGray(final Color color) {
-    final int y = (int) Math.round(intensity(color));
-    final Color gray = new Color(y, y, y);
+    final var y = (int) (Math.round(lum(color)));
+    final var gray = new Color(y, y, y);
     return gray;
   }
 
@@ -37,12 +48,16 @@ public class Luminance {
   public static void main(final String[] args) {
     Executor.execute(Luminance.class, args);
 
-    final int[] a = new int[6];
-    for (int i = 0; i < 6; i++) {
+    final var a = new int[6];
+    for (var i = 0; i < 6; i++) {
       a[i] = Integer.parseInt(args[i]);
     }
-    final Color c1 = new Color(a[0], a[1], a[2]);
-    final Color c2 = new Color(a[3], a[4], a[5]);
+    final var c1 = new Color(a[0], a[1], a[2]);
+    final var c2 = new Color(a[3], a[4], a[5]);
+    StdOut.println("c1 = " + c1);
+    StdOut.println("c2 = " + c2);
+    StdOut.println("intensity(c1) =  " + intensity(c1));
+    StdOut.println("intensity(c2) =  " + intensity(c2));
     StdOut.println(areCompatible(c1, c2));
   }
 }
