f558c3374fd381b220b49d3f060800424db27209
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-09 19:53:06 -0300

    Atualização: Program 4.3.8 Load balancing simulation.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter4/section43/LoadBalance.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter4/section43/LoadBalance.java
@@ -7,6 +7,14 @@ import edu.princeton.cs.introcs.StdStats;
 
 /**
  * Program 4.3.8 Load balancing simulation.
+ * <p>
+ * Compilation:  javac LoadBalance.java
+ * Execution:    java LoadBalance m n s
+ * Dependencies: Queue.java RandomQueue.java StdDraw.java StdStats.java
+ * <p>
+ * Simulate the process of assignment n items to a set of m servers.
+ * Requests are put on the shortest of a sample of s queues chosen
+ * at random.
  *
  * @author Marcelo dos Santos
  *
@@ -18,33 +26,34 @@ public class LoadBalance {
   public static void main(final String[] args) {
     Executor.execute(LoadBalance.class, args);
 
-    final int m = Integer.parseInt(args[0]);
-    final int n = Integer.parseInt(args[1]);
-    final int size = Integer.parseInt(args[2]);
+    final var m = Integer.parseInt(args[0]);
+    final var n = Integer.parseInt(args[1]);
+    final var s = Integer.parseInt(args[2]);
 
-    final RandomQueue<Queue<Integer>> servers;
-    servers = new RandomQueue<>();
-    for (int i = 0; i < m; i++) {
+    final var servers = new RandomQueue<Queue<Integer>>();
+
+    for (var i = 0; i < m; i++) {
       servers.enqueue(new Queue<>());
     }
 
-    for (int j = 0; j < n; j++) {
-      Queue<Integer> min = servers.sample();
-      for (int k = 1; k < size; k++) {
-        final Queue<Integer> queue = servers.sample();
-        if (queue.size() < min.size()) {
+    for (var j = 0; j < n; j++) {
+      var min = servers.sample();
+      for (var k = 1; k < s; k++) {
+        final var queue = servers.sample();
+        if (queue.length() < min.length()) {
           min = queue;
         }
       }
+
       min.enqueue(j);
     }
 
-    int i = 0;
-    final double[] lengths = new double[m];
-    for (final Queue<Integer> queue : servers) {
-      lengths[i++] = queue.size();
+    var i = 0;
+    final var lengths = new double[m];
+    for (final var queue : servers) {
+      lengths[i++] = queue.length();
+      StdDraw.setYscale(0, 2.0 * n / m);
+      StdStats.plotBars(lengths);
     }
-    StdDraw.setYscale(0, 2.0 * n / m);
-    StdStats.plotBars(lengths);
   }
 }
