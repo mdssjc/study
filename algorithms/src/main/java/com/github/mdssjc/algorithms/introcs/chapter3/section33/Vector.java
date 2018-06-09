f8f76a055e8cfdff0329cc2aaea5667ada9a93a5
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-09 18:24:19 -0300

    Atualização: Program 3.3.3 Spatial vectors.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter3/section33/Vector.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter3/section33/Vector.java
@@ -1,42 +1,68 @@
 package com.github.mdssjc.algorithms.introcs.chapter3.section33;
 
+import com.github.mdssjc.algorithms.utils.Executor;
+import com.github.mdssjc.algorithms.utils.TestDrive;
+import edu.princeton.cs.introcs.StdOut;
+
 /**
  * Program 3.3.3 Spatial vectors.
+ * <p>
+ * Compilation:  javac Vector.java
+ * Execution:    java Vector
+ * <p>
+ * Implementation of a vector of real numbers.
+ * <p>
+ * This class is implemented to be immutable: once the client program
+ * initialize a Vector, it cannot change any of its fields
+ * (N or data[i]) either directly or indirectly. Immutability is a
+ * very desirable feature of a data type.
+ * <p>
+ * <p>
+ * % java Vector
+ * x        =  (1.0, 2.0, 3.0, 4.0)
+ * y        =  (5.0, 2.0, 4.0, 1.0)
+ * x + y    =  (6.0, 4.0, 7.0, 5.0)
+ * 10x      =  (10.0, 20.0, 30.0, 40.0)
+ * |x|      =  5.477225575051661
+ * <x, y>   =  25.0
+ * |x - y|  =  5.0990195135927845
+ * <p>
+ * Note that java.util.Vector is an unrelated Java library class.
  *
  * @author Marcelo dos Santos
  *
  */
+@TestDrive
 public class Vector {
 
-  private final double[] coords;
+  private final int n;
+  private final double[] data;
 
-  public Vector(final double[] a) {
-    this.coords = new double[a.length];
-    for (int i = 0; i < a.length; i++) {
-      this.coords[i] = a[i];
+  public Vector(final int n) {
+    this.n = n;
+    this.data = new double[n];
+  }
+
+  public Vector(final double[] data) {
+    this.n = data.length;
+
+    this.data = new double[this.n];
+    for (var i = 0; i < this.n; i++) {
+      this.data[i] = data[i];
     }
   }
 
-  public Vector plus(final Vector that) {
-    final double[] result = new double[this.coords.length];
-    for (int i = 0; i < this.coords.length; i++) {
-      result[i] = this.coords[i] + that.coords[i];
-    }
-    return new Vector(result);
-  }
-
-  public Vector scale(final double alpha) {
-    final double[] result = new double[this.coords.length];
-    for (int i = 0; i < this.coords.length; i++) {
-      result[i] = alpha * this.coords[i];
-    }
-    return new Vector(result);
+  public int length() {
+    return this.n;
   }
 
   public double dot(final Vector that) {
-    double sum = 0.0;
-    for (int i = 0; i < this.coords.length; i++) {
-      sum += this.coords[i] * that.coords[i];
+    if (this.length() != that.length()) {
+      throw new IllegalArgumentException("dimensions disagree");
+    }
+    var sum = 0.0;
+    for (var i = 0; i < this.n; i++) {
+      sum += (this.data[i] * that.data[i]);
     }
     return sum;
   }
@@ -45,25 +71,92 @@ public class Vector {
     return Math.sqrt(this.dot(this));
   }
 
-  public Vector direction() {
-    return this.scale(1 / this.magnitude());
+  public double distanceTo(final Vector that) {
+    if (this.length() != that.length()) {
+      throw new IllegalArgumentException("dimensions disagree");
+    }
+    return this.minus(that).magnitude();
+  }
+
+  public Vector plus(final Vector that) {
+    if (this.length() != that.length()) {
+      throw new IllegalArgumentException("dimensions disagree");
+    }
+    final var c = new Vector(this.n);
+    for (var i = 0; i < this.n; i++) {
+      c.data[i] = this.data[i] + that.data[i];
+    }
+    return c;
+  }
+
+  public Vector minus(final Vector that) {
+    if (this.length() != that.length()) {
+      throw new IllegalArgumentException("dimensions disagree");
+    }
+    final var c = new Vector(this.n);
+    for (var i = 0; i < this.n; i++) {
+      c.data[i] = this.data[i] - that.data[i];
+    }
+    return c;
   }
 
   public double cartesian(final int i) {
-    return this.coords[i];
+    return this.data[i];
+  }
+
+  @Deprecated
+  public Vector times(final double factor) {
+    final var c = new Vector(this.n);
+    for (var i = 0; i < this.n; i++) {
+      c.data[i] = factor * this.data[i];
+    }
+    return c;
+  }
+
+  public Vector scale(final double factor) {
+    final var c = new Vector(this.n);
+    for (var i = 0; i < this.n; i++) {
+      c.data[i] = factor * this.data[i];
+    }
+    return c;
+  }
+
+  public Vector direction() {
+    if (this.magnitude() == 0.0) {
+      throw new ArithmeticException("zero-vector has no direction");
+    }
+    return this.times(1.0 / this.magnitude());
   }
 
   @Override
   public String toString() {
-    final StringBuilder s = new StringBuilder();
+    final var s = new StringBuilder();
     s.append('(');
-    for (int i = 0; i < this.coords.length; i++) {
-      s.append(String.format("%.3f", this.coords[i]));
-      if (i < this.coords.length - 1) {
+    for (var i = 0; i < this.n; i++) {
+      s.append(this.data[i]);
+      if (i < this.n - 1) {
         s.append(", ");
       }
     }
     s.append(')');
     return s.toString();
+  }
+
+  public static void main(final String[] args) {
+    Executor.execute(Vector.class, args);
+
+    final double[] xdata = { 1.0, 2.0, 3.0, 4.0 };
+    final double[] ydata = { 5.0, 2.0, 4.0, 1.0 };
+
+    final var x = new Vector(xdata);
+    final var y = new Vector(ydata);
+
+    StdOut.println("x        =  " + x);
+    StdOut.println("y        =  " + y);
+    StdOut.println("x + y    =  " + x.plus(y));
+    StdOut.println("10x      =  " + x.times(10.0));
+    StdOut.println("|x|      =  " + x.magnitude());
+    StdOut.println("<x, y>   =  " + x.dot(y));
+    StdOut.println("|x - y|  =  " + x.minus(y).magnitude());
   }
 }
