78556e0c3748250398dc5660beb3247eb6aaa114
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-08 21:01:45 -0300

    Atualização: Program 3.2.5 Spira mirabilis.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter3/section32/Spiral.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter3/section32/Spiral.java
@@ -5,6 +5,20 @@ import com.github.mdssjc.algorithms.utils.TestDrive;
 
 /**
  * Program 3.2.5 Spira mirabilis.
+ * <p>
+ * Compilation:  javac Spiral.java
+ * Execution:    java Spiral
+ * Dependencies: Turtle.java
+ * <p>
+ * Plots a logarithmic spiral.
+ * <p>
+ * % java Spiral 3 1.0         // equilateral triangle
+ * <p>
+ * % java Spiral 3 1.2
+ * <p>
+ * % java Spiral 1440 1.00004
+ * <p>
+ * % java Spiral 1440 1.0004  // logarithmic spiral
  *
  * @author Marcelo dos Santos
  *
@@ -18,13 +32,13 @@ public class Spiral {
   public static void main(final String[] args) {
     Executor.execute(Spiral.class, args);
 
-    final int n = Integer.parseInt(args[0]);
-    final double decay = Double.parseDouble(args[1]);
-    final double angle = 360.0 / n;
-    double step = Math.sin(Math.toRadians(angle / 2));
-    final Turtle turtle = new Turtle(0.5, 0, angle / 2);
+    final var n = Integer.parseInt(args[0]);
+    final var decay = Double.parseDouble(args[1]);
 
-    for (int i = 0; i < 10 * 360 / angle; i++) {
+    final var angle = 360.0 / n;
+    var step = Math.sin(Math.toRadians(angle / 2.0));
+    final var turtle = new Turtle(0.5, 0.0, angle / 2.0);
+    for (var i = 0; i < 10 * n; i++) {
       step /= decay;
       turtle.goForward(step);
       turtle.turnLeft(angle);
