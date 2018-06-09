db09e5d22be382f5155eb35182ddcedcb6e56635
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-09 18:56:55 -0300

    Atualização: Program 4.1.1 3-sum problem.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter4/section41/ThreeSum.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter4/section41/ThreeSum.java
@@ -1,5 +1,6 @@
 package com.github.mdssjc.algorithms.introcs.chapter4.section41;
 
+import com.github.mdssjc.algorithms.introcs.chapter3.section32.Stopwatch;
 import com.github.mdssjc.algorithms.utils.Executor;
 import com.github.mdssjc.algorithms.utils.TestDrive;
 import edu.princeton.cs.introcs.StdIn;
@@ -7,6 +8,37 @@ import edu.princeton.cs.introcs.StdOut;
 
 /**
  * Program 4.1.1 3-sum problem.
+ * <p>
+ * Compilation:  javac ThreeSum.java
+ * Execution:    java ThreeSum < input.txt
+ * java Generator n | java ThreeSum
+ * Data files:   http://www.cs.princeton.edu/introcs/41analysis/8ints.txt
+ *               http://www.cs.princeton.edu/introcs/41analysis/1Kints.txt
+ *               http://www.cs.princeton.edu/introcs/41analysis/2Kints.txt
+ *               http://www.cs.princeton.edu/introcs/41analysis/4Kints.txt
+ *               http://www.cs.princeton.edu/introcs/41analysis/8Kints.txt
+ *               http://www.cs.princeton.edu/introcs/41analysis/16Kints.txt
+ *               http://www.cs.princeton.edu/introcs/41analysis/32Kints.txt
+ *               http://www.cs.princeton.edu/introcs/41analysis/64Kints.txt
+ *               http://www.cs.princeton.edu/introcs/41analysis/128Kints.txt
+ * <p>
+ * A program with cubic running time. Reads in n integers
+ * and counts the number of triples that sum to exactly 0.
+ * <p>
+ * % java ThreeSum < 8ints.txt
+ * 4
+ * 30 -30 0
+ * 30 -20 -10
+ * -30 -10 40
+ * -10 0 10
+ * <p>
+ * % java ThreeSum < 1Kints.txt
+ * 0
+ * <p>
+ * <p>
+ * Limitations
+ * -----------
+ * - we ignore integer overflow
  *
  * @author Marcelo dos Santos
  *
@@ -15,11 +47,11 @@ import edu.princeton.cs.introcs.StdOut;
 @TestDrive(input = "1Kints.txt", inputFile = true)
 public class ThreeSum {
 
-  public static void printTriples(final int[] a) {
-    final int n = a.length;
-    for (int i = 0; i < n; i++) {
-      for (int j = i + 1; j < n; j++) {
-        for (int k = j + 1; k < n; k++) {
+  public static void printAll(final int[] a) {
+    final var n = a.length;
+    for (var i = 0; i < n; i++) {
+      for (var j = i + 1; j < n; j++) {
+        for (var k = j + 1; k < n; k++) {
           if (a[i] + a[j] + a[k] == 0) {
             StdOut.println(a[i] + " " + a[j] + " " + a[k]);
           }
@@ -28,12 +60,12 @@ public class ThreeSum {
     }
   }
 
-  public static int countTriples(final int[] a) {
-    final int n = a.length;
-    int count = 0;
-    for (int i = 0; i < n; i++) {
-      for (int j = i + 1; j < n; j++) {
-        for (int k = j + 1; k < n; k++) {
+  public static int count(final int[] a) {
+    final var n = a.length;
+    var count = 0;
+    for (var i = 0; i < n; i++) {
+      for (var j = i + 1; j < n; j++) {
+        for (var k = j + 1; k < n; k++) {
           if (a[i] + a[j] + a[k] == 0) {
             count++;
           }
@@ -46,11 +78,11 @@ public class ThreeSum {
   public static void main(final String[] args) {
     Executor.execute(ThreeSum.class, args);
 
-    final int[] a = StdIn.readAllInts();
-    final int count = countTriples(a);
+    final var a = StdIn.readAllInts();
+    final var timer = new Stopwatch();
+    final var count = count(a);
     StdOut.println(count);
-    if (count < 10) {
-      printTriples(a);
-    }
+    printAll(a);
+    StdOut.println("elapsed time = " + timer.elapsedTime());
   }
 }
