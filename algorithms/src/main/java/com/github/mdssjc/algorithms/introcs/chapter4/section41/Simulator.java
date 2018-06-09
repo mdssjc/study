2c127f87a38c856037b951029923042bdca2caec
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-09 18:57:30 -0300

    Simulador adicionado.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter4/section41/Simulator.java
@@ -0,0 +1,17 @@
+package com.github.mdssjc.algorithms.introcs.chapter4.section41;
+
+import com.github.mdssjc.algorithms.utils.Executor;
+
+/**
+ * Simulator Class.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+public class Simulator {
+
+  public static void main(final String[] args) {
+    Executor.execute(ThreeSum.class, "Program 4.1.1 3-sum problem");
+    Executor.execute(DoublingTest.class, "Program 4.1.2 Validating a doubling hypothesis");
+  }
+}
