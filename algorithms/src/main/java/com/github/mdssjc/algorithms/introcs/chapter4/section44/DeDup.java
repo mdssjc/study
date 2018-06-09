d6b3b119012e2e9373fc20ed0fbe120c51f97349
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-09 20:45:08 -0300

    Atualização: Program 4.4.5 Dedup filter.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter4/section44/DeDup.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter4/section44/DeDup.java
@@ -2,12 +2,18 @@ package com.github.mdssjc.algorithms.introcs.chapter4.section44;
 
 import com.github.mdssjc.algorithms.utils.Executor;
 import com.github.mdssjc.algorithms.utils.TestDrive;
-import edu.princeton.cs.algs4.SET;
 import edu.princeton.cs.introcs.StdIn;
 import edu.princeton.cs.introcs.StdOut;
 
 /**
  * Program 4.4.5 Dedup filter.
+ *
+ *  Compilation:  javac DeDup.java
+ *  Execution:    java DeDup < words.txt
+ *  Dependencies: SET.java StdIn.java
+ *
+ *  Read in a list of words from standard input and print out
+ *  each word, removing any duplicates.
  *
  * @author Marcelo dos Santos
  *
@@ -18,14 +24,13 @@ public class DeDup {
   public static void main(final String[] args) {
     Executor.execute(DeDup.class, args);
 
-    final SET<String> distinct = new SET<>();
+    final var distinct = new SET<String>();
     while (!StdIn.isEmpty()) {
-      final String key = StdIn.readString();
+      final var key = StdIn.readString();
       if (!distinct.contains(key)) {
         distinct.add(key);
-        StdOut.print(key);
+        StdOut.println(key);
       }
-      StdOut.println();
     }
   }
 }
