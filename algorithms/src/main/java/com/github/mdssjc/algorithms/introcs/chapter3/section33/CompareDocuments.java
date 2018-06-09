0f8a3705165fc9dc6844bce05ca8707601a297ed
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-09 18:24:58 -0300

    Atualização: Program 3.3.5 Similarity detection.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter3/section33/CompareDocuments.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter3/section33/CompareDocuments.java
@@ -8,6 +8,29 @@ import edu.princeton.cs.introcs.StdOut;
 
 /**
  * Program 3.3.5 Similarity detection.
+ * <p>
+ * Compilation:  javac CompareDocuments.java
+ * Execution:    java CompareDocuments 5 10000 < documents.txt
+ * Data files:   http://www.cs.princeton.edu/introcs/33design/documents.txt
+ *               http://www.cs.princeton.edu/introcs/33design/Constitution.txt
+ *               http://www.cs.princeton.edu/introcs/33design/TomSawyer.txt
+ *               http://www.cs.princeton.edu/introcs/33design/HuckFin.txt
+ *               http://www.cs.princeton.edu/introcs/33design/Prejudice.txt
+ *               http://www.cs.princeton.edu/introcs/33design/Picture.java
+ *               http://www.cs.princeton.edu/introcs/33design/DJIA.csv
+ *               http://www.cs.princeton.edu/introcs/33design/Amazon.html
+ *               http://www.cs.princeton.edu/introcs/33design/ATCG.txt
+ * <p>
+ * % java CompareDocuments 5 10000 < documents.txt
+ *         Cons    TomS    Huck    Prej    Pict    DJIA    Amaz    ATCG
+ * Cons    1.00    0.66    0.60    0.64    0.21    0.18    0.21    0.11
+ * TomS    0.66    1.00    0.93    0.88    0.14    0.24    0.18    0.14
+ * Huck    0.60    0.93    1.00    0.82    0.10    0.23    0.16    0.12
+ * Prej    0.64    0.88    0.82    1.00    0.13    0.25    0.19    0.15
+ * Pict    0.21    0.14    0.10    0.13    1.00    0.05    0.38    0.04
+ * DJIA    0.18    0.24    0.23    0.25    0.05    1.00    0.16    0.11
+ * Amaz    0.21    0.18    0.16    0.19    0.38    0.16    1.00    0.07
+ * ATCG    0.11    0.14    0.12    0.15    0.04    0.11    0.07    1.00
  *
  * @author Marcelo dos Santos
  *
@@ -18,24 +41,28 @@ public class CompareDocuments {
   public static void main(final String[] args) {
     Executor.execute(CompareDocuments.class, args);
 
-    final int k = Integer.parseInt(args[0]);
-    final int d = Integer.parseInt(args[1]);
+    final var k = Integer.parseInt(args[0]);
+    final var d = Integer.parseInt(args[1]);
+    final var filenames = StdIn.readAllStrings();
+    final var n = filenames.length;
 
-    final String[] filenames = StdIn.readAllStrings();
-    final int n = filenames.length;
-    final Sketch[] a = new Sketch[n];
-    for (int i = 0; i < n; i++) {
-      a[i] = new Sketch(new In(filenames[i]).readAll(), k, d);
+    final var sketches = new Sketch[n];
+    for (var i = 0; i < n; i++) {
+      final var in = new In(filenames[i]);
+      final var text = in.readAll();
+      sketches[i] = new Sketch(text, k, d);
     }
-    StdOut.print(" ");
-    for (int j = 0; j < n; j++) {
-      StdOut.printf("%8.4s", filenames[j]);
+
+    StdOut.print("    ");
+    for (var i = 0; i < n; i++) {
+      StdOut.printf("%8.4s", filenames[i]);
     }
     StdOut.println();
-    for (int i = 0; i < n; i++) {
+
+    for (var i = 0; i < n; i++) {
       StdOut.printf("%.4s", filenames[i]);
-      for (int j = 0; j < n; j++) {
-        StdOut.printf("%8.2f", a[i].similarTo(a[j]));
+      for (var j = 0; j < n; j++) {
+        StdOut.printf("%8.2f", sketches[i].similarTo(sketches[j]));
       }
       StdOut.println();
     }
