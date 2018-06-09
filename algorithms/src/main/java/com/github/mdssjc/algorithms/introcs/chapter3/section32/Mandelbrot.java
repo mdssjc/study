325c966a540dc7e32aaa52e5479822159ace96fe
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-08 21:02:13 -0300

    Atualização: Program 3.2.7 Mandelbrot set.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter3/section32/Mandelbrot.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter3/section32/Mandelbrot.java
@@ -8,17 +8,24 @@ import java.awt.*;
 
 /**
  * Program 3.2.7 Mandelbrot set.
+ * <p>
+ * Compilation:  javac Mandelbrot.java
+ * Execution:    java Mandelbrot xc yc size
+ * Dependencies: StdDraw.java
+ * <p>
+ * Plots the size-by-size region of the Mandelbrot set, centered on (xc, yc)
+ * <p>
+ * % java Mandelbrot -0.5 0 2
  *
  * @author Marcelo dos Santos
  *
  */
 @TestDrive({"-0.5", "0", "2"})
-@TestDrive({"0.1015", "-0.633", "0.01"})
 public class Mandelbrot {
 
-  private static int mand(final Complex z0, final int max) {
-    Complex z = z0;
-    for (int t = 0; t < max; t++) {
+  public static int mand(final Complex z0, final int max) {
+    var z = z0;
+    for (var t = 0; t < max; t++) {
       if (z.abs() > 2.0) {
         return t;
       }
@@ -31,19 +38,22 @@ public class Mandelbrot {
   public static void main(final String[] args) {
     Executor.execute(Mandelbrot.class, args);
 
-    final double xc = Double.parseDouble(args[0]);
-    final double yc = Double.parseDouble(args[1]);
-    final double size = Double.parseDouble(args[2]);
-    final int n = 512;
-    final Picture picture = new Picture(n, n);
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-        final double x0 = xc - size / 2 + size * i / n;
-        final double y0 = yc - size / 2 + size * j / n;
-        final Complex z0 = new Complex(x0, y0);
-        final int gray = 255 - mand(z0, 255);
-        final Color c = new Color(gray, gray, gray);
-        picture.set(i, n - 1 - j, c);
+    final var xc = Double.parseDouble(args[0]);
+    final var yc = Double.parseDouble(args[1]);
+    final var size = Double.parseDouble(args[2]);
+
+    final var n = 512;
+    final var max = 255;
+
+    final var picture = new Picture(n, n);
+    for (var i = 0; i < n; i++) {
+      for (var j = 0; j < n; j++) {
+        final var x0 = xc - size / 2 + size * i / n;
+        final var y0 = yc - size / 2 + size * j / n;
+        final var z0 = new Complex(x0, y0);
+        final var gray = max - mand(z0, max);
+        final var color = new Color(gray, gray, gray);
+        picture.set(i, n - 1 - j, color);
       }
     }
     picture.show();
