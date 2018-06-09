45254648f8095ded343ae1621ea8cd08f409674e
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-08 22:44:24 -0300

    Placeholder para 22 - Project: The Commerce of XML.

---
+++ lisp/racket/HtDP2e/IV/22-Project: The Commerce of XML.rkt
@@ -0,0 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |22-Project: The Commerce of XML|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; 22-Project: The Commerce of XML.rkt
+;; IV - Intertwined Data
+;; 22 - Project: The Commerce of XML
