01d6e2583dc6b28da60bd76af1a74ac25b3efc0d
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-08 21:00:38 -0300

    Atualização: Program 3.2.1 Charged particle.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter3/section32/Charge.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter3/section32/Charge.java
@@ -6,6 +6,9 @@ import edu.princeton.cs.introcs.StdOut;
 
 /**
  * Program 3.2.1 Charged particle.
+ * <p>
+ * Compilation:  javac Charge.java
+ * Execution:    java Charge x y
  *
  * @author Marcelo dos Santos
  *
@@ -14,7 +17,8 @@ import edu.princeton.cs.introcs.StdOut;
 @TestDrive({"0.51", "0.94"})
 public class Charge {
 
-  private final double rx, ry;
+  private final double rx;
+  private final double ry;
   private final double q;
 
   public Charge(final double x0, final double y0, final double q0) {
@@ -24,9 +28,9 @@ public class Charge {
   }
 
   public double potentialAt(final double x, final double y) {
-    final double k = 8.99e09;
-    final double dx = x - this.rx;
-    final double dy = y - this.ry;
+    final var k = 8.99e09;
+    final var dx = x - this.rx;
+    final var dy = y - this.ry;
     return k * this.q / Math.sqrt(dx * dx + dy * dy);
   }
 
@@ -38,14 +42,14 @@ public class Charge {
   public static void main(final String[] args) {
     Executor.execute(Charge.class, args);
 
-    final double x = Double.parseDouble(args[0]);
-    final double y = Double.parseDouble(args[1]);
-    final Charge c1 = new Charge(0.51, 0.63, 21.3);
-    final Charge c2 = new Charge(0.13, 0.94, 81.9);
+    final var x = Double.parseDouble(args[0]);
+    final var y = Double.parseDouble(args[1]);
+    final var c1 = new Charge(0.51, 0.63, 21.3);
+    final var c2 = new Charge(0.13, 0.94, 81.9);
     StdOut.println(c1);
     StdOut.println(c2);
-    final double v1 = c1.potentialAt(x, y);
-    final double v2 = c2.potentialAt(x, y);
-    StdOut.printf("%.2e%n", (v1 + v2));
+    final var v1 = c1.potentialAt(x, y);
+    final var v2 = c2.potentialAt(x, y);
+    StdOut.println(v1 + v2);
   }
 }
