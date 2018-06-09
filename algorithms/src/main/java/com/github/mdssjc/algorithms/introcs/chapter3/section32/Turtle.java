03abc8744ca508775aa4c78697a27c5589fdedda
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-08 21:01:29 -0300

    Atualização: Program 3.2.4 Turtle graphics.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter3/section32/Turtle.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter3/section32/Turtle.java
@@ -4,18 +4,24 @@ import com.github.mdssjc.algorithms.utils.Executor;
 import com.github.mdssjc.algorithms.utils.TestDrive;
 import edu.princeton.cs.introcs.StdDraw;
 
+import java.awt.*;
+
 /**
  * Program 3.2.4 Turtle graphics.
+ * <p>
+ * Compilation:  javac Turtle.java
+ * Execution:    java Turtle
+ * <p>
+ * Data type for turtle graphics using standard draw.
  *
  * @author Marcelo dos Santos
  *
  */
-@TestDrive("3")
-@TestDrive("7")
-@TestDrive("1000")
+@TestDrive
 public class Turtle {
 
-  private double x, y;
+  private double x;
+  private double y;
   private double angle;
 
   public Turtle(final double x0, final double y0, final double a0) {
@@ -29,23 +35,55 @@ public class Turtle {
   }
 
   public void goForward(final double step) {
-    final double oldx = this.x;
-    final double oldy = this.y;
+    final var oldx = this.x;
+    final var oldy = this.y;
     this.x += step * Math.cos(Math.toRadians(this.angle));
     this.y += step * Math.sin(Math.toRadians(this.angle));
     StdDraw.line(oldx, oldy, this.x, this.y);
   }
 
+  public void show() {
+    StdDraw.show();
+  }
+
+  public void pause(final int t) {
+    StdDraw.pause(t);
+  }
+
+  public void setPenColor(final Color color) {
+    StdDraw.setPenColor(color);
+  }
+
+  public void setPenRadius(final double radius) {
+    StdDraw.setPenRadius(radius);
+  }
+
+  public void setCanvasSize(final int width, final int height) {
+    StdDraw.setCanvasSize(width, height);
+  }
+
+  public void setXscale(final double min, final double max) {
+    StdDraw.setXscale(min, max);
+  }
+
+  public void setYscale(final double min, final double max) {
+    StdDraw.setYscale(min, max);
+  }
+
   public static void main(final String[] args) {
     Executor.execute(Turtle.class, args);
 
-    final int n = Integer.parseInt(args[0]);
-    final double angle = 360.0 / n;
-    final double step = Math.sin(Math.toRadians(angle / 2));
-    final Turtle turtle = new Turtle(0.5, 0.0, angle / 2);
-    for (int i = 0; i < n; i++) {
-      turtle.goForward(step);
-      turtle.turnLeft(angle);
-    }
+    StdDraw.enableDoubleBuffering();
+    final var x0 = 0.5;
+    final var y0 = 0.0;
+    final var a0 = 60.0;
+    final var step = Math.sqrt(3) / 2;
+    final var turtle = new Turtle(x0, y0, a0);
+    turtle.goForward(step);
+    turtle.turnLeft(120.0);
+    turtle.goForward(step);
+    turtle.turnLeft(120.0);
+    turtle.goForward(step);
+    turtle.turnLeft(120.0);
   }
 }
