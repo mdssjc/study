751b6b2f4b43f34cba2cc8be25848823bdc9aded
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-09 20:44:24 -0300

    Atualização: Program 4.4.2 Indexing.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter4/section44/Index.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter4/section44/Index.java
@@ -9,6 +9,27 @@ import edu.princeton.cs.introcs.StdOut;
 
 /**
  * Program 4.4.2 Indexing.
+ * <p>
+ * Compilation:  javac Index.java
+ * Execution:    java Index i j < input.txt
+ * Dependencies: ST.java Queue.java StdIn.java StdOut.java
+ * Data files:   http://www.cs.princeton.edu/introcs/44st/TaleOfTwoCities.java
+ * <p>
+ * Create an index of all words in the given input file such that
+ * the word has length >= i and occurs >= j times.
+ * <p>
+ * % java Index 9 30 < TaleOfTwoCities.txt
+ * confidence: 2794 23064 25031 34249 47907 48268 48577 ...
+ * courtyard: 11885 12062 17303 17451 32404 32522 38663 ...
+ * evremonde: 86211 90791 90798 90802 90814 90822 90856 ...
+ * expression: 3777 5575 6574 7116 7195 8509 8928 15015 ...
+ * gentleman: 2521 5290 5337 5698 6235 6301 6326 6338 ...
+ * influence: 27809 36881 43141 43150 48308 54049 54067 ...
+ * monseigneur: 85 90 36587 36590 36611 36636 36643 ...
+ * prisoners: 1012 20729 20770 21240 22123 22209 22590 ...
+ * something: 3406 3765 9283 13234 13239 15245 20257 ...
+ * sometimes: 4514 4530 4548 6082 20731 33883 34239 ...
+ * vengeance: 56041 63943 67705 79351 79941 79945 80225 ...
  *
  * @author Marcelo dos Santos
  *
@@ -19,27 +40,27 @@ public class Index {
   public static void main(final String[] args) {
     Executor.execute(Index.class, args);
 
-    final int minlen = Integer.parseInt(args[0]);
-    final int minocc = Integer.parseInt(args[1]);
+    final var minLength = Integer.parseInt(args[0]);
+    final var minOccurrence = Integer.parseInt(args[1]);
 
-    final ST<String, Queue<Integer>> st;
-    st = new ST<>();
-    for (int i = 0; !StdIn.isEmpty(); i++) {
-      final String word = StdIn.readString();
-      if (word.length() < minlen) {
+    final var words = StdIn.readAllStrings();
+
+    final var st = new ST<String, Queue<Integer>>();
+    for (var i = 0; i < words.length; i++) {
+      final var s = words[i];
+      if (s.length() < minLength) {
         continue;
       }
-      if (!st.contains(word)) {
-        st.put(word, new Queue<>());
+      if (!st.contains(s)) {
+        st.put(s, new Queue<Integer>());
       }
-      final Queue<Integer> queue = st.get(word);
-      queue.enqueue(i);
+      final var q = st.get(s);
+      q.enqueue(i);
     }
-
-    for (final String s : st) {
-      final Queue<Integer> queue = st.get(s);
-      if (queue.size() >= minocc) {
-        StdOut.println(s + ": " + queue);
+    for (final var s : st.keys()) {
+      final var q = st.get(s);
+      if (q.length() >= minOccurrence) {
+        StdOut.println(s + ": " + q);
       }
     }
   }
