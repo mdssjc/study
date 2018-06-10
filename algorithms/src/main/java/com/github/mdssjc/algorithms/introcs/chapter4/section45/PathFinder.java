f89bdf5af1e6f68cb1c01b7cea3894650105c96c
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-09 21:13:18 -0300

    Atualização: Program 4.5.3 Shortest-paths client e Program 4.5.4 Shortest-paths implementation.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter4/section45/PathFinder.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter4/section45/PathFinder.java
@@ -11,25 +11,51 @@ import edu.princeton.cs.introcs.StdOut;
 /**
  * Program 4.5.3 Shortest-paths client.
  * Program 4.5.4 Shortest-paths implementation.
+ * <p>
+ * Compilation:  javac PathFinder.java
+ * Execution:    java Pathfinder input.txt delimiter source
+ * Dependencies: Queue.java Stack.java Graph.java
+ * Data files:   https://introcs.cs.princeton.edu/45graph/routes.txt
+ * https://introcs.cs.princeton.edu/45graph/movies.txt
+ * <p>
+ * Runs breadth first search algorithm from source s on a graph G.
+ * After preprocessing the graph, can process shortest path queries
+ * from s to any vertex t.
+ * <p>
+ * % java PathFinder routes.txt " " JFK
+ * LAX
+ *    JFK
+ *    ORD
+ *    PHX
+ *    LAX
+ * distance 3
+ * MCO
+ *    JFK
+ *    MCO
+ * distance 1
+ * DFW
+ *    JFK
+ *    ORD
+ *    DFW
+ * distance 2
  *
  * @author Marcelo dos Santos
  *
  */
-@TestDrive(value = {"routes.txt", " ", "JFK"}, input = {"LAX", "DFW"})
+@TestDrive(value = {"routes.txt", " ", "JFK"}, input = {"LAX", "MCO", "DFW"})
 public class PathFinder {
 
-  private final ST<String, Integer> dist;
-  private final ST<String, String> prev;
+  private final ST<String, String> prev = new ST<>();
+  private final ST<String, Integer> dist = new ST<>();
 
   public PathFinder(final Graph G, final String s) {
-    this.prev = new ST<>();
-    this.dist = new ST<>();
     final Queue<String> queue = new Queue<>();
     queue.enqueue(s);
     this.dist.put(s, 0);
+
     while (!queue.isEmpty()) {
-      final String v = queue.dequeue();
-      for (final String w : G.adjacentTo(v)) {
+      final var v = queue.dequeue();
+      for (final var w : G.adjacentTo(v)) {
         if (!this.dist.contains(w)) {
           queue.enqueue(w);
           this.dist.put(w, 1 + this.dist.get(v));
@@ -39,7 +65,14 @@ public class PathFinder {
     }
   }
 
+  public boolean hasPathTo(final String v) {
+    return this.dist.contains(v);
+  }
+
   public int distanceTo(final String v) {
+    if (!hasPathTo(v)) {
+      return Integer.MAX_VALUE;
+    }
     return this.dist.get(v);
   }
 
@@ -55,19 +88,17 @@ public class PathFinder {
   public static void main(final String[] args) {
     Executor.execute(PathFinder.class, args);
 
-    final String filename = args[0];
-    final String delimiter = args[1];
-    final Graph G = new Graph(filename, delimiter);
-    final String s = args[2];
-    final PathFinder pf = new PathFinder(G, s);
-
-    while (StdIn.hasNextLine()) {
-      final String t = StdIn.readLine();
-      final int d = pf.distanceTo(t);
-      for (final String v : pf.pathTo(t)) {
-        StdOut.println(" " + v);
+    final var filename = args[0];
+    final var delimiter = args[1];
+    final var G = new Graph(filename, delimiter);
+    final var s = args[2];
+    final var pf = new PathFinder(G, s);
+    while (!StdIn.isEmpty()) {
+      final var t = StdIn.readLine();
+      for (final var v : pf.pathTo(t)) {
+        StdOut.println("   " + v);
       }
-      StdOut.println("distance " + d);
+      StdOut.println("distance " + pf.distanceTo(t));
     }
   }
 }
