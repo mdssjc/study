679c76950fb950b2c991397014398ba1491113b8
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-09 21:14:12 -0300

    Atualização: Program 4.5.6 Performer-performer graph.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter4/section45/Performer.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter4/section45/Performer.java
@@ -6,7 +6,26 @@ import edu.princeton.cs.introcs.In;
 import edu.princeton.cs.introcs.StdOut;
 
 /**
- * Program 4.5.6 Performer–performer graph.
+ * Program 4.5.6 Performer-performer graph.
+ * <p>
+ * Compilation:  javac SmallWorld.java
+ * Execution:    java SmallWorld filename delimiter
+ * Dependencies: Graph.java PathFinder.java StdOut.java In.java
+ * Data files:   https://introcs.cs.princeton.edu/45graph/tinyMovies.txt
+ * https://introcs.cs.princeton.edu/45graph/moviesG.txt
+ * <p>
+ * % java Performer tinyMovies.txt "/"
+ * number of vertices     =       5
+ * average degree         =   2.800
+ * average path length    =   1.300
+ * clustering coefficient =   0.767
+ * <p>
+ * % java Performer moviesG.txt "/"
+ * [ after a long time ]
+ * number of vertices     =   19044
+ * average degree         = 148.688
+ * average path length    =   3.494
+ * clustering coefficient =   0.911
  *
  * @author Marcelo dos Santos
  *
@@ -18,25 +37,24 @@ public class Performer {
   public static void main(final String[] args) {
     Executor.execute(Performer.class, args);
 
-    final String filename = args[0];
-    final String delimiter = args[1];
-    final Graph G = new Graph();
-
-    final In in = new In(filename);
+    final var filename = args[0];
+    final var delimiter = args[1];
+    final var graph = new Graph();
+    final var in = new In(filename);
     while (in.hasNextLine()) {
-      final String line = in.readLine();
-      final String[] names = line.split(delimiter);
-      for (int i = 1; i < names.length; i++) {
-        for (int j = i + 1; j < names.length; j++) {
-          G.addEdge(names[i], names[j]);
+      final var line = in.readLine();
+      final var names = line.split(delimiter);
+      for (var i = 1; i < names.length; i++) {
+        for (var j = i + 1; j < names.length; j++) {
+          graph.addEdge(names[i], names[j]);
         }
       }
     }
 
-    final double degree = SmallWorld.averageDegree(G);
-    final double length = SmallWorld.averagePathLength(G);
-    final double cluster = SmallWorld.clusteringCoefficient(G);
-    StdOut.printf("number of vertices     = %7d%n", G.V());
+    final var degree = SmallWorld.averageDegree(graph);
+    final var length = SmallWorld.averagePathLength(graph);
+    final var cluster = SmallWorld.clusteringCoefficient(graph);
+    StdOut.printf("number of vertices     = %7d%n", graph.V());
     StdOut.printf("average degree         = %7.3f%n", degree);
     StdOut.printf("average path length    = %7.3f%n", length);
     StdOut.printf("clustering coefficient = %7.3f%n", cluster);
