177aa0e1bd6f0698a454acbf78dd1a2e4b8144ac
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-09 21:12:53 -0300

    Atualização: Program 4.5.2 Using a graph to invert an index.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter4/section45/IndexGraph.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/introcs/chapter4/section45/IndexGraph.java
@@ -7,6 +7,27 @@ import edu.princeton.cs.introcs.StdOut;
 
 /**
  * Program 4.5.2 Using a graph to invert an index.
+ * <p>
+ * Compilation:  javac IndexGraph.java
+ * Dependencies: Graph.java In.java
+ * Execution:    java IndexGraph movies.txt "/"
+ * Data files:   https://introcs.cs.princeton.edu/45graph/tinyGraph.txt
+ *               https://introcs.cs.princeton.edu/45graph/movies.txt
+ *               https://introcs.cs.princeton.edu/45graph/amino.csv
+ * <p>
+ * Builds a graph, then accepts vertex names from standard input
+ * and prints its neighbors.
+ * <p>
+ * % java IndexGraph tinyGraph.txt " "
+ * C
+ *   A
+ *   B
+ *   G
+ * A
+ *   B
+ *   C
+ *   G
+ *   H
  *
  * @author Marcelo dos Santos
  *
@@ -17,14 +38,16 @@ public class IndexGraph {
   public static void main(final String[] args) {
     Executor.execute(IndexGraph.class, args);
 
-    final String filename = args[0];
-    final String delimiter = args[1];
-    final Graph G = new Graph(filename, delimiter);
+    final var filename = args[0];
+    final var delimiter = args[1];
+    final var G = new Graph(filename, delimiter);
 
-    while (StdIn.hasNextLine()) {
-      final String v = StdIn.readLine();
-      for (final String w : G.adjacentTo(v)) {
-        StdOut.println(" " + w);
+    while (!StdIn.isEmpty()) {
+      final var v = StdIn.readLine();
+      if (G.hasVertex(v)) {
+        for (final var w : G.adjacentTo(v)) {
+          StdOut.println("  " + w);
+        }
       }
     }
   }
