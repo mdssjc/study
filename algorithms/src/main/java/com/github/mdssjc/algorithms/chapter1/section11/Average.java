2645a25c20fb246e7a0c0c8765d9f41e392c1667
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-11 21:02:29 -0300

    Atualização: Average of a sequence of numbers.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section11/Average.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/section11/Average.java
@@ -7,26 +7,64 @@ import edu.princeton.cs.algs4.StdOut;
 
 /**
  * Average of a sequence of numbers.
+ * <p>
+ * Compilation:  javac Average.java
+ * Execution:    java Average < data.txt
+ * Dependencies: StdIn.java StdOut.java
+ * <p>
+ * Reads in a sequence of real numbers, and computes their average.
+ * <p>
+ * % java Average
+ * 10.0 5.0 6.0
+ * 3.0 7.0 32.0
+ * [Ctrl-d]
+ * Average is 10.5
+ * <p>
+ * Note [Ctrl-d] signifies the end of file on Unix.
+ * On windows use [Ctrl-z].
  *
  * @author Marcelo dos Santos
  *
  */
-@TestDrive(input = "1.23456 2.34567 3.45678 4.56789")
+
+/**
+ * The {@code Average} class provides a client for reading in a sequence
+ * of real numbers and printing out their average.
+ * <p>
+ * For additional documentation, see <a href="https://algs4.cs.princeton.edu/11model">Section
+ * 1.1</a> of
+ * <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ *
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
+ */
 @TestDrive(input = "10.0 5.0 6.0 3.0 7.0 32.0")
 public class Average {
 
+  private Average() {
+  }
+
+  /**
+   * Reads in a sequence of real numbers from standard input and prints
+   * out their average to standard output.
+   *
+   * @param args
+   *     the command-line arguments
+   */
   public static void main(final String[] args) {
     Executor.execute(Average.class, args);
 
-    int count = 0;
-    double sum = 0.0;
+    var count = 0;
+    var sum = 0.0;
 
     while (!StdIn.isEmpty()) {
-      sum += StdIn.readDouble();
+      final var value = StdIn.readDouble();
+      sum += value;
       count++;
     }
 
-    final double average = sum / count;
-    StdOut.printf("Average is %.5f%n", average);
+    final var average = sum / count;
+
+    StdOut.println("Average is " + average);
   }
 }
