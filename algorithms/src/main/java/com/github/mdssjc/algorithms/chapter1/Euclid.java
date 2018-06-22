077770b4f4f76b5a4c9424d85b14dd936fec0600
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-22 19:57:13 -0300

    Atualização: Euclid's algorithm.

--- algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/Euclid.java
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/Euclid.java
@@ -1,4 +1,4 @@
-package com.github.mdssjc.algorithms.chapter1.section11;
+package com.github.mdssjc.algorithms.chapter1;
 
 import com.github.mdssjc.algorithms.utils.Executor;
 import com.github.mdssjc.algorithms.utils.TestDrive;
@@ -13,17 +13,17 @@ import edu.princeton.cs.algs4.StdOut;
 @TestDrive
 public class Euclid {
 
-  public static void main(final String[] args) {
-    Executor.execute(Euclid.class, args);
-
-    StdOut.println(gcd(10, 12));
-  }
-
   public static int gcd(final int p, final int q) {
     if (q == 0) {
       return p;
     }
-    final int r = p % q;
+    final var r = p % q;
     return gcd(q, r);
+  }
+
+  public static void main(final String[] args) {
+    Executor.execute(Euclid.class, args);
+
+    StdOut.println(gcd(10, 12));
   }
 }
