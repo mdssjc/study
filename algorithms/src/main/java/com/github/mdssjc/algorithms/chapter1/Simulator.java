0fe8418834f00a1571cc5641b744bcc2bd40e634
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-22 20:54:12 -0300

    Simulador atualizado.

---
+++ algorithms/src/main/java/com/github/mdssjc/algorithms/chapter1/Simulator.java
@@ -0,0 +1,22 @@
+package com.github.mdssjc.algorithms.chapter1;
+
+import com.github.mdssjc.algorithms.utils.Executor;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Simulator Class.
+ *
+ * @author Marcelo dos Santos
+ *
+ */
+public class Simulator {
+
+  public static void main(final String[] args) {
+    StdOut.println("1 Fundamentals");
+    Executor.execute(Euclid.class, "Euclid's algorithm");
+
+    StdOut.println("1.1 Basic Programming Model");
+    Executor.execute(BinarySearch.class, "Binary search");
+    Executor.execute(TypicalArrayCodes.class, "Typical array-processing code");
+  }
+}
