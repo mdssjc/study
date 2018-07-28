2466c777a8c7e785c38ccb6385caabe2af5e547b
Marcelo dos Santos <mdssjc@gmail.com>
2018-07-27 21:23:38 -0300

    Continuação do Working With Visual Studio.

--- diversos/pro-asp.net-core-mvc-2/WorkingWithVisualStudio/WorkingWithVisualStudio/Models/SimpleRepository.cs
+++ diversos/pro-asp.net-core-mvc-2/WorkingWithVisualStudio/WorkingWithVisualStudio/Models/SimpleRepository.cs
@@ -22,6 +22,7 @@ namespace WorkingWithVisualStudio.Models
             {
                 AddProduct(p);
             }
+            products.Add("Error", null);
         }
 
         public IEnumerable<Product> Products => products.Values;
