9d4ec212169a1bf1e00ee43b5c842f0dce1d1dc8
Marcelo dos Santos <mdssjc@gmail.com>
2019-02-25 21:55:15 -0300

    Executor adicionado.

--- java/reactive-programming/temperature/src/main/java/com/github/mdssjc/temperature/TempSubscription.java
+++ java/reactive-programming/temperature/src/main/java/com/github/mdssjc/temperature/TempSubscription.java
@@ -1,5 +1,7 @@
 package com.github.mdssjc.temperature;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Flow.Subscriber;
 import java.util.concurrent.Flow.Subscription;
 
@@ -10,22 +12,25 @@ public class TempSubscription implements Subscription {
 
     private final Subscriber<? super TempInfo> subscriber;
     private final String town;
+    private static final ExecutorService executor = Executors.newSingleThreadExecutor();
 
-    public TempSubscription(Subscriber<? super TempInfo> subscriber,                            String town) {
+    public TempSubscription(Subscriber<? super TempInfo> subscriber, String town) {
         this.subscriber = subscriber;
         this.town = town;
     }
 
     @Override
     public void request(long n) {
-        for (long i = 0L; i < n; i++) {
-            try {
-                subscriber.onNext(TempInfo.fetch(town));
-            } catch (Exception e) {
-                subscriber.onError(e);
-                break;
+        executor.submit(() -> {
+            for (long i = 0L; i < n; i++) {
+                try {
+                    subscriber.onNext(TempInfo.fetch(town));
+                } catch (Exception e) {
+                    subscriber.onError(e);
+                    break;
+                }
             }
-        }
+        });
     }
 
     @Override
