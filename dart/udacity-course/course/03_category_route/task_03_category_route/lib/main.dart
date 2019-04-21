c9c1cc862e1a57b122b58504f6ebd7ff57b98c83
Marcelo dos Santos <mdssjc@gmail.com>
2019-04-21 15:29:38 -0300

    Category Route.

--- dart/udacity-course/course/03_category_route/task_03_category_route/lib/main.dart
+++ dart/udacity-course/course/03_category_route/task_03_category_route/lib/main.dart
@@ -4,8 +4,7 @@
 
 // You can read about packages here: https://flutter.io/using-packages/
 import 'package:flutter/material.dart';
-
-// TODO: Import the CategoryRoute widget
+import 'category_route.dart';
 
 /// The function that is called when main.dart is run.
 void main() {
@@ -19,11 +18,9 @@ class UnitConverterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       title: 'Unit Converter',
-      // TODO: Instead of pointing to exactly 1 Category widget,
-      // our home should now point to an instance of the CategoryRoute widget.
-      home: Container(),
+      home: CategoryRoute(),
     );
   }
 }
