1710d1ed09a66e9f4df5ec5e4469b608a2eaaf5c
Marcelo dos Santos <mdssjc@gmail.com>
2019-04-29 22:20:55 -0300

    Input.

--- dart/udacity-course/course/06_input/task_06_input/lib/main.dart
+++ dart/udacity-course/course/06_input/task_06_input/lib/main.dart
@@ -5,10 +5,10 @@
 // You can read about packages here: https://flutter.io/using-packages/
 import 'package:flutter/material.dart';
 
+import 'category_route.dart';
 // You can use a relative import, i.e. `import 'category_route.dart;'` or
 // a package import, as shown below.
 // More details at http://dart-lang.github.io/linter/lints/avoid_relative_lib_imports.html
-import 'category_route.dart';
 
 /// The function that is called when main.dart is run.
 void main() {
@@ -23,9 +23,16 @@ class UnitConverterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       title: 'Unit Converter',
-      // TODO: Fill out the theme parameter
+      theme: ThemeData(
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: Colors.black,
+              displayColor: Colors.grey[600],
+            ),
+        primaryColor: Colors.grey[600],
+        textSelectionHandleColor: Colors.green[500],
+      ),
       home: CategoryRoute(),
     );
   }
