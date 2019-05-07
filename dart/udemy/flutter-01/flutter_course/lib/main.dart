c7cbbd4a55443a0b6834fe77d34b7e0de8993914
Marcelo dos Santos <mdssjc@gmail.com>
2019-05-06 21:34:26 -0300

    Adicionado setState; Refatoração em arquivos.

--- dart/udemy/flutter-01/flutter_course/lib/main.dart
+++ dart/udemy/flutter-01/flutter_course/lib/main.dart
@@ -1,49 +1,18 @@
 import 'package:flutter/material.dart';
+import 'product_manager.dart';
 
 main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  List<String> _products = ['Food Tester'];
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('EasyList'),
-          ),
-          body: Column(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.all(10.0),
-                child: RaisedButton(
-                  onPressed: () {},
-                  child: Text('Add Product'),
-                ),
-              ),
-              Column(
-                children: _products
-                    .map(
-                      (element) => Card(
-                            child: Column(
-                              children: <Widget>[
-                                Image.asset('assets/food.jpg'),
-                                Text(element),
-                              ],
-                            ),
-                          ),
-                    )
-                    .toList(),
-              )
-            ],
-          )),
+        appBar: AppBar(
+          title: Text('EasyList'),
+        ),
+        body: ProductManager('Food Tester'),
+      ),
     );
   }
 }
