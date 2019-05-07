cfdadb856b7c4d8b8c55e5b0197d084327ba2d80
Marcelo dos Santos <mdssjc@gmail.com>
2019-05-06 21:17:39 -0300

    Convertido para Statefull.

--- dart/udemy/flutter-01/flutter_course/lib/main.dart
+++ dart/udemy/flutter-01/flutter_course/lib/main.dart
@@ -2,7 +2,16 @@ import 'package:flutter/material.dart';
 
 main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food Tester'];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,14 +28,20 @@ class MyApp extends StatelessWidget {
                   child: Text('Add Product'),
                 ),
               ),
-              Card(
-                child: Column(
-                  children: <Widget>[
-                    Image.asset('assets/food.jpg'),
-                    Text('Food Paradise'),
-                  ],
-                ),
-              ),
+              Column(
+                children: _products
+                    .map(
+                      (element) => Card(
+                            child: Column(
+                              children: <Widget>[
+                                Image.asset('assets/food.jpg'),
+                                Text(element),
+                              ],
+                            ),
+                          ),
+                    )
+                    .toList(),
+              )
             ],
           )),
     );
