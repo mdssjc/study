5f202f79ff1b0456a14badcac693eb443646faa6
Marcelo dos Santos <mdssjc@gmail.com>
2019-05-11 22:03:30 -0300

    Passing data up.

--- dart/udemy/flutter-01/flutter_course/lib/product_manager.dart
+++ dart/udemy/flutter-01/flutter_course/lib/product_manager.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'product_control.dart';
 import 'products.dart';
 
 class ProductManager extends StatefulWidget {
@@ -31,6 +32,12 @@ class _ProductManagerState extends State<ProductManager> {
     super.didUpdateWidget(oldWidget);
   }
 
+  void _addProduct(String product) {
+    setState(() {
+      _products.add(product);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     print('[ProductManager State] build()');
@@ -38,15 +45,7 @@ class _ProductManagerState extends State<ProductManager> {
       children: [
         Container(
           margin: EdgeInsets.all(10.0),
-          child: RaisedButton(
-            color: Theme.of(context).primaryColor,
-            onPressed: () {
-              setState(() {
-                _products.add('Advanced Food Tester');
-              });
-            },
-            child: Text('Add Product'),
-          ),
+          child: ProductControl(_addProduct),
         ),
         Products(_products)
       ],
