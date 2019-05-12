2dc7738d733ed8f7899bc57036994914fa8d85e0
Marcelo dos Santos <mdssjc@gmail.com>
2019-05-12 19:49:51 -0300

    Debugging the user interface.

--- dart/udemy/flutter-01/flutter_course/lib/main.dart
+++ dart/udemy/flutter-01/flutter_course/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'product_manager.dart';
 
-main() => runApp(MyApp());
+main() {
+  debugPaintSizeEnabled = true;
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
