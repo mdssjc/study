8bd45710da19df20d4e56414de488970833cf810
Marcelo dos Santos <mdssjc@gmail.com>
2019-05-11 21:43:15 -0300

    Assignment 1.

---
+++ dart/udemy/widgets-basics-assignment-problem/lib/text_control.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:assignment1/text_output.dart';
+
+class TextControl extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _TextControlState();
+  }
+}
+
+class _TextControlState extends State<TextControl> {
+  var _text = 'Hello World';
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Center(
+          child: Container(
+            margin: EdgeInsets.all(10.0),
+            child: RaisedButton(
+              onPressed: () {
+                setState(() {
+                  _text = 'This is Flutter!';
+                });
+              },
+              child: Text('Press me!'),
+            ),
+          ),
+        ),
+        TextOutput(_text),
+      ],
+    );
+  }
+}
