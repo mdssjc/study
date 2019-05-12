77b80215f91d54ad860b163f3a2bbb73261a6bc3
Marcelo dos Santos <mdssjc@gmail.com>
2019-05-12 19:56:33 -0300

    More about visual helpers.

--- dart/udemy/flutter-01/flutter_course/lib/main.dart
+++ dart/udemy/flutter-01/flutter_course/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+// import 'package:flutter/rendering.dart';
 import 'product_manager.dart';
 
 main() {
-  debugPaintSizeEnabled = true;
+  // debugPaintSizeEnabled = true;
+  // debugPaintBaselinesEnabled = true;
+  // debugPaintPointersEnabled = true;
   runApp(MyApp());
 }
 
@@ -11,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // debugShowMaterialGrid: true,
       theme: ThemeData(
           brightness: Brightness.light,
           primaryColor: Colors.deepOrange,
