65aad416913013da093a2c614685758daa23ae67
Marcelo dos Santos <mdssjc@gmail.com>
2019-05-13 22:24:06 -0300

    Using Spring Qualifier Annotations.

---
+++ java/udemy/spring-framework-guru/di-demo/src/main/java/com/github/mdssjc/services/ConstructorGreetingService.java
@@ -0,0 +1,12 @@
+package com.github.mdssjc.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConstructorGreetingService implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Hello - I was injected via the constructor!!!";
+    }
+}
