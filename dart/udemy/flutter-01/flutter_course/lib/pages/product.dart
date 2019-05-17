f4ab0802fa7927d1d8980bf304b724488bf25fb6
Marcelo dos Santos <mdssjc@gmail.com>
2019-05-16 21:03:05 -0300

    Improving the Look of the Product Page.

--- dart/udemy/flutter-01/flutter_course/lib/pages/product.dart
+++ dart/udemy/flutter-01/flutter_course/lib/pages/product.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ProductPage extends StatelessWidget {
   @override
@@ -8,11 +9,20 @@ class ProductPage extends StatelessWidget {
         title: Text('Product Detail'),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          Text('Details'),
-          RaisedButton(
-            child: Text('BACK'),
-            onPressed: () => Navigator.pop(context),
+          Image.asset('assets/food.jpg'),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text('Details!'),
+          ),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: RaisedButton(
+              color: Theme.of(context).accentColor,
+              child: Text('BACK'),
+              onPressed: () => Navigator.pop(context),
+            ),
           ),
         ],
       ),
