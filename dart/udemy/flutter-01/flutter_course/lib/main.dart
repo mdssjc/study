65f57f99914573c1b5a586d6f3caab7feb0ace7e
Marcelo dos Santos <mdssjc@gmail.com>
2019-05-18 13:13:41 -0300

    Adding the Basic Authentication Page & Replacing Routes.

--- dart/udemy/flutter-01/flutter_course/lib/main.dart
+++ dart/udemy/flutter-01/flutter_course/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_course/pages/home.dart';
+import 'package:flutter_course/pages/auth.dart';
 // import 'package:flutter/rendering.dart';
 import 'product_manager.dart';
 
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
           brightness: Brightness.light,
           primaryColor: Colors.deepOrange,
           accentColor: Colors.deepPurple),
-      home: HomePage(),
+      home: AuthPage(),
     );
   }
 }
