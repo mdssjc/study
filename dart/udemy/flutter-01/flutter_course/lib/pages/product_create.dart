dade4ecc010f5133f4faa6765b6cf999fb3f8282
Marcelo dos Santos <mdssjc@gmail.com>
2019-05-21 21:27:04 -0300

    Showing a Modal.

--- dart/udemy/flutter-01/flutter_course/lib/pages/product_create.dart
+++ dart/udemy/flutter-01/flutter_course/lib/pages/product_create.dart
@@ -1,10 +1,22 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class ProductCreatePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Create a Product'),
+      child: RaisedButton(
+        child: Text('SAVE'),
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (BuildContext context) {
+                return Center(
+                  child: Text('This is a Modal!'),
+                );
+              });
+        },
+      ),
     );
   }
 }
