ccf4ac520dad9d474b65dafa5cd3e10e6936adda
Marcelo dos Santos <mdssjc@gmail.com>
2019-05-27 22:32:33 -0300

    Refatoração.

--- java/udemy/spring-framework-guru/jokes-app/src/main/java/com/github/mdssjc/jokesapp/controllers/JokeController.java
+++ java/udemy/spring-framework-guru/jokes-app/src/main/java/com/github/mdssjc/jokesapp/controllers/JokeController.java
@@ -1,22 +1,22 @@
 package com.github.mdssjc.jokesapp.controllers;
 
-import com.github.mdssjc.jokesapp.services.QuoteService;
+import com.github.mdssjc.jokesapp.services.JokeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class QuoteController {
+public class JokeController {
 
-    private QuoteService service;
+    private JokeService service;
 
-    public QuoteController(QuoteService service) {
+    public JokeController(JokeService service) {
         this.service = service;
     }
 
     @GetMapping({"/", ""})
     public String root(Model model) {
-        model.addAttribute("joke", service.quote());
+        model.addAttribute("joke", service.getJoke());
         return "chucknorris";
     }
 }
