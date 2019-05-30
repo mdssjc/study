2ec9e323d7dd145a4c6419bcfd61ced6e2426221
Marcelo dos Santos <mdssjc@gmail.com>
2019-05-29 21:30:46 -0300

    Setting Borders.

--- dart/udemy/flutter-01/flutter_course/lib/products.dart
+++ dart/udemy/flutter-01/flutter_course/lib/products.dart
@@ -38,6 +38,13 @@ class Products extends StatelessWidget {
               ],
             ),
           ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 6.0, vertical: 2.5),
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey, width: 1.0),
+                borderRadius: BorderRadius.circular(4.0)),
+            child: Text('Union Square, San Franscico'),
+          ),
           ButtonBar(
             alignment: MainAxisAlignment.center,
             children: <Widget>[
