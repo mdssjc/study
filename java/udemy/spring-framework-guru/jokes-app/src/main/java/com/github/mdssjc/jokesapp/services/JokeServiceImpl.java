71590ce218046ae2f38f5ab690619af4bd993834
Marcelo dos Santos <mdssjc@gmail.com>
2019-06-03 22:24:40 -0300

    Java Configuration Example.

--- java/udemy/spring-framework-guru/jokes-app/src/main/java/com/github/mdssjc/jokesapp/services/JokeServiceImpl.java
+++ java/udemy/spring-framework-guru/jokes-app/src/main/java/com/github/mdssjc/jokesapp/services/JokeServiceImpl.java
@@ -6,14 +6,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class JokeServiceImpl implements JokeService {
 
-    private final ChuckNorrisQuotes quotes;
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public JokeServiceImpl() {
-        quotes = new ChuckNorrisQuotes();
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
 
     @Override
     public String getJoke() {
-        return quotes.getRandomQuote();
+        return chuckNorrisQuotes.getRandomQuote();
     }
 }
