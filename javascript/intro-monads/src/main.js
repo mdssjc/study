0a928ed1d060bc2275cc46f9b37184f6d2f010b2
Marcelo dos Santos <mdssjc@gmail.com>
2019-06-09 19:59:46 -0300

    Remoção dos comentários.

--- javascript/intro-monads/src/main.js
+++ javascript/intro-monads/src/main.js
@@ -2,10 +2,6 @@ import { Identity } from "./monads/identity";
 import { List } from "./monads/list";
 
 // Identity
-// emit: join, value
-// chain: flatMap, bind
-// map: fmap
-// inspect
 const one = Identity.of(1);
 const two = one.map(a => a + 1);
 
@@ -15,13 +11,6 @@ console.log(one.map(a => a + 1));
 console.log(two.inspect());
 
 // List
-// emit: join, value
-// chain: flatMap, bind
-// map: fmap
-// inspect
-// concat
-// head
-// tail
 const myNumbers = List.of([1, 3, 4, 7, 10]);
 
 console.log(myNumbers.concat([12]).inspect());
