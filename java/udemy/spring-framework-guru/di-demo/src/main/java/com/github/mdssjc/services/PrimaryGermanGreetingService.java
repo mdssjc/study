a184e9f6b6e51a2284f806842ad20733078ed8ef
Marcelo dos Santos <mdssjc@gmail.com>
2019-06-09 12:59:00 -0300

    Using Spring Factory Beans.

--- java/udemy/spring-framework-guru/di-demo/src/main/java/com/github/mdssjc/services/PrimaryGermanGreetingService.java
+++ java/udemy/spring-framework-guru/di-demo/src/main/java/com/github/mdssjc/services/PrimaryGermanGreetingService.java
@@ -1,12 +1,5 @@
 package com.github.mdssjc.services;
 
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
-
-@Service
-@Primary
-@Profile("de")
 public class PrimaryGermanGreetingService implements GreetingService {
 
     private GreetingRepository greetingRepository;
