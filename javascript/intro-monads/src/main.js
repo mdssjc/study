5ba48abb3a8cd891cb05630540ca88798406429c
Marcelo dos Santos <mdssjc@gmail.com>
2019-06-09 19:52:41 -0300

    List Monad.

--- javascript/intro-monads/src/main.js
+++ javascript/intro-monads/src/main.js
@@ -1,9 +1,11 @@
 import { Identity } from "./monads/identity";
+import { List } from "./monads/list";
 
 // Identity
 // emit: join, value
 // chain: flatMap, bind
 // map: fmap
+// inspect
 const one = Identity.of(1);
 const two = one.map(a => a + 1);
 
@@ -11,3 +13,16 @@ console.log(one.emit());
 console.log(one.chain(a => a + 1));
 console.log(one.map(a => a + 1));
 console.log(two.inspect());
+
+// List
+// emit: join, value
+// chain: flatMap, bind
+// map: fmap
+// inspect
+// concat
+// head
+const myNumbers = List.of([1, 3, 4, 7, 10]);
+
+console.log(myNumbers.concat([12]).inspect());
+console.log(myNumbers.head());
+console.log(myNumbers.tail());
---
+++ javascript/intro-monads/src/monads/list.js
@@ -0,0 +1,17 @@
+const List = x => ({
+    emit: () => x,
+    chain: f => f(x),
+    map: f => List(f(x)),
+    inspect: () => `List(${x})`,
+    concat: a => ListOf(x.concat(a)),
+    head: () => x[0],
+    tail: () => x[x.length - 1],
+});
+
+const ListOf = x => List(x);
+
+const exportList = {
+    of: ListOf,
+}
+
+export { exportList as List };
