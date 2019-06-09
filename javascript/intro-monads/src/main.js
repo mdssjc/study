c031b7599d9465ff000f56db4f6c794ac8d15919
Marcelo dos Santos <mdssjc@gmail.com>
2019-06-09 19:34:24 -0300

    Completando o monad.

--- javascript/intro-monads/src/main.js
+++ javascript/intro-monads/src/main.js
@@ -1,6 +1,13 @@
 import { Identity } from "./monads/identity";
 
 // Identity
-const one = Identity(1);
+// emit: join, value
+// chain: flatMap, bind
+// map: fmap
+const one = Identity.of(1);
+const two = one.map(a => a + 1);
 
 console.log(one.emit());
+console.log(one.chain(a => a + 1));
+console.log(one.map(a => a + 1));
+console.log(two.inspect());
--- javascript/intro-monads/src/monads/identity.js
+++ javascript/intro-monads/src/monads/identity.js
@@ -1,5 +1,14 @@
-export const Identity = x => ({
+const Identity = x => ({
     emit: () => x,
     chain: f => f(x),
     map: f => Identity(f(x)),
+    inspect: () => `Identity(${x})`
 });
+
+const IdentityOf = x => Identity(x);
+
+const exportIdentity = {
+    of: IdentityOf,
+}
+
+export { exportIdentity as Identity };
