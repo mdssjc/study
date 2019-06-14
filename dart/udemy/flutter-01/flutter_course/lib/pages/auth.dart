1e01a1dcab93829810bdc0d7fb346ee16e5da416
Marcelo dos Santos <mdssjc@gmail.com>
2019-06-13 21:34:39 -0300

    Correção da tarefa.

--- dart/udemy/flutter-01/flutter_course/lib/pages/auth.dart
+++ dart/udemy/flutter-01/flutter_course/lib/pages/auth.dart
@@ -8,105 +8,87 @@ class AuthPage extends StatefulWidget {
 }
 
 class _AuthPageState extends State<AuthPage> {
-  String email;
-  String password;
-  String helpMessage = '';
-  bool _acceptTerms = false;
-  bool nonValidEmail = true;
-  bool nonValidPassword = true;
+  final Map<String, dynamic> _formData = {
+    'email': null,
+    'password': null,
+    'acceptTerms': false,
+  };
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   DecorationImage _buildBackgroundImage() {
     return DecorationImage(
       fit: BoxFit.cover,
       colorFilter:
-          ColorFilter.mode(Colors.black.withOpacity(0.3), BlendMode.dstATop),
+          ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.dstATop),
       image: AssetImage('assets/background.jpg'),
     );
   }
 
   Widget _buildEmailTextField() {
-    return TextField(
+    return TextFormField(
       decoration: InputDecoration(
         labelText: 'E-mail',
         filled: true,
         fillColor: Colors.white,
       ),
-      onChanged: (String value) {
-        setState(() {
-          nonValidEmail = (value.isEmpty ||
-              !RegExp(r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
-                  .hasMatch(value));
-          email = value.trim();
-        });
+      keyboardType: TextInputType.emailAddress,
+      validator: (String value) {
+        if (value.isEmpty ||
+            !RegExp(r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+                .hasMatch(value)) {
+          return 'Please enter a valid email';
+        }
+      },
+      onSaved: (String value) {
+        _formData['email'] = value;
       },
     );
   }
 
   Widget _buildPasswordTextField() {
-    return TextField(
+    return TextFormField(
       decoration: InputDecoration(
         labelText: 'Password',
         filled: true,
         fillColor: Colors.white,
       ),
       obscureText: true,
-      onChanged: (String value) {
-        setState(() {
-          nonValidPassword = (value.isEmpty || value.length < 6);
-          password = value.trim();
-        });
+      validator: (String value) {
+        if (value.isEmpty || value.length < 6) {
+          return 'Password invalid';
+        }
+      },
+      onSaved: (String value) {
+        _formData['password'] = value;
       },
     );
   }
 
   Widget _buildAcceptSwitch() {
     return SwitchListTile(
-      value: _acceptTerms,
+      value: _formData['acceptTerms'],
       onChanged: (bool value) {
         setState(() {
-          _acceptTerms = value;
+          _formData['acceptTerms'] = value;
         });
       },
       title: Text('Accept Terms'),
     );
   }
 
-  Widget _buildHelpMessageTextField() {
-    return Text(
-      helpMessage,
-      textAlign: TextAlign.center,
-      style: TextStyle(
-        color: Colors.red,
-        fontWeight: FontWeight.bold,
-      ),
-    );
-  }
-
   void _submitForm() {
-    bool isValid = true;
-
-    setState(() {
-      if (nonValidEmail) {
-        helpMessage = 'E-mail invalid!';
-        isValid = false;
-      } else if (nonValidPassword) {
-        helpMessage = 'Password weak!';
-        isValid = false;
-      } else if (!_acceptTerms) {
-        helpMessage = 'Accept Terms required!';
-        isValid = false;
-      }
-    });
-    if (isValid) {
-      Navigator.pushReplacementNamed(context, "/products");
+    if (!_formKey.currentState.validate() || !_formData['acceptTerms']) {
+      return;
     }
+    _formKey.currentState.save();
+    print(_formData);
+    Navigator.pushReplacementNamed(context, "/products");
   }
 
   @override
   Widget build(BuildContext context) {
     final double deviceWidth = MediaQuery.of(context).size.width;
-    final double targetWidth =
-        deviceWidth > 768.0 ? 500.0 : MediaQuery.of(context).size.width * 0.95;
+    final double targetWidth = deviceWidth > 550.0 ? 500.0 : deviceWidth * 0.95;
 
     return Scaffold(
       appBar: AppBar(title: Text('Login')),
@@ -117,21 +99,22 @@ class _AuthPageState extends State<AuthPage> {
           child: SingleChildScrollView(
             child: Container(
               width: targetWidth,
-              child: Column(
-                children: <Widget>[
-                  _buildEmailTextField(),
-                  SizedBox(height: 10.0),
-                  _buildPasswordTextField(),
-                  _buildAcceptSwitch(),
-                  SizedBox(height: 6.0),
-                  _buildHelpMessageTextField(),
-                  SizedBox(height: 6.0),
-                  RaisedButton(
-                    child: Text('LOGIN'),
-                    textColor: Colors.white,
-                    onPressed: _submitForm,
-                  ),
-                ],
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    _buildEmailTextField(),
+                    SizedBox(height: 10.0),
+                    _buildPasswordTextField(),
+                    _buildAcceptSwitch(),
+                    SizedBox(height: 10.0),
+                    RaisedButton(
+                      textColor: Colors.white,
+                      child: Text('LOGIN'),
+                      onPressed: _submitForm,
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
