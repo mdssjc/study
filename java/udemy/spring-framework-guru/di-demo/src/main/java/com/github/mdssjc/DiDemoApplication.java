766783abd8f37e8abbaeceb1d83ef0fb23d9861e
Marcelo dos Santos <mdssjc@gmail.com>
2019-06-16 13:34:56 -0300

    Multiple Property Files.

--- java/udemy/spring-framework-guru/di-demo/src/main/java/com/github/mdssjc/DiDemoApplication.java
+++ java/udemy/spring-framework-guru/di-demo/src/main/java/com/github/mdssjc/DiDemoApplication.java
@@ -2,6 +2,7 @@ package com.github.mdssjc;
 
 import com.github.mdssjc.controllers.MyController;
 import com.github.mdssjc.examplebeans.FakeDataSource;
+import com.github.mdssjc.examplebeans.FakeJmsBroker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -13,7 +14,11 @@ public class DiDemoApplication {
         ConfigurableApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
 
         MyController controller = (MyController) ctx.getBean("myController");
+
         FakeDataSource fakeDataSource = ctx.getBean(FakeDataSource.class);
         System.out.println(fakeDataSource.getUser());
+
+        FakeJmsBroker fakeJmsBroker = ctx.getBean(FakeJmsBroker.class);
+        System.out.println(fakeJmsBroker.getUsername());
     }
 }
--- java/udemy/spring-framework-guru/di-demo/src/main/java/com/github/mdssjc/config/PropertyConfig.java
+++ java/udemy/spring-framework-guru/di-demo/src/main/java/com/github/mdssjc/config/PropertyConfig.java
@@ -1,16 +1,22 @@
 package com.github.mdssjc.config;
 
 import com.github.mdssjc.examplebeans.FakeDataSource;
+import com.github.mdssjc.examplebeans.FakeJmsBroker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
+// @PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
+@PropertySources({
+                         @PropertySource("classpath:datasource.properties"),
+                         @PropertySource("classpath:jms.properties"),
+                 })
 public class PropertyConfig {
 
     @Autowired
@@ -23,6 +29,13 @@ public class PropertyConfig {
     @Value("${guru.dburl}")
     private String url;
 
+    @Value("${guru.jms.username}")
+    private String jmsUsername;
+    @Value("${guru.jms.password}")
+    private String jmsPassword;
+    @Value("${guru.jms.url}")
+    private String jmsUrl;
+
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
@@ -30,6 +43,15 @@ public class PropertyConfig {
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
+    }
+
+    @Bean
+    public FakeJmsBroker fakeJmsBroker() {
+        FakeJmsBroker jmsBroker = new FakeJmsBroker();
+        jmsBroker.setUsername(jmsUsername);
+        jmsBroker.setPassword(jmsPassword);
+        jmsBroker.setUrl(jmsUrl);
+        return jmsBroker;
     }
 
     @Bean
