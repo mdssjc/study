a5667de6f5433dc50c03b59cfd7491946d762870
Marcelo dos Santos <mdssjc@gmail.com>
2019-06-17 21:41:09 -0300

    Deleting Products Upon Swipe.

--- dart/udemy/flutter-01/flutter_course/lib/pages/product_list.dart
+++ dart/udemy/flutter-01/flutter_course/lib/pages/product_list.dart
@@ -3,9 +3,10 @@ import 'package:flutter_course/pages/product_edit.dart';
 
 class ProductListPage extends StatelessWidget {
   final Function updateProduct;
+  final Function deleteProduct;
   final List<Map<String, dynamic>> products;
 
-  ProductListPage(this.products, this.updateProduct);
+  ProductListPage(this.products, this.updateProduct, this.deleteProduct);
 
   @override
   Widget build(BuildContext context) {
@@ -13,9 +14,16 @@ class ProductListPage extends StatelessWidget {
       itemBuilder: (BuildContext context, int index) {
         return Dismissible(
           key: Key(products[index]['title']),
-          background: Container(
-            color: Colors.red,
-          ),
+          onDismissed: (DismissDirection direction) {
+            if (direction == DismissDirection.endToStart) {
+              deleteProduct(index);
+            } else if (direction == DismissDirection.startToEnd) {
+              print('Swiped start to end');
+            } else {
+              print('Other swiping');
+            }
+          },
+          background: Container(color: Colors.red),
           child: Column(
             children: <Widget>[
               ListTile(
