dd31217773e076071c88b77c0efe99dae2d331ef
Marcelo dos Santos <mdssjc@gmail.com>
2019-06-25 19:58:01 -0300

    One To Many JPA Relationships.

---
+++ java/udemy/spring-framework-guru/spring5-recipe-app/src/main/java/guru/springframework/domain/Ingredient.java
@@ -0,0 +1,24 @@
+package guru.springframework.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private BigDecimal amount;
+
+    // private UnitOfMeasure uom;
+
+    @ManyToOne
+    private Recipe recipe;
+}
