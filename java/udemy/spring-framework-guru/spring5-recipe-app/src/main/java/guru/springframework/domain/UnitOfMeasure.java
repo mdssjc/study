357446bc122499572cf828b4657f87a32054af11
Marcelo dos Santos <mdssjc@gmail.com>
2019-06-25 20:09:38 -0300

    Assignment - Create One to One Relationship.

--- java/udemy/spring-framework-guru/spring5-recipe-app/src/main/java/guru/springframework/domain/UnitOfMeasure.java
+++ java/udemy/spring-framework-guru/spring5-recipe-app/src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -3,23 +3,18 @@ package guru.springframework.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
-import java.math.BigDecimal;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Getter
 @Setter
 @Entity
-public class Ingredient {
+public class UnitOfMeasure {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String description;
-    private BigDecimal amount;
-
-    @OneToOne(fetch = FetchType.EAGER)
-    private UnitOfMeasure uom;
-
-    @ManyToOne
-    private Recipe recipe;
 }
