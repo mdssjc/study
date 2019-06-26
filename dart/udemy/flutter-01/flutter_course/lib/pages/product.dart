fc799d992e97bfeb3a23179df86b2444e4f4732f
Marcelo dos Santos <mdssjc@gmail.com>
2019-06-25 21:55:45 -0300

    Viewing Single Products.

--- dart/udemy/flutter-01/flutter_course/lib/pages/product.dart
+++ dart/udemy/flutter-01/flutter_course/lib/pages/product.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_course/models/Product.dart';
+import 'package:flutter_course/scoped-models/products.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 import '../widgets/ui_elements/title_default.dart';
 
 class ProductPage extends StatelessWidget {
-  final String title;
-  final String imageUrl;
-  final double price;
-  final String description;
+  final int productIndex;
 
-  ProductPage(this.title, this.imageUrl, this.price, this.description);
+  ProductPage(this.productIndex);
 
-  Widget _buildAddressPriceRow() {
+  Widget _buildAddressPriceRow(double price) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
@@ -36,25 +36,30 @@ class ProductPage extends StatelessWidget {
         Navigator.pop(context, false);
         return Future.value(false);
       },
-      child: Scaffold(
-        appBar: AppBar(title: Text(title)),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(imageUrl),
-            SizedBox(height: 10.0),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              child: TitleDefault(title),
-            ),
-            _buildAddressPriceRow(),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              child: Text(description, textAlign: TextAlign.center),
-            ),
-          ],
-        ),
-      ),
+      child: ScopedModelDescendant<ProductsModel>(
+          builder: (BuildContext context, Widget child, ProductsModel model) {
+        final Product product = model.products[productIndex];
+
+        return Scaffold(
+          appBar: AppBar(title: Text(product.title)),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(product.image),
+              SizedBox(height: 10.0),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: TitleDefault(product.title),
+              ),
+              _buildAddressPriceRow(product.price),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: Text(product.description, textAlign: TextAlign.center),
+              ),
+            ],
+          ),
+        );
+      }),
     );
   }
 }
