4e1a01fb50d8af805662cc7e695a4addc45e9334
Marcelo dos Santos <mdssjc@gmail.com>
2019-07-01 22:42:52 -0300

    Structs.

--- rust/learning-rust/src/main.rs
+++ rust/learning-rust/src/main.rs
@@ -1,3 +1,11 @@
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+struct Kilometers(i32);
+
 fn main() {
     println!("Hello, world!");
 
@@ -6,4 +14,11 @@ fn main() {
         x * x
     };
     println!("{}", square(x));
+
+    let black = Color { red: 0, green: 0, blue: 0 };
+    println!("Black = rgb({}, {}, {})", black.red, black.green, black.blue);
+
+    let distance = Kilometers(20);
+    let Kilometers(distance_in_km) = distance;
+    println!("The distance: {} km", distance_in_km);
 }
