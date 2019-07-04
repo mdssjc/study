f95496f44933e133732adbd87db2508b63a61b0f
Marcelo dos Santos <mdssjc@gmail.com>
2019-07-03 23:00:21 -0300

    Traits e Impl.

--- rust/learning-rust/src/main.rs
+++ rust/learning-rust/src/main.rs
@@ -6,6 +6,27 @@ struct Color {
 
 struct Kilometers(i32);
 
+struct Player {
+    first_name: String,
+    last_name: String,
+}
+
+trait FullName {
+    fn full_name(&self) -> String;
+}
+
+impl Player {
+    fn full_name(&self) -> String {
+        format!("{} {}", self.first_name, self.last_name)
+    }
+}
+
+impl FullName for Player {
+    fn full_name(&self) -> String {
+        format!("{} {}", self.first_name, self.last_name)
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 
@@ -21,4 +42,15 @@ fn main() {
     let distance = Kilometers(20);
     let Kilometers(distance_in_km) = distance;
     println!("The distance: {} km", distance_in_km);
+
+    let player_1 = Player {
+        first_name: "Rafael".to_string(),
+        last_name: "Nadal".to_string(),
+    };
+    let player_2 = Player {
+        first_name: "Roger".to_string(),
+        last_name: "Federer".to_string(),
+    };
+    println!("Player 01: {}", player_1.full_name());
+    println!("Player 02: {}", player_2.full_name());
 }
