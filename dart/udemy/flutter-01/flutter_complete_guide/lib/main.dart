439e9472046063afe1b2c73734d73edad4116dc7
Marcelo dos Santos <mdssjc@gmail.com>
2019-07-09 20:49:38 -0300

    Connecting Functions & Buttons.

--- dart/udemy/flutter-01/flutter_complete_guide/lib/main.dart
+++ dart/udemy/flutter-01/flutter_complete_guide/lib/main.dart
@@ -3,11 +3,15 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
     var questions = [
       'What\'s your favorite color?',
-      'What\'s your favorite animal?'
+      'What\'s your favorite animal?',
     ];
 
     return MaterialApp(
@@ -20,15 +24,15 @@ class MyApp extends StatelessWidget {
             Text('The question!'),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: null,
+              onPressed: answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 2'),
-              onPressed: null,
+              onPressed: answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 3'),
-              onPressed: null,
+              onPressed: answerQuestion,
             ),
           ],
         ),
