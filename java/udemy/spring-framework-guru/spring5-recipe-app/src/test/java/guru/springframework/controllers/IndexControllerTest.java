27e9b7608ffba367eec2fff8f139d5a1d7f3daf3
Marcelo dos Santos <mdssjc@gmail.com>
2019-07-23 22:02:53 -0300

    Introduction to Spring MockMVC.

--- java/udemy/spring-framework-guru/spring5-recipe-app/src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ java/udemy/spring-framework-guru/spring5-recipe-app/src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -7,7 +7,10 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -15,6 +18,9 @@ import java.util.Set;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class IndexControllerTest {
 
@@ -29,6 +35,17 @@ public class IndexControllerTest {
     public void setUp() {
         MockitoAnnotations.initMocks(this);
         controller = new IndexController(recipeService);
+    }
+
+    @Test
+    public void testMockMVC() throws Exception {
+        WebApplicationContext context;
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller)
+                                         .build();
+
+        mockMvc.perform(get("/"))
+               .andExpect(status().isOk())
+               .andExpect(view().name("index"));
     }
 
     @Test
