0fa3ccce3c64ce53a8a9818155b61ecc715a01d7
Marcelo dos Santos <mdssjc@gmail.com>
2019-08-04 14:27:09 -0300

    Create a Recipe.

--- java/udemy/spring-framework-guru/spring5-recipe-app/src/main/java/guru/springframework/controllers/RecipeController.java
+++ java/udemy/spring-framework-guru/spring5-recipe-app/src/main/java/guru/springframework/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package guru.springframework.controllers;
 
+import guru.springframework.commands.RecipeCommand;
 import guru.springframework.services.RecipeService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @AllArgsConstructor
 @Controller
@@ -18,5 +21,19 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
 
         return "recipe/show";
+    }
+
+    @GetMapping("/recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("/recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
