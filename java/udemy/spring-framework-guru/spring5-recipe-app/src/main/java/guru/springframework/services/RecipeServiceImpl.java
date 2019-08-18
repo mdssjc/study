d2b7240129c2f98ef9bea51b19abbdc8fd71ee9d
Marcelo dos Santos <mdssjc@gmail.com>
2019-08-18 14:41:36 -0300

    Using Spring MVC Annotation @ResponseStatus.

--- java/udemy/spring-framework-guru/spring5-recipe-app/src/main/java/guru/springframework/services/RecipeServiceImpl.java
+++ java/udemy/spring-framework-guru/spring5-recipe-app/src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -4,6 +4,7 @@ import guru.springframework.commands.RecipeCommand;
 import guru.springframework.converters.RecipeCommandToRecipe;
 import guru.springframework.converters.RecipeToRecipeCommand;
 import guru.springframework.domain.Recipe;
+import guru.springframework.exceptions.NotFoundException;
 import guru.springframework.repositories.RecipeRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -38,7 +39,7 @@ public class RecipeServiceImpl implements RecipeService {
         Optional<Recipe> recipeOptional = recipeRepository.findById(id);
 
         if (recipeOptional.isEmpty()) {
-            throw new RuntimeException(("Recipe Not Found!"));
+            throw new NotFoundException("Recipe Not Found!");
         }
 
         return recipeOptional.get();
--- java/udemy/spring-framework-guru/spring5-recipe-app/src/test/java/guru/springframework/services/RecipeServiceImplTest.java
+++ java/udemy/spring-framework-guru/spring5-recipe-app/src/test/java/guru/springframework/services/RecipeServiceImplTest.java
@@ -4,6 +4,7 @@ import guru.springframework.commands.RecipeCommand;
 import guru.springframework.converters.RecipeCommandToRecipe;
 import guru.springframework.converters.RecipeToRecipeCommand;
 import guru.springframework.domain.Recipe;
+import guru.springframework.exceptions.NotFoundException;
 import guru.springframework.repositories.RecipeRepository;
 import org.junit.Before;
 import org.junit.Test;
@@ -74,10 +75,39 @@ public class RecipeServiceImplTest {
         verify(recipeRepository, never()).findAll();
     }
 
+     @Test(expected = NotFoundException.class)
+    public void getRecipeByIdTestNotFound() throws Exception {
+        Optional<Recipe> recipeOptional = Optional.empty();
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        Recipe recipeReturned = recipeService.findById(1L);
+    }
+
+    @Test
+    public void getRecipeCommandByIdTest() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        RecipeCommand recipeCommand = new RecipeCommand();
+        recipeCommand.setId(1L);
+
+        when(recipeToRecipeCommand.convert(any())).thenReturn(recipeCommand);
+
+        RecipeCommand commandById = recipeService.findCommandById(1L);
+
+        assertNotNull("Null recipe returned", commandById);
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
+    }
+
     @Test
     public void getRecipesTest() throws Exception {
         Recipe recipe = new Recipe();
-        Set<Recipe> receipesData = new HashSet<>();
+        HashSet receipesData = new HashSet();
         receipesData.add(recipe);
 
         when(recipeService.getRecipes()).thenReturn(receipesData);
@@ -90,8 +120,9 @@ public class RecipeServiceImplTest {
     }
 
     @Test
-    public void testDeleteById() {
-        Long idToDelete = 2L;
+    public void testDeleteById() throws Exception {
+        Long idToDelete = Long.valueOf(2L);
+
         recipeService.deleteById(idToDelete);
 
         verify(recipeRepository, times(1)).deleteById(anyLong());
