5b51317e658d58a18dc2ad2a844747db78cb881f
Marcelo dos Santos <mdssjc@gmail.com>
2019-08-20 19:14:38 -0300

    Data Validation with Spring MVC.

--- java/udemy/spring-framework-guru/spring5-recipe-app/src/main/java/guru/springframework/controllers/RecipeController.java
+++ java/udemy/spring-framework-guru/spring5-recipe-app/src/main/java/guru/springframework/controllers/RecipeController.java
@@ -3,20 +3,26 @@ package guru.springframework.controllers;
 import guru.springframework.commands.RecipeCommand;
 import guru.springframework.exceptions.NotFoundException;
 import guru.springframework.services.RecipeService;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-@AllArgsConstructor
+import javax.validation.Valid;
+
 @Slf4j
 @Controller
 public class RecipeController {
 
+    private static final String RECIPE_RECIPEFORM_URL = "recipe/recipeform";
     private final RecipeService recipeService;
+
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
 
     @GetMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model) {
@@ -39,8 +45,14 @@ public class RecipeController {
         return "recipe/recipeform";
     }
 
-    @PostMapping("/recipe")
-    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+    @PostMapping("recipe")
+    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            bindingResult.getAllErrors()
+                         .forEach(objectError -> log.debug(objectError.toString()));
+            return RECIPE_RECIPEFORM_URL;
+        }
+
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
         return "redirect:/recipe/" + savedCommand.getId() + "/show";
