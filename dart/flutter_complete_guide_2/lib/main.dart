bf0fa3b28dac4b607fea54b63030c6dddc318bde
Marcelo dos Santos <mdssjc@gmail.com>
2019-08-19 22:20:27 -0300

    Time to Practice: Builder Methods.

--- dart/flutter_complete_guide_2/lib/main.dart
+++ dart/flutter_complete_guide_2/lib/main.dart
@@ -145,11 +145,8 @@ class _MyHomePageState extends State<MyHomePage> {
     ];
   }
 
-  @override
-  Widget build(BuildContext context) {
-    final mediaQuery = MediaQuery.of(context);
-    final isLandscape = mediaQuery.orientation == Orientation.landscape;
-    final PreferredSizeWidget appBar = Platform.isIOS
+  Widget _buildAppBar() {
+    return Platform.isIOS
         ? CupertinoNavigationBar(
             middle: Text('Personal Expenses'),
             trailing: Row(
@@ -171,7 +168,13 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ],
           );
+  }
 
+  @override
+  Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+    final isLandscape = mediaQuery.orientation == Orientation.landscape;
+    final PreferredSizeWidget appBar = _buildAppBar();
     final txListWidget = Container(
         height: (mediaQuery.size.height -
                 appBar.preferredSize.height -
