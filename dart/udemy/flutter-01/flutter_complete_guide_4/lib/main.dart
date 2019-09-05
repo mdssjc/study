d84d95457dda424b4c7ece1482e9c440e3053193
Marcelo dos Santos <mdssjc@gmail.com>
2019-09-04 21:23:45 -0300

    Adding Navigation to the App.

--- dart/udemy/flutter-01/flutter_complete_guide_4/lib/main.dart
+++ dart/udemy/flutter-01/flutter_complete_guide_4/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide_4/screens/product_datail_screen.dart';
 import 'package:flutter_complete_guide_4/screens/products_overview_screen.dart';
 
 void main() => runApp(MyApp());
@@ -14,6 +15,9 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Lato',
       ),
       home: ProductsOverviewScreen(),
+      routes: {
+        ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+      },
     );
   }
 }
--- dart/udemy/flutter-01/flutter_complete_guide_4/lib/widgets/product_item.dart
+++ dart/udemy/flutter-01/flutter_complete_guide_4/lib/widgets/product_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide_4/screens/product_datail_screen.dart';
 
 class ProductItem extends StatelessWidget {
   final String id;
@@ -17,9 +18,17 @@ class ProductItem extends StatelessWidget {
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
-        child: Image.network(
-          imageUrl,
-          fit: BoxFit.cover,
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              ProductDetailScreen.routeName,
+              arguments: id,
+            );
+          },
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
