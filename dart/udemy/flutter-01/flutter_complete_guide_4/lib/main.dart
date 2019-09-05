a3a44c8a5928434150fbbf7577eee2bdfbc72ff9
Marcelo dos Santos <mdssjc@gmail.com>
2019-09-04 22:00:32 -0300

    Working with Providers & Listeners.

--- dart/udemy/flutter-01/flutter_complete_guide_4/lib/main.dart
+++ dart/udemy/flutter-01/flutter_complete_guide_4/lib/main.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide_4/providers/products.dart';
 import 'package:flutter_complete_guide_4/screens/product_datail_screen.dart';
 import 'package:flutter_complete_guide_4/screens/products_overview_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      builder: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+        ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
       ),
-      home: ProductsOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-      },
     );
   }
 }
