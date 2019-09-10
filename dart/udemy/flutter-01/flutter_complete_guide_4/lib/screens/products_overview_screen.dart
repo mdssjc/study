a0f61c2ae9c42c7ac385def981efe5d1aa3211a5
Marcelo dos Santos <mdssjc@gmail.com>
2019-09-09 21:32:24 -0300

    Local State vs App-wide State.

--- dart/udemy/flutter-01/flutter_complete_guide_4/lib/screens/products_overview_screen.dart
+++ dart/udemy/flutter-01/flutter_complete_guide_4/lib/screens/products_overview_screen.dart
@@ -1,14 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide_4/widgets/products_grid.dart';
 
-class ProductsOverviewScreen extends StatelessWidget {
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  var _showOnlyFavorites = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('MyShop'),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              setState(() {
+                if (selectedValue == FilterOptions.Favorites) {
+                  _showOnlyFavorites = true;
+                } else {
+                  _showOnlyFavorites = false;
+                }
+              });
+            },
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                  child: Text('Only Favorites'),
+                  value: FilterOptions.Favorites),
+              PopupMenuItem(child: Text('Show All'), value: FilterOptions.All),
+            ],
+          ),
+        ],
       ),
-      body: ProductsGrid(),
+      body: ProductsGrid(_showOnlyFavorites),
     );
   }
+}
+
+enum FilterOptions {
+  Favorites,
+  All,
 }
