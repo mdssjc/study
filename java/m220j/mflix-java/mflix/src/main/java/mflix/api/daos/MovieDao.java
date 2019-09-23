b06c867cb3d9760b06becc0c54edd530cf75432a
Marcelo dos Santos <mdssjc@gmail.com>
2019-09-23 19:12:22 -0300

    Progresso nos exercícios.

--- java/m220j/mflix-java/mflix/src/main/java/mflix/api/daos/MovieDao.java
+++ java/m220j/mflix-java/mflix/src/main/java/mflix/api/daos/MovieDao.java
@@ -117,11 +117,13 @@ public class MovieDao extends AbstractMFlixDao {
    * @return List of matching Document objects.
    */
   public List<Document> getMoviesByCountry(String... country) {
-
-    Bson queryFilter = new Document();
-    Bson projection = new Document();
-    //TODO> Ticket: Projection - implement the query and projection required by the unit test
+    Bson queryFilter = Filters.all("countries", country);
+    Bson projection = Projections.include("title");
     List<Document> movies = new ArrayList<>();
+    moviesCollection.find(queryFilter)
+                    .projection(projection)
+                    .iterator()
+                    .forEachRemaining(movies::add);
 
     return movies;
   }
@@ -162,10 +164,8 @@ public class MovieDao extends AbstractMFlixDao {
    * @return List of documents sorted by sortKey that match the cast selector.
    */
   public List<Document> getMoviesByCast(String sortKey, int limit, int skip, String... cast) {
-    Bson castFilter = null;
-    Bson sort = null;
-    //TODO> Ticket: Subfield Text Search - implement the expected cast
-    // filter and sort
+    Bson castFilter = Filters.in("cast", cast);
+    Bson sort = Sorts.descending(sortKey);
     List<Document> movies = new ArrayList<>();
     moviesCollection
         .find(castFilter)
