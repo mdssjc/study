1398d8641d5c8b8658742c0f725d1843ed43a6a3
Marcelo dos Santos <mdssjc@gmail.com>
2019-09-27 21:46:19 -0300

    Fixed imports.

--- dart/udemy/flutter-01/flutter_complete_guide_4/lib/widgets/product_item.dart
+++ dart/udemy/flutter-01/flutter_complete_guide_4/lib/widgets/product_item.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide_4/providers/cart.dart';
 import 'package:flutter_complete_guide_4/providers/product.dart';
