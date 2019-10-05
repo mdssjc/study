432e05b1e5f336533e2ff5159f6dbda475939569
Marcelo dos Santos <mdssjc@gmail.com>
2019-10-04 21:23:13 -0300

    Handling Timeouts.

--- java/m220j/mflix-java/mflix/src/main/java/mflix/config/MongoDBConfiguration.java
+++ java/m220j/mflix-java/mflix/src/main/java/mflix/config/MongoDBConfiguration.java
@@ -1,6 +1,8 @@
 package mflix.config;
 
 import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.WriteConcern;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,20 +12,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.TimeUnit;
+
 @Configuration
 @Service
 public class MongoDBConfiguration {
 
-  @Bean
-  @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
-  public MongoClient mongoClient(@Value("${spring.mongodb.uri}") String connectionString) {
+    @Bean
+    @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
+    public MongoClient mongoClient(@Value("${spring.mongodb.uri}") String connectionString) {
+        ConnectionString connString = new ConnectionString(connectionString);
 
-    ConnectionString connString = new ConnectionString(connectionString);
-
-    //TODO> Ticket: Handling Timeouts - configure the expected
-    // WriteConcern `wtimeout` and `connectTimeoutMS` values
-    MongoClient mongoClient = MongoClients.create(connectionString);
-
-    return mongoClient;
-  }
+        return MongoClients.create(MongoClientSettings.builder()
+                                                      .applyConnectionString(connString)
+                                                      .writeConcern(new WriteConcern("majority").withWTimeout(2500, TimeUnit.MILLISECONDS))
+                                                      .applyToClusterSettings(builder -> builder.applyConnectionString(connString)
+                                                                                                .serverSelectionTimeout(5000, TimeUnit.MILLISECONDS)
+                                                                                                .build())
+                                                      .build());
+    }
 }
