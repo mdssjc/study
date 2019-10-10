9dd17c166c71b6015e0416b1d2c4c26405e58b0c
Marcelo dos Santos <mdssjc@gmail.com>
2019-10-09 21:56:09 -0300

    Creating Custom Exceptions & More Error Handling.

--- dart/udemy/flutter-01/flutter_complete_guide_4/lib/providers/products.dart
+++ dart/udemy/flutter-01/flutter_complete_guide_4/lib/providers/products.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide_4/models/http_exception.dart';
 import 'package:flutter_complete_guide_4/providers/product.dart';
 import 'package:http/http.dart' as http;
 
@@ -126,20 +127,20 @@ class Products with ChangeNotifier {
     }
   }
 
-  void deleteProduct(String id) {
+  Future<void> deleteProduct(String id) async {
     final url = 'https://flutter-update-a4daa.firebaseio.com/products/$id.json';
     final existingProductIndex = _items.indexWhere((prod) => prod.id == id);
     var existingProduct = _items[existingProductIndex];
 
-    http.delete(url).then((response) {
-      if (response.statusCode >= 400) {}
-      existingProduct = null;
-    }).catchError((_) {
-      _items.insert(existingProductIndex, existingProduct);
-      notifyListeners();
-    });
-
     _items.removeAt(existingProductIndex);
     notifyListeners();
+
+    final response = await http.delete(url);
+    if (response.statusCode >= 400) {
+      _items.insert(existingProductIndex, existingProduct);
+      notifyListeners();
+      throw HttpException('Could not delete product.');
+    }
+    existingProduct = null;
   }
 }
