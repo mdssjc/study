01e18d235c7d1b85e9d926bc8e76ad1af28e848c
Marcelo dos Santos <mdssjc@gmail.com>
2019-10-10 21:19:16 -0300

    Storing Orders in the Web.

--- dart/udemy/flutter-01/flutter_complete_guide_4/lib/providers/orders.dart
+++ dart/udemy/flutter-01/flutter_complete_guide_4/lib/providers/orders.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter_complete_guide_4/providers/cart.dart';
+import 'package:http/http.dart' as http;
 
 class OrdemItem {
   final String id;
@@ -22,16 +25,36 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    const url = 'https://flutter-update-a4daa.firebaseio.com/orders.json';
+    final timestamp = DateTime.now();
+    final response = await http.post(
+      url,
+      body: json.encode({
+        'amount': total,
+        'dateTime': timestamp.toIso8601String(),
+        'products': cartProducts
+            .map((cp) => {
+                  'id': cp.id,
+                  'title': cp.title,
+                  'quantity': cp.quantity,
+                  'price': cp.price,
+                })
+            .toList(),
+        'id': timestamp.toString(),
+      }),
+    );
+
     _orders.insert(
       0,
       OrdemItem(
-        id: DateTime.now().toString(),
+        id: json.decode(response.body)['name'],
         amount: total,
+        dateTime: timestamp,
         products: cartProducts,
-        dateTime: DateTime.now(),
       ),
     );
+
     notifyListeners();
   }
 }
