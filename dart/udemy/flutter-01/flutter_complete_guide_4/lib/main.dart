a6dff95d9aa00e820901ddfded897ad5e7dd0298
Marcelo dos Santos <mdssjc@gmail.com>
2019-10-14 21:27:02 -0300

    Adding the Auth Screen.

--- dart/udemy/flutter-01/flutter_complete_guide_4/lib/main.dart
+++ dart/udemy/flutter-01/flutter_complete_guide_4/lib/main.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide_4/providers/cart.dart';
 import 'package:flutter_complete_guide_4/providers/orders.dart';
 import 'package:flutter_complete_guide_4/providers/products.dart';
+import 'package:flutter_complete_guide_4/screens/auth_screen.dart';
 import 'package:flutter_complete_guide_4/screens/cart_screen.dart';
 import 'package:flutter_complete_guide_4/screens/edit_product_screen.dart';
 import 'package:flutter_complete_guide_4/screens/orders_screen.dart';
 import 'package:flutter_complete_guide_4/screens/product_detail_screen.dart';
-import 'package:flutter_complete_guide_4/screens/products_overview_screen.dart';
 import 'package:flutter_complete_guide_4/screens/user_products_screen.dart';
 import 'package:provider/provider.dart';
 
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
           accentColor: Colors.deepOrange,
           fontFamily: 'Lato',
         ),
-        home: ProductsOverviewScreen(),
+        home: AuthScreen(),
         routes: {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
