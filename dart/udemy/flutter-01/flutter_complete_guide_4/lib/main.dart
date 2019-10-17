5edb147a2de54a83e2f63bbd929a0bcb102aff20
Marcelo dos Santos <mdssjc@gmail.com>
2019-10-16 22:11:02 -0300

    Attaching Orders to Users.

--- dart/udemy/flutter-01/flutter_complete_guide_4/lib/main.dart
+++ dart/udemy/flutter-01/flutter_complete_guide_4/lib/main.dart
@@ -31,6 +31,7 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProxyProvider<Auth, Orders>(
           builder: (ctx, auth, previousOrders) => Orders(
             auth.token,
+            auth.userId,
             previousOrders == null ? [] : previousOrders.orders,
           ),
         ),
--- dart/udemy/flutter-01/flutter_complete_guide_4/lib/providers/orders.dart
+++ dart/udemy/flutter-01/flutter_complete_guide_4/lib/providers/orders.dart
@@ -21,8 +21,9 @@ class OrdemItem {
 class Orders with ChangeNotifier {
   List<OrdemItem> _orders = [];
   final String authToken;
+  final String userId;
 
-  Orders(this.authToken, this._orders);
+  Orders(this.authToken, this.userId, this._orders);
 
   List<OrdemItem> get orders {
     return [..._orders];
@@ -30,7 +31,7 @@ class Orders with ChangeNotifier {
 
   Future<void> fetchAndSetOrders() async {
     final url =
-        'https://flutter-update-a4daa.firebaseio.com/orders.json?auth=$authToken';
+        'https://flutter-update-a4daa.firebaseio.com/orders/$userId.json?auth=$authToken';
     final response = await http.get(url);
     final extractedData = json.decode(response.body) as Map<String, dynamic>;
     final List<OrdemItem> loadedOrders = [];
@@ -62,7 +63,7 @@ class Orders with ChangeNotifier {
 
   Future<void> addOrder(List<CartItem> cartProducts, double total) async {
     final url =
-        'https://flutter-update-a4daa.firebaseio.com/orders.json?auth=$authToken';
+        'https://flutter-update-a4daa.firebaseio.com/orders/$userId.json?auth=$authToken';
     final timestamp = DateTime.now();
     final response = await http.post(
       url,
