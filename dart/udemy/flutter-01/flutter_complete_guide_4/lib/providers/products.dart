fdc5a93e06f633fd6eca8ebada14b7403d9880d9
Marcelo dos Santos <mdssjc@gmail.com>
2019-10-16 22:06:43 -0300

    Attaching Products to Users & Filtering By Creator.

--- dart/udemy/flutter-01/flutter_complete_guide_4/lib/providers/products.dart
+++ dart/udemy/flutter-01/flutter_complete_guide_4/lib/providers/products.dart
@@ -58,9 +58,10 @@ class Products with ChangeNotifier {
     return _items.firstWhere((prod) => prod.id == id);
   }
 
-  Future<void> fetchAndSetProducts() async {
+  Future<void> fetchAndSetProducts([bool filterByUser = false]) async {
+final filterString = filterByUser ? 'orderBy="creatorId"&equalTo="$userId"': '';
     var url =
-        'https://flutter-update-a4daa.firebaseio.com/products.json?auth=$authToken';
+        'https://flutter-update-a4daa.firebaseio.com/products.json?auth=$authToken&$filterString';
 
     try {
       final response = await http.get(url);
@@ -104,6 +105,7 @@ class Products with ChangeNotifier {
           'description': product.description,
           'imageUrl': product.imageUrl,
           'price': product.price,
+          'creatorId': userId,
         }),
       );
       final newProduct = Product(
