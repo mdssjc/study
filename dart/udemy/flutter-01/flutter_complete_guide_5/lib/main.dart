340334fc856131783486b22db401c6e8c5251184
Marcelo dos Santos <mdssjc@gmail.com>
2019-11-04 21:37:06 -0300

    Adding the "Add Place" Screen & An Image Input.

--- dart/udemy/flutter-01/flutter_complete_guide_5/lib/main.dart
+++ dart/udemy/flutter-01/flutter_complete_guide_5/lib/main.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide_5/providers/great_places.dart';
+import 'package:flutter_complete_guide_5/screens/add_place_screen.dart';
+import 'package:flutter_complete_guide_5/screens/places_list_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Great Places',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        accentColor: Colors.indigo,
+    return ChangeNotifierProvider.value(
+      value: GreatPlaces(),
+      child: MaterialApp(
+        title: 'Great Places',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          accentColor: Colors.amber,
+        ),
+        home: PlacesListScreen(),
+        routes: {
+          AddPlaceScreen.routeName: (ctx) => AddPlaceScreen(),
+        },
       ),
-      home: null,
     );
   }
 }
