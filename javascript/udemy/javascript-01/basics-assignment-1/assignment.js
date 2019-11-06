918162801aea06fce3b9b13652350561926c5fd7
Marcelo dos Santos <mdssjc@gmail.com>
2019-11-06 19:14:59 -0300

    Tarefa 1: Time to Practice: Variables, Constants, Operators & Core Data Types.

---
+++ javascript/udemy/javascript-01/basics-assignment-1/assignment.js
@@ -0,0 +1,18 @@
+// Step 1
+const userInput = 10;
+let result;
+
+// Step 2
+result = 18 + userInput;
+
+// Step 3
+result -= 5;
+result *= 100;
+result /= 2.0;
+
+// Step 4
+// input -> const
+
+// Step 5
+alert('input: ' + userInput);
+alert('result: ' + result);
