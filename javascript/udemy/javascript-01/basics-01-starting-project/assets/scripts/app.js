538ab155f6559115f6381cbba0a099c29f7ed610
Marcelo dos Santos <mdssjc@gmail.com>
2019-11-18 21:46:12 -0300

    Splitting Code into Functions.

--- javascript/udemy/javascript-01/basics-01-starting-project/assets/scripts/app.js
+++ javascript/udemy/javascript-01/basics-01-starting-project/assets/scripts/app.js
@@ -1,9 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
+
 function add() {
-  currentResult += parseInt(userInput.value);
-  outputResult(currentResult, '');
+  const enteredNumber = getUserNumberInput();
+  const calcDescription = `${currentResult} + ${enteredNumber}`;
+  currentResult += enteredNumber;
+  outputResult(currentResult, calcDescription);
 }
 
 addBtn.addEventListener('click', add);
