bd121fea3e8281ce1f338b38d33724a6313f0b1a
Marcelo dos Santos <mdssjc@gmail.com>
2019-11-28 18:51:46 -0300

    Working with the "switch-case" Statement.

--- javascript/udemy/javascript-01/control-03-monster-killer-project/assets/scripts/app.js
+++ javascript/udemy/javascript-01/control-03-monster-killer-project/assets/scripts/app.js
@@ -35,15 +35,23 @@ function writeToLog(ev, val, monsterHealth, playerHealth) {
     finalPlayerHealth: playerHealth,
   };
 
-  if (ev === LOG_EVENT_PLAYER_ATTACK) {
-    logEntry.target = 'MONSTER';
-  } else if (ev === LOG_EVENT_PLAYER_STRONG_ATTACK) {
-    logEntry.target = 'MONSTER';
-  } else if (ev === LOG_EVENT_MONSTER_ATTACK) {
-    logEntry.target = 'PLAYER';
-  } else if (ev === LOG_EVENT_PLAYER_HEAL) {
-    logEntry.target = 'PLAYER';
-  } else if (ev === LOG_EVENT_GAME_OVER) {
+  switch (ev) {
+    case LOG_EVENT_PLAYER_ATTACK:
+      logEntry.target = 'MONSTER';
+      break;
+    case LOG_EVENT_PLAYER_STRONG_ATTACK:
+      logEntry.target = 'MONSTER';
+      break;
+    case LOG_EVENT_MONSTER_ATTACK:
+      logEntry.target = 'PLAYER';
+      break;
+    case LOG_EVENT_PLAYER_HEAL:
+      logEntry.target = 'PLAYER';
+      break;
+    case LOG_EVENT_GAME_OVER:
+      break;
+    default:
+      break;
   }
 
   battleLog.push(logEntry);
