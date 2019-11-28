2f9ddc5b946dd50f2453ef25d0ee8e719a52b5b7
Marcelo dos Santos <mdssjc@gmail.com>
2019-11-27 21:15:32 -0300

    Utilizing Global Constants as Identifiers in Conditional Code.

--- javascript/udemy/javascript-01/control-03-monster-killer-project/assets/scripts/app.js
+++ javascript/udemy/javascript-01/control-03-monster-killer-project/assets/scripts/app.js
@@ -3,6 +3,9 @@ const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
+const MODE_ATTACK = 'ATTACK';
+const MODE_STRONG_ATTACK = 'STRONG_ATTACK';
+
 const enteredValue = prompt('Maximum life for you and the monster.', '100');
 
 let chosenMaxLife = parseInt(enteredValue);
@@ -52,9 +55,9 @@ function endRound() {
 function attackMonster(mode) {
   let maxDamage;
 
-  if (mode === 'ATTACK') {
+  if (mode === MODE_ATTACK) {
     maxDamage = ATTACK_VALUE;
-  } else if (mode === 'STRONG_ATTACK') {
+  } else if (mode === MODE_STRONG_ATTACK) {
     maxDamage = STRONG_ATTACK_VALUE;
   }
 
@@ -64,11 +67,11 @@ function attackMonster(mode) {
 }
 
 function attackHandler() {
-  attackMonster('ATTACK');
+  attackMonster(MODE_ATTACK);
 }
 
 function strongAttackHandler() {
-  attackMonster('STRONG_ATTACK');
+  attackMonster(MODE_STRONG_ATTACK);
 }
 
 function healPlayerHandler() {
