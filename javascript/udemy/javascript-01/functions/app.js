9239d877807cdbd6efa2bbdc8621aed5f4a5f064
Marcelo dos Santos <mdssjc@gmail.com>
2019-12-04 21:29:30 -0300

    Functions vs Methods.

---
+++ javascript/udemy/javascript-01/functions/app.js
@@ -0,0 +1,7 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+function startGame() {
+  console.log('Game is loading...');
+}
+
+startGameBtn.addEventListener('click', startGame);
