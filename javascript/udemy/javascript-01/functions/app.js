d1ddc48e3a3137d83b4be63b7cb25d1d0b4318ea
Marcelo dos Santos <mdssjc@gmail.com>
2019-12-04 21:43:40 -0300

    Function Expressions: Storing Functions in Variables.

--- javascript/udemy/javascript-01/functions/app.js
+++ javascript/udemy/javascript-01/functions/app.js
@@ -1,9 +1,9 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-function startGame() {
-  console.log('Game is loading...');
-}
+const start = function() {
+  console.log('Game is starting...');
+};
 
 console.dir(startGame);
 
-startGameBtn.addEventListener('click', startGame);
+startGameBtn.addEventListener('click', start);
