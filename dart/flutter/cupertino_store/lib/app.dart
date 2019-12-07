3e2faa446d845f919c17b7313affdd4f5c4b081e
Marcelo dos Santos <mdssjc@gmail.com>
2019-12-07 15:31:51 -0300

    Create structure for a 3-tab app.

--- dart/flutter/cupertino_store/lib/app.dart
+++ dart/flutter/cupertino_store/lib/app.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
 
+import 'product_list_tab.dart';
+import 'search_tab.dart';
+import 'shopping_cart_tab.dart';
+
 class CupertinoStoreApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,11 +16,42 @@ class CupertinoStoreApp extends StatelessWidget {
 class CupertinoStoreHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        middle: const Text('Cupertino Store'),
-      ),
-      child: Container(),
-    );
+    return CupertinoTabScaffold(
+        tabBar: CupertinoTabBar(items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            title: Text('Products'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.search),
+            title: Text('Search'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.shopping_cart),
+            title: Text('Cart'),
+          ),
+        ]),
+        tabBuilder: (context, index) {
+          switch (index) {
+            case 0:
+              return CupertinoTabView(builder: (context) {
+                return CupertinoPageScaffold(
+                  child: ProductListTab(),
+                );
+              });
+            case 1:
+              return CupertinoTabView(builder: (context) {
+                return CupertinoPageScaffold(
+                  child: SearchTab(),
+                );
+              });
+            case 2:
+              return CupertinoTabView(builder: (context) {
+                return CupertinoPageScaffold(
+                  child: ShoppingCartTab(),
+                );
+              });
+          }
+        });
   }
 }
---
+++ dart/flutter/cupertino_store/lib/product_list_tab.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+
+class ProductListTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<AppStateModel>(
+      builder: (context, model, child) {
+        return CustomScrollView(
+          slivers: <Widget>[
+            CupertinoSliverNavigationBar(
+              largeTitle: Text('Cupertino Store'),
+            )
+          ],
+        );
+      },
+    );
+  }
+}
