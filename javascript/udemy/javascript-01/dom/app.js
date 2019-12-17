304942e619fb2f4f918eff18fb0e4d124fd73b10
Marcelo dos Santos <mdssjc@gmail.com>
2019-12-17 19:16:33 -0300

    DOM Traversal vs Query Methods.

--- javascript/udemy/javascript-01/dom/app.js
+++ javascript/udemy/javascript-01/dom/app.js
@@ -1,15 +1,4 @@
-const h1 = document.getElementById('main-title');
-h1.textContent = 'Some new title!';
-h1.style.color = 'white';
-h1.style.backgroundColor = 'black';
+const ul = document.body.firstElementChild.nextElementSibling;
+const firstLi = ul.firstElementChild;
 
-const li = document.querySelector('li:last-of-type');
-li.textContent += ' (Changed!)';
-
-const body = document.body;
-
-const listItemsElements = document.getElementsByTagName('li');
-
-for (const listItemEl of listItemsElements) {
-  console.dir(listItemEl);
-}
+console.log(firstLi);
