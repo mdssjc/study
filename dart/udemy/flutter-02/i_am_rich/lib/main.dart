f37dff33f8c080ec0776d93ec8c49723d4cec93d
Marcelo dos Santos <mdssjc@gmail.com>
2019-12-18 19:26:06 -0300

    Scaffolding a Flutter App.

--- dart/udemy/flutter-02/i_am_rich/lib/main.dart
+++ dart/udemy/flutter-02/i_am_rich/lib/main.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'),
+          ),
+        ),
       ),
     ),
   );
