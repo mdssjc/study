43e2794d4a350313f460dce28b7d42335d0fae2d
Marcelo dos Santos <mdssjc@gmail.com>
2019-12-25 22:23:59 -0300

    Challenge 2.

--- dart/udemy/flutter-02/dicee-flutter/lib/main.dart
+++ dart/udemy/flutter-02/dicee-flutter/lib/main.dart
@@ -26,6 +26,11 @@ class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
 
+  void play() {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -35,7 +40,7 @@ class _DicePageState extends State<DicePage> {
             child: FlatButton(
               onPressed: () {
                 setState(() {
-                  leftDiceNumber = Random().nextInt(6) + 1;
+                  play();
                 });
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
@@ -45,7 +50,7 @@ class _DicePageState extends State<DicePage> {
             child: FlatButton(
               onPressed: () {
                 setState(() {
-                  rightDiceNumber = Random().nextInt(6) + 1;
+                  play();
                 });
               },
               child: Image.asset('images/dice$rightDiceNumber.png'),
