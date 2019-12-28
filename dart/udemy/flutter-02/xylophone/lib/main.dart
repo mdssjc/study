6f2c20426c908a1a0cf193de56e0910365ffd1cf
Marcelo dos Santos <mdssjc@gmail.com>
2019-12-28 12:55:21 -0300

    Updating the UI of Our App.

--- dart/udemy/flutter-02/xylophone/lib/main.dart
+++ dart/udemy/flutter-02/xylophone/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:audioplayers/audio_cache.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
@@ -9,16 +8,18 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              buildFlatButton(1, Colors.red),
-              buildFlatButton(2, Colors.orange),
-              buildFlatButton(3, Colors.yellow),
-              buildFlatButton(4, Colors.green),
-              buildFlatButton(5, Colors.teal),
-              buildFlatButton(6, Colors.blue),
-              buildFlatButton(7, Colors.purple),
+              buildKey(1, Colors.red),
+              buildKey(2, Colors.orange),
+              buildKey(3, Colors.yellow),
+              buildKey(4, Colors.green),
+              buildKey(5, Colors.teal),
+              buildKey(6, Colors.blue),
+              buildKey(7, Colors.purple),
             ],
           ),
         ),
@@ -31,11 +32,13 @@ class XylophoneApp extends StatelessWidget {
     player.play('note$soundNumber.wav');
   }
 
-  Widget buildFlatButton(int soundNumber, Color color) {
-    return FlatButton(
-      onPressed: () => playSound(soundNumber),
-      color: color,
-      child: null,
+  Widget buildKey(int soundNumber, Color color) {
+    return Expanded(
+      child: FlatButton(
+        onPressed: () => playSound(soundNumber),
+        color: color,
+        child: null,
+      ),
     );
   }
 }
