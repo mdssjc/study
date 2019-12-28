3fe483ed7c3274c412abb2e037941df88049db38
Marcelo dos Santos <mdssjc@gmail.com>
2019-12-28 12:10:49 -0300

    How to Play Multiple Sounds.

--- dart/udemy/flutter-02/xylophone/lib/main.dart
+++ dart/udemy/flutter-02/xylophone/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:audioplayers/audio_cache.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
@@ -9,17 +10,32 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text('Click Me'),
-            ),
+          child: Column(
+            children: <Widget>[
+              buildFlatButton(1, Colors.red),
+              buildFlatButton(2, Colors.orange),
+              buildFlatButton(3, Colors.yellow),
+              buildFlatButton(4, Colors.green),
+              buildFlatButton(5, Colors.teal),
+              buildFlatButton(6, Colors.blue),
+              buildFlatButton(7, Colors.purple),
+            ],
           ),
         ),
       ),
+    );
+  }
+
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  Widget buildFlatButton(int soundNumber, Color color) {
+    return FlatButton(
+      onPressed: () => playSound(soundNumber),
+      color: color,
+      child: null,
     );
   }
 }
