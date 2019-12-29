ae9facb87290a0dfe651918ccaf24673945fa483
Marcelo dos Santos <mdssjc@gmail.com>
2019-12-28 22:11:02 -0300

    Flutter Design Patterns: 2 - Adapter.

--- dart/community/flutter-design-patterns/lib/router.dart
+++ dart/community/flutter-design-patterns/lib/router.dart
@@ -5,6 +5,7 @@ import 'package:flutter_design_patterns/data/models/design_pattern_category.dart
 import 'package:flutter_design_patterns/screens/category/category.dart';
 import 'package:flutter_design_patterns/screens/design_pattern_details/design_pattern_details.dart';
 import 'package:flutter_design_patterns/screens/main_menu/main_menu.dart';
+import 'package:flutter_design_patterns/widgets/design_patterns/adapter/adapter_example.dart';
 import 'package:flutter_design_patterns/widgets/design_patterns/singleton/singleton_example.dart';
 
 class Router {
@@ -26,6 +27,12 @@ class Router {
         return _buildDesignPatternDetailsPageRoute(
           settings,
           SingletonExample(),
+        );
+      // Structural
+      case _DesignPatternRoutes.adapterRoute:
+        return _buildDesignPatternDetailsPageRoute(
+          settings,
+          AdapterExample(),
         );
       default:
         return MaterialPageRoute(
@@ -50,4 +57,5 @@ class Router {
 
 class _DesignPatternRoutes {
   static const String singletonRoute = '/singleton';
+  static const String adapterRoute = '/adapter';
 }
