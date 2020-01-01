a5920585abf2b1cdfa0bed4574ef25be0c2e8ba4
Marcelo dos Santos <mdssjc@gmail.com>
2020-01-01 20:21:20 -0300

    Displaying the Questions.

--- dart/udemy/flutter-02/quizzler/lib/main.dart
+++ dart/udemy/flutter-02/quizzler/lib/main.dart
@@ -25,28 +25,23 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Widget> scoreKeeper = [
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
+  List<Widget> scoreKeeper = [];
+
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
   ];
+
+  int questionNumber = 0;
+
+  void nextQuestion() {
+    setState(() {
+      if (questionNumber < questions.length - 1) {
+        questionNumber++;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -60,7 +55,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -85,14 +80,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
-                setState(() {
-                  scoreKeeper.add(
-                    Icon(
-                      Icons.check,
-                      color: Colors.green,
-                    ),
-                  );
-                });
+                nextQuestion();
               },
             ),
           ),
@@ -111,6 +99,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                nextQuestion();
               },
             ),
           ),
