6c1f4efc9f729078dbbc0cb0a1aa6408760ab703
Marcelo dos Santos <mdssjc@gmail.com>
2020-01-04 19:55:06 -0300

    Encapsulation in Action.

--- dart/udemy/flutter-02/quizzler/lib/main.dart
+++ dart/udemy/flutter-02/quizzler/lib/main.dart
@@ -30,16 +30,6 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Widget> scoreKeeper = [];
 
-  int questionNumber = 0;
-
-  void nextQuestion() {
-    setState(() {
-      if (questionNumber < quizBrain.questionBank.length - 1) {
-        questionNumber++;
-      }
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -52,7 +42,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                quizBrain.questionBank[questionNumber].questionText,
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -77,8 +67,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
-                bool correctAnswer =
-                    quizBrain.questionBank[questionNumber].questionAnswer;
+                bool correctAnswer = quizBrain.getCorrectAnswer();
 
                 if (correctAnswer) {
                   print('user got it right!');
@@ -86,7 +75,9 @@ class _QuizPageState extends State<QuizPage> {
                   print('user got it wrong!');
                 }
 
-                nextQuestion();
+                setState(() {
+                  quizBrain.nextQuestion();
+                });
               },
             ),
           ),
@@ -105,8 +96,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
-                bool correctAnswer =
-                    quizBrain.questionBank[questionNumber].questionAnswer;
+                bool correctAnswer = quizBrain.getCorrectAnswer();
 
                 if (!correctAnswer) {
                   print('user got it right!');
@@ -114,7 +104,9 @@ class _QuizPageState extends State<QuizPage> {
                   print('user got it wrong!');
                 }
 
-                nextQuestion();
+                setState(() {
+                  quizBrain.nextQuestion();
+                });
               },
             ),
           ),
