1aa5ea8368efe3b1900be8baa567ad93422c2361
Marcelo dos Santos <mdssjc@gmail.com>
2020-01-13 23:01:13 -0300

    Adding the BMI Calculator Functionality.

--- dart/udemy/flutter-02/bmi-calculator/lib/screens/resullts_page.dart
+++ dart/udemy/flutter-02/bmi-calculator/lib/screens/resullts_page.dart
@@ -4,6 +4,16 @@ import 'package:bmi_calculator/constants.dart';
 import 'package:flutter/material.dart';
 
 class ResultsPage extends StatelessWidget {
+  final String bmiResult;
+  final String resultText;
+  final String interpretation;
+
+  ResultsPage({
+    @required this.bmiResult,
+    @required this.resultText,
+    @required this.interpretation,
+  });
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,13 +39,10 @@ class ResultsPage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
-                  Text('Normal', style: kResultTextStyle),
-                  Text('18.3', style: kBMITextStyle),
-                  Text(
-                    'Your BMI result is quite low, you should eat more!',
-                    textAlign: TextAlign.center,
-                    style: kBodyTextStyle,
-                  ),
+                  Text(resultText.toUpperCase(), style: kResultTextStyle),
+                  Text(bmiResult, style: kBMITextStyle),
+                  Text(interpretation,
+                      textAlign: TextAlign.center, style: kBodyTextStyle),
                 ],
               ),
             ),
