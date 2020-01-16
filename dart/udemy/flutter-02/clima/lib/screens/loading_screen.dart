366af3d59ffee31229558786ea0702e826a81677
Marcelo dos Santos <mdssjc@gmail.com>
2020-01-15 21:25:40 -0300

    Stateful Widget Lifecycle Methods.

--- dart/udemy/flutter-02/clima/lib/screens/loading_screen.dart
+++ dart/udemy/flutter-02/clima/lib/screens/loading_screen.dart
@@ -14,6 +14,12 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
