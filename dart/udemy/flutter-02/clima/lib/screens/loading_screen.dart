45ab973f4a0d86a5e31029274a9b60b461213035
Marcelo dos Santos <mdssjc@gmail.com>
2020-01-19 13:16:41 -0300

    Getting Actual Weather Data from the OpenWeatherMap API.

--- dart/udemy/flutter-02/clima/lib/screens/loading_screen.dart
+++ dart/udemy/flutter-02/clima/lib/screens/loading_screen.dart
@@ -1,8 +1,8 @@
-import 'dart:convert';
-
 import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+
+const apiKey = '---';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -10,48 +10,26 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
+
   @override
   void initState() {
     super.initState();
-    getLocation();
-    getData();
+    getLocationData();
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
 
-    print(location.latitude);
-    print(location.longitude);
-  }
+    latitude = location.latitude;
+    longitude = location.longitude;
 
-  void getData() async {
-    http.Response response = await http.get(
-        'http://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+    NetworkHelper networkHelper = NetworkHelper(
+        'http://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
 
-    if (response.statusCode == 200) {
-      String data = response.body;
-
-      var decodeData = jsonDecode(data);
-
-      var longitude = decodeData['coord']['lon'];
-      var weatherDescription = decodeData['weather'][0]['description'];
-
-      var temperature = decodeData['main']['temp'];
-      var condition = decodeData['weather'][0]['id'];
-      var cityName = decodeData['name'];
-
-      print(data);
-
-      print(longitude);
-      print(weatherDescription);
-
-      print(temperature);
-      print(condition);
-      print(cityName);
-    } else {
-      print(response.statusCode);
-    }
+    var weatherData = await networkHelper.getData();
   }
 
   @override
@@ -59,7 +37,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
     return Scaffold(
       body: Center(
         child: RaisedButton(
-          onPressed: () => getLocation(),
+          onPressed: () => getLocationData(),
           child: Text('Get Location'),
         ),
       ),
