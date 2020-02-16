aacc2be097c03bb91f78df47e88c21c276cf6f0f
Marcelo dos Santos <mdssjc@gmail.com>
2020-02-16 13:17:36 -0300

    Two-Way Binding Shortcut.

--- javascript/udemy/sveltejs-01/project/src/App.svelte
+++ javascript/udemy/sveltejs-01/project/src/App.svelte
@@ -34,4 +34,5 @@
 <h1>Hello {upperCaseName}!, my age is {age}!</h1>
 <button on:click={incrementAge}>Change Age</button>
 <!-- <button on:click={changeName}>Change Name</button> -->
-<input type="text" value={name} on:input={nameInput} />
+<!-- <input type="text" value={name} on:input={nameInput} /> -->
+<input type="text" bind:value={name} />
