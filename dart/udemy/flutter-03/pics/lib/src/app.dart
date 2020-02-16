d69708bdcaea9c118c572ef0b5d067da048c153c
Marcelo dos Santos <mdssjc@gmail.com>
2020-02-15 23:00:10 -0300

    Adding Custom Widgets.

---
+++ dart/udemy/flutter-03/pics/lib/src/app.dart
@@ -0,0 +1,8 @@
+// Import flutter helper library
+
+
+// Create a class that will br out custom widget
+// This class must extend the 'StatelessWidget' base class
+
+// Must define a 'build' methos that returns
+// the widgets that *this* widget will show
