f7737c20edd6702ada97c710c639591a93d1b9d2
Marcelo dos Santos <mdssjc@gmail.com>
2020-02-16 14:28:00 -0300

    Tarefa 1: Time to Practice: The Basics.

---
+++ javascript/udemy/sveltejs-01/assignment-problem/src/CourseGoal.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let goal;
+
+  $: doWarning = goal.includes("!");
+</script>
+
+<style>
+  .warning {
+    color: red;
+  }
+</style>
+
+<h1 class:warning={doWarning}>{goal}</h1>
