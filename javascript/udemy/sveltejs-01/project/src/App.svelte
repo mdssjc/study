1346f5b35c463581ec313e63651942f8d5b00504
Marcelo dos Santos <mdssjc@gmail.com>
2020-02-16 11:08:54 -0300

    Binding to Element Properties.

--- javascript/udemy/sveltejs-01/project/src/App.svelte
+++ javascript/udemy/sveltejs-01/project/src/App.svelte
@@ -18,6 +18,11 @@
   function changeName() {
     name = 'Maximilian'
   }
+
+  function nameInput(event) {
+    const enteredValue = event.target.value
+    name = enteredValue
+  }
 </script>
 
 <style>
@@ -28,4 +33,5 @@
 
 <h1>Hello {upperCaseName}!, my age is {age}!</h1>
 <button on:click={incrementAge}>Change Age</button>
-<button on:click={changeName}>Change Name</button>
+<!-- <button on:click={changeName}>Change Name</button> -->
+<input type="text" value={name} on:input={nameInput} />
