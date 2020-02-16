6438de09ebd2a35726560c050ed561003272604e
Marcelo dos Santos <mdssjc@gmail.com>
2020-02-15 22:24:17 -0300

    Creating Widgets.

--- dart/udemy/flutter-03/pics/lib/main.dart
+++ dart/udemy/flutter-03/pics/lib/main.dart
@@ -3,8 +3,12 @@
 import 'package:flutter/material.dart';
 
 // Define a 'main' function to run when our app starts
+void main() {
+  // Create a new text widget to show some text
+  // on the screen
+  var app = MaterialApp(
+    home: Text('Hi there!'),
+  );
 
-// Create a new text widget to show some text
-// on the screen
-
-// Take that widget and get it on the screen
+  // Take that widget and get it on the screen
+}
