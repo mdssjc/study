9be7eddef74a85b1077da4dc7431aea1f8e6ca2f
Marcelo dos Santos <mdssjc@gmail.com>
2020-02-15 22:43:10 -0300

    Named Parameter Clarification.

--- dart/udemy/flutter-03/pics/lib/main.dart
+++ dart/udemy/flutter-03/pics/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   var app = MaterialApp(
     home: Scaffold(
+      floatingActionButton: FloatingActionButton(),
       appBar: AppBar(
         title: Text('Let\'s see some images!'),
       ),
