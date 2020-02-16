b25b07eeae9dd46061d1dd85e66345ddfc6cbda8
Marcelo dos Santos <mdssjc@gmail.com>
2020-02-15 22:14:24 -0300

    The Four Step Design Process.

---
+++ dart/udemy/flutter-03/pics/lib/main.dart
@@ -0,0 +1,9 @@
+// I need to import a helper library
+// form Flutter to get content on the screen
+
+// Define a 'main' function to run when our app starts
+
+// Create a new text widget to show some text
+// on the screen
+
+// Take that widget and get it on the screen
