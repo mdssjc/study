ed03134cfedb80cace21e945f12a7c641ca73d35
Marcelo dos Santos <mdssjc@gmail.com>
2020-02-16 22:12:52 -0300

    The Build Method.

--- dart/udemy/flutter-03/pics/lib/src/app.dart
+++ dart/udemy/flutter-03/pics/lib/src/app.dart
@@ -1,11 +1,21 @@
 // Import flutter helper library
 import 'package:flutter/material.dart';
 
-// Create a class that will br out custom widget
-// This class must extend the 'StatelessWidget' base class
 class App extends StatelessWidget {
-
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            print('Hi there!');
+          },
+        ),
+        appBar: AppBar(
+          title: Text('Let\'s see some images!'),
+        ),
+      ),
+    );
+  }
 }
-
-// Must define a 'build' methods that returns
-// the widgets that *this* widget will show
