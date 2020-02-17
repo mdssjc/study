ddca5e02eac253fedf99606e9b38eaac7ca2c3c3
Marcelo dos Santos <mdssjc@gmail.com>
2020-02-16 22:04:22 -0300

    Stateless vs Stateful Widgets.

--- dart/udemy/flutter-03/pics/lib/src/app.dart
+++ dart/udemy/flutter-03/pics/lib/src/app.dart
@@ -1,8 +1,11 @@
 // Import flutter helper library
-
+import 'package:flutter/material.dart';
 
 // Create a class that will br out custom widget
 // This class must extend the 'StatelessWidget' base class
+class App extends StatelessWidget {
 
-// Must define a 'build' methos that returns
+}
+
+// Must define a 'build' methods that returns
 // the widgets that *this* widget will show
