b1152a5d170b948c74fc3ce3487e5d7a86a8d2ee
Marcelo dos Santos <mdssjc@gmail.com>
2020-02-16 22:27:59 -0300

    Refactoring Stateless to Stateful.

--- dart/udemy/flutter-03/pics/lib/src/app.dart
+++ dart/udemy/flutter-03/pics/lib/src/app.dart
@@ -1,7 +1,12 @@
 // Import flutter helper library
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
