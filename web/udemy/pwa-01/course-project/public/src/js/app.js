40aa9fd4621bb8fc7842bb76b0f2549540d610dc
Marcelo dos Santos <mdssjc@gmail.com>
2020-02-18 19:24:49 -0300

    Async Code in JavaScript.

--- web/udemy/pwa-01/course-project/public/src/js/app.js
+++ web/udemy/pwa-01/course-project/public/src/js/app.js
@@ -12,3 +12,9 @@ window.addEventListener('beforeinstallprompt', function(event) {
   deferredPrompt = event;
   return false;
 });
+
+setTimeout(() => {
+  console.log('This is executed once the timer is done');
+}, 3000);
+
+console.log('This is executed right after setTimeout()');
