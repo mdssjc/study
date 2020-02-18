6c45f5453f9b95e6df4284dfd3c819d6fab5056d
Marcelo dos Santos <mdssjc@gmail.com>
2020-02-18 11:13:50 -0300

    Adding Data.

--- javascript/udemy/sveltejs-01/project-02/src/main.js
+++ javascript/udemy/sveltejs-01/project-02/src/main.js
@@ -1,12 +1,7 @@
 import App from './App.svelte';
-import Header from './UI/Header.svelte';
 
 const app = new App({
-  target: document.querySelector('#app'),
-});
-
-const header = new Header({
-  target: document.querySelector('#header'),
+  target: document.body,
 });
 
 export default app;
