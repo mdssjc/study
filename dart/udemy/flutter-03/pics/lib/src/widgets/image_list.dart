935f7ad671e3088daa14821982d646ba7dae29c5
Marcelo dos Santos <mdssjc@gmail.com>
2020-02-18 22:11:20 -0300

    Column Widgets for Layout.

--- dart/udemy/flutter-03/pics/lib/src/widgets/image_list.dart
+++ dart/udemy/flutter-03/pics/lib/src/widgets/image_list.dart
@@ -11,15 +11,24 @@ class ImageList extends StatelessWidget {
     return ListView.builder(
       itemCount: images.length,
       itemBuilder: (context, index) {
-        return Container(
-          margin: EdgeInsets.all(20.0),
-          padding: EdgeInsets.all(20.0),
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.grey),
-          ),
-          child: Image.network(images[index].url),
-        );
+        return buildImage(images[index]);
       },
+    );
+  }
+
+  Widget buildImage(ImageModel image) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      padding: EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+      ),
+      child: Column(
+        children: <Widget>[
+          Image.network(image.url),
+          Text(image.title),
+        ],
+      ),
     );
   }
 }
