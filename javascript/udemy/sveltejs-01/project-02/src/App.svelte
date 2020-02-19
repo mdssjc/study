38192b4f43729fb220a77115ec27076b200c1660
Marcelo dos Santos <mdssjc@gmail.com>
2020-02-19 11:36:22 -0300

    Adding New Meetups via a Form.

--- javascript/udemy/sveltejs-01/project-02/src/App.svelte
+++ javascript/udemy/sveltejs-01/project-02/src/App.svelte
@@ -2,7 +2,14 @@
   import Header from "./UI/Header.svelte";
   import MeetupGrid from "./Meetups/MeetupGrid.svelte";
 
-  const meetups = [
+  let title = "";
+  let subtitle = "";
+  let address = "";
+  let email = "";
+  let description = "";
+  let imageUrl = "";
+
+  let meetups = [
     {
       id: "m1",
       title: "Coding Bootcamp",
@@ -25,6 +32,20 @@
       contactEmail: "swim@test.com"
     }
   ];
+
+  function addMeetup() {
+    const newMeetup = {
+      id: Math.random().toString,
+      title: title,
+      subtitle: subtitle,
+      description: description,
+      imageUrl: imageUrl,
+      contactEmail: email,
+      address: address
+    };
+
+    meetups = [...meetups, newMeetup];
+  }
 </script>
 
 <style>
@@ -36,5 +57,34 @@
 <Header />
 
 <main>
+  <form on:submit|preventDefault={addMeetup}>
+    <div class="form-control">
+      <label for="title">Title</label>
+      <input type="text" id="title" bind:value={title} />
+    </div>
+    <div class="form-control">
+      <label for="subtitle">Subtitle</label>
+      <input type="text" id="subtitle" bind:value={subtitle} />
+    </div>
+    <div class="form-control">
+      <label for="address">Address</label>
+      <input type="text" id="address" bind:value={address} />
+    </div>
+    <div class="form-control">
+      <label for="imageUrl">ImageUrl</label>
+      <input type="text" id="imageUrl" bind:value={imageUrl} />
+    </div>
+    <div class="form-control">
+      <label for="email">E-Mail</label>
+      <input type="text" id="email" bind:value={email} />
+    </div>
+    <div class="form-control">
+      <label for="description">Description</label>
+      <textarea id="description" rows="3" bind:value={description} />
+    </div>
+
+    <button type="submit">Save</button>
+  </form>
+
   <MeetupGrid {meetups} />
 </main>
