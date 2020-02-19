8178d7753c16ac9f6cc4aabfff119a9290b5eaa6
Marcelo dos Santos <mdssjc@gmail.com>
2020-02-19 10:56:28 -0300

    Adding a New Component (MeetupItem).

--- javascript/udemy/sveltejs-01/project-02/src/App.svelte
+++ javascript/udemy/sveltejs-01/project-02/src/App.svelte
@@ -1,5 +1,6 @@
 <script>
   import Header from "./UI/Header.svelte";
+  import MeetupItem from "./Meetups/MeetupItem.svelte";
 
   const meetups = [
     {
@@ -26,4 +27,16 @@
   ];
 </script>
 
+<style>
+  #meetups {
+    margin-top: 5rem;
+  }
+</style>
+
 <Header />
+
+<section id="meetups">
+  {#each meetups as meetup}
+    <MeetupItem />
+  {/each}
+</section>
