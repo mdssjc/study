44e7c11dc62e250b9f9ca1918d69e8a2f55b8de7
Marcelo dos Santos <mdssjc@gmail.com>
2020-02-18 21:24:16 -0300

    Building Lists of Widgets.

---
+++ dart/udemy/flutter-03/pics/lib/src/widgets/image_list.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:pics/src/models/image_model.dart';
+
+class ImageList extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
