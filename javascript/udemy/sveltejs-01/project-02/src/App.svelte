eda565b0ff44e784b8a2673810d347be2c20df85
Marcelo dos Santos <mdssjc@gmail.com>
2020-02-21 12:41:47 -0300

    Communicating Between Components.

--- javascript/udemy/sveltejs-01/project-02/src/App.svelte
+++ javascript/udemy/sveltejs-01/project-02/src/App.svelte
@@ -33,18 +33,19 @@
 
   let editMode;
 
-  function addMeetup() {
+  function addMeetup(event) {
     const newMeetup = {
       id: Math.random().toString,
-      title: title,
-      subtitle: subtitle,
-      description: description,
-      imageUrl: imageUrl,
-      contactEmail: email,
-      address: address
+      title: event.detail.title,
+      subtitle: event.detail.subtitle,
+      description: event.detail.description,
+      imageUrl: event.detail.imageUrl,
+      contactEmail: event.detail.email,
+      address: event.detail.address
     };
 
     meetups = [...meetups, newMeetup];
+    editMode = null;
   }
 
   function toggleFavorite(event) {
@@ -65,14 +66,20 @@
   main {
     margin-top: 5rem;
   }
+
+  .meetup-controls {
+    margin: 1rem;
+  }
 </style>
 
 <Header />
 
 <main>
+  <div class="meetup-controls" />
+
   <Button caption="New Meetup" on:click={() => (editMode = 'add')} />
   {#if editMode === 'add'}
-    <EditMeetup />
+    <EditMeetup on:save={addMeetup} />
   {/if}
   <MeetupGrid {meetups} on:togglefavorite={toggleFavorite} />
 </main>
