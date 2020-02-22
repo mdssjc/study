16562b7bf209470ce752ee57caba67c4d0d54cdb
Marcelo dos Santos <mdssjc@gmail.com>
2020-02-22 13:24:50 -0300

    Finishing the "Modal" Component.

--- javascript/udemy/sveltejs-01/project-02/src/Meetups/EditMeetup.svelte
+++ javascript/udemy/sveltejs-01/project-02/src/Meetups/EditMeetup.svelte
@@ -23,6 +23,10 @@
       imageUrl: imageUrl
     });
   }
+
+  function cancel() {
+    dispatch("cancel");
+  }
 </script>
 
 <style>
@@ -70,7 +74,10 @@
       controlType="textarea"
       value={description}
       on:input={event => (description = event.target.value)} />
-
-    <!-- <Button type="submit">Save</Button> -->
   </form>
+
+  <div slot="footer">
+    <Button type="button" mode="outline" on:click={cancel}>Cancel</Button>
+    <Button type="button" on:click={submitForm}>Save</Button>
+  </div>
 </Modal>
