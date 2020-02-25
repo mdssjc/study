e6da5088bfc38e02c98b3f2575721422f7d714af
Marcelo dos Santos <mdssjc@gmail.com>
2020-02-24 21:53:42 -0300

    The Provider in Action.

--- dart/udemy/flutter-03/login_bloc/lib/src/app.dart
+++ dart/udemy/flutter-03/login_bloc/lib/src/app.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:login_bloc/src/blocs/provider.dart';
 import 'package:login_bloc/src/screens/login_screen.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Log Me In',
-      home: Scaffold(
-        body: LoginScreen(),
+    return Provider(
+      child: MaterialApp(
+        title: 'Log Me In',
+        home: Scaffold(
+          body: LoginScreen(),
+        ),
       ),
     );
   }
