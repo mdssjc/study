88a20c63d4daff142127f37f5a5f5e29fed2a99a
Marcelo dos Santos <mdssjc@gmail.com>
2020-02-25 22:28:48 -0300

    Interpreting Stream Values.

--- dart/udemy/flutter-03/login_bloc/lib/src/screens/login_screen.dart
+++ dart/udemy/flutter-03/login_bloc/lib/src/screens/login_screen.dart
@@ -60,7 +60,11 @@ class LoginScreen extends StatelessWidget {
         return RaisedButton(
           child: Text('Login'),
           color: Colors.blue,
-          onPressed: () {},
+          onPressed: snapshot.hasError
+              ? null
+              : () {
+                  print('Hi there!');
+                },
         );
       },
     );
