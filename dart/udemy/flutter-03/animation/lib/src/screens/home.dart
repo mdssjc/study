21b9371071d3d769a88bff75178116ae8570ba0c
Marcelo dos Santos <mdssjc@gmail.com>
2020-03-12 22:59:00 -0300

    Constraining Animation Range.

--- dart/udemy/flutter-03/animation/lib/src/screens/home.dart
+++ dart/udemy/flutter-03/animation/lib/src/screens/home.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:animation/src/widgets/cat.dart';
 import 'package:flutter/material.dart';
 
@@ -17,20 +19,22 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
     super.initState();
 
     boxController = AnimationController(
-      duration: Duration(seconds: 2),
+      duration: Duration(milliseconds: 300),
       vsync: this,
     );
 
     boxAnimation = Tween(
-      begin: 0.0,
-      end: 3.14,
+      begin: pi * 0.6,
+      end: pi * 0.65,
     ).animate(CurvedAnimation(
       parent: boxController,
       curve: Curves.linear,
     ));
     boxAnimation.addStatusListener((status) {
       if (status == AnimationStatus.completed) {
-        boxController.repeat();
+        boxController.reverse();
+      }else if (status ==AnimationStatus.dismissed) {
+        boxController.forward();
       }
     });
     boxController.forward();
