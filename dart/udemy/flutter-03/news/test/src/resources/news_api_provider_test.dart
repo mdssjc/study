cf893adc34aa10cdc2d9771b2f129bf389f704fe
Marcelo dos Santos <mdssjc@gmail.com>
2020-04-19 13:56:14 -0300

    Writing Expectations.

--- dart/udemy/flutter-03/news/test/src/resources/news_api_provider_test.dart
+++ dart/udemy/flutter-03/news/test/src/resources/news_api_provider_test.dart
@@ -3,3 +3,11 @@ import 'dart:convert';
 import 'package:test/test.dart';
 import 'package:http/http.dart';
 import 'package:http/testing.dart';
+
+void main() {
+  test('FetchTopIds returns a list of ids', () {
+    final sum = 1 + 3;
+
+    expect(sum, 4);
+  });
+}
