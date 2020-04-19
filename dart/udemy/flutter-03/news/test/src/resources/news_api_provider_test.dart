6254473c9684a7d07dfa51a76d5bbdd27eb978ce
Marcelo dos Santos <mdssjc@gmail.com>
2020-04-19 14:11:53 -0300

    Returning JSON.

--- dart/udemy/flutter-03/news/test/src/resources/news_api_provider_test.dart
+++ dart/udemy/flutter-03/news/test/src/resources/news_api_provider_test.dart
@@ -1,15 +1,19 @@
-import 'package:news/resources/news_api_provider.dart';
 import 'dart:convert';
-import 'package:test/test.dart';
+
 import 'package:http/http.dart';
 import 'package:http/testing.dart';
+import 'package:news/resources/news_api_provider.dart';
+import 'package:test/test.dart';
 
 void main() {
-  test('FetchTopIds returns a list of ids', () {
+  test('FetchTopIds returns a list of ids', () async {
     final newsApi = NewsApiProvider();
-    MockClient((request){
-
+    newsApi.client = MockClient((request) async {
+      return Response(json.encode([1, 2, 3, 4]), 200);
     });
 
+    final ids = await newsApi.fetchTopIds();
+
+    expect(ids, [1, 2, 3, 4]);
   });
 }
