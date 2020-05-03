8a9ce09c3853e6ec59a8f457c3ccefb21dacee9c
Marcelo dos Santos <mdssjc@gmail.com>
2020-05-03 15:13:17 -0300

    User Interface.

--- dart/flutter-tdd-clean-architecture-course/lib/main.dart
+++ dart/flutter-tdd-clean-architecture-course/lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:clean_architecture_tdd_course/features/number_trivia/presentation/pages/number_trivia_page.dart';
 import 'package:flutter/material.dart';
 
 import 'injection_container.dart' as di;
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await di.init();
   runApp(MyApp());
 }
@@ -10,6 +12,13 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return null;
+    return MaterialApp(
+      title: 'Number Trivia',
+      theme: ThemeData(
+        primaryColor: Colors.green.shade800,
+        accentColor: Colors.green.shade600,
+      ),
+      home: NumberTriviaPage(),
+    );
   }
 }
