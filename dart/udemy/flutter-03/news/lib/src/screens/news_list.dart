6e89fd5c157d63cfd499be73500f1d7e0e9e9481
Marcelo dos Santos <mdssjc@gmail.com>
2020-05-05 22:39:15 -0300

    The Stories Provider.

--- dart/udemy/flutter-03/news/lib/src/screens/news_list.dart
+++ dart/udemy/flutter-03/news/lib/src/screens/news_list.dart
@@ -7,33 +7,7 @@ class NewsList extends StatelessWidget {
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: _buildList(),
-    );
-  }
-
-  Widget _buildList() {
-    return ListView.builder(
-      itemCount: 1000,
-      itemBuilder: (context, index) {
-        return FutureBuilder(
-          future: getFuture(),
-          builder: (context, snapshot) {
-            return Container(
-              height: 80.0,
-              child: snapshot.hasData
-                  ? Text('Im visible $index')
-                  : Text('I havent fetched data yet $index'),
-            );
-          },
-        );
-      },
-    );
-  }
-
-  getFuture() {
-    return Future.delayed(
-      Duration(seconds: 2),
-      () => 'hi',
+      body: Text('Show a list'),
     );
   }
 }
