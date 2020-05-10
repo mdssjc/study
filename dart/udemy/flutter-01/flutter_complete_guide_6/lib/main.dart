789d5584475425c8a61561cc6d30b55ad8189c36
Marcelo dos Santos <mdssjc@gmail.com>
2020-05-10 18:43:11 -0300

    Creating a New App.

---
+++ dart/udemy/flutter-01/flutter_complete_guide_6/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide_6/screens/chat_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutterChat',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: ChatScreen(),
+    );
+  }
+}
