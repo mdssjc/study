7574ab3ff23213200d60aebb6db6b953245df5b1
Marcelo dos Santos <mdssjc@gmail.com>
2020-05-19 21:58:05 -0300

    Improving the Chat Bubbles.

--- dart/udemy/flutter-01/flutter_complete_guide_6/lib/widgets/chat/message_bubble.dart
+++ dart/udemy/flutter-01/flutter_complete_guide_6/lib/widgets/chat/message_bubble.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
 
 class MessageBubble extends StatelessWidget {
+  final Key key;
+
   final String message;
 
-  MessageBubble(this.message);
+  final bool isMe;
+
+  MessageBubble(this.message, this.isMe, {this.key});
 
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
       children: <Widget>[
         Container(
           decoration: BoxDecoration(
-            color: Theme.of(context).accentColor,
-            borderRadius: BorderRadius.circular(12.0),
+            color: isMe ? Colors.grey[300] : Theme.of(context).accentColor,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(12.0),
+              topRight: Radius.circular(12.0),
+              bottomLeft: isMe ? Radius.circular(12.0) : Radius.circular(0.0),
+              bottomRight: isMe ? Radius.circular(0.0) : Radius.circular(12.0),
+            ),
           ),
           width: 140,
           padding: EdgeInsets.symmetric(
@@ -25,8 +35,10 @@ class MessageBubble extends StatelessWidget {
           ),
           child: Text(
             message,
-            style:
-                TextStyle(color: Theme.of(context).accentTextTheme.headline1.color),
+            style: TextStyle(
+                color: isMe
+                    ? Colors.black
+                    : Theme.of(context).accentTextTheme.headline1.color),
           ),
         ),
       ],
