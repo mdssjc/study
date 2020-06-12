195c4241d2e4018dc2db4fa7e906636b43e0b110
Marcelo dos Santos <mdssjc@gmail.com>
2020-06-12 13:39:06 -0300

    Reversing input numbers.

---
+++ java/hyperskill/Tic-Tac-Toe/Problems/Reversing input numbers/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n1 = scanner.nextInt();
+        int n2 = scanner.nextInt();
+
+        System.out.print(n2 + " " + n1);
+    }
+}
