cc9024c36757c97d1ef41b6316f3c8306bd51c5a
Marcelo dos Santos <mdssjc@gmail.com>
2020-06-12 13:49:03 -0300

    Desks.

---
+++ java/hyperskill/Tic-Tac-Toe/Problems/Desks/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int group1 = scanner.nextInt();
+        int group2 = scanner.nextInt();
+        int group3 = scanner.nextInt();
+
+        int total;
+
+        total = group1 / 2 + group1 % 2;
+        total += group2 / 2 + group2 % 2;
+        total += group3 / 2 + group3 % 2;
+
+        System.out.println(total);
+    }
+}
