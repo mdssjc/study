0a8ac5381f13b9948d93be96569c1ea39a665b27
Marcelo dos Santos <mdssjc@gmail.com>
2020-06-12 14:24:15 -0300

    Difference of times.

---
+++ java/hyperskill/Tic-Tac-Toe/Problems/Difference of times/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int hours1 = scanner.nextInt();
+        int minutes1 = scanner.nextInt();
+        int seconds1 = scanner.nextInt();
+
+        int hours2 = scanner.nextInt();
+        int minutes2 = scanner.nextInt();
+        int seconds2 = scanner.nextInt();
+
+        int start = hours1 * 60 * 60 + minutes1 * 60 + seconds1;
+        int end = hours2 * 60 * 60 + minutes2 * 60 + seconds2;
+
+        System.out.println(end - start);
+    }
+}
