72d80d11c36c37d93200ab7158bc014323939f1b
Marcelo dos Santos <mdssjc@gmail.com>
2020-06-12 15:48:09 -0300

    Bloc Testing.

--- dart/udemy/flutter-03/news/lib/src/screens/news_list.dart
+++ dart/udemy/flutter-03/news/lib/src/screens/news_list.dart
@@ -5,12 +5,31 @@ class NewsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final bloc = StoriesProvider.of(context);
+    bloc.fetchTopIds();
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: Text('Show a list'),
+      body: _buildList(bloc),
+    );
+  }
+
+  Widget _buildList(StoriesBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.topIds,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Text('Still waiting on Ids');
+        }
+
+        return ListView.builder(
+          itemCount: snapshot.data.length,
+          itemBuilder: (context, index) {
+            return Text(snapshot.data[index]);
+          },
+        );
+      },
     );
   }
 }
