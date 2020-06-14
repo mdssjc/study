57a16876679981d0a1d9560d8973ae8db32d8b76
Marcelo dos Santos <mdssjc@gmail.com>
2020-06-14 14:36:50 -0300

    Replacing 'a' with 'b'.

---
+++ java/hyperskill/Tic-Tac-Toe/Problems/Replacing 'a' with 'b'/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        input = input.replaceAll("a", "b");
+
+        System.out.println(input);
+    }
+}
