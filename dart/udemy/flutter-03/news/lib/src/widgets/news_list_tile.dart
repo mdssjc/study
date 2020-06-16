5fe7c87e69eeb77ad7e8666ae3100c4a596fc928
Marcelo dos Santos <mdssjc@gmail.com>
2020-06-15 21:33:26 -0300

    StreamBuilder Setup.

--- dart/udemy/flutter-03/news/lib/src/widgets/news_list_tile.dart
+++ dart/udemy/flutter-03/news/lib/src/widgets/news_list_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news/src/blocs/stories_provider.dart';
+import 'package:news/src/models/item_model.dart';
 
 class NewsListTile extends StatelessWidget {
   final int itemId;
@@ -9,5 +10,14 @@ class NewsListTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final bloc = StoriesProvider.of(context);
+
+    return StreamBuilder(
+      stream: bloc.items,
+      builder: (context, AsyncSnapshot<Map<int, Future<ItemModel>>> snapshot) {
+        if (!snapshot.hasData) {
+          return Text('Stream still loading');
+        }
+      },
+    );
   }
 }
