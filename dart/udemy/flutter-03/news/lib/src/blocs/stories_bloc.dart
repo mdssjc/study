7869b1d0020e1a8821155f514b19e3df5a56eb9b
Marcelo dos Santos <mdssjc@gmail.com>
2020-06-16 22:10:11 -0300

    A Laborious Refactor.

--- dart/udemy/flutter-03/news/lib/src/blocs/stories_bloc.dart
+++ dart/udemy/flutter-03/news/lib/src/blocs/stories_bloc.dart
@@ -5,16 +5,17 @@ import 'package:rxdart/rxdart.dart';
 class StoriesBloc {
   final _repository = Repository();
   final _topIds = PublishSubject<List<int>>();
-  final _items = BehaviorSubject<int>();
-
-  Observable<Map<int, Future<ItemModel>>> items;
+  final _itemsFetcher = PublishSubject<int>();
+  final _itemsOutput = BehaviorSubject<Map<int, Future<ItemModel>>>();
 
   Observable<List<int>> get topIds => _topIds.stream;
 
-  Function(int) get fetchItem => _items.sink.add;
+  Observable<Map<int, Future<ItemModel>>> get items => _itemsOutput.stream;
+
+  Function(int) get fetchItem => _itemsFetcher.sink.add;
 
   StoriesBloc() {
-    items = _items.stream.transform(_itemsTransformer());
+    _itemsFetcher.stream.transform(_itemsTransformer()).pipe(_itemsOutput);
   }
 
   fetchTopIds() async {
@@ -35,6 +36,7 @@ class StoriesBloc {
 
   dispose() {
     _topIds.close();
-    _items.close();
+    _itemsFetcher.close();
+    _itemsOutput.close();
   }
 }
