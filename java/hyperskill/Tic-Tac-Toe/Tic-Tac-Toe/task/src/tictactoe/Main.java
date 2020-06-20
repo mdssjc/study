64b1db4e89b792327897523c278028bc8be0f110
Marcelo dos Santos <mdssjc@gmail.com>
2020-06-20 10:10:07 -0300

    Atualização.

--- java/hyperskill/Tic-Tac-Toe/Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ java/hyperskill/Tic-Tac-Toe/Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,9 +1,16 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+        Scanner scanner = new Scanner(System.in);
+        String cells = scanner.nextLine();
+
+        System.out.println("---------");
+        System.out.println("| " + cells.charAt(0) + " " + cells.charAt(1) + " " + cells.charAt(2) + " |");
+        System.out.println("| " + cells.charAt(3) + " " + cells.charAt(4) + " " + cells.charAt(5) + " |");
+        System.out.println("| " + cells.charAt(6) + " " + cells.charAt(7) + " " + cells.charAt(8) + " |");
+        System.out.println("---------");
     }
 }
