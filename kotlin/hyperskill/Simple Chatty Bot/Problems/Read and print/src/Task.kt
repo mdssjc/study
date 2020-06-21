4558d932b58ddedd7f1ac81a9867e30e83bb1651
Marcelo dos Santos <mdssjc@gmail.com>
2020-06-21 15:05:44 -0300

    Read and print.

---
+++ kotlin/hyperskill/Simple Chatty Bot/Problems/Read and print/src/Task.kt
@@ -0,0 +1,14 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    val line1 = scanner.nextLine()
+    val line2 = scanner.nextLine()
+    val a = scanner.nextInt()
+    val b = scanner.nextInt()
+
+    println(line1)
+    println(line2)
+    println(a)
+    println(b)
+}
