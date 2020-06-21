499d6c4977cf684a14c012908346c7b7feca7179
Marcelo dos Santos <mdssjc@gmail.com>
2020-06-21 15:07:14 -0300

    Printing each word in a new line.

---
+++ kotlin/hyperskill/Simple Chatty Bot/Problems/Printing each word in a new line/src/Task.kt
@@ -0,0 +1,16 @@
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    val w1 = scanner.next()
+    val w2 = scanner.next()
+    val w3 = scanner.next()
+    val w4 = scanner.next()
+    val w5 = scanner.next()
+
+    println(w1)
+    println(w2)
+    println(w3)
+    println(w4)
+    println(w5)
+}
