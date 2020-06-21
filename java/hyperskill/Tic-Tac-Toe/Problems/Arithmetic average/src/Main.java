a2d08a2132e667e56dc4ed322d2f61c3c9c2d950
Marcelo dos Santos <mdssjc@gmail.com>
2020-06-21 14:45:00 -0300

    Arithmetic average.

---
+++ java/hyperskill/Tic-Tac-Toe/Problems/Arithmetic average/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        double sum = 0;
+        int count = 0;
+        for (int i = a; i <= b; i++) {
+            if (i % 3 == 0) {
+                sum += i;
+                count++;
+            }
+        }
+
+        System.out.println(sum / count);
+    }
+}
