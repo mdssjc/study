066bb09df1e61e39734bb33866126a767e58745d
Marcelo dos Santos <mdssjc@gmail.com>
2020-06-21 14:39:43 -0300

    The count of numbers divisible by N.

---
+++ java/hyperskill/Tic-Tac-Toe/Problems/The count of numbers divisible by N/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int n = scanner.nextInt();
+
+        System.out.println((b - a) / n + 1);
+    }
+}
