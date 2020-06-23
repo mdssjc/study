5d65fd4a52788c22c6cad3812588497a262f1cd3
Marcelo dos Santos <mdssjc@gmail.com>
2020-06-23 20:17:09 -0300

    The sum of elements.

---
+++ java/hyperskill/Tic-Tac-Toe/Problems/The sum of elements/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n;
+        int sum = 0;
+        do {
+            n = scanner.nextInt();
+            sum += n;
+        } while (n != 0);
+
+        System.out.println(sum);
+    }
+}
