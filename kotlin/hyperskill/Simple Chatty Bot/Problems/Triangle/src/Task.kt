8e05454dda087a8379f0ee9c2a4df0a8f628ed5c
Marcelo dos Santos <mdssjc@gmail.com>
2020-06-27 15:16:26 -0300

    Triangle.

---
+++ kotlin/hyperskill/Simple Chatty Bot/Problems/Triangle/src/Task.kt
@@ -0,0 +1,15 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+
+    val a = scanner.nextInt()
+    val b = scanner.nextInt()
+    val c = scanner.nextInt()
+
+    if (a + b > c && b + c > a && c + a > b) {
+        println("YES")
+    } else {
+        println("NO")
+    }
+}
