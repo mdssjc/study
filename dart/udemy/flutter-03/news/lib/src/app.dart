3b9c0306c5be1723b08b8b3dba62e3aa634acea8
Marcelo dos Santos <mdssjc@gmail.com>
2020-06-30 21:42:11 -0300

    A PageRoute for NewsDetail.

--- dart/udemy/flutter-03/news/lib/src/app.dart
+++ dart/udemy/flutter-03/news/lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news/src/blocs/stories_provider.dart';
+import 'package:news/src/screens/news_datail.dart';
 import 'package:news/src/screens/news_list.dart';
 
 class App extends StatelessWidget {
@@ -8,14 +9,24 @@ class App extends StatelessWidget {
     return StoriesProvider(
       child: MaterialApp(
         title: 'News!',
-        onGenerateRoute: (RouteSettings settings) {
-          return MaterialPageRoute(
-            builder: (context) {
-              return NewsList();
-            },
-          );
-        },
+        onGenerateRoute: routes,
       ),
     );
+  }
+
+  Route routes(RouteSettings settings) {
+    if (settings.name == '/') {
+      return MaterialPageRoute(
+        builder: (context) {
+          return NewsList();
+        },
+      );
+    } else {
+      return MaterialPageRoute(
+        builder: (context) {
+          return NewsDetail();
+        },
+      );
+    }
   }
 }
