c98dba1ca9d4d8b7699d93f5b9f4987873f38ed8
Marcelo dos Santos <mdssjc@gmail.com>
2020-07-01 21:41:17 -0300

    More on Comments Bloc.

--- dart/udemy/flutter-03/news/lib/src/blocs/comments_bloc.dart
+++ dart/udemy/flutter-03/news/lib/src/blocs/comments_bloc.dart
@@ -1,1 +1,17 @@
-class CommentsBloc {}
+import 'package:news/src/models/item_model.dart';
+import 'package:rxdart/rxdart.dart';
+
+class CommentsBloc {
+  final _commentsFetcher = PublishSubject<int>();
+  final _commentsOutput = BehaviorSubject<Map<int, Future<ItemModel>>>();
+
+  Observable<Map<int, Future<ItemModel>>> get itemWithComments =>
+      _commentsOutput.stream;
+
+  Function(int) get fetchItemWithComments => _commentsFetcher.sink.add;
+
+  dispose() {
+    _commentsFetcher.close();
+    _commentsOutput.close();
+  }
+}
