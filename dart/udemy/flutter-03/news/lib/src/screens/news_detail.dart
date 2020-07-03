bff0a52deecfcf6b60b75f6b14692aef57d8c988
Marcelo dos Santos <mdssjc@gmail.com>
2020-07-02 21:03:10 -0300

    Consuming the Item Map.

--- dart/udemy/flutter-03/news/lib/src/screens/news_detail.dart
+++ dart/udemy/flutter-03/news/lib/src/screens/news_detail.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:news/src/blocs/comments_provider.dart';
+import 'package:news/src/models/item_model.dart';
 
 class NewsDetail extends StatelessWidget {
   final int itemId;
@@ -7,11 +9,21 @@ class NewsDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final bloc = CommentsProvider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Detail'),
       ),
-      body: Text('$itemId'),
+      body: _buildBody(bloc),
+    );
+  }
+
+  Widget _buildBody(CommentsBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.itemWithComments,
+      builder:
+          (context, AsyncSnapshot<Map<int, Future<ItemModel>>> snapshot) {},
     );
   }
 }
