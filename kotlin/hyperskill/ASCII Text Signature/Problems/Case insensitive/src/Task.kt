da55f0af584b1730f2f3208bbdf5772965dceb09
Marcelo dos Santos <mdssjc@gmail.com>
2020-07-20 21:56:16 -0300

    Case insensitive.

---
+++ kotlin/hyperskill/ASCII Text Signature/Problems/Case insensitive/src/Task.kt
@@ -0,0 +1,9 @@
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val line1 = scanner.nextLine()
+    val line2 = scanner.nextLine()
+
+    println(line1.toLowerCase() == line2.toLowerCase())
+}
