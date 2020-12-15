234bf23dd77815d4ec5f86a7bde1a58d2c82e222
Marcelo dos Santos <mdssjc@gmail.com>
2020-12-14 22:59:29 -0300

    [Python] - Objects - Understanding Attributes and Methods.

--- python/data-science-machine-learning-bootcamp/MLProjects/life.py
+++ python/data-science-machine-learning-bootcamp/MLProjects/life.py
@@ -1,1 +1,9 @@
+import math
+
 theAnswer = 42
+
+def quote_marvin():
+    print('I\'ve calculated your change of survival, but I don\'t think you\'ll like it.')
+
+def square_root(x):
+    return math.sqrt(x)
