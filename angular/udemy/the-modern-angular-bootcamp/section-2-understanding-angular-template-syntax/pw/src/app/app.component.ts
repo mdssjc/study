fe771f29b775eadaefe08ae062888660f418a289
Marcelo dos Santos <mdssjc@gmail.com>
2021-07-19 21:29:40 -0300

    feat: add Tracking Input State

--- angular/udemy/the-modern-angular-bootcamp/section-2-understanding-angular-template-syntax/pw/src/app/app.component.ts
+++ angular/udemy/the-modern-angular-bootcamp/section-2-understanding-angular-template-syntax/pw/src/app/app.component.ts
@@ -6,9 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  includeLetters = false;
   password = '';
 
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
   onButtonClick() {
+    console.log(this.includeLetters);
     this.password = 'MY PASSWORD!!!';
   }
 }
