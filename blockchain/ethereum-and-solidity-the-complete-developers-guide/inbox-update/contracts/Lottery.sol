944f515ba316f9579a8f48b8ef67e5e85d8c5460
Marcelo dos Santos <mdssjc@gmail.com>
2022-09-05 22:49:12 -0300

    feat: add lottery base

---
+++ blockchain/ethereum-and-solidity-the-complete-developers-guide/inbox-update/contracts/Lottery.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+contract Lottery {
+    address public manager;
+    address[] public players;
+
+    constructor() {
+        manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require(msg.value > 0.01 ether);
+        players.push(msg.sender);
+    }
+
+    function random() private view returns (uint256) {
+        return
+            uint256(
+                keccak256(
+                    abi.encode(block.difficulty, block.timestamp, players)
+                )
+            );
+    }
+}
